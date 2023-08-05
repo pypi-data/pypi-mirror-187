aebbeb6a152993c6ad7d968993303ed1ccdbe59b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:47 +0000

    Add package ipfy-asset-management-database/0.0.2/ipfy_asset_management_database-0.0.2.tar.gz

---
+++ packages/ipfy-asset-management-database/ipfy_asset_management_database-0.0.2.tar.gz/ipfy_asset_management_database-0.0.2/README.md
@@ -0,0 +1,2 @@
+# package-python-ipfy-asset-management-database
+IPFY Asset management project database management.
---
+++ packages/ipfy-asset-management-database/ipfy_asset_management_database-0.0.2.tar.gz/ipfy_asset_management_database-0.0.2/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ipfy_asset_management_database']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.24.1,<2.0.0',
+ 'pandas>=1.5.2,<2.0.0',
+ 'pylint>=2.15.10,<3.0.0',
+ 'pypiserver>=1.5.1,<2.0.0',
+ 'pytest>=7.2.0,<8.0.0',
+ 'typing==3.5']
+
+setup_kwargs = {
+    'name': 'ipfy-asset-management-database',
+    'version': '0.0.2',
+    'description': 'Python package to manage the ipfy asset management database',
+    'long_description': '# package-python-ipfy-asset-management-database\nIPFY Asset management project database management.\n',
+    'author': 'Yannick Flores',
+    'author_email': 'yannick.flores1992@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
