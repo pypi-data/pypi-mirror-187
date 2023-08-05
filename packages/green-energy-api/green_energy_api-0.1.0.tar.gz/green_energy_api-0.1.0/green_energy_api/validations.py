6d9a63809cf828fee0b9ca4d9b1d260ac0a6848e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:22 +0000

    Add package green-energy-api/0.1.0/green_energy_api-0.1.0.tar.gz

---
+++ packages/green-energy-api/green_energy_api-0.1.0.tar.gz/green_energy_api-0.1.0/green_energy_api/validations.py
@@ -0,0 +1,8 @@
+"""Green Energy validations module"""
+
+def validate_empty_string(val: str, entity_name: str) -> None:
+    """
+    Validate (assert) if the string is empty or not.
+    """
+
+    assert isinstance(val, str) and val != "", f"{entity_name} must be specified"
