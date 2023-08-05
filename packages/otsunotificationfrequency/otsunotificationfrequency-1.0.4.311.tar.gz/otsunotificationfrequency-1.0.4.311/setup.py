711b1ee13c9af8b1de25e269765eb2ad33d1c0c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:48 +0000

    Add package otsunotificationfrequency/1.0.4.311/otsunotificationfrequency-1.0.4.311.tar.gz

---
+++ packages/otsunotificationfrequency/otsunotificationfrequency-1.0.4.311.tar.gz/otsunotificationfrequency-1.0.4.311/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+__VERSION__ = "1.0.4.311"
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+with open("LICENSE", "r", encoding="utf-8") as f:
+    lcs = f.read()
+
+setup(
+    name="otsunotificationfrequency",
+    version=__VERSION__,
+    url="https://github.com/Otsuhachi/NotificationFrequency",
+    description="要素数が確定されたシーケンスの途中で処理を挟むタイミングの判定を補助します。",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    author="Otsuhachi",
+    author_email="agequodagis.tufuiegoeris@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    license=lcs,
+    keywords="Python Notification Frequency",
+    python_requires=">=3.11",
+)
