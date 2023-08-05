d24382261aa0deeb2ab19ba789de3712de3f0357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:53 +0000

    Add package stdpe/0.0.7/stdpe-0.0.7-py3-none-any.whl

---
+++ packages/stdpe/stdpe-0.0.7-py3-none-any.whl/stdpe/utils/__init__.py
@@ -0,0 +1,10 @@
+"""
+utils: Package for general utilities
+====================================
+
+Contains
+--------
+logger: logs can be output to the console and/or Azure in a clear and defined format
+"""
+
+__all__ = ['logger.py']
