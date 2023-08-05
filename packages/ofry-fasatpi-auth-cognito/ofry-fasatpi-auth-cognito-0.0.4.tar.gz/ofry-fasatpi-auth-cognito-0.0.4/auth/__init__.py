395c3bd3fd15deed7090d3187e0b1c625bce5257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:23:07 +0000

    Add package ofry-fasatpi-auth-cognito/0.0.4/ofry-fasatpi-auth-cognito-0.0.4.tar.gz

---
+++ packages/ofry-fasatpi-auth-cognito/ofry-fasatpi-auth-cognito-0.0.4.tar.gz/ofry-fasatpi-auth-cognito-0.0.4/auth/__init__.py
@@ -0,0 +1,11 @@
+from .permission import PermissionCheck , auth
+from .router import auth_router
+from .models import (
+    User,
+    Group,
+    GroupUser,
+    Role,
+    RolesEntities,
+)
+
+# from .db_connection import Base
---
+++ packages/ofry-fasatpi-auth-cognito/ofry-fasatpi-auth-cognito-0.0.4.tar.gz/ofry-fasatpi-auth-cognito-0.0.4/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4' 
+DESCRIPTION = 'User management authentiation'
+
+try:
+    # read the contents of README file
+    from pathlib import Path
+    this_directory = Path(__file__).parent
+    LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+except:
+    LONG_DESCRIPTION = 'User management authentication and authorization for FastAPI using AWS Cognito service'
+
+# Setting up
+setup(
+        name="ofry-fasatpi-auth-cognito", 
+        version=VERSION,
+        author="Ofry Makdasy",
+        author_email="ofry.makdsy@tech-19.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        install_requires=[
+            'fastapi',
+            'python-dotenv==0.21.0',
+            'python-jose==3.3.0',
+            'requests==2.28.2',
+            'SQLAlchemy==1.4.46',
+            ],
+        
+        keywords=['python', 'first package' , 'fastapi' , 'cognito' , 'jwt'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
