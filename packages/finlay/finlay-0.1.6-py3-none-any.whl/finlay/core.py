7c4f64412557a961d861db922b912fc7da595d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:19:11 +0000

    Add package finlay/0.1.6/finlay-0.1.6-py3-none-any.whl

---
+++ packages/finlay/finlay-0.1.6-py3-none-any.whl/finlay/core.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+author: S.M. Sabbir Amin
+date: 21 Jan 2023
+email: sabbir.amin@goava.com, sabbiramin.cse11ruet@gmail.com
+
+"""
+
+
+class Hello:
+    def __init__(self):
+        pass
+
+    def print_hello(self):
+        print('hello world')
