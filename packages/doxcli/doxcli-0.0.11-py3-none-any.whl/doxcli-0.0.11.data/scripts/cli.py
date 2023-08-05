5740c12c72d173e23075aaf403a9750169a40366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:28 +0000

    Add package doxcli/0.0.11/doxcli-0.0.11-py3-none-any.whl

---
+++ packages/doxcli/doxcli-0.0.11-py3-none-any.whl/doxcli-0.0.11.data/scripts/cli.py
@@ -0,0 +1,18 @@
+#!python
+
+import sys
+from doxcli.driver import DoxCliDriver
+from doxcli.utils import print_error
+
+
+def main():
+    try:
+        return DoxCliDriver().main()
+    except FileNotFoundError as e:
+        print_error(e)
+    except Exception as e:
+        print_error(e)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
