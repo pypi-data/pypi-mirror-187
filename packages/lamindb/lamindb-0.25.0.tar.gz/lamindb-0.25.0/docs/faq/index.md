aa3bf178186896c92e8d9ad9b78afd2ee2c400c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:19:13 +0000

    Add package lamindb/0.25.0/lamindb-0.25.0.tar.gz

---
+++ packages/lamindb/lamindb-0.25.0.tar.gz/lamindb-0.25.0/docs/faq/index.md
@@ -0,0 +1,19 @@
+# FAQ
+
+```{toctree}
+:maxdepth: 1
+
+setup
+session
+acid
+ingest
+nb
+select
+storage
+ingest-same-file-twice
+linked-select
+ingest-bfx
+postgres
+zarr-ingest-load
+streaming-objects
+```
---
+++ packages/lamindb/lamindb-0.25.0.tar.gz/lamindb-0.25.0/lamindb/dev/datasets/__init__.py
@@ -0,0 +1,19 @@
+"""Small example datasets.
+
+.. autosummary::
+   :toctree: .
+
+   file_fcs
+   file_jpg_paradisi05
+   dir_scrnaseq_cellranger
+   anndata_mouse_sc_lymph_node
+   anndata_human_immune_cells
+"""
+
+from ._core import (
+    anndata_human_immune_cells,
+    anndata_mouse_sc_lymph_node,
+    dir_scrnaseq_cellranger,
+    file_fcs,
+    file_jpg_paradisi05,
+)
