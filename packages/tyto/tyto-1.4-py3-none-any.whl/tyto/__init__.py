27990ca136baaaac9acee7825bf5b8b060ed0ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:40 +0000

    Add package tyto/1.4/tyto-1.4-py3-none-any.whl

---
+++ packages/tyto/tyto-1.4-py3-none-any.whl/tyto/__init__.py
@@ -0,0 +1,13 @@
+from .tyto import Ontology, URI, Term, configure_cache_size
+from .endpoint import Ontobee, EBIOntologyLookupService, PubChemAPI
+from .sbo import SBO
+from .so import SO
+from .ncit import NCIT
+from .om import OM
+from .ncbi_taxon import NCBITaxon
+from .sbol2 import SBOL2
+from .sbol3 import SBOL3
+from .edam import EDAM
+from .pubchem import PubChem
+from .paml import PAML
+from .uml import UML
