3d213c09f6a57369303d2a67f12ba1c4b5b867b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:48 +0000

    Add package streamlit-bridge/1.1.3/streamlit_bridge-1.1.3-py3-none-any.whl

---
+++ packages/streamlit-bridge/streamlit_bridge-1.1.3-py3-none-any.whl/st_bridge/bridge/build/precache-manifest.fad985818978633f1a71f25d0a3349e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83498d3a337a0cc5e1918e1ef635b7e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79f3664c334f065fbae9",
+    "url": "./static/js/2.d57250d1.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.d57250d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5707067d3a368089b77",
+    "url": "./static/js/main.4c504d7b.chunk.js"
+  },
+  {
+    "revision": "5e8ec43ca3bba2a86cdb",
+    "url": "./static/js/runtime-main.f43d5da3.js"
+  }
+]);
---
+++ packages/streamlit-bridge/streamlit_bridge-1.1.3-py3-none-any.whl/st_bridge/bridge/src/index.tsx
@@ -0,0 +1,65 @@
+import { Streamlit, RenderData } from "streamlit-component-lib";
+
+class Bridge {
+  public name: string;
+  public key: string | number | null;
+
+  constructor(name: string, key: string | number | null) {
+    this.name = name;
+    this.key = key;
+  }
+
+  public send(data: any) {
+    Streamlit.setComponentValue(data);
+  }
+}
+
+// store data in the topmost window in the window hierarchy as the component
+// is rendered inside an iframe.
+
+function getGlobal() {
+  let global: Window;
+  try {
+    /* Try to access a property of window.top. This will fail when window.top
+       is unset or cross-origin doesn't allow us to access window.top
+     */
+    global = window.top || window.parent;
+  } catch {
+    global = window.parent;
+  }
+  return global;
+}
+
+const global = getGlobal();
+
+if ((global as any).stBridges === undefined) {
+  const stBridges = {
+    bridges: {} as { [key: string]: Bridge },
+    send: (name: string, data: any) => {
+      stBridges.bridges[name].send(data);
+    },
+  };
+  (global as any).stBridges = stBridges;
+}
+
+/**
+ * The component's render function. This will be called immediately after
+ * the component is initially loaded, and then again every time the
+ * component gets new data from Python.
+ */
+function onRender(event: Event): void {
+  // Get the RenderData from the event
+  const data = (event as CustomEvent<RenderData>).detail;
+
+  const stBridges = (global as any).stBridges;
+  const bridge = data.args["name"];
+  const key = data.args["key"];
+  stBridges.bridges[bridge] = new Bridge(bridge, key);
+}
+
+// Attach our `onRender` handler to Streamlit's render event.
+Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender);
+
+// Tell Streamlit we're ready to start receiving data. We won't get our
+// first RENDER_EVENT until we call this function.
+Streamlit.setComponentReady();
