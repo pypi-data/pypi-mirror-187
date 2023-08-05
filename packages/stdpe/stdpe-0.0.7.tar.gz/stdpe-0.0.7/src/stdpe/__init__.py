0ff296e0dd128fd2cbe8bccb9a6c1f360959190e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:53 +0000

    Add package stdpe/0.0.7/stdpe-0.0.7.tar.gz

---
+++ packages/stdpe/stdpe-0.0.7.tar.gz/stdpe-0.0.7/src/stdpe/__init__.py
@@ -0,0 +1,12 @@
+"""
+stpde: Steinemann DPE Library
+=============================
+
+Python library for the Steinemann DPE company.
+
+Provides
+--------
+utils: general utilities e.g. logger
+"""
+
+__all__ = ['utils']
