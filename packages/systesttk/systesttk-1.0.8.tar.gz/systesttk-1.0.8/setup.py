18de6a6fab8362afe9cc31a5ac2d8dc01c2034f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:49 +0000

    Add package systesttk/1.0.8/systesttk-1.0.8.tar.gz

---
+++ packages/systesttk/systesttk-1.0.8.tar.gz/systesttk-1.0.8/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['systesttk']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'systesttk',
+    'version': '1.0.8',
+    'description': 'toolkit',
+    'long_description': '',
+    'author': 'wayfaring-stranger',
+    'author_email': 'zw6p226m@duck.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
