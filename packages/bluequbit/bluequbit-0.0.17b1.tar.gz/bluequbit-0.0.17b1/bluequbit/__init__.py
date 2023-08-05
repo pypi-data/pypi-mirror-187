e44f2fab5e56b51ba5935eeeb619bb23711fb5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:22 +0000

    Add package bluequbit/0.0.17b1/bluequbit-0.0.17b1.tar.gz

---
+++ packages/bluequbit/bluequbit-0.0.17b1.tar.gz/bluequbit-0.0.17b1/bluequbit/__init__.py
@@ -0,0 +1,30 @@
+# Mypy; for the `|` operator purpose
+# Remove this __future__ import once the oldest supported Python is 3.10
+import logging
+
+from . import exceptions
+from .bluequbit_client import BQClient
+from .estimate_result import EstimateResult
+from .job_result import JobResult
+from .version import __version__
+
+formatter = logging.Formatter(fmt="BQ-PYTHON-SDK - %(levelname)s - %(message)s")
+# formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+logger = logging.getLogger("bluequbit-python-sdk")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+
+def init(api_token: str | None = None) -> BQClient:
+    """Returns :class:`BQClient` instance for managing jobs on BlueQubit platform.
+
+    :param api_token: API token of the user. If ``None``, the token will be looked
+                      in default configuration file ``$HOME/.config/bluequbit/config.json``.
+                      If not ``None``, the token will also be saved in the same
+                      default configuration file.
+    """
+    return BQClient(api_token)
