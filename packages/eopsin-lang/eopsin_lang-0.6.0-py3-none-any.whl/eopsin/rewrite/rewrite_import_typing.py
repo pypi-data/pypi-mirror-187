bddfc39fdaf334ceaefa98db2e697486b38d37e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:18 +0000

    Add package eopsin-lang/0.6.0/eopsin_lang-0.6.0-py3-none-any.whl

---
+++ packages/eopsin-lang/eopsin_lang-0.6.0-py3-none-any.whl/eopsin/rewrite/rewrite_import_typing.py
@@ -0,0 +1,33 @@
+from ast import *
+from typing import Optional
+
+"""
+Checks that there was an import of dataclass if there are any class definitions
+"""
+
+
+class RewriteImportTyping(NodeTransformer):
+
+    imports_typing = False
+
+    def visit_ImportFrom(self, node: ImportFrom) -> Optional[ImportFrom]:
+        if node.module != "typing":
+            return node
+        assert (
+            len(node.names) == 3
+        ), "The program must contain one 'from typing import Dict, List, Union'"
+        for i, n in enumerate(["Dict", "List", "Union"]):
+            assert (
+                node.names[i].name == n
+            ), "The program must contain one 'from typing import Dict, List, Union'"
+            assert (
+                node.names[i].asname == None
+            ), "The program must contain one 'from typing import Dict, List, Union'"
+        self.imports_typing = True
+        return None
+
+    def visit_ClassDef(self, node: ClassDef) -> ClassDef:
+        assert (
+            self.imports_typing
+        ), "typing must be imported in order to use datum classes"
+        return node
