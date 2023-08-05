0667cb156305bbfa4c35ebc400149e60b802f75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:23 +0000

    Add package pyvalueobjects/0.8.1/pyvalueobjects-0.8.1.tar.gz

---
+++ packages/pyvalueobjects/pyvalueobjects-0.8.1.tar.gz/pyvalueobjects-0.8.1/pyvalueobjects/__init__.py
@@ -0,0 +1,20 @@
+# ----------------------------
+# ---------- Basics ----------
+# ----------------------------
+
+from pyvalueobjects.abstract.valueobject import ValueObject
+from pyvalueobjects.errors.ValueObjectError import ValueObjectError
+
+
+# -----------------------------
+# ---------- Numbers ----------
+# -----------------------------
+
+from pyvalueobjects.numbers.int import Int
+from pyvalueobjects.numbers.nullable_int import NullableInt
+from pyvalueobjects.numbers.positive_int import PositiveInt
+from pyvalueobjects.numbers.negative_int import NegativeInt
+from pyvalueobjects.numbers.nullable_negative_int import NullableNegativeInt
+from pyvalueobjects.numbers.positive_or_zero_int import PositiveOrZeroInt
+from pyvalueobjects.numbers.negative_or_zero_int import NegativeOrZeroInt
+from pyvalueobjects.numbers.nullable_negative_or_zero_int import NullableNegativeOrZeroInt
