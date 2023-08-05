73a0dd12c6f3c6ad0b9a0447ce68684a515965fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:59 +0000

    Add package playwright-recaptcha/0.0.4/playwright-recaptcha-0.0.4.tar.gz

---
+++ packages/playwright-recaptcha/playwright-recaptcha-0.0.4.tar.gz/playwright-recaptcha-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+from playwright_recaptcha import __version__
+
+setup(
+    name="playwright-recaptcha",
+    version=__version__,
+    author="Xewdy444",
+    author_email="xewdy@xewdy.tech",
+    description="A libary for solving reCAPTCHA v2 and v3 with Playwright",
+    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Xewdy444/Playwright-reCAPTCHA",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "httpx==0.23.3",
+        "playwright==1.29.1",
+        "pydub==0.25.1",
+        "SpeechRecognition==3.9.0",
+    ],
+)
