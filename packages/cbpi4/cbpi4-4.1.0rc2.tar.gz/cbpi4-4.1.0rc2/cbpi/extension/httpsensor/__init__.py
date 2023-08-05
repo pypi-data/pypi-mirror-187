9dae501dd0a4f6f4383172cd335324f3364dd968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:04 +0000

    Add package cbpi4/4.1.0rc2/cbpi4-4.1.0rc2.tar.gz

---
+++ packages/cbpi4/cbpi4-4.1.0rc2.tar.gz/cbpi4-4.1.0rc2/cbpi/extension/httpsensor/__init__.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+import asyncio
+from aiohttp import web
+from cbpi.api import *
+
+import re
+import random
+
+
+cache = {}
+
+@parameters([Property.Text(label="Key", configurable=True, description="Http Key")])
+class HTTPSensor(CBPiSensor):
+    def __init__(self, cbpi, id, props):
+        super(HTTPSensor, self).__init__(cbpi, id, props)
+        self.running = True
+        self.value = 0
+
+    async def run(self):
+        '''
+        This method is executed asynchronousely 
+        In this example the code is executed every second
+        '''
+        while self.running is True:
+            try:
+                cache_value = cache.pop(self.props.get("Key"), None)
+                if cache_value is not None:
+                    self.value = float(cache_value)
+                    self.push_update(self.value)
+            except Exception as e:
+                pass
+            await asyncio.sleep(1)
+
+    def get_state(self):
+        # return the current state of the sensor
+        return dict(value=self.value)
+
+class HTTPSensorEndpoint(CBPiExtension):
+
+
+    def __init__(self, cbpi):
+        '''
+        Initializer
+
+        :param cbpi:
+        '''
+        self.pattern_check = re.compile("^[a-zA-Z0-9,.]{0,10}$")
+
+        self.cbpi = cbpi
+        # register component for http, events
+        # In addtion the sub folder static is exposed to access static content via http
+        self.cbpi.register(self, "/httpsensor")
+
+
+    @request_mapping(path="/{key}/{value}", auth_required=False)
+    async def http_new_value2(self, request):
+        """
+        ---
+        description: Kettle Heater on
+        tags:
+        - HttpSensor
+        parameters:
+        - name: "key"
+          in: "path"
+          description: "Sensor Key"
+          required: true
+          type: "string"
+        - name: "value"
+          in: "path"
+          description: "Value"
+          required: true
+          type: "integer"
+          format: "int64"
+        responses:
+            "204":
+                description: successful operation
+        """
+
+        global cache
+        key = request.match_info['key']
+        value = request.match_info['value']
+        if self.pattern_check.match(key) is None:
+            return web.json_response(status=422, data={'error': "Key not matching pattern ^[a-zA-Z0-9,.]{0,10}$"})
+
+        if self.pattern_check.match(value) is None:
+            return web.json_response(status=422, data={'error': "Data not matching pattern ^[a-zA-Z0-9,.]{0,10}$"})
+
+        
+        cache[key] = value
+
+        return web.Response(status=204)
+
+
+def setup(cbpi):
+
+    '''
+    This method is called by the server during startup 
+    Here you need to register your plugins at the server
+    
+    :param cbpi: the cbpi core 
+    :return: 
+    '''
+    cbpi.plugin.register("HTTPSensor", HTTPSensor)
+    cbpi.plugin.register("HTTPSensorEndpoint", HTTPSensorEndpoint)
+
---
+++ packages/cbpi4/cbpi4-4.1.0rc2.tar.gz/cbpi4-4.1.0rc2/cbpi/extension/mqtt_sensor/__init__.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import asyncio
+
+from cbpi.api import parameters, Property, CBPiSensor
+from cbpi.api import *
+import logging
+import json
+
+@parameters([Property.Text(label="Topic", configurable=True, description="MQTT Topic"),
+             Property.Text(label="PayloadDictionary", configurable=True, default_value="",
+                           description="Where to find msg in payload, leave blank for raw payload")])
+class MQTTSensor(CBPiSensor):
+
+    def __init__(self, cbpi, id, props):
+        super(MQTTSensor, self).__init__(cbpi, id, props)
+        self.Topic = self.props.get("Topic", None)
+        self.payload_text = self.props.get("PayloadDictionary", None)
+        if self.payload_text != None:
+            self.payload_text = self.payload_text.split('.')
+        self.mqtt_task = self.cbpi.satellite.subcribe(self.Topic, self.on_message)
+        self.value: float = 999
+
+    async def on_message(self, message):
+        val = json.loads(message)
+        try:
+            if self.payload_text is not None:
+                for key in self.payload_text:
+                    val = val.get(key, None)
+
+            if isinstance(val, (int, float, str)):
+                self.value = float(val)
+                self.log_data(self.value)
+                self.push_update(self.value)
+        except Exception as e:
+            logging.info("MQTT Sensor Error {}".format(e))
+
+    async def run(self):
+        while self.running:
+            await asyncio.sleep(1)
+
+    def get_state(self):
+        return dict(value=self.value)
+
+    async def on_stop(self):
+        if self.mqtt_task.done() is False:
+            self.mqtt_task.cancel()
+            try:
+                await self.mqtt_task
+            except asyncio.CancelledError:
+                pass
+
+
+def setup(cbpi):
+    '''
+    This method is called by the server during startup
+    Here you need to register your plugins at the server
+
+    :param cbpi: the cbpi core
+    :return:
+    '''
+    if str(cbpi.static_config.get("mqtt", False)).lower() == "true":
+        cbpi.plugin.register("MQTTSensor", MQTTSensor)
