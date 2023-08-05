05fce04ede1a0416210e717448ed2fe2a0ff0770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:13 +0000

    Add package Rahimcalc/1.8/Rahimcalc-1.8.tar.gz

---
+++ packages/Rahimcalc/Rahimcalc-1.8.tar.gz/Rahimcalc-1.8/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_packages
+c=["Programming Language :: Python :: 3",
+   "License :: OSI Approved :: MIT License",
+   "Operating System :: OS Independent",]
+d="Student pass or fail package"
+setup(
+    name='Rahimcalc',
+    version='1.8',
+    author='Abdurrahim',
+    package=['Rahimcalc'],
+    author_email='abdurrahim251103@gmail.com',
+    description=d,
+    long_description="Sample of the programs",
+    long_description_content_type='text/markdown',
+    keywords='calculator',
+    license="MIT",
+    packages=find_packages(),
+    classifiers=c,
+    url="https://pypi.org/user/AbdurRahim2003/",
+    entry_points={'console_scripts':['Rahimcalc=Rahimcalc.comment:main']},
+    download_url="https://pypi.org/project/Rahimcalc/0.3.2/#files",
+    project_urls=
+    {
+        'Source Code':'https://github.com/Abdurrahimgithub/Abdurrahim/blob/main/__init__.py',
+        'Documentation':'https://github.com/Abdurrahimgithub/Abdurrahim/blob/main/README.md'
+        }
+    )
