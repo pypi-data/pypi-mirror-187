a65bf04a47ae076156bbe9161e659bb75adca305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:28 +0000

    Add package bilibili-api-python/14.1.0/bilibili_api_python-14.1.0-py3-none-any.whl

---
+++ packages/bilibili-api-python/bilibili_api_python-14.1.0-py3-none-any.whl/bilibili_api/utils/short.py
@@ -0,0 +1,44 @@
+"""
+bilibili_api.utils.short
+
+一个很简单的处理短链接的模块，主要是读取跳转链接。
+"""
+import httpx
+from .network_httpx import get_session
+from .. import settings
+
+
+async def get_real_url(short_url: str) -> str:
+    """
+    获取短链接跳转目标，以进行操作。
+    Params:
+        short_url(str): 短链接。
+    Returns:
+        目标链接（如果不是有效的链接会报错）
+    """
+    config = {}
+    config["method"] = "GET"
+    config["url"] = short_url
+    config["follow_redirects"] = False
+    if settings.proxy:
+        config["proxies"] = {"all://": settings.proxy}
+    try:
+        resp = await get_session().head(url=short_url, follow_redirects=True)
+        u = resp.url
+        return str(u)
+    except Exception as e:
+        raise e
+
+
+async def get_headers(short_url: str) -> httpx.Headers:
+    """
+    获取链接的 headers。
+    """
+    config = {}
+    config["method"] = "GET"
+    config["url"] = short_url
+    config["follow_redirects"] = False
+    if settings.proxy:
+        config["proxies"] = {"all://": settings.proxy}
+    resp = await get_session().head(url=short_url, follow_redirects=False)
+    return resp.headers
