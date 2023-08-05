085188c9d45f4134f42ac54d690f466675d7f953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:22:02 +0000

    Add package stream-tui/0.5.2/stream_tui-0.5.2.tar.gz

---
+++ packages/stream-tui/stream_tui-0.5.2.tar.gz/stream_tui-0.5.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['stream_tui']
+install_requires = \
+['blessed>=1.19.1,<2.0.0', 'rich>=12.6.0,<13.0.0']
+
+setup_kwargs = {
+    'name': 'stream-tui',
+    'version': '0.5.2',
+    'description': 'Easily create pretty user interfaces for your CLI apps',
+    'long_description': '# Stream TUI',
+    'author': 'Jakob Pinterits',
+    'author_email': 'jakob.pinterits@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
