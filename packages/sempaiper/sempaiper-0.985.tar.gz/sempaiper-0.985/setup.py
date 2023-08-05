e7cda537d5024c22ac8b2add8f370189e37d0b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:41 +0000

    Add package sempaiper/0.985/sempaiper-0.985.tar.gz

---
+++ packages/sempaiper/sempaiper-0.985.tar.gz/sempaiper-0.985/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_namespace_packages
+from os import listdir
+setup(name='sempaiper',
+
+      version='0.985',
+
+      url='https://github.com/parlorsky/sempaiper',
+
+      license='MIT',
+
+      author='Levap Vobayr',
+
+      author_email='tffriend015@gmail.com',
+
+      description='',
+      packages=find_namespace_packages(where="src"),
+      package_dir={"": "src"},
+      package_data={**{
+        f"sempaiper.q1_1.{x}": ["*.png"] for x in range(1,len(listdir('src/sempaiper/q1_1'))+1)
+        },**{
+        f"sempaiper.q2_1.{x}": ["*.png"] for x in range(1,len(listdir('src/sempaiper/q2_1'))+1)
+        },**{
+        f"sempaiper.q3_1.{x}": ["*.png"] for x in range(1,len(listdir('src/sempaiper/q3_1'))+1)
+        }},
+
+
+      zip_safe=False)
