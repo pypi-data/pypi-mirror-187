6480c01faf36672c983e53a6f42edc2adf7cbdc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:25 +0000

    Add package alas-tools3/1.0.4/alas_tools3-1.0.4-py2-none-any.whl

---
+++ packages/alas-tools3/alas_tools3-1.0.4-py2-none-any.whl/alas_tools3/clients/management/product_template.py
@@ -0,0 +1,5 @@
+from alas_tools3.common.clients.client_base import EntityClientBase
+
+
+class ProductTemplateClient(EntityClientBase):
+    entity_endpoint_base_url = '/management/product-templates/'
