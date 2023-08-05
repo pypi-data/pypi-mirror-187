72557ed52e389a2f71006b74aabe9d2c04671c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:22:10 +0000

    Add package algora-sdk/1.4.56/algora_sdk-1.4.56-py3-none-any.whl

---
+++ packages/algora-sdk/algora_sdk-1.4.56-py3-none-any.whl/algora/api/data/query/__init__.py
@@ -0,0 +1,5 @@
+"""
+Data API query functions for timeseries data.
+"""
+from algora.api.data.query.asynchronous import async_query_dataset, async_query_dataset_csv, async_query_timeseries
+from algora.api.data.query.synchronous import query_dataset, query_dataset_csv, query_timeseries
