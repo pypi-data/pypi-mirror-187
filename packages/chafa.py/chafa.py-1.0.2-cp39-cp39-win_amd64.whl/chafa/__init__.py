d81a7c8d09f713997a8df23b55f7a51a0d6e6371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:19 +0000

    Add package chafa.py/1.0.2/chafa.py-1.0.2-cp39-cp39-win_amd64.whl

---
+++ packages/chafa.py/chafa.py-1.0.2-cp39-cp39-win_amd64.whl/chafa/__init__.py
@@ -0,0 +1,17 @@
+from .chafa import PixelMode
+from .chafa import DitherMode
+from .chafa import CanvasMode
+from .chafa import PixelType
+from .chafa import SymbolTags
+from .chafa import Optimizations
+
+from .chafa import SymbolMap
+from .chafa import ReadOnlySymbolMap
+from .chafa import Canvas
+from .chafa import CanvasConfig
+from .chafa import ReadOnlyCanvasConfig
+from .chafa import TermDb
+from .chafa import TermInfo
+from .chafa import Canvas
+
+
