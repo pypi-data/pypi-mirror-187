3ef3ea3140723105a64aa509e303757298c26fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:48 +0000

    Add package CnkiPythonMeta/0.0.5/CnkiPythonMeta-0.0.5.tar.gz

---
+++ packages/CnkiPythonMeta/CnkiPythonMeta-0.0.5.tar.gz/CnkiPythonMeta-0.0.5/setup.py
@@ -0,0 +1,21 @@
+from time import time
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    setuptools.setup(
+    name="CnkiPythonMeta",
+    version="0.0.5",
+    author="YANG Guixian",
+    author_email="Ygxyoung@hotmail.com",
+    description="This is a CnkiPythonMeta package for processing data from CNKI(https://www.cnki.net/), which the file type must be EXT format.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=['CnkiPythonMeta'],
+    python_requires=">=3.7",
+)
