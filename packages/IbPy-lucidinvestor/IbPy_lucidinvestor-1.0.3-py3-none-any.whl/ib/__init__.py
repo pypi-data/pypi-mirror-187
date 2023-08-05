b69bc2b238cb40a8dd0c273566d841a21faa1122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:21 +0000

    Add package IbPy-lucidinvestor/1.0.3/IbPy_lucidinvestor-1.0.3-py3-none-any.whl

---
+++ packages/IbPy-lucidinvestor/IbPy_lucidinvestor-1.0.3-py3-none-any.whl/ib/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##
+# IbPy package root.
+#
+##
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+# these values substituted during release build.
+from .version import __version__, __ibpyversion__
+
+
+
---
+++ packages/IbPy-lucidinvestor/IbPy_lucidinvestor-1.0.3-py3-none-any.whl/ib/version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+
+__version__ = '1.0.3'
+
+__ibpyversion__ = tuple(int(x) for x in __version__.split('.'))
