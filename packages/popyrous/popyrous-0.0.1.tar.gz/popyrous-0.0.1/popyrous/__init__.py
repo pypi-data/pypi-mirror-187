678fc02e65d43e5e6bb3715b059895a99ac8120b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:32 +0000

    Add package popyrous/0.0.1/popyrous-0.0.1.tar.gz

---
+++ packages/popyrous/popyrous-0.0.1.tar.gz/popyrous-0.0.1/popyrous/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.0.1"
+
+
+
+_POPYROUS_IMPORT_EVERYTHING_ELSE_ = False
+
+if _POPYROUS_IMPORT_EVERYTHING_ELSE_:
+    from . matlab import *
+    from . packages import *
+    from . web import *
+    from . zipfiles import *
+    from . timeseries import *
+    from . utils import *
