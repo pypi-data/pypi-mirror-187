9a9ad98facedf8020c61f59cc2b1717f29c8c632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:25 +0000

    Add package alas-tools3/1.0.4/alas-tools3-1.0.4.tar.gz

---
+++ packages/alas-tools3/alas-tools3-1.0.4.tar.gz/alas-tools3-1.0.4/alas_tools3/clients/management/resource.py
@@ -0,0 +1,5 @@
+from alas_tools3.common.clients.client_base import EntityClientBase
+
+
+class ResourceClient(EntityClientBase):
+    entity_endpoint_base_url = '/management/resources/'
---
+++ packages/alas-tools3/alas-tools3-1.0.4.tar.gz/alas-tools3-1.0.4/alas_tools3/clients/management/workflow.py
@@ -0,0 +1,5 @@
+from alas_tools3.common.clients.client_base import EntityClientBase
+
+
+class WorkflowClient(EntityClientBase):
+    entity_endpoint_base_url = '/management/workflows/'
