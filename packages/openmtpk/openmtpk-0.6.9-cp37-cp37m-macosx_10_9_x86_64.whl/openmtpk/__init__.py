33f6a49fb415dd03314bcfaa8bb8fbf1b27c5c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:55 +0000

    Add package openmtpk/0.6.9/openmtpk-0.6.9-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/openmtpk/openmtpk-0.6.9-cp37-cp37m-macosx_10_9_x86_64.whl/openmtpk/__init__.py
@@ -0,0 +1,14 @@
+"""
+openMTPK
+========
+Python API for C++ based Mathematics Package. See the following links for
+details and documentation: 
+    SOURCE CODE : https://github.com/akielaries/openMTPK/
+    DOCS        : https://akielaries.github.io/openMTPK/
+    PyPI        : https://pypi.org/project/openmtpk/
+
+"""
+
+__all__: list[str]
+__path__: list[str]
+
