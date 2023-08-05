872bd20e2f40b75ecb74237eaacf8f638111cc40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:54 +0000

    Add package dbt-athena-community/1.3.5/dbt-athena-community-1.3.5.tar.gz

---
+++ packages/dbt-athena-community/dbt-athena-community-1.3.5.tar.gz/dbt-athena-community-1.3.5/dbt/include/athena/macros/adapters/relation.sql
@@ -0,0 +1,16 @@
+{% macro athena__drop_relation(relation) -%}
+  {% set rel_type = adapter.get_table_type(relation.schema, relation.table) %}
+  {%- if rel_type is not none and rel_type == 'table' %}
+    {%- do adapter.clean_up_table(relation.schema, relation.table) -%}
+  {%- endif %}
+    {% call statement('drop_relation', auto_begin=False) -%}
+    drop {{ relation.type }} if exists {{ relation }}
+  {%- endcall %}
+{% endmacro %}
+
+{% macro set_table_classification(relation) -%}
+  {%- set format = config.get('format', default='parquet') -%}
+  {% call statement('set_table_classification', auto_begin=False) -%}
+    alter table {{ relation }} set tblproperties ('classification' = '{{ format }}')
+  {%- endcall %}
+{%- endmacro %}
