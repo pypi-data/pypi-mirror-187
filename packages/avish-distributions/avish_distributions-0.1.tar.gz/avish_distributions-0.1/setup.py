ae72251ec4811761abcc4aab55623d53885fa160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:59 +0000

    Add package avish-distributions/0.1/avish_distributions-0.1.tar.gz

---
+++ packages/avish-distributions/avish_distributions-0.1.tar.gz/avish_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='avish_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['avish_distributions'],
+      zip_safe=False)
