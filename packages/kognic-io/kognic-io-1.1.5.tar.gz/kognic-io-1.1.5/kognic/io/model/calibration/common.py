676d29de1b67ec5a165ae3eb4781617b8b62db38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:17:51 +0000

    Add package kognic-io/1.1.5/kognic-io-1.1.5.tar.gz

---
+++ packages/kognic-io/kognic-io-1.1.5.tar.gz/kognic-io-1.1.5/kognic/io/model/calibration/common.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+from kognic.io.model.base_serializer import BaseSerializer
+
+
+class CalibrationType(str, Enum):
+    PINHOLE = "pinhole"
+    FISHEYE = "fisheye"
+    KANNALA = "kannala"
+    LIDAR = "lidar"
+    PRINCIPALPOINTDIST = "principal_point_distortion"
+
+
+class RotationQuaternion(BaseSerializer):
+    w: float
+    x: float
+    y: float
+    z: float
+
+
+class Position(BaseSerializer):
+    x: float
+    y: float
+    z: float
+
+
+class BaseCalibration(BaseSerializer):
+    position: Position
+    rotation_quaternion: RotationQuaternion
