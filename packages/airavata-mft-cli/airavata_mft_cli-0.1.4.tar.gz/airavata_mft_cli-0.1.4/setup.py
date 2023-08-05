c7299a0f91fca22485ed590c16182af704937751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:19:12 +0000

    Add package airavata-mft-cli/0.1.4/airavata_mft_cli-0.1.4.tar.gz

---
+++ packages/airavata-mft-cli/airavata_mft_cli-0.1.4.tar.gz/airavata_mft_cli-0.1.4/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['airavata_mft_cli', 'airavata_mft_cli.storage']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['airavata_mft_sdk==0.0.1-alpha21',
+ 'grpcio-tools==1.46.3',
+ 'grpcio==1.46.3',
+ 'pick==2.2.0',
+ 'typer[all]>=0.7.0,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['mft = airavata_mft_cli.main:app']}
+
+setup_kwargs = {
+    'name': 'airavata-mft-cli',
+    'version': '0.1.4',
+    'description': 'Command Line Client for Airavata MFT data transfer framework',
+    'long_description': '# Airavata MFT Command Line Client',
+    'author': 'Dimuthu Wannipurage',
+    'author_email': 'dimuthu.upeksha2@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
