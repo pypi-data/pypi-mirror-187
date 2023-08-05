18d5fdbe83cbc0da0713c6dc111bc5cf4d04ece8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:11 +0000

    Add package nextline-graphql/0.3.4/nextline_graphql-0.3.4.tar.gz

---
+++ packages/nextline-graphql/nextline_graphql-0.3.4.tar.gz/nextline_graphql-0.3.4/nextlinegraphql/spec.py
@@ -0,0 +1,42 @@
+'''Hook specification for Nextline GraphQL plugin.'''
+from typing import Mapping, Optional, Tuple, Union
+
+from nextline import Nextline
+from starlette.applications import Starlette
+
+from nextlinegraphql.custom import pluggy
+from nextlinegraphql.custom.decorator import asynccontextmanager
+
+hookspec = pluggy.HookspecMarker('nextline')
+hookimpl = pluggy.HookimplMarker('nextline')
+
+
+@hookspec
+def initial_run_no() -> Optional[int]:
+    '''Run No. of the first run.'''
+    pass
+
+
+@hookspec
+def initial_script() -> Optional[str]:
+    '''The script of the first run.'''
+    pass
+
+
+@hookspec
+def schema() -> Optional[Tuple[type, Union[type, None], Union[type, None]]]:
+    '''The GraphQL schema (Query, Mutation, Subscription)'''
+    pass
+
+
+@hookspec
+@asynccontextmanager
+async def lifespan(app: Starlette, nextline: Nextline):
+    '''Starlette lifespan'''
+    pass
+
+
+@hookspec
+async def get_context(context: Mapping) -> Optional[Mapping]:
+    '''Strawberry GraphQL context'''
+    pass
