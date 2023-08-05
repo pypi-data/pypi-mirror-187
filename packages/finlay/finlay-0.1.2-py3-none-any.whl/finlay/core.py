719fff0e9d7bff915c69e7325b4bdeac9ef18bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:57 +0000

    Add package finlay/0.1.2/finlay-0.1.2-py3-none-any.whl

---
+++ packages/finlay/finlay-0.1.2-py3-none-any.whl/finlay/core.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+author: S.M. Sabbir Amin
+date: 21 Jan 2023
+email: sabbir.amin@goava.com, sabbiramin.cse11ruet@gmail.com
+
+"""
+
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    def addition(self, a: int, b: int) -> int:
+        return a + b
+
