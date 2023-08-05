4e03b0426f8fe2e6e4ff9fcc67e48cbf3b212a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:03 +0000

    Add package otsutil/1.0.4.310/otsutil-1.0.4.310.tar.gz

---
+++ packages/otsutil/otsutil-1.0.4.310.tar.gz/otsutil-1.0.4.310/otsutil/__init__.py
@@ -0,0 +1,30 @@
+"""個人的によく使う関数、クラス、型ヒントを纏めたライブラリです。
+"""
+
+__all__ = (
+    "FLOAT_INT",
+    "ObjectSaver",
+    "OtsuNone",
+    "T",
+    "Timer",
+    "deduplicate",
+    "hmsValue",
+    "load_json",
+    "pathLike",
+    "read_lines",
+    "save_json",
+    "setup_path",
+    "str_to_path",
+    "write_lines",
+)
+from .classes import ObjectSaver, OtsuNone, Timer
+from .funcs import (
+    deduplicate,
+    load_json,
+    read_lines,
+    save_json,
+    setup_path,
+    str_to_path,
+    write_lines,
+)
+from .types import FLOAT_INT, T, hmsValue, pathLike
