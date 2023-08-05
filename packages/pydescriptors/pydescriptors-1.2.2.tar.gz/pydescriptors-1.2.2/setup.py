7189e6228eeab35f70910d780413bdb67d0f9261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:19:46 +0000

    Add package pydescriptors/1.2.2/pydescriptors-1.2.2.tar.gz

---
+++ packages/pydescriptors/pydescriptors-1.2.2.tar.gz/pydescriptors-1.2.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().split('\n')
+
+setuptools.setup(
+	name="pydescriptors",
+	version="1.2.2",
+	author="Alexey-Chebotarev",
+	description="A package with several simple descriptors",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/Chebotarev-Alexey/PyDescriptors",
+	packages=setuptools.find_packages(),
+	install_requires=requirements,
+	classifiers=[
+        "Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
