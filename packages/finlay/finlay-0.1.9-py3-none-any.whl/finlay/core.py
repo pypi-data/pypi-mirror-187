e960c5bb91004bc395c68c51045bc93d837820c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:19:34 +0000

    Add package finlay/0.1.9/finlay-0.1.9-py3-none-any.whl

---
+++ packages/finlay/finlay-0.1.9-py3-none-any.whl/finlay/core.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+author: S.M. Sabbir Amin
+date: 21 Jan 2023
+email: sabbir.amin@goava.com, sabbiramin.cse11ruet@gmail.com
+
+"""
+
+
+class Printer:
+    def __init__(self): pass
+
+    def print_hello(self):
+        print('hello world')
