1a19a4e2b6f0c58b0ce4712be6ca92db62a1687a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:41 +0000

    Add package abrain/1.0rc0/abrain-1.0rc0-pp39-pypy39_pp73-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/abrain/abrain-1.0rc0-pp39-pypy39_pp73-manylinux_2_17_i686.manylinux2014_i686.whl/abrain/_cpp/config.h
@@ -0,0 +1,49 @@
+#ifndef KGD_GENOTYPE_CONFIG_H
+#define KGD_GENOTYPE_CONFIG_H
+
+#include <vector>
+#include <set>
+#include <map>
+
+#include "genotype.h"
+
+namespace kgd::eshn {
+
+template <typename T>
+struct Bounds {
+  T min, rndMin, rndMax, max, stddev;
+};
+
+struct Config {
+  /// ================================================
+  /// CPPN genotype parameters
+
+  using FID = genotype::CPPNData::Node::FuncID;
+  using Functions = std::vector<FID>;
+
+  using FunctionSet = Functions;
+  static FunctionSet functionSet;
+
+  using OutputFunctions = Functions;
+  static OutputFunctions outputFunctions;
+
+  using MutationRates = std::map<std::string, float>;
+  static MutationRates mutationRates;
+
+  using FBounds = Bounds<float>;
+  static FBounds cppnWeightBounds;
+
+  /// ================================================
+  /// ANN parameters (none?)
+  static float annWeightsRange;
+  static FID activationFunc;
+
+  /// ================================================
+  /// ES-HyperNEAT parameters
+  static uint initialDepth, maxDepth, iterations;
+  static float divThr, varThr, bndThr;
+};
+
+} // end of namespace kgd::eshn
+
+#endif // KGD_GENOTYPE_CONFIG_H
