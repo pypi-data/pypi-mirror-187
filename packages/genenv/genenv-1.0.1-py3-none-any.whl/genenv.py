b370dd24be1449edcfd5d591d7a524b66c0d3b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:41 +0000

    Add package genenv/1.0.1/genenv-1.0.1-py3-none-any.whl

---
+++ packages/genenv/genenv-1.0.1-py3-none-any.whl/genenv.py
@@ -0,0 +1,32 @@
+import subprocess
+import click
+
+script ="./createnv.sh"
+# test = subprocess.run(["./createnv.sh","name",],check=True)
+# print(test.returncode)
+# print(test.args)
+@click.command()
+@click.option("--name", "-n", "name", required=True ,help="provide name to be used for your virtual environment")
+@click.argument("dependencies", nargs=-1 )
+def cli(name,dependencies):
+    """
+    specify package names after name flag to install them,read documentation for how that works
+    """
+    if not dependencies:
+        commands = [script, name]
+        try:
+             process = subprocess.run(commands)
+             if process.returncode == 0:
+                exit(0)
+        except Exception as e:
+            click.echo(e)
+
+    commands = list(dependencies)
+    commands.insert(0,name)
+    commands.insert(0,script)
+    try:
+        process = subprocess.run(commands)
+    except Exception as e:
+        click.echo(e)
+    
+
