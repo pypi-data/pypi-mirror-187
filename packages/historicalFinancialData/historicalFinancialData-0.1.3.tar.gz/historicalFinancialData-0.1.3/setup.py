9c6d98afb931fb59bcfdd3fa73a497a223d89cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:40 +0000

    Add package historicalFinancialData/0.1.3/historicalFinancialData-0.1.3.tar.gz

---
+++ packages/historicalFinancialData/historicalFinancialData-0.1.3.tar.gz/historicalFinancialData-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='historicalFinancialData',
+    version='0.1.3',
+    description="A package for public companies' historical financial data",
+    url='https://github.com/DanielMistrik/HistoricFinancialData',
+    author='Daniel Mistrik',
+    author_email='danielkomist@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Apache 2.0 License',
+    packages=['historicalFinancialData'],
+    install_requires=['ratelimit', 'numpy', 'requests', 'pypandoc'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: Apache Software License',
+        "Operating System :: OS Independent",
+    ],
+)
