056d0de9eb6b2334dcf4d2faec8fd2a5df76081d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:31 +0000

    Add package invenio-administration/1.0.3/invenio-administration-1.0.3.tar.gz

---
+++ packages/invenio-administration/invenio-administration-1.0.3.tar.gz/invenio-administration-1.0.3/invenio_administration/assets/semantic-ui/js/invenio_administration/src/actions/Delete.js
@@ -0,0 +1,59 @@
+import { DeleteModalTrigger } from "./DeleteModalTrigger";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Popup } from "semantic-ui-react";
+import { i18next } from "@translations/invenio_administration/i18next";
+import _get from "lodash/get";
+import Overridable from "react-overridable";
+
+class DeleteCmp extends Component {
+  render() {
+    const {
+      disabledMessage,
+      disable,
+      title,
+      resourceName,
+      successCallback,
+      idKeyPath,
+      resource,
+    } = this.props;
+    return (
+      <Popup
+        content={disabledMessage}
+        disabled={!disable}
+        trigger={
+          <span>
+            <DeleteModalTrigger
+              title={title}
+              resourceName={resourceName}
+              resource={resource}
+              successCallback={successCallback}
+              idKeyPath={idKeyPath}
+              disabled={disable(resource)}
+              apiEndpoint={_get(resource, "links.self")}
+              disabledDeleteMessage={disabledMessage}
+            />
+          </span>
+        }
+      />
+    );
+  }
+}
+
+DeleteCmp.propTypes = {
+  disabledMessage: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  resourceName: PropTypes.string.isRequired,
+  successCallback: PropTypes.func.isRequired,
+  idKeyPath: PropTypes.string,
+  disable: PropTypes.func,
+  resource: PropTypes.object.isRequired,
+};
+
+DeleteCmp.defaultProps = {
+  disabledMessage: i18next.t("Resource is not deletable."),
+  idKeyPath: "pid",
+  disable: () => false,
+};
+
+export default Overridable.component("InvenioAdministration.DeleteAction", DeleteCmp);
