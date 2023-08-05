4a2f19566053d6bcc49f442c61f6d587a5877b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:21 +0000

    Add package thehive4py/2.0.0b4/thehive4py-2.0.0b4-py3-none-any.whl

---
+++ packages/thehive4py/thehive4py-2.0.0b4-py3-none-any.whl/thehive4py/types/procedure.py
@@ -0,0 +1,34 @@
+from typing import TypedDict
+
+
+class InputProcedureRequired(TypedDict):
+    occurDate: int
+    tactic: str
+    patternId: str
+
+
+class InputProcedure(InputProcedureRequired, total=False):
+    description: str
+
+
+class OutputProcedureRequired(TypedDict):
+    _id: str
+    _createdAt: int
+    _createdBy: str
+    occurDate: int
+    tactic: str
+    tacticLabel: str
+    extraData: dict
+
+
+class OutputProcedure(OutputProcedureRequired, total=False):
+    _updatedAt: int
+    _updatedBy: str
+    description: str
+    patternId: str
+    patternName: str
+
+
+class InputUpdateProcedure(TypedDict, total=False):
+    description: str
+    occurDate: int
