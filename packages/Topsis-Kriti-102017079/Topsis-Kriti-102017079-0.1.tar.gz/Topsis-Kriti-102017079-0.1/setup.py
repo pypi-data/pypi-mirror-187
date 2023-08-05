25ff30f20790a26b21557c2a22b7a59533b5e78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:07 +0000

    Add package Topsis-Kriti-102017079/0.1/Topsis-Kriti-102017079-0.1.tar.gz

---
+++ packages/Topsis-Kriti-102017079/Topsis-Kriti-102017079-0.1.tar.gz/Topsis-Kriti-102017079-0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+    name='Topsis-Kriti-102017079',
+    packages=['Topsis-Kriti-102017079'],
+    version='0.1',
+    license='MIT',
+    description='A convenient python package for Topsis rank and score calculation for a given dataset, weights and impacts',
+    author='Kriti Singhal',
+    author_email='kritisinghal711@gmail.com',
+    url='https://github.com/Kriti-bit/Topsis-Kriti-102017079',
+    download_url='https://github.com/Kriti-bit/Topsis-Kriti-102017079/archive/refs/tags/v0.1.tar.gz',
+    keywords=['topsis'],
+    install_requires=[
+        'pandas',
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
