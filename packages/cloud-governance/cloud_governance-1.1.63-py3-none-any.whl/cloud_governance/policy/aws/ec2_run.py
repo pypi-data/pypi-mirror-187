d96791715f57b81cafb481bb528efc8c28d11c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:58 +0000

    Add package cloud-governance/1.1.63/cloud_governance-1.1.63-py3-none-any.whl

---
+++ packages/cloud-governance/cloud_governance-1.1.63-py3-none-any.whl/cloud_governance/policy/aws/ec2_run.py
@@ -0,0 +1,21 @@
+
+from cloud_governance.policy.policy_operations.aws.zombie_non_cluster.run_zombie_non_cluster_policies import NonClusterZombiePolicy
+
+
+class EC2Run(NonClusterZombiePolicy):
+
+    def __init__(self):
+        super(EC2Run, self).__init__()
+
+    def run(self):
+        """
+        This method list all in-use ebs volumes
+        @return:
+        """
+        running_instances = []
+        instances = self._ec2_operations.get_instances()
+        for instance in instances:
+            for resource in instance['Instances']:
+                if resource.get('State').get('Name') == 'running':
+                    running_instances.append(resource)
+        return self._organise_instance_data(running_instances)
