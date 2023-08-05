2faaab202bbd9f4f89ff0126aafdfe7dd6c992a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:31 +0000

    Add package 22BEE0060/0.0.1/22BEE0060-0.0.1.tar.gz

---
+++ packages/22BEE0060/22BEE0060-0.0.1.tar.gz/22BEE0060-0.0.1/src/22BEE0060/main.py
@@ -0,0 +1,18 @@
+a = int(input('Enter the first number'))
+b = int(input('Enter the second number'))
+def addition(a,b):
+    print(a+b)
+
+def subtraction(a,b):
+    print(a-b)
+
+def multiplication(a,b):
+    print(a*b)
+
+def division(a,b):
+    print(a/b)
+
+addition(a,b)
+subtraction(a,b)
+multiplication(a,b)
+division(a,b)
