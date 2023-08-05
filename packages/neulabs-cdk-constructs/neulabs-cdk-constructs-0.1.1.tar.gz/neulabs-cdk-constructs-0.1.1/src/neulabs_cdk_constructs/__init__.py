4b69ca714c3f673b9798a992352c844401321a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:35 +0000

    Add package neulabs-cdk-constructs/0.1.1/neulabs-cdk-constructs-0.1.1.tar.gz

---
+++ packages/neulabs-cdk-constructs/neulabs-cdk-constructs-0.1.1.tar.gz/neulabs-cdk-constructs-0.1.1/src/neulabs_cdk_constructs/__init__.py
@@ -0,0 +1,25 @@
+'''
+# replace this
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+from ._jsii import *
+
+__all__ = [
+    "stacks",
+]
+
+publication.publish()
+
+# Loading modules to ensure their types are registered with the jsii runtime library
+from . import stacks
