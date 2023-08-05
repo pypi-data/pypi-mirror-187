f6deedf4424c2677894f7c0ccd40aa6122807241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:16 +0000

    Add package glinux-welcome/32/glinux-welcome-32.tar.gz

---
+++ packages/glinux-welcome/glinux-welcome-32.tar.gz/glinux-welcome-32/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+import reporter
+
+setup(name='glinux-welcome', version='32', description='Ethical research - dep confusion package. This is not a real package, do not install.')
