ee36e5425f25ef3cebc9a3eae3b76529873591d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:47 +0000

    Add package prowler-cloud/3.1.1/prowler-cloud-3.1.1.tar.gz

---
+++ packages/prowler-cloud/prowler-cloud-3.1.1.tar.gz/prowler-cloud-3.1.1/prowler/providers/aws/services/iam/iam_policy_no_administrative_privileges/iam_policy_no_administrative_privileges.py
@@ -0,0 +1,32 @@
+from prowler.lib.check.models import Check, Check_Report_AWS
+from prowler.providers.aws.services.iam.iam_client import iam_client
+
+
+class iam_policy_no_administrative_privileges(Check):
+    def execute(self) -> Check_Report_AWS:
+        findings = []
+        for index, policy_document in enumerate(iam_client.list_policies_version):
+            report = Check_Report_AWS(self.metadata())
+            report.region = iam_client.region
+            report.resource_arn = iam_client.policies[index]["Arn"]
+            report.resource_id = iam_client.policies[index]["PolicyName"]
+            report.status = "PASS"
+            report.status_extended = f"Policy {iam_client.policies[index]['PolicyName']} does not allow '*:*' administrative privileges"
+            # Check the statements, if one includes *:* stop iterating over the rest
+            if type(policy_document["Statement"]) != list:
+                policy_statements = [policy_document["Statement"]]
+            else:
+                policy_statements = policy_document["Statement"]
+            for statement in policy_statements:
+                if (
+                    statement["Effect"] == "Allow"
+                    and "Action" in statement
+                    and "*" in statement["Action"]
+                    and "*" in statement["Resource"]
+                ):
+                    report.status = "FAIL"
+                    report.status_extended = f"Policy {iam_client.policies[index]['PolicyName']} allows '*:*' administrative privileges"
+                    break
+
+            findings.append(report)
+        return findings
