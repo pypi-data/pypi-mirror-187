db1f64f40b229ff52c996e550056d447ae6fbccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:33 +0000

    Add package bluequbit/0.0.18b1/bluequbit-0.0.18b1.tar.gz

---
+++ packages/bluequbit/bluequbit-0.0.18b1.tar.gz/bluequbit-0.0.18b1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read()
+requirements = requirements.splitlines()
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+readme = "\n".join(readme.split("\n")[2:])
+
+packages = find_packages()
+
+with open("bluequbit/version.py") as f:
+    Version = f.read()
+
+Version = Version.rstrip()
+Version = Version[15:-1]
+
+setup(
+    name="bluequbit",
+    version=Version,
+    description="Python SDK to BlueQubit app",
+    license="Apache 2.0",
+    author="BlueQubit",
+    author_email="hovnatan@bluequbit.io",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    packages=find_packages(exclude=("tests",)),
+    python_requires=">=3.7",
+)
