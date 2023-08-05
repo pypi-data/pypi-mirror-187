089f2562a391149b2f2cbfe2c011a69ebfd4fbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:34 +0000

    Add package beginai/1.5.3/beginai-1.5.3.tar.gz

---
+++ packages/beginai/beginai-1.5.3.tar.gz/beginai-1.5.3/beginai/exec/embeddings/schema.py
@@ -0,0 +1,8 @@
+from ...orchapi.api import OrchAPI
+
+class Schema(object):
+    def __init__(self, app_id):
+        self.app_id = app_id
+
+    def send_schema(self, schema):
+        submit_schema
