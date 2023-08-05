56a6e95a5db3536279120d69caa3282a839477d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:21:26 +0000

    Add package hera-calibration/3.2.2/hera_calibration-3.2.2-py3-none-any.whl

---
+++ packages/hera-calibration/hera_calibration-3.2.2-py3-none-any.whl/hera_calibration-3.2.2.data/scripts/auto_reflection_run.py
@@ -0,0 +1,16 @@
+#!python
+# -*- coding: utf-8 -*-
+# Copyright 2019 the HERA Project
+# Licensed under the MIT License
+
+from hera_cal import reflections
+
+parser = reflections.auto_reflection_argparser()
+a = parser.parse_args()
+
+kwargs = dict(vars(a))
+kwargs.pop('data')
+kwargs.pop('output_fname')
+kwargs.pop('dly_ranges')
+
+reflections.auto_reflection_run(a.data, a.dly_ranges, a.output_fname, **kwargs)
---
+++ packages/hera-calibration/hera_calibration-3.2.2-py3-none-any.whl/hera_calibration-3.2.2.data/scripts/noise_from_autos.py
@@ -0,0 +1,14 @@
+#!python
+# -*- coding: utf-8 -*-
+# Copyright 2019 the HERA Project
+# Licensed under the MIT License
+
+"""Command-line drive script for hera_cal.noise"""
+
+from hera_cal import noise
+import sys
+
+a = noise.noise_std_argparser()
+args = a.parse_args()
+noise.write_per_antenna_noise_std_from_autos(args.infile, args.outfile, calfile=args.calfile, gain_convention=args.gain_convention,
+                                             add_to_history=' '.join(sys.argv), clobber=args.clobber)
