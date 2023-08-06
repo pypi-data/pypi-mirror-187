ed146daa3bcd4fc87eb8110a776456bd58d208e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:39 +0000

    Add package robin-sd-download/0.2.5/robin_sd_download-0.2.5.tar.gz

---
+++ packages/robin-sd-download/robin_sd_download-0.2.5.tar.gz/robin_sd_download-0.2.5/robin_sd_download/create_config.py
@@ -0,0 +1,21 @@
+import yaml
+import os
+
+def create_config(config_file):
+    print("Creating config file at " + config_file)
+    config = {
+        "robin_email": "your email address",
+        "robin_password": "your password",
+        "api_url": "api url",
+        "radar_id": "id of the radar you want to download the software for"
+    }
+    # create the directory if it doesn't exist
+    os.makedirs(os.path.dirname(config_file), exist_ok=True)
+    # write the config file
+    with open(config_file, "w") as stream:
+        yaml.dump(config, stream)
+    print("Config file created at " + config_file)
+    # open the config file in the default editor
+    os.system("start " + config_file)
+    # save
+    print("Please save the config file and run this script again.")
