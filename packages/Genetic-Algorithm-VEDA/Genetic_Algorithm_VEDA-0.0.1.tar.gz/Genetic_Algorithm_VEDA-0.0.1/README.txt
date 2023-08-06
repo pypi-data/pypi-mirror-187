ccda8f1422f2fb0493e1df0e8be463b86f6c43d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:34 +0000

    Add package Genetic-Algorithm-VEDA/0.0.1/Genetic_Algorithm_VEDA-0.0.1.tar.gz

---
+++ packages/Genetic-Algorithm-VEDA/Genetic_Algorithm_VEDA-0.0.1.tar.gz/Genetic_Algorithm_VEDA-0.0.1/README.txt
@@ -0,0 +1,1 @@
+Teste de elaboração de uma biblioteca
---
+++ packages/Genetic-Algorithm-VEDA/Genetic_Algorithm_VEDA-0.0.1.tar.gz/Genetic_Algorithm_VEDA-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+packages = \
+['Genetic_Algorithm_VEDA']
+
+package_data = \
+{'': ['*']}
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'  
+]
+
+setup(
+    name='Genetic_Algorithm_VEDA',
+    version='0.0.1',
+    description='Algoritmo Genético para estudo da evolução de circuitos lógicos combinacionais na presença de falhas.',
+    long_description=open('README.txt').read() + '\n\n' + open("CHANGELOG.txt").read(),
+    url='',
+    author='Humberto Távora',
+    author_email='humbertocct@hotmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='Genetic Algorithm',
+    packages=find_packages(),
+    install_requires = ['']
+)
