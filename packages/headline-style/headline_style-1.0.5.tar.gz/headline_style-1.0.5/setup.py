b5a86c5685ed63ed6e416f28b77ca39108dda29c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:03:13 +0000

    Add package headline-style/1.0.5/headline_style-1.0.5.tar.gz

---
+++ packages/headline-style/headline_style-1.0.5.tar.gz/headline_style-1.0.5/setup.py
@@ -0,0 +1,38 @@
+"""
+Setup file for headLineStyle.
+"""
+from os import path
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst'), encoding='utf-8') as f:
+    lines = f.read()
+
+try:
+    long_description = open('README.rst').read()
+except(IOError, ImportError):
+    long_description = open('README.rst').read()
+
+import setuptools
+
+setuptools.setup(
+    name="headline_style",  # This is the name of the package
+    version="1.0.5",  # The initial release version
+    author="Vinod Baste",  # Full name of the author
+    url="https://github.com/vinodbaste/python_headline_style",
+    packages=setuptools.find_packages(),  # List of all python modules to be installed
+    readme="README.md",
+    description="About This filter changes a given text to Title Caps, and attempts to be clever about SMALL words "
+                "like a/an/the in the input. The list of SMALL words which are not capped comes from the New York "
+                "Times Manual of Style, plus some others like 'vs' and 'v'.",
+    long_description=lines,
+    long_description_content_type="text/x-rst; charset=UTF-8",
+    use_pyscaffold=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],  # Information to filter the project on PyPi website
+    python_requires='>=3.7',  # Minimum version requirement of the package
+    py_modules=["headline_style"],  # Name of the python package
+    install_requires=["regex >=2020.4.4"],  # Install other dependencies if any
+    download_url="https://github.com/vinodbaste/python_headline_style/archive/refs/heads/main.zip"
+)
