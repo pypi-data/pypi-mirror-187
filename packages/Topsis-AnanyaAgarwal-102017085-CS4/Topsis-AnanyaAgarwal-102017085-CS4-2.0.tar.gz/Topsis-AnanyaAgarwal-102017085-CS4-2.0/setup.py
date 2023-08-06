de24ed561dbcca0dbe42f4a35fdc4f9e727ff3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:35 +0000

    Add package Topsis-AnanyaAgarwal-102017085-CS4/2.0/Topsis-AnanyaAgarwal-102017085-CS4-2.0.tar.gz

---
+++ packages/Topsis-AnanyaAgarwal-102017085-CS4/Topsis-AnanyaAgarwal-102017085-CS4-2.0.tar.gz/Topsis-AnanyaAgarwal-102017085-CS4-2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Topsis-AnanyaAgarwal-102017085-CS4',
+    version='2.0',
+    description='Python implementation of the Technique for Order of Preference by Similarity to Ideal Solution (TOPSIS)...',
+    author='Ananya Agarwal',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas'
+    ],
+)
