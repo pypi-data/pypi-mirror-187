bdd4fd7a0b0db8f3fad5f2c41498ecf649eda303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:30 +0000

    Add package 102003634/0.0.6/102003634-0.0.6.tar.gz

---
+++ packages/102003634/102003634-0.0.6.tar.gz/102003634-0.0.6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+VERSION = '0.0.6'
+DESCRIPTION = 'Script implementing TOPSIS'
+
+
+# Setting up
+setup(
+    name="102003634",
+    version=VERSION,
+    author="Falguni Sharma",
+    author_email="falguni7572@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    entry_points={
+        'console_scripts': [
+            'topsis=topsisLibrary.topsis:main'
+        ]
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires='>=3.5',
+)
