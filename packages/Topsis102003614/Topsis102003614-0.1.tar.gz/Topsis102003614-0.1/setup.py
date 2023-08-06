530178f585e8c42160da1dcc36c6a819c1bd5102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:14 +0000

    Add package Topsis102003614/0.1/Topsis102003614-0.1.tar.gz

---
+++ packages/Topsis102003614/Topsis102003614-0.1.tar.gz/Topsis102003614-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='Topsis102003614',
+ version='0.1',
+ author="Ashutosh Bajpai",
+ author_email="ashutoshbajpai171@gmail.com",
+ description="CalcTopsis is a Python package implementing Topsis method sed for multi-criteria decision analysis. Topsis stands for Technique for Order of Preference by Similarity to Ideal Solution",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
