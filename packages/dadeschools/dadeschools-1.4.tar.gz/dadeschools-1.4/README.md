c0c35c37941f093146f85feea43f3033df10fbdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:33 +0000

    Add package dadeschools/1.4/dadeschools-1.4.tar.gz

---
+++ packages/dadeschools/dadeschools-1.4.tar.gz/dadeschools-1.4/README.md
@@ -0,0 +1,11 @@
+# dadeschools
+
+## Installation
+`pip install dadeschools`
+
+## Usage
+Run 'dadeschools' in the terminal
+
+`dadeschools`
+
+Then enter your ID number to get grades from dadeschools.net
---
+++ packages/dadeschools/dadeschools-1.4.tar.gz/dadeschools-1.4/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['dadeschools']
+entry_points = \
+{'console_scripts': ['dadeschools = dadeschools:main']}
+
+setup_kwargs = {
+    'name': 'dadeschools',
+    'version': '1.4',
+    'description': 'The Ultimate Dadeschools CLI',
+    'long_description': "# dadeschools\n\n## Installation\n`pip install dadeschools`\n\n## Usage\nRun 'dadeschools' in the terminal\n\n`dadeschools`\n\nThen enter your ID number to get grades from dadeschools.net\n",
+    'author': 'BananaApache',
+    'author_email': 'daniel.miami2005@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
