100555d584b3c0924d9c75fbda6350d16106ee32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:37 +0000

    Add package TransformerModels/0.3/TransformerModels-0.3.tar.gz

---
+++ packages/TransformerModels/TransformerModels-0.3.tar.gz/TransformerModels-0.3/TransformerModelDI/TransformerModel.py
@@ -0,0 +1,18 @@
+from setfit import SetFitModel
+import os
+import pandas as pd
+
+
+class TransformerModel:
+    def __init__(self, classifier=None):
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'transformer_model_new_env')
+        self.classifier = SetFitModel.from_pretrained(path)
+        
+    def predict(self, documents):
+        text = [d['text'] for d in documents]
+        ids = [d['id'] for d in documents]
+        predictions = self.classifier(text)
+        return [{'id': v[0],'text':v[1],'label': v[2]} for v in zip(ids,text,predictions)]
+
+    def __call__(self, documents, *args, **kwargs):
+        return self.predict(documents)
