4e03b6e265f793f367175cbf05d6e566d24b8330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:33 +0000

    Add package uploader-client/0.1.1/uploader_client-0.1.1-py3-none-any.whl

---
+++ packages/uploader-client/uploader_client-0.1.1-py3-none-any.whl/uploader_client/const.py
@@ -0,0 +1,3 @@
+DEFAULT_REQUEST_TIMEOUT = 300  # Таймаут запроса, секунд
+DEFAULT_REQUEST_RETRIES = 5  # Количество повторных попыток
+DEFAULT_RETRY_FACTOR = 1  # Шаг увеличения задержки м.д. запросами
