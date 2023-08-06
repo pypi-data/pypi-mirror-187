c0ae9bba220e3106c8c783f782de124ba3fcfd95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:29 +0000

    Add package empiric-network/1.4.7/empiric-network-1.4.7.tar.gz

---
+++ packages/empiric-network/empiric-network-1.4.7.tar.gz/empiric-network-1.4.7/empiric/publisher/fetchers/__init__.py
@@ -0,0 +1,6 @@
+from .bitstamp import BitstampFetcher
+from .cex import CexFetcher
+from .coinbase import CoinbaseFetcher
+from .coingecko import CoingeckoFetcher
+from .gemini import GeminiFetcher
+from .thegraph import TheGraphFetcher
