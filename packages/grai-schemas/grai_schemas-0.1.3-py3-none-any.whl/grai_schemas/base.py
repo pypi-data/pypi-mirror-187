2712f47eb6c0c060f78b764506de686d9943062e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:41 +0000

    Add package grai-schemas/0.1.3/grai_schemas-0.1.3-py3-none-any.whl

---
+++ packages/grai-schemas/grai_schemas-0.1.3-py3-none-any.whl/grai_schemas/base.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+from grai_schemas.models import GraiEdgeMetadata, GraiNodeMetadata
+from pydantic import BaseModel
+
+
+class EdgeMetadata(BaseModel):
+    grai: GraiEdgeMetadata
+
+
+class NodeMetadata(BaseModel):
+    grai: GraiNodeMetadata
+
+
+__all__ = ['EdgeMetadata', 'NodeMetadata']
