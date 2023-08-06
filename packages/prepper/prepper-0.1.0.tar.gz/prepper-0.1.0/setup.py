28e210a3edb159aaecee5f4f1985168f9ace8362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:06 +0000

    Add package prepper/0.1.0/prepper-0.1.0.tar.gz

---
+++ packages/prepper/prepper-0.1.0.tar.gz/prepper-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['prepper']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aenum>=3.1.11,<4.0.0',
+ 'h5py>=3.8.0,<4.0.0',
+ 'loguru>=0.6.0,<0.7.0',
+ 'numpy>=1.24.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'prepper',
+    'version': '0.1.0',
+    'description': 'Allows python objects to be stored and loaded from an HDF5 file',
+    'long_description': '',
+    'author': 'Varchas Gopalaswamy',
+    'author_email': 'vgop@lle.rochester.edu',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
