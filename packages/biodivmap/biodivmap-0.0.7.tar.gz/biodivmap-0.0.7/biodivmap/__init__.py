88ec8b48e12838e386ac942e143b8ecd7073b8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:20 +0000

    Add package biodivmap/0.0.7/biodivmap-0.0.7.tar.gz

---
+++ packages/biodivmap/biodivmap-0.0.7.tar.gz/biodivmap-0.0.7/biodivmap/__init__.py
@@ -0,0 +1,5 @@
+def fun1(bl1):
+    print("su")
+
+def fun2(bl2):
+    print("fun2"+str(bl2))
---
+++ packages/biodivmap/biodivmap-0.0.7.tar.gz/biodivmap-0.0.7/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+   name='biodivmap',        # 项目名
+   version='0.0.7',       # 版本号
+   description='Biodiversity mapping',
+   packages=find_packages(),   # 包括在安装包内的Python包
+   author='Zijie Wang',
+   install_requires=[
+        'numpy',
+        'geopandas',
+    ],
+)
