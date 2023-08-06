f95aea3fe9dbbe3d3d7af09c5ac6bba065800a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:48 +0000

    Add package chaco/5.1.1/chaco-5.1.1-cp36-cp36m-win32.whl

---
+++ packages/chaco/chaco-5.1.1-cp36-cp36m-win32.whl/chaco/__init__.py
@@ -0,0 +1,20 @@
+#  Copyright (c) 2005-2014 by Enthought, Inc.
+#  All rights reserved.
+""" Two-dimensional plotting application toolkit.
+    Part of the Chaco project of the Enthought Tool Suite.
+"""
+try:
+    from chaco._version import full_version as __version__  # noqa
+except ImportError:
+    __version__ = "not-built"
+
+
+__requires__ = [
+    "traits>=6.2.0", "traitsui", "pyface>=7.2.0", "numpy", "enable>=5.2.0"
+]
+
+
+__extras_require__ = {
+    "docs": ["enthought-sphinx-theme", "sphinx", "sphinx-copybutton"],
+    'examples': ['encore', 'scipy', 'pandas']
+}
