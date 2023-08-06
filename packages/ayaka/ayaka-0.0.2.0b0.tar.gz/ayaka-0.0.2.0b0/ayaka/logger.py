331a339f639890c7a7d69dde446533dcb0b94f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:13 +0000

    Add package ayaka/0.0.2.0b0/ayaka-0.0.2.0b0.tar.gz

---
+++ packages/ayaka/ayaka-0.0.2.0b0.tar.gz/ayaka-0.0.2.0b0/ayaka/logger.py
@@ -0,0 +1,28 @@
+import sys
+from loguru import logger
+from functools import partial
+
+logger.level("AYAKA", no=27, icon="⚡", color="<blue>")
+
+ayaka_clog = partial(logger.opt(colors=True).log, "AYAKA")
+'''彩色 ayaka log方法（有<>不匹配风险）'''
+ayaka_log = partial(logger.log, "AYAKA")
+'''单色 ayaka log方法'''
+clogger = logger.opt(colors=True)
+'''彩色 logger（有<>不匹配风险）'''
+
+logger_format = "<g>{time:MM-DD HH:mm:ss}</g> [<lvl>{level}</lvl>] <c><u>{name}</u></c> | {message}"
+
+
+# 仅移除原生logger
+try:
+    logger.remove(0)
+except ValueError:
+    pass
+
+# 若无logger，则需新建（避免与nonebot同时建立两种格式的handler）
+# 注意：该特性并无正式用法，loguru维护者不保证其在未来可用
+# https://github.com/Delgan/loguru/issues/201
+if not logger._core.handlers:
+    # 输出至终端
+    logger.add(sys.stdout, level=0, diagnose=False, format=logger_format)
