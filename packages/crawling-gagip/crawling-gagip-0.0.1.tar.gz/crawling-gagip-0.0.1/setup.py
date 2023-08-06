7b3b4a91ffcc1829be49ec8c63e68858b604604b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:32 +0000

    Add package crawling-gagip/0.0.1/crawling-gagip-0.0.1.tar.gz

---
+++ packages/crawling-gagip/crawling-gagip-0.0.1.tar.gz/crawling-gagip-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+    
+
+setuptools.setup(
+    name ='crawling-gagip',
+    version = '0.0.1',
+    description = 'crawling',
+    author = 'gagip',
+    author_email = 'y02645@gmail.com',
+    url = '',
+    py_modules = setuptools.find_packages()
+)
