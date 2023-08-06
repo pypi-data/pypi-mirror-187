bea941a6b6a6377b8004d0da95bf93a41ee9d485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:27 +0000

    Add package cffm/0.2.4/cffm-0.2.4-py3-none-any.whl

---
+++ packages/cffm/cffm-0.2.4-py3-none-any.whl/cffm/__init__.py
@@ -0,0 +1,16 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
+
+
+from cffm.field import MISSING, field, property_field
+from cffm.config import config, section, sections_from_entrypoints
+from cffm.multi import MultiSourceConfig
+from cffm.source import *
