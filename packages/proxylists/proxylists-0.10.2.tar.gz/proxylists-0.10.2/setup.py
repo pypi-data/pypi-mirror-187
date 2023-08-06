d3261d096af6180f2f2ac66cf9251cb9b7039858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:52 +0000

    Add package proxylists/0.10.2/proxylists-0.10.2.tar.gz

---
+++ packages/proxylists/proxylists-0.10.2.tar.gz/proxylists-0.10.2/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""ProxyLists.
+
+    Get a list of free Proxy Servers
+See:
+https://github.com/phenobarbital/proxylists
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='proxylists',
+    version=open("VERSION").read().strip(),
+    python_requires=">=3.8.0",
+    url='https://github.com/phenobarbital/proxylists',
+    description='List of Proxy Servers',
+    long_description='Get a list of free Proxy Servers',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    author='Jesus Lara',
+    author_email='jlara@trocglobal.com',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    license='BSD',
+    license_file='LICENSE',
+    setup_requires=[
+        "asyncio==3.4.3"
+    ],
+    install_requires=[
+        "asyncio==3.4.3",
+        "uvloop>=0.16.0",
+        "aiohttp==3.8.3",
+        'requests>=2.25.0',
+        'requests[socks]>=2.25.1',
+        'orjson==3.8.5',
+        'lxml==4.9.2'
+    ],
+    tests_require=[
+            'pytest>=5.4.0',
+            'coverage'
+    ],
+    project_urls={  # Optional
+        'Source': 'https://github.com/phenobarbital/proxylists',
+        'Funding': 'https://paypal.me/phenobarbital',
+        'Say Thanks!': 'https://saythanks.io/to/phenobarbital',
+    },
+)
