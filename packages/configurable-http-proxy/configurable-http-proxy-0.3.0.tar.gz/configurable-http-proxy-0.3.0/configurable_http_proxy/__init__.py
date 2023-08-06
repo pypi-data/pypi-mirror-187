4ecf456742e18b6dac51ff5ba35b8ccb3b0ed68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:31 +0000

    Add package configurable-http-proxy/0.3.0/configurable-http-proxy-0.3.0.tar.gz

---
+++ packages/configurable-http-proxy/configurable-http-proxy-0.3.0.tar.gz/configurable-http-proxy-0.3.0/configurable_http_proxy/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+from configurable_http_proxy._version import version as __version__  # noqa: F401
+
+log = logging.getLogger("configurable_http_proxy")
+log.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("[%(levelname)-.1s %(asctime)s %(name)s] %(message)s"))
+logging.root.addHandler(handler)
