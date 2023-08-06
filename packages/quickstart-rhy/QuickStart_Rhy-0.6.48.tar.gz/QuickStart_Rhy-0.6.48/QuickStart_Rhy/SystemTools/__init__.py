e307ee276f27917477406d8027cd920d53551535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:26 +0000

    Add package quickstart-rhy/0.6.48/QuickStart_Rhy-0.6.48.tar.gz

---
+++ packages/quickstart-rhy/QuickStart_Rhy-0.6.48.tar.gz/QuickStart_Rhy-0.6.48/QuickStart_Rhy/SystemTools/__init__.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+"""
+系统工具库
+
+System tool library
+"""
+
+
+def clear_mem():
+    """
+    清理系统内存
+
+    Clean system memory
+
+    :return: None
+    """
+    from .. import platform, external_exec
+
+    if platform.startswith("win"):
+        print("Not support")
+    else:
+        if platform.startswith("darwin"):
+            external_exec("sudo purge")
+        else:
+            external_exec("sync")
+            external_exec("echo 3 > /proc/sys/vm/drop_caches")
+
+
+def get_core_num():
+    """
+    获取核数量
+
+    :return: core num
+    """
+    from .. import requirePackage
+
+    return requirePackage("psutil").cpu_count()
