b4140d0c739104cf91c9fd7897e49b3787cedee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:18 +0000

    Add package typer-cloup-cli/0.4.0/typer_cloup_cli-0.4.0-py3-none-any.whl

---
+++ packages/typer-cloup-cli/typer_cloup_cli-0.4.0-py3-none-any.whl/typer_cloup_cli/__init__.py
@@ -0,0 +1,8 @@
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata  # pragma: no cover
+
+__version__ = metadata.version("typer-cloup-cli")
