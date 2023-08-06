4cf1fcfd988d5f3a92f61c5b1d1b0aaa5d5c2000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:04 +0000

    Add package SNMatrix/0.1/SNMatrix-0.1-py3-none-any.whl

---
+++ packages/SNMatrix/SNMatrix-0.1-py3-none-any.whl/SNMatrix/__init__.py
@@ -0,0 +1,65 @@
+#INPUT MATRIX
+def input_matrix(rows,columns): 
+    A=[]
+    print('Enter the entries row wise:')
+    a=[]
+    for i in range(rows):
+        a=[]
+        for j in range(columns):
+            a.append(int(input()))
+        A.append(a)
+    for i in range(rows):
+        for j in range(columns):
+            print(A[i][j],end=' ')
+        print()
+    return A
+
+#MATRIX MULTIPLICATION
+def matrix_multiplication(A,B):
+    mul_result=[]
+    for i in range(len(A)):
+        c=[]
+        for j in range(len(B[0])):
+            result=0
+            for k in range(len(B)):
+                result+=A[i][k]*B[k][j]
+            c.append(result)
+        mul_result.append(c)
+    print('Matrix Multiplication=')
+    for r in range (len(mul_result)):
+        for j in range(len(mul_result[0])):
+            print(mul_result[r][j],end=' ')
+        print()
+    return mul_result
+
+#MATRIX ADDITION
+def matrix_addition(A,B):
+    mat_addition=[]
+    for i in range(len(A)):
+        c=[]
+        for j in range(len(B)):
+            if A[i][j]==B[i][j]:
+                c.append(A[i][j]+B[i][j])
+        mat_addition.append(c)
+    print('Matrix Addition=')
+    for z in range(len(mat_addition)):
+        for w in range(len(mat_addition[0])):
+            print(mat_addition[z][w],end=' ')
+        print()
+    return mat_addition
+
+#MATRIX SUBRACTION
+def matrix_subraction(A,B):
+    mat_subraction=[]
+    for i in range(len(A)):
+        c=[]
+        for j in range(len(B)):
+            if A[i][j]==B[i][j]:
+                c.append(A[i][j]-B[i][j])
+        mat_subraction.append(c)
+    print('Matrix Subraction=')
+    for i in range(len(mat_subraction)):
+        for j in range(len(mat_subraction[0])):
+            print(mat_subraction[i][j],end=' ')
+        print()
+    return mat_subraction
