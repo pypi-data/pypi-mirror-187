541d4d57e480341fc9be8d06b3ba9dc825333d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:56 +0000

    Add package gpt-traceback/0.1.11/gpt_traceback-0.1.11.tar.gz

---
+++ packages/gpt-traceback/gpt_traceback-0.1.11.tar.gz/gpt_traceback-0.1.11/src/gpt_traceback/prompt.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+def predict_prompt(prompt):
+  apiKey = os.environ.get("OPENAI_API_KEY")
+  if apiKey is None:
+    print("Api Key is missing. Head over to:\nhttps://beta.openai.com/account/api-keys")
+    apiKey = input("Please enter OPENAI API KEY:")
+    apiKey = apiKey.strip()
+    assert "sk-" in apiKey, "API Key needs to start with sk-..."
+    os.environ["OPENAI_API_KEY"] = apiKey
+  model = 'text-davinci-003'
+  temperature = 0.
+  maxTokens = 150
+
+  url = 'https://api.openai.com/v1/completions'
+  headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + apiKey
+  }
+  payload = {
+      'model': model,
+      'prompt': prompt,
+      'temperature': temperature,
+      'max_tokens': maxTokens
+  }
+
+  response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+  response_json = json.loads(response.text)
+  return response_json['choices'][0]['text']
