be629fefb8d0ffcb7692fe660346295a8b11dc97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:36 +0000

    Add package wgpu/0.9.0/wgpu-0.9.0-py3-none-win_amd64.whl

---
+++ packages/wgpu/wgpu-0.9.0-py3-none-win_amd64.whl/wgpu/gui/__init__.py
@@ -0,0 +1,6 @@
+"""
+Code to provide a canvas to render to.
+"""
+
+from .base import WgpuCanvasInterface, WgpuCanvasBase, WgpuAutoGui  # noqa: F401
+from ._offscreen import WgpuOffscreenCanvas  # noqa: F401
