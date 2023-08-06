cb5e9a01a9ad621dc94f71d9f74c5da1970b93f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:09 +0000

    Add package peppyproject/0.2.4/peppyproject-0.2.4-py3-none-any.whl

---
+++ packages/peppyproject/peppyproject-0.2.4-py3-none-any.whl/peppyproject/cli.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import typer
+
+from peppyproject import PyProjectConfiguration
+
+
+def peppyproject_command(input: Path = None, output: Path = None):
+    if input is None:
+        input = Path.cwd()
+
+    configuration = PyProjectConfiguration.from_directory(directory=input)
+    toml_string = configuration.configuration
+    if output is None:
+        print(toml_string)
+    else:
+        if not output.parent.exists():
+            output.mkdir(parents=True, exist_ok=True)
+        with open(output, "w") as toml_file:
+            toml_file.write(toml_string)
+
+
+def main():
+    typer.run(peppyproject_command)
