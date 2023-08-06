a9004f7d638b45d1dcb9f4ef5ca0ed082430a71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:44 +0000

    Add package grpc-auth-service/0.0.18/grpc_auth_service-0.0.18-py3-none-any.whl

---
+++ packages/grpc-auth-service/grpc_auth_service-0.0.18-py3-none-any.whl/grpc_auth_service/utils/services.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Services(str, Enum):
+    MOVIES_SEARCH_SERVICE: str = "movies_search_service"
+    PROXY_AUTH_SERVICE: str = "proxy_auth_service"
+    MOVIES_ADMIN_PANEL_SERVICE: str = "movies_admin_panel_service"
