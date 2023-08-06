188662bced1cc740c6f454ef9bc8008c06ab901e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:54 +0000

    Add package richset/0.1.6/richset-0.1.6-py3-none-any.whl

---
+++ packages/richset/richset-0.1.6-py3-none-any.whl/richset/comparable.py
@@ -0,0 +1,16 @@
+from typing import Protocol, TypeVar, Union
+
+T_contra = TypeVar("T_contra", contravariant=True)
+
+
+class SupportsDunderLT(Protocol[T_contra]):  # pragma: no cover
+    def __lt__(self, __other: T_contra) -> bool:
+        ...
+
+
+class SupportsDunderGT(Protocol[T_contra]):  # pragma: no cover
+    def __gt__(self, __other: T_contra) -> bool:
+        ...
+
+
+Comparable = Union[SupportsDunderLT, SupportsDunderGT]
