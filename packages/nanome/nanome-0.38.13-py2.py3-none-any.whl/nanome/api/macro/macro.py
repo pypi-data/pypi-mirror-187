6d4290852b5f89e0d183f52248ae64ec566ee97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:15 +0000

    Add package nanome/0.38.13/nanome-0.38.13-py2.py3-none-any.whl

---
+++ packages/nanome/nanome-0.38.13-py2.py3-none-any.whl/nanome/api/macro/macro.py
@@ -0,0 +1,52 @@
+from nanome._internal.macro.models import _Macro
+
+
+class Macro(_Macro):
+    def __init__(self, title="", logic=""):
+        self.title = title
+        self.logic = logic
+        super(Macro, self).__init__()
+
+    @classmethod
+    def get_plugin_identifier(cls):
+        return _Macro._plugin_identifier
+
+    @classmethod
+    def set_plugin_identifier(cls, value):
+        _Macro._plugin_identifier = value
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
+
+    @property
+    def logic(self):
+        return self._logic
+
+    @logic.setter
+    def logic(self, value):
+        self._logic = value
+
+    def save(self, all_users=False):
+        self._save(all_users)
+
+    def run(self, callback=None):
+        return self._run(callback)
+
+    def delete(self, all_users=False):
+        self._delete(all_users)
+
+    @classmethod
+    def stop(cls):
+        cls._stop()
+
+    @classmethod
+    def get_live(cls, callback=None):
+        return cls._get_live(callback)
+
+
+_Macro._create = Macro
