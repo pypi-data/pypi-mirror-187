e0dd1cfc796d1497886a183fc446054df565bc0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:55 +0000

    Add package morsepy-ru/0.1/morsepy_ru-0.1.tar.gz

---
+++ packages/morsepy-ru/morsepy_ru-0.1.tar.gz/morsepy_ru-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='morsepy_ru',
+      version='0.1',
+      description='morse decriptor and encriptor from russian language',
+      packages=['morsepy_ru'],
+      author_email='katetlk@mail.ru',
+      zip_safe=False)
