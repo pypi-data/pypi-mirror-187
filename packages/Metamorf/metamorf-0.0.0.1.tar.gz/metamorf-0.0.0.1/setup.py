c30e871603b006e8da72270705c086beaab28e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:33 +0000

    Add package Metamorf/0.0.0.1/metamorf-0.0.0.1.tar.gz

---
+++ packages/Metamorf/metamorf-0.0.0.1.tar.gz/metamorf-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='metamorf',
+    version='0.0.0.1',
+    description='',
+    long_description='',
+    url='',
+    author='me',
+    author_email='guillermoaumatell@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
