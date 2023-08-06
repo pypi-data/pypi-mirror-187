ddcc9446f5bb139bd59430bdd16b6c80cbdc3467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:18 +0000

    Add package calculateuroaoai/0.2.4/calculateuroaoai-0.2.4-py3-none-any.whl

---
+++ packages/calculateuroaoai/calculateuroaoai-0.2.4-py3-none-any.whl/calculateuroaoai/input.py
@@ -0,0 +1,17 @@
+import config
+import pandas as pd
+
+def config_file():
+    print(f"Hello {config.my_name} !!")
+
+def other_package():
+    data_frame = pd.read_excel(config.my_file, dtype='object')
+    json_contact_list = {}
+    for index, row in data_frame.iterrows():
+        json_contact_list[row['Nom du site']] = {
+            "Nom": row['Nom'],
+            "Prénom": row['Prénom'],
+            "Mail": row['Mail'],
+            "Téléphone": row['Téléphone']
+        }
+    return json_contact_list
