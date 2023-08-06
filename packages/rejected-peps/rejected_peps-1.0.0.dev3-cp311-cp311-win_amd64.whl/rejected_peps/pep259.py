395f4d62faf4b471b44f9184449bc3974b98f972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:03:10 +0000

    Add package rejected-peps/1.0.0.dev3/rejected_peps-1.0.0.dev3-cp311-cp311-win_amd64.whl

---
+++ packages/rejected-peps/rejected_peps-1.0.0.dev3-cp311-cp311-win_amd64.whl/rejected_peps/pep259.py
@@ -0,0 +1,34 @@
+"""\
+PEP INFO
+
+PEP 259 -- Omit printing newline after newline
+Status: Rejected
+Created: 2001-06-11
+
+MODULE INFO
+
+This module provides the print() function that omits the trailing
+newline of the last argument if `end` starts with '\\n'.
+
+REFERENCES
+
+PEP 259: <https://peps.python.org/pep-0259/>
+"""
+PEP = 259
+import builtins as _b
+
+def print(*args, sep: str = ' ', end: str = '\n', file=None,
+          flush: bool = False) -> None:
+    """Same as `builtins.print()`, but omit the trailing newline
+    of the last argument if `end` starts with '\\n'.
+    """
+    if not args:
+        return _b.print(sep=sep, end=end, file=file, flush=flush)
+    # Use str.removesuffix as soon as support for Python 3.8 is dropped
+    if not isinstance(end, (str, type(None))):
+        raise TypeError(f'end must be None or a string, '
+                        f'not {type(sep).__name__}')
+    if str(args[-1]).endswith('\n') and end.startswith('\n'):
+        return _b.print(*args[:-1], str(args[-1])[:-1],
+                        sep=sep, end=end, file=file, flush=flush)
+    return _b.print(*args, sep=sep, end=end, file=file, flush=flush)
