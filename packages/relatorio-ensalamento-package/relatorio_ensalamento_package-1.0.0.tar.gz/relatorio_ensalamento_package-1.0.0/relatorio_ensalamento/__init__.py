20d72ae580b5f8712f5063588673e1e35c9f8553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:37 +0000

    Add package relatorio-ensalamento-package/1.0.0/relatorio_ensalamento_package-1.0.0.tar.gz

---
+++ packages/relatorio-ensalamento-package/relatorio_ensalamento_package-1.0.0.tar.gz/relatorio_ensalamento_package-1.0.0/relatorio_ensalamento/__init__.py
@@ -0,0 +1,1 @@
+from .Relatorio import *
---
+++ packages/relatorio-ensalamento-package/relatorio_ensalamento_package-1.0.0.tar.gz/relatorio_ensalamento_package-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'relatorio_ensalamento_package',
+    version = '1.0.0',
+    author = 'Matheus Henrique Rosa',
+    author_email = 'm.rosa1@pucpr.br',
+    packages = ['relatorio_ensalamento']
+)
