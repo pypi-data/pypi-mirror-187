f6082a1b7393fa8e2a0ec4540d4aaf24118be194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:35 +0000

    Add package visual-novel-toolkit/1.13.1/visual_novel_toolkit-1.13.1.tar.gz

---
+++ packages/visual-novel-toolkit/visual_novel_toolkit-1.13.1.tar.gz/visual_novel_toolkit-1.13.1/src/visual_novel_toolkit/drafts/new.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from click import edit
+
+
+def make_draft() -> None:
+    docs = Path("docs")
+    docs.mkdir(exist_ok=True)
+
+    drafts = docs / "drafts"
+    drafts.mkdir(exist_ok=True)
+
+    existed_drafts = sorted(drafts.glob("*.md"))
+    last_draft = int(existed_drafts[-1].stem) if existed_drafts else 0
+
+    new_draft = drafts / f"{last_draft+1:04}.md"
+    new_draft.touch()
+
+    edit(filename=str(new_draft))
