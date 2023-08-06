c9fed4b20eff1df3e6708e51562e4fff4957fb98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:22:40 +0000

    Add package uplc/0.4.2/uplc-0.4.2.tar.gz

---
+++ packages/uplc/uplc-0.4.2.tar.gz/uplc-0.4.2/uplc/lexer.py
@@ -0,0 +1,39 @@
+from rply import LexerGenerator
+import re
+
+
+TOKENS = {
+    "PROGRAM": r"\bprogram\b",
+    "LAMBDA": r"\blam\b",
+    "FORCE": r"\bforce\b",
+    "DELAY": r"\bdelay\b",
+    "BUILTIN": r"\bbuiltin\b",
+    "CON": r"\bcon\b",
+    "ERROR": r"\berror\b",
+    "PAREN_OPEN": r"\(",
+    "PAREN_CLOSE": r"\)",
+    "BRACK_OPEN": r"\[",
+    "BRACK_CLOSE": r"\]",
+    "CARET_OPEN": r"\<",
+    "CARET_CLOSE": r"\>",
+    "TEXT": r'"[^\r\n"]*"',
+    "COMMA": r",",
+    "DOT": r"\.",
+    "NUMBER": r"\-?\d+",
+    "NAME": r"[\w_~][\w\d_~!#]*",
+    "HEX": r"#([\dabcdefABCDEF][\dabcdefABCDEF])*",
+}
+
+
+class Lexer:
+    def __init__(self):
+        self.lexer = LexerGenerator()
+
+    def _add_tokens(self):
+        for k, v in TOKENS.items():
+            self.lexer.add(k, v)
+        self.lexer.ignore("\s+")
+
+    def get_lexer(self):
+        self._add_tokens()
+        return self.lexer.build()
