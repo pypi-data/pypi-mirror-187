a20fee49b4d46aae8caea08745017823d100a295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:23 +0000

    Add package ptyx/22.4.0/ptyx-22.4.0.tar.gz

---
+++ packages/ptyx/ptyx-22.4.0.tar.gz/ptyx-22.4.0/ptyx/custom_config.py
@@ -0,0 +1,8 @@
+# <custom_configuration>
+param = {
+    "import_paths": [
+        "~/Dropbox/Programmation/wxgeometrie/wxgeometrie",
+        "~/Dropbox/Programmation/wxgeometrie",
+    ]
+}
+# </custom_configuration>
