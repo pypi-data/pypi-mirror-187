ca32da105870de0b1800e97c7ca2272620da1f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:29 +0000

    Add package commitizen/2.40.0/commitizen-2.40.0.tar.gz

---
+++ packages/commitizen/commitizen-2.40.0.tar.gz/commitizen-2.40.0/commitizen/cmd.py
@@ -0,0 +1,46 @@
+import subprocess
+from typing import NamedTuple
+
+from charset_normalizer import from_bytes
+
+from commitizen.exceptions import CharacterSetDecodeError
+
+
+class Command(NamedTuple):
+    out: str
+    err: str
+    stdout: bytes
+    stderr: bytes
+    return_code: int
+
+
+def _try_decode(bytes_: bytes) -> str:
+    try:
+        return bytes_.decode("utf-8")
+    except UnicodeDecodeError:
+        charset_match = from_bytes(bytes_).best()
+        if charset_match is None:
+            raise CharacterSetDecodeError()
+        try:
+            return bytes_.decode(charset_match.encoding)
+        except UnicodeDecodeError as e:
+            raise CharacterSetDecodeError() from e
+
+
+def run(cmd: str) -> Command:
+    process = subprocess.Popen(
+        cmd,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+    )
+    stdout, stderr = process.communicate()
+    return_code = process.returncode
+    return Command(
+        _try_decode(stdout),
+        _try_decode(stderr),
+        stdout,
+        stderr,
+        return_code,
+    )
