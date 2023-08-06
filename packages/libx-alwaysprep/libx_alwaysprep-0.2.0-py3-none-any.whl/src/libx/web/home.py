80e1f8ca6e69f7b09ff995121483a71b6a1ad11c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:02 +0000

    Add package libx-alwaysprep/0.2.0/libx_alwaysprep-0.2.0-py3-none-any.whl

---
+++ packages/libx-alwaysprep/libx_alwaysprep-0.2.0-py3-none-any.whl/src/libx/web/home.py
@@ -0,0 +1,16 @@
+import numpy
+
+from flask import Blueprint
+
+from src.core.time.utils import get_curent_time
+
+
+home = Blueprint('home', __name__)
+
+@home.route('/')
+def print_hello():
+    return {
+        "time": get_curent_time(),
+        "numpy array": (numpy.arange(10)**2).tolist(),
+        "numpy": numpy.__version__
+    }
