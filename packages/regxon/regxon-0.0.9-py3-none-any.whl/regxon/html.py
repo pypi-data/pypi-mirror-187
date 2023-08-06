8cbf9c3b874fcec5eb41d92b517de342f4f63d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:16 +0000

    Add package regxon/0.0.9/regxon-0.0.9-py3-none-any.whl

---
+++ packages/regxon/regxon-0.0.9-py3-none-any.whl/regxon/html.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+
+
+class RegxonHTML:
+    excluded_tags = [
+        'script'
+    ]
+
+    excluded_attributes = {
+        'img': ['onload', 'src'],
+    }
+
+    escaped_tags = [
+        'p'
+    ]
+
+    def __init__(self, excluded_tags=None, excluded_attributes=None):
+        if excluded_tags:
+            self.excluded_tags = excluded_tags
+        if excluded_attributes:
+            self.excluded_attributes = excluded_attributes
+
+    def get_sanitized_content(self, html):
+        """
+        Validate html.
+        """
+        soup = BeautifulSoup(html, 'html.parser')
+        for tag in soup.find_all():
+            if tag.name in self.excluded_tags:
+                tag_soup = BeautifulSoup(str(tag), 'html.parser')
+                tag.replace_with(tag_soup.new_string(''))
+
+            if tag.name in self.excluded_attributes.keys():
+                for attribute in self.excluded_attributes[tag.name]:
+                    if tag.has_attr(attribute):
+                        del tag[attribute]
+
+            if tag.name in self.escaped_tags:
+                tag_soup = BeautifulSoup(str(tag), 'html.parser')
+                tag.replace_with(tag_soup.new_string(str(tag)))
+
+        return str(soup)
