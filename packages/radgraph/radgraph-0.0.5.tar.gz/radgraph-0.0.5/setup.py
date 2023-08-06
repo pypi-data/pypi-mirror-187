8f6fd180c7cf146c8a493cd01d23a88e99ed8d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:46 +0000

    Add package radgraph/0.0.5/radgraph-0.0.5.tar.gz

---
+++ packages/radgraph/radgraph-0.0.5.tar.gz/radgraph-0.0.5/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 8):
+    sys.exit('Sorry, Python >=3.8 is required for radgraph.')
+
+setup(
+    name='radgraph',
+    version='0.0.5',
+    author='Jean-Benoit Delbrouck',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    python_requires='>=3.8',
+    install_requires=['torch>=1.8.1',
+                      'transformers==4.23.1',
+                      "appdirs",
+                      'overrides==3.1.0',
+                      'jsonpickle',
+                      'filelock',
+                      'h5py',
+                      'spacy',
+                      'nltk',
+                      ],
+    packages=find_packages(),
+    zip_safe=False)
