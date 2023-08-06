f39449680f0adf3ab38956e3c87b309ed497939a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:21 +0000

    Add package mytestpkgone/0.0.1/mytestpkgone-0.0.1.tar.gz

---
+++ packages/mytestpkgone/mytestpkgone-0.0.1.tar.gz/mytestpkgone-0.0.1/mytestpkgone/__init__.py
@@ -0,0 +1,8 @@
+class Achha:
+    def __init__(self):
+        print("Constructor ban gaya")
+
+
+    def achhafunc(self, num):
+        print("This is a Number")
+        return num
---
+++ packages/mytestpkgone/mytestpkgone-0.0.1.tar.gz/mytestpkgone-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Streaming video data via networks'
+
+# Setting up
+setup(
+	name="mytestpkgone",
+	version=VERSION,
+    	author="NeuralNine (Florian Dedov)",
+    	author_email="<mail@neuralnine.com>",
+    	description=DESCRIPTION,
+    	packages=['mytestpkgone'],
+    	install_requires=[]
+)
