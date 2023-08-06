ce25d6b6daabb6f30f47db9836a0c51ab9b5b9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:59 +0000

    Add package pymolstyles/0.2.0/pymolstyles-0.2.0.tar.gz

---
+++ packages/pymolstyles/pymolstyles-0.2.0.tar.gz/pymolstyles-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pymolstyles', 'pymolstyles.external']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pymolstyles',
+    'version': '0.2.0',
+    'description': 'Scripts for small molecule visualization using PyMOL',
+    'long_description': "!['pymolstyles logo'](./docs/img/pymolstyles.png)\n\n",
+    'author': 'Attilio Chiavegatti',
+    'author_email': 'attiliochiavegatti@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
