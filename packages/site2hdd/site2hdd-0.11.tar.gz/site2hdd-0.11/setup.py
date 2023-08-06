8ebee7e83556303708d2b02aa078a78ba0842e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:27 +0000

    Add package site2hdd/0.11/site2hdd-0.11.tar.gz

---
+++ packages/site2hdd/site2hdd-0.11.tar.gz/site2hdd-0.11/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md'), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.11'
+DESCRIPTION = "Download sites with public proxies - threading"
+
+# Setting up
+setup(
+    name="site2hdd",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/site2hdd',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=['a_pandas_ex_apply_ignore_exceptions', 'a_pandas_ex_horizontal_explode', 'cprinter', 'freeproxydownloader', 'kthread_sleep', 'numba', 'pandas', 'regex', 'requests', 'threadingbatch', 'tolerant_isinstance', 'touchtouch', 'url_analyzer', 'windows_filepath'],
+    keywords=['download', 'scrape', 'site'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Programming Language :: Python :: 3.9', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Text Editors :: Text Processing', 'Topic :: Text Processing :: General', 'Topic :: Text Processing :: Indexing', 'Topic :: Text Processing :: Filters', 'Topic :: Utilities'],
+    install_requires=['a_pandas_ex_apply_ignore_exceptions', 'a_pandas_ex_horizontal_explode', 'cprinter', 'freeproxydownloader', 'kthread_sleep', 'numba', 'pandas', 'regex', 'requests', 'threadingbatch', 'tolerant_isinstance', 'touchtouch', 'url_analyzer', 'windows_filepath'],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
