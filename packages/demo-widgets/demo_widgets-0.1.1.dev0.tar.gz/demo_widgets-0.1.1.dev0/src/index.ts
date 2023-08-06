a595d1e045905678a404de41545b45a95062f9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:06 +0000

    Add package demo-widgets/0.1.1.dev0/demo_widgets-0.1.1.dev0.tar.gz

---
+++ packages/demo-widgets/demo_widgets-0.1.1.dev0.tar.gz/demo_widgets-0.1.1.dev0/src/index.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Eric
+// Distributed under the terms of the Modified BSD License.
+
+export * from './version';
+export * from './widgets/example_widget';
+export * from './widgets/stock_lookup_widget';
+export * from './widgets/stock_list_widget';
