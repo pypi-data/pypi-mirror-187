0b232af788cc072ecf6f38d4e7624e90e64d5609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:56 +0000

    Add package realnet/0.0.18/realnet-0.0.18.tar.gz

---
+++ packages/realnet/realnet-0.0.18.tar.gz/realnet-0.0.18/setup.py
@@ -0,0 +1,42 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="realnet",
+    version="0.0.18",
+    description="Realnet command line interface",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/virtual-space/realnet",
+    author="Marko Laban",
+    author_email="marko.laban@l33tsystems.com",
+    license="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["python-dotenv",
+                      "boto3",
+                      "SQLAlchemy==1.4.23",
+                      "psycopg2-binary",
+                      "sqlalchemy-serializer",
+                      "shapely==1.8.5",
+                      "GeoAlchemy2",
+                      "Werkzeug",
+                      "authlib"],
+    entry_points={
+        "console_scripts": [
+            "realnet=realnet.__main__:main",
+        ]
+    },
+)
