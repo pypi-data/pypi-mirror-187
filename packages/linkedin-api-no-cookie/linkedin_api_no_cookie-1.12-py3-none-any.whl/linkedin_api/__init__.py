a23b4803177f495d6cb197238baafb2e59897830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:51 +0000

    Add package linkedin-api-no-cookie/1.12/linkedin_api_no_cookie-1.12-py3-none-any.whl

---
+++ packages/linkedin-api-no-cookie/linkedin_api_no_cookie-1.12-py3-none-any.whl/linkedin_api/__init__.py
@@ -0,0 +1,15 @@
+"""
+    linkedin-api-no-cookie
+"""
+from .linkedin import Linkedin
+
+__title__ = "linkedin_api_no_cookie"
+__version__ = "1.12"
+__description__ = "Python Wrapper for the Linkedin API"
+
+__license__ = "MIT"
+
+__author__ = "Alejandrina Gonzalez"
+__email__ = "me@alejandrina.me"
+
+__all__ = ["Linkedin"]
