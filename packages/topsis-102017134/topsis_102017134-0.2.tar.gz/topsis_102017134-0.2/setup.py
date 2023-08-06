d784d996c7b15f101db007e075018d7ee3d8f3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:39 +0000

    Add package topsis-102017134/0.2/topsis_102017134-0.2.tar.gz

---
+++ packages/topsis-102017134/topsis_102017134-0.2.tar.gz/topsis_102017134-0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+	name="topsis_102017134",
+	version='0.2',
+	author='Soumya Srivastava',
+	author_email='ssrivastava_be20@thapar.edu',
+	description='topsis package for MCDM problems',
+	long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sri-soumya/topsis",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+	)
