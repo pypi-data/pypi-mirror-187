fd0cdfcbef35a82ad0535f35bb310482139d63dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:36 +0000

    Add package headline-style/1.0.1/headline_style-1.0.1.tar.gz

---
+++ packages/headline-style/headline_style-1.0.1.tar.gz/headline_style-1.0.1/setup.py
@@ -0,0 +1,21 @@
+"""
+Setup file for headLineStyle.
+"""
+import setuptools
+
+setuptools.setup(
+    name="headline_style",  # This is the name of the package
+    version="1.0.1",  # The initial release version
+    author="Vinod Baste",  # Full name of the author
+    url="https://github.com/vinodbaste/python_headline_style",
+    packages=setuptools.find_packages(),  # List of all python modules to be installed
+    readme="README.md",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],  # Information to filter the project on PyPi website
+    python_requires='>=3.7',  # Minimum version requirement of the package
+    py_modules=["headLineStyle"],  # Name of the python package
+    install_requires=["regex >=2020.4.4"]  # Install other dependencies if any
+)
