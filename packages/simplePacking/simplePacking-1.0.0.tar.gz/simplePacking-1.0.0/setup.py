0c12521f702052e3f859208a4c11ce10babe8072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:41 +0000

    Add package simplePacking/1.0.0/simplePacking-1.0.0.tar.gz

---
+++ packages/simplePacking/simplePacking-1.0.0.tar.gz/simplePacking-1.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='simplePacking',
+    packages=['simplePacking'],
+    version='1.0.0',
+    license='MIT',
+    python_requires='>=3.6',
+    description='Simple Python packing example',
+    author="Noy Sartori"
+)
