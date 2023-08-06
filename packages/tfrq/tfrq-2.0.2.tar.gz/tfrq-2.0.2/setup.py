54a674a9e2706dc1e1d43ff27cfc285878d7b23a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:48 +0000

    Add package tfrq/2.0.2/tfrq-2.0.2.tar.gz

---
+++ packages/tfrq/tfrq-2.0.2.tar.gz/tfrq-2.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tfrq',
+    version='2.0.2',
+    description='A library to parallelize the execution of a function in python',
+    author='Foad Abo Dahood',
+    author_email='Foad.ad5491@gmail.com',
+    py_modules=['tfrq'],
+    install_requires=['futures==2.2.0'],
+)
