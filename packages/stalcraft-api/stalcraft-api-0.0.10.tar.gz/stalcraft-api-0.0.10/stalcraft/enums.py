1779921f51a63da5862e7a161441c5cf383997c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:39 +0000

    Add package stalcraft-api/0.0.10/stalcraft-api-0.0.10.tar.gz

---
+++ packages/stalcraft-api/stalcraft-api-0.0.10.tar.gz/stalcraft-api-0.0.10/stalcraft/enums.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+
+class BaseUrl(Enum):
+    """
+    DEMO: Demo API
+    PRODUCTION: Production API
+    """
+
+    DEMO = "http://dapi.stalcraft.net"
+    PRODUCTION = "http://eapi.stalcraft.net"
+
+
+class Region(Enum):
+    """
+    RU: Russia
+    EU: Europe
+    NA: North America
+    SEA: South East Asia
+    """
+
+    RU = "ru"
+    EU = "eu"
+    NA = "na"
+    SEA = "sea"
+
+
+class Sort(Enum):
+    TIME_CREATED = "time_created"
+    TIME_LEFT = "time_left"
+    CURRENT_PRICE = "current_price"
+    BUYOUT_PRICE = "buyout_price"
+
+
+class Order(Enum):
+    ASCENDING = "asc"
+    DESCENDING = "desc"
