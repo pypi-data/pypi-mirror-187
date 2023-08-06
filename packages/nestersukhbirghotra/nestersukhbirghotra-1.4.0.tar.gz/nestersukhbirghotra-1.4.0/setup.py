b00f621ea1c25432d9a0379daa91b861b19ab067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:07 +0000

    Add package nestersukhbirghotra/1.4.0/nestersukhbirghotra-1.4.0.tar.gz

---
+++ packages/nestersukhbirghotra/nestersukhbirghotra-1.4.0.tar.gz/nestersukhbirghotra-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nestersukhbirghotra',
+        version = '1.4.0',
+        py_modules = ['nestersukhbirghotra'],
+        author = 'SukhbirGhotra',
+        author_email = 'sukhbirghotra@gmail.com',
+        url = 'http://www.sukhbirghotra.com',
+        description = 'A simple printer of nested lists.',
+)
