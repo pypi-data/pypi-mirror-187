d59253a09672bdb185d383b6ee628c99263237ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:31 +0000

    Add package netbox-storage/0.0.0.0.329/netbox_storage-0.0.0.0.329-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.329-py3-none-any.whl/netbox_storage/tables/physical_volume.py
@@ -0,0 +1,44 @@
+import django_tables2 as tables
+
+from netbox.tables import (
+    NetBoxTable,
+    ToggleColumn,
+)
+
+from netbox_storage.models import PhysicalVolume
+
+
+class PhysicalVolumeTable(NetBoxTable):
+    """Table for displaying PhysicalVolume objects."""
+
+    pk = ToggleColumn()
+    drive = tables.Column(
+        linkify=True
+    )
+    partition = tables.Column(
+        linkify=True
+    )
+    pv_name = tables.Column(
+        linkify=True
+    )
+    description = tables.Column(
+        linkify=True
+    )
+
+    class Meta(NetBoxTable.Meta):
+        model = PhysicalVolume
+        fields = (
+            "pk",
+            "drive",
+            "partition",
+            "pv_name",
+            "description",
+        )
+        default_columns = (
+            "pk",
+            "drive",
+            "partition",
+            "pv_name",
+            "description",
+        )
+
