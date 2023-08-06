a77a0db3511dcdcc2fbce63249ba27db30f9cadf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:20 +0000

    Add package PdfInfoExtractor/0.1.11/PdfInfoExtractor-0.1.11-py2.py3-none-any.whl

---
+++ packages/PdfInfoExtractor/PdfInfoExtractor-0.1.11-py2.py3-none-any.whl/PdfInfoExtractor/__init__.py
@@ -0,0 +1,10 @@
+"""
+PdfInfoExtractor
+
+"""
+
+from utilities import ImagesExtractor, ClassifyFiles, extract_info
+
+__version__ = "0.1.11"
+__author__ = 'Steve Richards, Mehak Singal, Nidhish Kumar'
+# __credits__ = 'Argonne National Laboratory'
