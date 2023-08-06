127070a2e1b13ed44667274c4865400c8cd28a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:58 +0000

    Add package pal-app/0.1.8/pal_app-0.1.8.tar.gz

---
+++ packages/pal-app/pal_app-0.1.8.tar.gz/pal_app-0.1.8/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2022 PAL Robotics S.L. All rights reserved.
+
+from pathlib import Path
+from distutils.core import setup
+
+NAME = "pal_app"
+
+TPLS = [
+    ("share/%s/%s" % (NAME, t.parent), [str(t)])
+    for t in Path("tpl").rglob("*")
+    if t.is_file()
+]
+
+setup(
+    name=NAME,
+    version="0.1.8",
+    license="GPLv3",
+    description="A tool to create application controller skeletons for interactive robots",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    requires=["jinja2"],
+    author="Séverin Lemaignan",
+    author_email="severin.lemaignan@pal-robotics.com",
+    scripts=["scripts/pal_app"],
+    package_dir={"": "src"},
+    packages=["pal_app"],
+    data_files=TPLS + [("share/doc/pal_app", ["README.md", "LICENSE"])],
+)
