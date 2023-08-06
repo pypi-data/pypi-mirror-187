108ebaee60b09bafb88e6555eec0e0db66764769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:11 +0000

    Add package python-tools-scripts/0.9.7/python-tools-scripts-0.9.7.tar.gz

---
+++ packages/python-tools-scripts/python-tools-scripts-0.9.7.tar.gz/python-tools-scripts-0.9.7/src/ptscripts/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import ptscripts.logs
+from ptscripts.parser import command_group
+from ptscripts.parser import Context
+
+__all__ = ["command_group", "Context"]
