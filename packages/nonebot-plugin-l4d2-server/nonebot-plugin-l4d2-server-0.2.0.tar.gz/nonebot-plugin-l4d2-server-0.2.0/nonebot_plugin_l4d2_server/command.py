ce98ea38355a1fcd404fcef3a02cb1b7c1aea22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:57 +0000

    Add package nonebot-plugin-l4d2-server/0.2.0/nonebot-plugin-l4d2-server-0.2.0.tar.gz

---
+++ packages/nonebot-plugin-l4d2-server/nonebot-plugin-l4d2-server-0.2.0.tar.gz/nonebot-plugin-l4d2-server-0.2.0/nonebot_plugin_l4d2_server/command.py
@@ -0,0 +1,34 @@
+from nonebot import on_notice,on_command,on_regex
+import re
+from .config import Master
+
+
+# 服务器
+up = on_notice()
+rename_vpk = on_regex(
+        r"^求生地图\s*(\S+.*?)\s*(改|改名)?\s*(\S+.*?)\s*$",
+    flags=  re.S,
+    block= True,
+    priority= 20,
+    permission= Master,
+)
+
+find_vpk = on_command("l4_map",aliases={"求生地图","查看求生地图"},priority=25,block=True)
+del_vpk = on_command("l4_del_map",aliases={"求生地图删除","地图删除"},priority=20,block=True,permission= Master)
+rcon_to_server = on_command('rcon',aliases={"求生服务器指令","服务器指令","求生服务器控制台"},block=True,permission= Master)
+
+# anne
+anne_player = on_command('Ranne',aliases={"求生anne"},priority=25,block=True)
+anne_bind = on_command('Rbind',aliases={'steam绑定','求生绑定','anne绑定'},priority=20,block=True)
+del_bind = on_command('del_bind',aliases={'steam解绑','求生解绑','anne解绑'},priority=20,block=True)
+
+
+# 查询
+queries = on_command('queries',aliases={'求生ip','求生服务器'},priority=20,block=True)
+add_queries = on_command('addq',aliases={"求生添加订阅"},priority=20,block=True,permission= Master)
+del_queries = on_command('delq',aliases={"求生取消订阅"},priority=20,block=True,permission= Master)
+show_queries = on_command('showq',aliases={"求生订阅"},priority=20,block=True)
+join_server = on_command('showq',aliases={"求生加入"},priority=20,block=True)
+
+# 下载内容
+up_workshop = on_command('workshop',aliases={'创意工坊下载','求生创意工坊'},priority=20,block=True)
