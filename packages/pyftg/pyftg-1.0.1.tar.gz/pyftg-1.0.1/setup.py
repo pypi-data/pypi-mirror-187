d13932eb434cfa247412fa7405370f8e7e9ee278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:42 +0000

    Add package pyftg/1.0.1/pyftg-1.0.1.tar.gz

---
+++ packages/pyftg/pyftg-1.0.1.tar.gz/pyftg-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyftg",
+    version="1.0.1",
+    author="Staciiaz",
+    description="An interface for implementing python AI in DareFightingICE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Staciiaz/pyftg",
+    python_requires=">=3.8",
+    install_requires=["grpcio"],
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    include_package_data=True,
+)
