c3cc00ccfcde63395db65b03111ee8ebde658c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:46 +0000

    Add package ipcs/0.1.3/ipcs-0.1.3.tar.gz

---
+++ packages/ipcs/ipcs-0.1.3.tar.gz/ipcs-0.1.3/ipcs/__init__.py
@@ -0,0 +1,18 @@
+# ipcs
+
+from .connection import Connection
+from .server import *
+from .client import *
+from .server import *
+from .errors import *
+
+
+__all__ = (
+    "__version__", "Request", "AbcClient", "Client", "logger",
+    "Connection", "Server", "ConnectionForServer", "IpcsError", "IdIsNotFound",
+    "RequestError", "FailedToProcessError", "ClosedConnectionError"
+)
+
+
+__version__ = "0.1.3"
+"The version of icps."
