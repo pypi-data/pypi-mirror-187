fe4ac7ec7a83072ae671100dc9924b1047f52bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:56 +0000

    Add package kenny/0.0.1/kenny-0.0.1-py3-none-any.whl

---
+++ packages/kenny/kenny-0.0.1-py3-none-any.whl/kenny/__init__.py
@@ -0,0 +1,13 @@
+"""kenny provides feature-rich clients to HTTP(s) APIs."""
+
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
