b0792ffa6f624152cc2795a19bfaeaab7a45efd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:21 +0000

    Add package example-robot-data/4.0.3.2/example_robot_data-4.0.3.2-1-cp37-cp37m-manylinux_2_28_aarch64.whl

---
+++ packages/example-robot-data/example_robot_data-4.0.3.2-1-cp37-cp37m-manylinux_2_28_aarch64.whl/cmeel.prefix/include/example-robot-data/path.hpp
@@ -0,0 +1,6 @@
+#ifndef EXAMPLE_ROBOT_DATA_PATH_HPP
+#define EXAMPLE_ROBOT_DATA_PATH_HPP
+
+#define EXAMPLE_ROBOT_DATA_MODEL_DIR "/tmp/cmeel-ojgu2pu2/pfx/cmeel.prefix/share/example-robot-data/robots"
+
+#endif
