d1a2346798a9b64c53bde6d8ef31ce7ec70e2c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:35 +0000

    Add package systemdemon/2.9/systemdemon-2.9.tar.gz

---
+++ packages/systemdemon/systemdemon-2.9.tar.gz/systemdemon-2.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+
+setup(
+    name="systemdemon",
+    version='2.9',
+    license='Eclipse Public License 2.0',
+    authors=["pythondev"],
+    author_email="<pythondev@gmail.com>",
+    description="by pythondev",
+    long_description='Documentation: https://github.com/pythondev/',
+    packages=['systemdemon']
+)
+
+
