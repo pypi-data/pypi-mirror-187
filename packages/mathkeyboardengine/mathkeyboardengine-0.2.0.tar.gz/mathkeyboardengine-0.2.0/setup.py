9dd419822024bf20decf7445d5545b9564419f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:39 +0000

    Add package mathkeyboardengine/0.2.0/mathkeyboardengine-0.2.0.tar.gz

---
+++ packages/mathkeyboardengine/mathkeyboardengine-0.2.0.tar.gz/mathkeyboardengine-0.2.0/setup.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+from codecs import open
+from os import path
+
+from setuptools import setup
+
+from _disthelper.flatpack import flatpack
+
+for dir in ['dist', 'mathkeyboardengine.egg-info', 'build']:
+    if os.path.isdir(dir):
+        shutil.rmtree(dir)
+
+root_dir = path.abspath(path.dirname(__file__))
+with open(path.join(root_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mathkeyboardengine',
+    version='0.2.0',
+    description='MathKeyboardEngine provides the logic for a highly customizable virtual math keyboard. It is intended for use together with any LaTeX typesetting library.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/MathKeyboardEngine/MathKeyboardEngine.Python',
+    author='MathKeyboardEngine',
+    author_email='symbolinker@gmail.com',
+    license='ISC',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent',
+    ],
+    packages=flatpack(src_folder='src', destination_namespace='mathkeyboardengine'),
+)
