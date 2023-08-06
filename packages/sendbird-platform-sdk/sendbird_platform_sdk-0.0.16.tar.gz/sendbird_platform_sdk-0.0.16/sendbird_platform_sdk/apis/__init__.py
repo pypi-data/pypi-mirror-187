da0ccd665c824e95cb663b0445d64fd2ef396b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:08 +0000

    Add package sendbird-platform-sdk/0.0.16/sendbird_platform_sdk-0.0.16.tar.gz

---
+++ packages/sendbird-platform-sdk/sendbird_platform_sdk-0.0.16.tar.gz/sendbird_platform_sdk-0.0.16/sendbird_platform_sdk/apis/__init__.py
@@ -0,0 +1,30 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from sendbird_platform_sdk.api.announcement_api import AnnouncementApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from sendbird_platform_sdk.api.announcement_api import AnnouncementApi
+from sendbird_platform_sdk.api.application_api import ApplicationApi
+from sendbird_platform_sdk.api.bot_api import BotApi
+from sendbird_platform_sdk.api.data_export_api import DataExportApi
+from sendbird_platform_sdk.api.group_channel_api import GroupChannelApi
+from sendbird_platform_sdk.api.message_api import MessageApi
+from sendbird_platform_sdk.api.metadata_api import MetadataApi
+from sendbird_platform_sdk.api.moderation_api import ModerationApi
+from sendbird_platform_sdk.api.open_channel_api import OpenChannelApi
+from sendbird_platform_sdk.api.privacy_api import PrivacyApi
+from sendbird_platform_sdk.api.report_api import ReportApi
+from sendbird_platform_sdk.api.statistics_api import StatisticsApi
+from sendbird_platform_sdk.api.user_api import UserApi
+from sendbird_platform_sdk.api.webhook_api import WebhookApi
