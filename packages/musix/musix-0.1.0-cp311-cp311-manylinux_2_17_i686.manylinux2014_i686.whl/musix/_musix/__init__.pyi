117825b107c5e17adcdbc21fe268804584d96f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:49 +0000

    Add package musix/0.1.0/musix-0.1.0-cp311-cp311-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/musix/musix-0.1.0-cp311-cp311-manylinux_2_17_i686.manylinux2014_i686.whl/musix/_musix/__init__.pyi
@@ -0,0 +1,38 @@
+from __future__ import annotations
+import musix._musix
+import typing
+
+__all__ = [
+    "Player",
+    "init",
+    "load"
+]
+
+
+class Player():
+    def get_meta(self, name: str) -> typing.Union[str, float, int]: 
+        """
+        Get meta data about the loaded song.
+        """
+    def on_meta(self, arg0: typing.Callable[[typing.List[str]], None]) -> None: ...
+        """
+        Set callback to receive meta updates.
+        """
+    def render(self, count: int) -> bytes: 
+        """
+        Generate `count` number of samples and return `count*2` bytes
+        """
+    def seek(self, song: int, seconds: int = -1) -> bool: ...
+        """
+        Switch subsong, or seek to position if plugin supports it.
+        """
+    pass
+
+def init() -> None:
+    """
+    Init musix
+    """
+def load(name: str) -> Player:
+    """
+    Load music file
+    """
