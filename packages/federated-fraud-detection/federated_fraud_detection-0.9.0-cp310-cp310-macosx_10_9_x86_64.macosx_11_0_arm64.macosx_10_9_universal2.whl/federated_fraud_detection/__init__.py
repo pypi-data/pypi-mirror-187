e5e718a64fa53ae346a04c1034050ac2e9994c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:27 +0000

    Add package federated-fraud-detection/0.9.0/federated_fraud_detection-0.9.0-cp310-cp310-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl

---
+++ packages/federated-fraud-detection/federated_fraud_detection-0.9.0-cp310-cp310-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl/federated_fraud_detection/__init__.py
@@ -0,0 +1,5 @@
+from .federated_fraud_detection import *
+
+__doc__ = federated_fraud_detection.__doc__
+if hasattr(federated_fraud_detection, "__all__"):
+    __all__ = federated_fraud_detection.__all__
