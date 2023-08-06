2ad09bbbd5505f8c6cecae2067504ca3ffa33d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:48 +0000

    Add package wodoo/0.3.95/wodoo-0.3.95.tar.gz

---
+++ packages/wodoo/wodoo-0.3.95.tar.gz/wodoo-0.3.95/wodoo/lib_talk.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+import subprocess
+import inquirer
+import sys
+from datetime import datetime
+import os
+import click
+from .odoo_config import current_version
+from .odoo_config import MANIFEST
+from .tools import _is_dirty
+from .odoo_config import customs_dir
+from .cli import cli, pass_config
+from .lib_clickhelpers import AliasedGroup
+from .tools import _execute_sql
+from tabulate import tabulate
+
+
+@cli.group(cls=AliasedGroup)
+@pass_config
+def talk(config): 
+    pass
+
+@talk.command()
+@click.option("-M", "--module")
+@click.option("-m", "--model")
+@pass_config
+def xmlids(config, module, model):
+    conn = config.get_odoo_conn()
+    where = " where 1 = 1"
+    if model:
+        where += f" AND model = '{model}'"
+    if module:
+        where += f" AND module = '{model}'"
+    rows = _execute_sql(
+        conn,
+        sql=(
+            "SELECT module||'.'|| name as xmlid, model, res_id from ir_model_data "
+            f"{where} "
+            "order by module, name, model "
+        ),
+        fetchall=True,
+        return_columns=True,
+    )
+    click.secho(tabulate(rows[1], rows[0], tablefmt="fancy_grid"), fg="yellow")
+
+@talk.command()
+@click.argument("field", nargs=-1)
+@pass_config
+def deactivate_field_in_views(config, field):
+    conn = config.get_odoo_conn()
+    for field in field:
+        click.secho(f"Turning {field} into create_date.", fg='green')
+        _execute_sql(
+            conn,
+            sql=(
+                "UPDATE ir_ui_view set arch_db = "
+                f"replace(arch_db, '{field}', 'create_date')"
+            ),
+            fetchall=False,
+            return_columns=False,
+        )
