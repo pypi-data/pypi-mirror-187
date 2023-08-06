463e469c6e993279655428db1f2c1d141dcf10e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:01 +0000

    Add package Leetease/0.1.2/Leetease-0.1.2-py3-none-any.whl

---
+++ packages/Leetease/Leetease-0.1.2-py3-none-any.whl/Leetease/__init__.py
@@ -0,0 +1,19 @@
+def Int_to_Roman(s):
+	res = 0; prev = 0
+	save = {
+	"I" : 1,
+	"V" : 5,
+	"X" : 10,
+	"L" : 50,
+	"C" : 100,
+	"D" : 500,
+	"M" : 1000
+	}
+	for i in reversed(s):
+		if save[i] >= prev:
+			res += save[i]
+		else:
+			res -= save[i]
+		prev = save[i]
+
+	print(f"The number you entered in roman form is {res}")
