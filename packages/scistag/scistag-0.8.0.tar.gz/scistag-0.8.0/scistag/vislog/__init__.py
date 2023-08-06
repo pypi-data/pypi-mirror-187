ebe3a21df1d3705816334615dad6d7c9461ed668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:03 +0000

    Add package scistag/0.8.0/scistag-0.8.0.tar.gz

---
+++ packages/scistag/scistag-0.8.0.tar.gz/scistag-0.8.0/scistag/vislog/__init__.py
@@ -0,0 +1,34 @@
+"""
+Integrates VisualLog and VisualTestLog
+"""
+from .sessions.page_session import MD, HTML, TXT, CONSOLE
+from .visual_log import VisualLog
+
+from .visual_test_log import VisualTestLog
+from .log_builder import LogBuilder
+from .common.log_backup import LogBackup
+from .extensions.builder_extension import BuilderExtension
+from .common.log_statistics import LogStatistics
+from .extensions.cell_logger import Cell
+from .log_elements import HTMLCode, MDCode
+from .cells import cell, section, data, stream, once
+
+__all__ = [
+    "VisualLog",
+    "VisualTestLog",
+    "LogBuilder",
+    "LogStatistics",
+    "BuilderExtension",
+    "Cell",
+    "HTMLCode",
+    "MDCode",
+    "MD",
+    "HTML",
+    "TXT",
+    "CONSOLE",
+    "cell",
+    "section",
+    "data",
+    "stream",
+    "once",
+]
