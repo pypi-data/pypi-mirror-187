3ef18a8357e741dd80d4dfe6e37a2189dea004bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:36 +0000

    Add package analysis-runner-ms/0.9.10/analysis-runner-ms-0.9.10.tar.gz

---
+++ packages/analysis-runner-ms/analysis-runner-ms-0.9.10.tar.gz/analysis-runner-ms-0.9.10/analysis_runner/constants.py
@@ -0,0 +1,27 @@
+from cpg_utils.deploy_config import get_deploy_config
+
+"""Constants for analysis-runner"""
+
+
+SERVER_ENDPOINT = 'https://server-a2pko7ameq-ts.a.run.app'
+SERVER_TEST_ENDPOINT = 'https://server-test-a2pko7ameq-ts.a.run.app'
+ANALYSIS_RUNNER_PROJECT_ID = 'analysis-runner'
+CROMWELL_URL = 'https://cromwell.populationgenomics.org.au'
+CROMWELL_AUDIENCE = (
+    '717631777761-ec4u8pffntsekut9kef58hts126v7usl.apps.googleusercontent.com'
+)
+GCLOUD_ACTIVATE_AUTH = (
+    'gcloud -q auth activate-service-account --key-file=/gsa-key/key.json'
+)
+
+
+def get_server_endpoint(is_test: bool = False):
+    """
+    Get the server endpoint {production / test}
+    Do it in a function so it's easy to fix if the logic changes
+    """
+    if is_test:
+        # TODO GRS configure test endpoint
+        return SERVER_TEST_ENDPOINT
+
+    return get_deploy_config().analysis_runner_host
