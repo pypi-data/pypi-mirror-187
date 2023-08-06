4d35590b2060b8172c781652072161d042a999b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:52 +0000

    Add package PyCryptor/0.1.1/PyCryptor-0.1.1.tar.gz

---
+++ packages/PyCryptor/PyCryptor-0.1.1.tar.gz/PyCryptor-0.1.1/pycryptor/utils.py
@@ -0,0 +1,48 @@
+from glob import glob
+from pathlib import Path
+from hashlib import sha256
+from contextlib import suppress
+from typing import Union, List
+
+
+def hash_password(password: bytes) -> bytes:
+	for _ in range(100000):
+		password = sha256(password).digest()
+	return password
+
+
+def find_pattern_match(pattern: str) -> Union[List[Path], None]:
+	"""
+	finds all paths matching the given pattern
+
+	Args:
+	- `pattern` (str): path pattern to be processed
+
+	Returns:
+	- `List[Path]`: a list of paths matching the pattern
+	- `None`: if the no matches were found for the pattern a null is returned
+	"""
+	with suppress(SyntaxError):
+		return list(map(Path, glob(pattern)))
+
+
+def validate_path(file_path: Path) -> None:
+	"""
+	validates that the path given exists and is a file
+
+	Args:
+	- file_path (Path): filepath to be validated
+	
+	Raises:
+	- `TypeError`|`ValueError`: unable to convert file_path arg to pathlib.Path
+	- `FileNotFoundError`: the file_path arg does not lead to a file
+	- `OSError`: the file_path arg leads to a directory
+	"""
+	if not isinstance(file_path, Path):
+		file_path = Path(file_path)
+
+	if not file_path.is_file():
+		raise FileNotFoundError(f"unable to locate file: {file_path}")
+	
+	elif file_path.is_dir():
+		raise OSError("path must be a file not a directory")
