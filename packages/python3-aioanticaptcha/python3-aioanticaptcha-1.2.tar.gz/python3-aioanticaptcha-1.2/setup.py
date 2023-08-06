cc6c38af1405cd6554d5607a1ed3ba7eb82d0f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:43 +0000

    Add package python3-aioanticaptcha/1.2/python3-aioanticaptcha-1.2.tar.gz

---
+++ packages/python3-aioanticaptcha/python3-aioanticaptcha-1.2.tar.gz/python3-aioanticaptcha-1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = "1.2"
+DESCRIPTION = "Async lib for  Anti-Captcha service."
+
+
+# Setting up
+setup(
+    name="python3-aioanticaptcha",
+    version=VERSION,
+    author="André França",
+    author_email="rsp.assistencia@gmail.com",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["aiohttp", "aiofiles"],
+    url="https://github.com/andrersp/aioanticaptcha",
+    keywords=["python", "anti-captcha", "async"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+)
