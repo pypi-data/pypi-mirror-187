a1c801675288e7577256228c24a8e6391863ec75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:25 +0000

    Add package pytask-stata/0.3.0/pytask_stata-0.3.0.tar.gz

---
+++ packages/pytask-stata/pytask_stata-0.3.0.tar.gz/pytask_stata-0.3.0/src/pytask_stata/collect.py
@@ -0,0 +1,135 @@
+"""Collect tasks."""
+from __future__ import annotations
+
+import functools
+import subprocess
+from pathlib import Path
+from types import FunctionType
+from typing import Any
+
+from pytask import depends_on
+from pytask import has_mark
+from pytask import hookimpl
+from pytask import Mark
+from pytask import parse_nodes
+from pytask import produces
+from pytask import remove_marks
+from pytask import Session
+from pytask import Task
+from pytask_stata.shared import convert_task_id_to_name_of_log_file
+from pytask_stata.shared import stata
+
+
+def run_stata_script(
+    executable: str, script: Path, options: list[str], log_name: list[str], cwd: Path
+) -> None:
+    """Run an R script."""
+    cmd = [executable, "-e", "do", script.as_posix(), *options, *log_name]
+    print("Executing " + " ".join(cmd) + ".")  # noqa: T201
+    subprocess.run(cmd, cwd=cwd, check=True)
+
+
+@hookimpl
+def pytask_collect_task(
+    session: Session, path: Path, name: str, obj: Any
+) -> Task | None:
+    """Perform some checks and prepare the task function."""
+    __tracebackhide__ = True
+
+    if (
+        (name.startswith("task_") or has_mark(obj, "task"))
+        and callable(obj)
+        and has_mark(obj, "stata")
+    ):
+        obj, marks = remove_marks(obj, "stata")
+
+        if len(marks) > 1:
+            raise ValueError(
+                f"Task {name!r} has multiple @pytask.mark.stata marks, but only one is "
+                "allowed."
+            )
+
+        mark = _parse_stata_mark(mark=marks[0])
+        script, options = stata(**marks[0].kwargs)
+
+        obj.pytask_meta.markers.append(mark)
+
+        dependencies = parse_nodes(session, path, name, obj, depends_on)
+        products = parse_nodes(session, path, name, obj, produces)
+
+        markers = obj.pytask_meta.markers if hasattr(obj, "pytask_meta") else []
+        kwargs = obj.pytask_meta.kwargs if hasattr(obj, "pytask_meta") else {}
+
+        task = Task(
+            base_name=name,
+            path=path,
+            function=_copy_func(run_stata_script),  # type: ignore[arg-type]
+            depends_on=dependencies,
+            produces=products,
+            markers=markers,
+            kwargs=kwargs,
+        )
+
+        script_node = session.hook.pytask_collect_node(
+            session=session, path=path, node=script
+        )
+
+        if isinstance(task.depends_on, dict):
+            task.depends_on["__script"] = script_node
+        else:
+            task.depends_on = {0: task.depends_on, "__script": script_node}
+
+        if session.config["platform"] == "win32":
+            log_name = convert_task_id_to_name_of_log_file(task.short_name)
+            log_name_arg = [f"-{log_name}"]
+        else:
+            log_name_arg = []
+
+        stata_function = functools.partial(
+            task.function,
+            executable=session.config["stata"],
+            script=task.depends_on["__script"].path,
+            options=options,
+            log_name=log_name_arg,
+            cwd=task.path.parent,
+        )
+
+        task.function = stata_function
+
+        return task
+    return None
+
+
+def _parse_stata_mark(mark: Mark) -> Mark:
+    """Parse a Stata mark."""
+    script, options = stata(**mark.kwargs)
+
+    parsed_kwargs = {"script": script or None, "options": options or []}
+
+    mark = Mark("stata", (), parsed_kwargs)
+    return mark
+
+
+def _copy_func(func: FunctionType) -> FunctionType:
+    """Create a copy of a function.
+
+    Based on https://stackoverflow.com/a/13503277/7523785.
+
+    Example
+    -------
+    >>> def _func(): pass
+    >>> copied_func = _copy_func(_func)
+    >>> _func is copied_func
+    False
+
+    """
+    new_func = FunctionType(
+        func.__code__,
+        func.__globals__,
+        name=func.__name__,
+        argdefs=func.__defaults__,
+        closure=func.__closure__,
+    )
+    new_func = functools.update_wrapper(new_func, func)
+    new_func.__kwdefaults__ = func.__kwdefaults__
+    return new_func
---
+++ packages/pytask-stata/pytask_stata-0.3.0.tar.gz/pytask_stata-0.3.0/src/pytask_stata/shared.py
@@ -0,0 +1,106 @@
+"""Shared functions and variables."""
+from __future__ import annotations
+
+import sys
+from pathlib import Path
+from typing import Any
+from typing import Iterable
+from typing import Sequence
+
+
+if sys.platform == "darwin":
+    STATA_COMMANDS = [
+        "Stata64MP",
+        "StataMP",
+        "Stata64SE",
+        "StataSE",
+        "Stata64",
+        "Stata",
+    ]
+elif sys.platform == "linux":
+    STATA_COMMANDS = ["stata-mp", "stata-se", "stata"]
+elif sys.platform == "win32":
+    STATA_COMMANDS = [
+        "StataMP-64",
+        "StataMP-ia",
+        "StataMP",
+        "StataSE-64",
+        "StataSE-ia",
+        "StataSE",
+        "Stata-64",
+        "Stata-ia",
+        "Stata",
+        "WMPSTATA",
+        "WSESTATA",
+        "WSTATA",
+    ]
+else:
+    STATA_COMMANDS = []
+
+
+def stata(
+    *,
+    script: str | Path | None = None,
+    options: str | Iterable[str] | None = None,
+) -> tuple[str | Path | None, str | Iterable[str] | None]:
+    """Specify command line options for Stata.
+
+    Parameters
+    ----------
+    options : str | Iterable[str] | None
+        One or multiple command line options passed to Stata.
+
+    """
+    options = [] if options is None else list(map(str, _to_list(options)))
+    return script, options
+
+
+def convert_task_id_to_name_of_log_file(id_: str) -> str:
+    """Convert task to id to name of log file.
+
+    If one passes the complete task id as the log file name, Stata would remove parent
+    directories and cut the string at the double colons for parametrized task. Here is
+    an example:
+
+    .. code-block:: none
+
+        C:/task_example.py::task_example[arg1] -> task_example.log
+
+    This function creates a new id starting from the task module and by replacing dots
+    and double colons with underscores.
+
+    Example
+    -------
+    >>> convert_task_id_to_name_of_log_file("C:/task_example.py::task_example[arg1]")
+    'task_example_py_task_example[arg1]'
+
+    """
+    id_without_parent_directories = id_.rsplit("/")[-1]
+    converted_id = id_without_parent_directories.replace(".", "_").replace("::", "_")
+    return converted_id
+
+
+def _to_list(scalar_or_iter: Any) -> list[Any]:
+    """Convert scalars and iterables to list.
+
+    Parameters
+    ----------
+    scalar_or_iter
+
+    Returns
+    -------
+    list
+
+    Examples
+    --------
+    >>> _to_list("a")
+    ['a']
+    >>> _to_list(["b"])
+    ['b']
+
+    """
+    return (
+        [scalar_or_iter]
+        if isinstance(scalar_or_iter, str) or not isinstance(scalar_or_iter, Sequence)
+        else list(scalar_or_iter)
+    )
