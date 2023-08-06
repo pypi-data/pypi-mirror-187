df18477d3444869e0d6b0e893c71b484b286c09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:00 +0000

    Add package gpt-traceback/0.1.12/gpt_traceback-0.1.12.tar.gz

---
+++ packages/gpt-traceback/gpt_traceback-0.1.12.tar.gz/gpt_traceback-0.1.12/src/gpt_traceback/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.1.12"
+
+import warnings
+
+try:
+    from .ipython_traceback import *
+except ImportError:
+    warnings.warn("IPython could not be imported, are you in Google Colab?")
+
+def deactivate():
+    IPython.get_ipython().set_custom_exc((Exception,),
+                                     None)
