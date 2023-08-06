d2220137e0dd89bda9e1d61a68a64de8074fb548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:40 +0000

    Add package tweetpy/1.0.1/tweetpy-1.0.1.tar.gz

---
+++ packages/tweetpy/tweetpy-1.0.1.tar.gz/tweetpy-1.0.1/README.md
@@ -0,0 +1,18 @@
+# tweet-py
+Python package which to access the Tweet data without API
+
+## Usage
+
+Download it by clicking the green download button here on Github. You only need to parse argument specific Twitter keyword.
+```python
+>>> from tweetpy.tweet import Tweet
+>>> tweet = Tweet(1567531687130632199)
+```
+
+<!--
+python  -m venv venv
+source venv/bin/activate
+pip install wheel setuptools twine
+python setup.py sdist bdist_wheel
+python -m twine upload dist/*
+-->
---
+++ packages/tweetpy/tweetpy-1.0.1.tar.gz/tweetpy-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from os.path import join, dirname
+
+with open(join(dirname(__file__),"README.md"),'r') as f:
+    long_description =f.read()
+
+setup(
+	name='tweetpy',
+	packages=find_packages(),
+	author_email="<abderrahmane@elhellal.com>",
+	url="https://tweetpy.elhellal.com/",
+	project_urls = {
+	  'Homepage': 'https://tweetpy.elhellal.com/',
+	  'Source': 'https://github.com/abdouhl/tweetpy/',
+	  'Documentation':'https://tweetpy.elhellal.com#doc'
+	},
+	version='1.0.1',
+	description='Python package that allows to access the Tweet data without API',
+	long_description_content_type="text/markdown",
+	long_description=long_description,
+	keywords=['twitter', 'tweets', 'tweet', 'twitter_scraper', 'twitter_sentiment', 'status'],
+	author='abdouhl',
+	license='MIT',
+)
