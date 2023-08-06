d821ded25948ed2c9ee9aab96e92e4f4a82de481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:59 +0000

    Add package com.castsoftware.uc.python.common/0.1.4/com.castsoftware.uc.python.common-0.1.4.tar.gz

---
+++ packages/com.castsoftware.uc.python.common/com.castsoftware.uc.python.common-0.1.4.tar.gz/com.castsoftware.uc.python.common-0.1.4/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup (
+    name="com.castsoftware.uc.python.common",
+    version="0.1.4",
+    author="Nevin Kaplan",
+    description="A set of common classes and methods for use with python projects",
+    packages=["cast-common"]
+)
