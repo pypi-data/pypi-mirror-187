72f2c4347162c0e3d5d76033ba5fb78593888bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:06 +0000

    Add package screenlogicpy/0.6.4/screenlogicpy-0.6.4.tar.gz

---
+++ packages/screenlogicpy/screenlogicpy-0.6.4.tar.gz/screenlogicpy-0.6.4/screenlogicpy/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.6.4"
+# flake8: noqa F401
+from screenlogicpy.gateway import ScreenLogicGateway
+from screenlogicpy.const import ScreenLogicError
+from screenlogicpy.discovery import async_discover
