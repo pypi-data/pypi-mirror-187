19156d9d31243d11f793155673b112d6eaa246f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:23 +0000

    Add package pycob/0.0.6/pycob-0.0.6-py3-none-any.whl

---
+++ packages/pycob/pycob-0.0.6-py3-none-any.whl/pycob/request.py
@@ -0,0 +1,19 @@
+import flask
+
+class Request:
+    def __init__(self, flask_request: flask.Request):
+        self.flask_request = flask_request
+    
+    def get_query_parameter(self, key: str) -> str:
+        unsanitized = self.flask_request.args.get(key, '')
+
+        if unsanitized is None or unsanitized == '':
+            unsanitized = self.flask_request.form.get(key, '')
+
+        if unsanitized is None or unsanitized == '':
+            unsanitized = self.flask_request.values.get(key, '')
+
+        # if unsanitized is None or unsanitized == '':
+        #     unsanitized = self.flask_request.json.__getattribute__(key)
+
+        return unsanitized.replace('<', '&lt;').replace('>', '&gt;').replace("'", "''").replace(";", "")
