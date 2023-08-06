0d009492b0b7e96810cf1809e1de21e5e11afaaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:43 +0000

    Add package GoogleImageScraper/2.3.5/GoogleImageScraper-2.3.5.tar.gz

---
+++ packages/GoogleImageScraper/GoogleImageScraper-2.3.5.tar.gz/GoogleImageScraper-2.3.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+README = (this_directory / "README.md").read_text()
+
+setup(
+    name='GoogleImageScraper',
+    packages=find_packages(exclude=("tests")),
+    version='2.3.5',
+    description='This is a library for retrieving urls and downloading images from Google Images.',
+    url='https://github.com/commonkestrel/GoogleImageScraper',
+    author='Jibble',
+    license='MIT',
+    install_requires=['DynamicHtml'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    python_requires='>= 3.4',
+    test_suite='tests',
+    long_description=README,
+    long_description_content_type='text/markdown'
+)
