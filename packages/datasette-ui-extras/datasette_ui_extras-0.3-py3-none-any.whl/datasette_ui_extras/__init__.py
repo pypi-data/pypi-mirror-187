a005650dbdb1083443a5f75ddbb70b1fcc30c267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:49 +0000

    Add package datasette-ui-extras/0.3/datasette_ui_extras-0.3-py3-none-any.whl

---
+++ packages/datasette-ui-extras/datasette_ui_extras-0.3-py3-none-any.whl/datasette_ui_extras/__init__.py
@@ -0,0 +1,17 @@
+from datasette import hookimpl
+
+from datasette import hookimpl
+
+
+@hookimpl
+def extra_css_urls(datasette):
+    return [
+        datasette.urls.static_plugins("datasette-ui-extras", "facet-treatment.css")
+    ]
+
+@hookimpl
+def extra_js_urls(datasette):
+    return [
+        datasette.urls.static_plugins("datasette-ui-extras", "sticky-table-headers.js"),
+        datasette.urls.static_plugins("datasette-ui-extras", "focus-search-box.js")
+    ]
