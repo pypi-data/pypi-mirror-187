76fef1a17322289348f0464fb92663b91407f0b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:50 +0000

    Add package rshanker779-common/1.0.1/rshanker779_common-1.0.1-py3-none-any.whl

---
+++ packages/rshanker779-common/rshanker779_common-1.0.1-py3-none-any.whl/rshanker779_common/mixins/json_mixin.py
@@ -0,0 +1,26 @@
+import datetime
+import json
+import datetime
+
+
+class JSONMixin:
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+    def to_dict(self):
+        return self.__convert_item(self.__dict__)
+
+    def __convert_item(self, value):
+        if isinstance(value, JSONMixin):
+            return {value.__class__.__name__: value.to_dict()}
+        elif isinstance(value, dict):
+            return {
+                i: self.__convert_item(v)
+                for i, v in value.items()
+                if not i.startswith("_")
+            }
+        elif isinstance(value, (list, tuple, set)):
+            return [self.__convert_item(i) for i in value]
+        elif isinstance(value, datetime.timedelta):
+            return value.total_seconds()
+        return value
