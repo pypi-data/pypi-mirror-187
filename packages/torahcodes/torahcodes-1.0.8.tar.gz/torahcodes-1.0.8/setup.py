a4f75e451376ff493c7da69958df5c1fa337d575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:20 +0000

    Add package torahcodes/1.0.8/torahcodes-1.0.8.tar.gz

---
+++ packages/torahcodes/torahcodes-1.0.8.tar.gz/torahcodes-1.0.8/setup.py
@@ -0,0 +1,51 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='torahcodes',
+    version='1.0.8',
+    author='torahcodes',
+    include_package_data=True,
+    author_email='',
+    description='An understandable multilanguaje and multithreading bible codes . Study the Torah as never before Bible Codes python library . An understandable multilanguaje and multithreading bible codes . Study the Torah as never before',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',
+    #packages=find_packages(exclude=['tests']),
+    #packages=find_packages(),
+    packages=['torahcodes', 'torahcodes/modules', 'torahcodes/resources/func', 'torahcodes/resources/data'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9'
+    ],
+    keywords='torahcodes',
+    python_requires='>=3.6.0',
+    install_requires=[
+        'configparser',
+        'lxml',
+        'python-hebrew-numbers',
+        'textblob',
+        'deep_translator'
+    ],
+    entry_points={
+        'console_scripts': [
+            'tbc-cli=torahcodes.TBC:main'
+        ],
+    },
+    project_urls={
+        'Source': 'https://github.com/pedroelbanquero/torahcodespython',
+    },
+)
