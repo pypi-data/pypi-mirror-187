576ebea56b56bf3f9df751eaf14019617022a30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:17 +0000

    Add package pdivas/0.1.0/pdivas-0.1.0-py3-none-any.whl

---
+++ packages/pdivas/pdivas-0.1.0-py3-none-any.whl/pdivas/__init__.py
@@ -0,0 +1,13 @@
+import os
+__copyright__ = 'Copyright (C) 2023 Ryo Kurosawa'
+__version__ = "0.1.0"
+__author__ = 'Ryo Kurosawa'
+__author_email__ = 'a0160561@yahoo.co.jp'
+#__url__          = 'http://github.com/account/repository'
+
+
+
+__cur_path__ = __file__
+
+
+
