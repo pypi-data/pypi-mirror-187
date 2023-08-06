da93b73e710490127ae10f4fc4562b5a105d5fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:14 +0000

    Add package pymyeasysql/0.0/pymyeasysql-0.0.tar.gz

---
+++ packages/pymyeasysql/pymyeasysql-0.0.tar.gz/pymyeasysql-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pymyeasysql',
+      version='0.0',
+      description='easy sql',
+      packages=[],
+      author_email='progithub13@gmail.com',
+      zip_safe=False)
