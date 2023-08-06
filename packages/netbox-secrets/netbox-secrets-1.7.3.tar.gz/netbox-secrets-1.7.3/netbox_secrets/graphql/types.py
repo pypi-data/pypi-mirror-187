6bae1b4fb7f5da9384398cb11a281d7922e73cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:39 +0000

    Add package netbox-secrets/1.7.3/netbox-secrets-1.7.3.tar.gz

---
+++ packages/netbox-secrets/netbox-secrets-1.7.3.tar.gz/netbox-secrets-1.7.3/netbox_secrets/graphql/types.py
@@ -0,0 +1,23 @@
+from netbox_secrets import filtersets, models
+from netbox.graphql.types import ObjectType, NetBoxObjectType
+
+__all__ = (
+    'SecretRoleType',
+    'SecretType',
+)
+
+
+class SecretRoleType(ObjectType):
+
+    class Meta:
+        model = models.SecretRole
+        fields = '__all__'
+        filterset_class = filtersets.SecretRoleFilterSet
+
+
+class SecretType(NetBoxObjectType):
+
+    class Meta:
+        model = models.Secret
+        fields = '__all__'
+        filterset_class = filtersets.SecretFilterSet
