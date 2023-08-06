39c8eb0dde90201558800027317407ea5dce7997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:55 +0000

    Add package ziglang/0.10.1.post1/ziglang-0.10.1.post1-py3-none-macosx_10_9_x86_64.whl

---
+++ packages/ziglang/ziglang-0.10.1.post1-py3-none-macosx_10_9_x86_64.whl/ziglang/lib/compiler_rt/floatunsitf.zig
@@ -0,0 +1,21 @@
+const common = @import("./common.zig");
+const intToFloat = @import("./int_to_float.zig").intToFloat;
+
+pub const panic = common.panic;
+
+comptime {
+    if (common.want_ppc_abi) {
+        @export(__floatunsitf, .{ .name = "__floatunsikf", .linkage = common.linkage });
+    } else if (common.want_sparc_abi) {
+        @export(_Qp_uitoq, .{ .name = "_Qp_uitoq", .linkage = common.linkage });
+    }
+    @export(__floatunsitf, .{ .name = "__floatunsitf", .linkage = common.linkage });
+}
+
+pub fn __floatunsitf(a: u32) callconv(.C) f128 {
+    return intToFloat(f128, a);
+}
+
+fn _Qp_uitoq(c: *f128, a: u32) callconv(.C) void {
+    c.* = intToFloat(f128, a);
+}
