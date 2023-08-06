000b72dcf5ede4bacf1ffcc6ea80a7cdedd51d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:19 +0000

    Add package playgroundlocal/0.0.1/playgroundlocal-0.0.1.tar.gz

---
+++ packages/playgroundlocal/playgroundlocal-0.0.1.tar.gz/playgroundlocal-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'OpenAI Playground for local models'
+LONG_DESCRIPTION = 'OpenAI Playground for local models'
+
+# Setting up
+setup(
+    name="playgroundlocal",
+    version=VERSION,
+    author="eefh",
+    author_email="<ethanhasbrouck02@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['django'],
+    keywords=['python', 'ai'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
