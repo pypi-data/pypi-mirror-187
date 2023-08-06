3980fd88ddadd45d6ee1ca6d6c616afb808b6fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:01 +0000

    Add package sequential-calibrationWithSA/0.0.5/sequential_calibrationWithSA-0.0.5-py3-none-any.whl

---
+++ packages/sequential-calibrationWithSA/sequential_calibrationWithSA-0.0.5-py3-none-any.whl/sequential_calibrationWithSA/seqC.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Variance_based sensitivity analysis
+
+'''
+function
+1) VSA
+2) Calibration
+3) 
+'''
+
+
+# In[1]:
+
+
+def VSA(msg):
+    print(msg + "asd")
+
+
+# In[6]:
+
+
+def SA(a, b):
+    c = a + b 
+    return c
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[14]:
+
+
+
+# In[ ]:
+
+
+
+
