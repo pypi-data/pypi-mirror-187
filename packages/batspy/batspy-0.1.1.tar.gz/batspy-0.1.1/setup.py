69338c5801122d035fca58df15b92f981d7e106a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:37 +0000

    Add package batspy/0.1.1/batspy-0.1.1.tar.gz

---
+++ packages/batspy/batspy-0.1.1.tar.gz/batspy-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='batspy',
+    version='0.1.1',
+    description='A simple library that prints "Hello, world!"',
+    author='Tom Shabtay',
+    author_email='tomshabtay@gmail.com',
+    url='https://github.com/tomshabtay/batspy',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+)
