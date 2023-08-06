90a3bd3efda2d5d7c2c19832e37030af57f786db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:34 +0000

    Add package pagseguroCTS/0.0.7/pagseguroCTS-0.0.7.tar.gz

---
+++ packages/pagseguroCTS/pagseguroCTS-0.0.7.tar.gz/pagseguroCTS-0.0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='pagseguroCTS',
+    version='0.0.7',
+    license='MIT License',
+    author='SDK_PS',
+    url='https://pypi.org/project/pagseguro/',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='leopoldo.bernardes@monetizze.com.br',
+    keywords='pagseguro',
+    description=u'SDK pagseguro',
+    packages=['pagseguroCTS'],)
