3da5926f6d71fa650aaed2ec878af60333e51a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:16 +0000

    Add package Topsis-Yatin-102003655/0.1/Topsis-Yatin-102003655-0.1.tar.gz

---
+++ packages/Topsis-Yatin-102003655/Topsis-Yatin-102003655-0.1.tar.gz/Topsis-Yatin-102003655-0.1/setup.py
@@ -0,0 +1,5 @@
+import setuptools
+setuptools.setup( name='Topsis-Yatin-102003655', version='0.1',
+author="yatin goyal", author_email="goyalyatin2002@gmail.com", description="Topsis", packages=setuptools.find_packages(), classifiers=[
+"Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent",
+], )
