dc47949dd00095a7e08bf1d69bbc88c3246478a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:50 +0000

    Add package real-json/1.0.0/real_json-1.0.0.tar.gz

---
+++ packages/real-json/real_json-1.0.0.tar.gz/real_json-1.0.0/README.md
@@ -0,0 +1,2 @@
+# python_js_json
+Wrapper class so that you can access `dict` and `list` as easily as in Javascript
---
+++ packages/real-json/real_json-1.0.0.tar.gz/real_json-1.0.0/src/real_json/__init__.py
@@ -0,0 +1,7 @@
+from js_json.GetWrappedJson import GetWrappedJson
+
+__version__ = "1.0.0"
+
+
+def ify(obj):
+    return GetWrappedJson(obj)
