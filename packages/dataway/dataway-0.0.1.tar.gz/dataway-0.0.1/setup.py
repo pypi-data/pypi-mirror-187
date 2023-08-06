9519ea156e224f8c8bd33de5049935bde4c98417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:19 +0000

    Add package dataway/0.0.1/dataway-0.0.1.tar.gz

---
+++ packages/dataway/dataway-0.0.1.tar.gz/dataway-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as rea:
+    readme = rea.read()
+
+setup(name='dataway',
+    version='0.0.1',
+    license='MIT License',
+    author='Anselmo Oliveira',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='anselmo.oliveira@dataway.info',
+    keywords='analytics dataway atlas',
+    description=u'Biblioteca para de apoio para realização de webscraping em tabelas',
+    packages=['Dataway'],
+    install_requires= ['requests', 'Pandas'],)
