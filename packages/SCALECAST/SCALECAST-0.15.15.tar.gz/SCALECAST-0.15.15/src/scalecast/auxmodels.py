2f504cb3133423b455a357d1284a0db2ddb3110d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:14 +0000

    Add package SCALECAST/0.15.15/SCALECAST-0.15.15.tar.gz

---
+++ packages/SCALECAST/SCALECAST-0.15.15.tar.gz/SCALECAST-0.15.15/src/scalecast/auxmodels.py
@@ -0,0 +1,246 @@
+from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import BaggingRegressor
+from sklearn.ensemble import StackingRegressor
+from statsmodels.tsa.vector_ar.vecm import VECM
+from scalecast.Forecaster import _sklearn_imports_
+
+class vecm:
+    def __init__(
+        self,
+        exog_coint=None,
+        k_ar_diff=1, # always 0
+        coint_rank=1,
+        deterministic="n",
+        seasons=0,
+        first_season=0,
+        freq = None,
+    ):
+        """ initializes a Vector Error Correction Model.
+        uses the statsmodels implementation: https://www.statsmodels.org/dev/generated/statsmodels.tsa.vector_ar.vecm.VECM.html.
+        see it used with scalecast: https://scalecast-examples.readthedocs.io/en/latest/vecm/vecm.html
+
+        Args:
+            exog_coint (ndarray): default None. deterministic terms inside the cointegration relation.
+            k_ar_diff (int): default 1. number of lagged differences in the model.
+            coint_rank (int): cointegration rank.
+            deterministic (str): one of {"n", "co", "ci", "lo", "li"}. default "n".
+                "n" - no deterministic terms.
+                "co" - constant outside the cointegration relation.
+                "ci" - constant within the cointegration relation.
+                "lo" - linear trend outside the cointegration relation.
+                "li" - linear trend within the cointegration relation.
+                Combinations of these are possible (e.g. "cili" or "colo" for linear trend with intercept). 
+                When using a constant term you have to choose whether you want to restrict it to the cointegration relation 
+                (i.e. "ci") or leave it unrestricted (i.e. "co"). Do not use both "ci" and "co". The same applies for "li" 
+                and "lo" when using a linear term. 
+            seasons (int): default 0. Number of periods in a seasonal cycle. 0 means no seasons.
+            first_season (int): default 0. Season of the first observation.
+            freq (str): optional. the frequency of the time-series. 
+                a pandas offset or 'B', 'D', 'W', 'M', 'A', or 'Q'.
+        """
+        self.k_ar_diff = k_ar_diff
+        self.exog_coint = exog_coint
+        self.coint_rank = coint_rank
+        self.deterministic = deterministic
+        self.seasons = seasons
+        self.first_season = first_season
+        self.freq = freq
+        self._scalecast_set = ['dates','n_series'] # these attrs are set when imported into scalecast
+
+    def fit(self,X,y=None):
+        """ fits the model.
+
+        Args:
+            X (ndarray): the known observations (all known series plus exogenous regressors in a matrix).
+                MVForecaster will split endog from exog appropriately.
+            y: not required for this model and left None. kept in to be consistent
+                with other scikit-learn models syntax.
+        """
+        exog = X[:,self.n_series:]
+        X = X[:,:self.n_series]
+
+        self.exogdim = exog.shape[1]
+        self.Xdim = X.shape[1]
+
+        exog = None if exog.shape[1] == 0 else exog
+
+        self.mod = VECM(
+            X,
+            exog=exog,
+            k_ar_diff=self.k_ar_diff,
+            exog_coint=self.exog_coint,
+            coint_rank=self.coint_rank,
+            deterministic=self.deterministic,
+            seasons=self.seasons,
+            first_season=self.first_season,
+            dates=self.dates[:X.shape[0]],
+            freq = self.freq,
+        ).fit()
+        
+    def predict(self,X):
+        """ forecasts into an unknown horizon
+
+        Args:
+            X (ndarray): the future sereis values and future exognenous regressors.
+                MVForecaster will split this up appropriately without leaking.
+        """
+        # testing
+        if X.shape[1] == self.Xdim + self.exogdim:
+            exog = X[:,self.n_series:]
+            exog = None if exog.shape[1] == 0 else exog
+        # true forecast
+        elif X.shape[1] == self.exogdim:
+            exog = None if X.shape[1] == 0 else X
+        else: # i don't see how this would ever happen but i'd like to know if it does
+            raise Exception(
+                'something went wrong. ' 
+                'please consider raising this issue on the scalecast issues tab: '
+                'https://github.com/mikekeith52/scalecast/issues'
+            )
+        return self.mod.predict(steps=X.shape[0],exog_fc=exog)
+
+def auto_arima(f,call_me='auto_arima',Xvars=None,**kwargs):
+    """ adds a forecast to a `Forecaster` object using the auto_arima function from pmdarima.
+    this function attempts to find the optimal arima order by minimizing information criteria
+    on the training slice of data only.
+
+    Args:
+        f (Forecaster): the object to add the forecast to
+        call_me (str): default 'auto_arima'. the name of the resulting model.
+        Xvars (str or list-like): optional. Xvars to add to the model.
+        **kwargs: passed to the auto_arima function from pmdarima.
+            see https://alkaline-ml.com/pmdarima/modules/generated/pmdarima.arima.auto_arima.html
+
+    Returns:
+        None
+
+    >>> from scalecast.util import pdr_load
+    >>> from scalecast.auxmodels import auto_arima
+    >>> f = pdr_load('HOUSTNSA',start='1900-01-01',end='2021-06-01',future_dates=24)
+    >>> auto_arima(f,m=12) # saves a model called auto_arima
+    >>> print(f.auto_arima_params) # access the selected orders
+    """
+    import pmdarima
+    train = f.y.values[:-f.test_length]
+    auto_model = pmdarima.auto_arima(train,**kwargs)
+    best_params = auto_model.get_params()
+    f.auto_arima_params = best_params
+    order = best_params['order']
+    seasonal_order = best_params['seasonal_order']
+    trend = best_params['trend']
+
+    f.set_estimator('arima')
+    f.manual_forecast(
+        order = order,
+        seasonal_order = seasonal_order,
+        trend = trend,
+        Xvars = Xvars,
+        call_me = call_me,
+    )
+
+def mlp_stack(
+    f,
+    model_nicknames,
+    max_samples=0.9,
+    max_features=0.5,
+    n_estimators=10,
+    hidden_layer_sizes=(100,100,100),
+    solver='lbfgs',
+    call_me='mlp_stack',
+    probabilistic=False,
+    **kwargs,
+):
+    """ applies a stacking model using a bagged MLP regressor as the final estimator and adds it to a `Forecaster` or `MVForecaster` object.
+    see what it does: https://scalecast-examples.readthedocs.io/en/latest/sklearn/sklearn.html#StackingRegressor
+    this function is not meant to be a model that allows for full customization but full customization is possible when using the 
+    `Forecaster` and `MVForecaster` objects.
+    default values usually perform pretty well from what we have observed.
+    recommended to use at least four models in the stack.
+
+    Args:
+        f (Forecaster or MVForecaster): the object to add the model to.
+        model_nicknames (list-like): the names of models previously evaluated within the object.
+            must be sklearn api models.
+        max_samples (float or int): default 0.9.
+            the number of samples to draw with replacement from training set to train each base estimator.
+            if int, then draw max_samples samples.
+            if float, then draw that percentage of samples.
+        max_features (float or int): default 0.5
+            the number of features to draw from training set to train each base estimator.
+            if int, then draw max_features features.
+            if float, then draw that percentage of features.
+        n_estimators (int): default 10.
+            the number of base estimators in the ensemble.
+        hidden_layer_sizes (tuple): default (100,100,100).
+            the layer/hidden layer sizes for the bagged mlp regressor that is the final estimator in the stacked model.
+        solver (str): default 'lbfgs'.
+            the mlp solver.
+        call_me (str): default 'mlp_stack'. the name of the resulting model.
+        probabilistic (bool): default False. whether to use probabilistic modeling.
+        **kwargs: passed to the `manual_forecast()` or `proba_forecast()` method.
+
+    Returns:
+        None
+
+    >>> from scalecast.util import pdr_load
+    >>> from scalecast.auxmodels import mlp_stack
+    >>> from scalecast import GridGenerator
+
+    >>> GridGenerator.get_example_grids()
+    >>> models = ('xgboost','lightgbm','knn','elasticnet')
+    >>> f = pdr_load('HOUSTNSA',start='1900-01-01',end='2021-06-01',future_dates=24)
+    >>> f.set_test_length(24)
+    >>> f.add_ar_terms(1)
+    >>> f.add_AR_terms((1,6))
+    >>> f.add_AR_terms((1,12))
+    >>> f.add_seasonal_regressors('month',raw=False,sincos=True)
+    >>> f.diff()
+    >>> f.tune_test_forecast(models,cross_validate=True)
+    >>> mlp_stack(f,model_nicknames=models) # saves a model called mlp_stack
+    >>> f.export('model_summaries',models='mlp_stack')
+    """
+    results = f.export('model_summaries')
+    
+    estimators = [
+        (
+            m,
+             _sklearn_imports_[
+                results.loc[
+                    results['ModelNickname'] == m,
+                    'Estimator'
+                ].values[0]
+             ](
+                 **results.loc[
+                    results['ModelNickname'] == m,
+                    'HyperParams'
+                ].values[0]
+             )
+        ) for m in model_nicknames
+    ]
+
+    final_estimator = BaggingRegressor(
+        base_estimator = MLPRegressor(
+            hidden_layer_sizes=hidden_layer_sizes,
+            solver=solver,
+        ),
+        max_samples = max_samples,
+        max_features = max_features,
+        n_estimators = n_estimators,
+    )
+
+    f.add_sklearn_estimator(StackingRegressor,'stacking')
+    f.set_estimator('stacking')
+    if not probabilistic:
+        f.manual_forecast(
+            estimators=estimators,
+            final_estimator=final_estimator,
+            call_me=call_me,
+            **kwargs,
+        )
+    else:
+        f.proba_forecast(
+            estimators=estimators,
+            final_estimator=final_estimator,
+            call_me=call_me,
+            **kwargs,
+        )
---
+++ packages/SCALECAST/SCALECAST-0.15.15.tar.gz/SCALECAST-0.15.15/src/scalecast/notebook.py
@@ -0,0 +1,286 @@
+import typing
+from typing import Dict, Union
+
+from ipywidgets import widgets
+from IPython.display import display, clear_output
+
+from scalecast.Forecaster import Forecaster, _determine_best_by_, _estimators_
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from tqdm.notebook import tqdm as log_progress
+
+
+def results_vis(
+    f_dict: Dict[str, Forecaster],
+    plot_type: str = "forecast",
+    print_attr: list = [],
+    include_train: Union[bool, int] = True,
+    figsize = (12,6),
+) -> None:
+    """ visualize the forecast results from many different Forecaster objects leveraging Jupyter widgets.
+
+    Args:
+        f_dict (dict[str,Forecaster]): dictionary of forcaster objects.
+            works best if two or more models have been evaluated in each dictionary value.
+        plot_type (str): one of {"forecast","test"}, default "forecast".
+            the type of results to visualize.
+        print_attr (list): optional.
+            the attributes from history to print.
+            passed to print_attr parameter when plot_type = 'forecast'.
+            ignored when plot_type = 'test'.
+        include_train (bool or int): optional.
+            whether to include the complete training set in the plot or how many traning-set observations to include.
+            passed to include_train parameter when plot_type = 'test'.
+            ignored when plot_type = 'forecast'.
+        figsize (tuple): default (12,6). size of the resulting figure.
+
+    Returns:
+        None 
+    """
+    if plot_type not in {"forecast", "test"}:
+        raise ValueError(f'plot_type must be "forecast" or "test", got {plot_type}')
+
+    def display_user_selections(
+        ts_selection, mo_selection, lv_selection, ci_selection, me_selection
+    ):
+        selected_data = f_dict[ts_selection]
+        if plot_type == "forecast":
+            selected_data.plot(
+                models=f"top_{mo_selection}",
+                order_by=me_selection,
+                level=lv_selection,
+                print_attr=print_attr,
+                ci=ci_selection,
+                figsize=figsize,
+            )
+        else:
+            selected_data.plot_test_set(
+                models=f"top_{mo_selection}",
+                order_by=me_selection,
+                include_train=include_train,
+                level=lv_selection,
+                ci=ci_selection,
+                figsize=figsize,
+            )
+        plt.title(ts_selection + " Forecast Results", size=16)
+        plt.show()
+
+    def on_button_clicked(b):
+        mo_selection = mo_dd.value
+        ts_selection = ts_dd.value
+        lv_selection = lv_dd.value
+        ci_selection = ci_dd.value
+        me_selection = me_dd.value
+        with output:
+            clear_output()
+            display_user_selections(
+                ts_selection, mo_selection, lv_selection, ci_selection, me_selection
+            )
+
+    all_models = []
+    for k, f in f_dict.items():
+        all_models += [fcst for fcst in f.history.keys() if fcst not in all_models]
+    ts_dd = widgets.Dropdown(options=f_dict.keys(), description="Time Series:")
+    mo_dd = widgets.Dropdown(
+        options=range(1, len(all_models) + 1), description="No. Models"
+    )
+    lv_dd = widgets.Dropdown(options=[True, False], description="View Level")
+    ci_dd = widgets.Dropdown(
+        options=[True, False], description="View Confidence Intervals"
+    )
+    me_dd = widgets.Dropdown(
+        options=sorted(_determine_best_by_), description="Order By"
+    )
+
+    # never changes
+    button = widgets.Button(description="Select Time Series")
+    output = widgets.Output()
+
+    display(ts_dd, mo_dd, lv_dd, ci_dd, me_dd)
+    display(button, output)
+
+    button.on_click(on_button_clicked)
+
+
+def results_vis_mv(f_dict, plot_type="forecast", include_train=True, figsize = (12,6)):
+    """ visualize the forecast results from many different MVForecaster objects leveraging Jupyter widgets.
+
+    Args:
+        f_dict (dict[str,MVForecaster]): dictionary of forcaster objects.
+            works best if two or more models have been evaluated in each dictionary value.
+        plot_type (str): one of {"forecast","test"}, default "forecast".
+            the type of results to visualize.
+        include_train (bool or int): optional.
+            whether to include the complete training set in the plot or how many traning-set observations to include.
+            passed to include_train parameter when plot_type = 'test'.
+            ignored when plot_type = 'forecast'.
+        figsize (tuple): default (12,6). size of the resulting figure.
+
+    Returns:
+        None
+    """
+    if plot_type not in {"forecast", "test"}:
+        raise ValueError(f'plot_type must be "forecast" or "test", got {plot_type}')
+
+    def display_user_selections(
+        mo_selection, ts_selection, lv_selection, ci_selection, se_selection
+    ):
+        selected_data = f_dict[ts_selection]
+        if plot_type == "forecast":
+            selected_data.plot(
+                models=mo_selection,
+                series=se_selection,
+                level=lv_selection,
+                ci=ci_selection,
+                figsize=figsize,
+            )
+        else:
+            selected_data.plot_test_set(
+                models=mo_selection,
+                series=se_selection,
+                level=lv_selection,
+                ci=ci_selection,
+                include_train=include_train,
+                figsize=figsize,
+            )
+        plt.title(ts_selection + " Forecast Results", size=16)
+        plt.show()
+
+    def on_button_clicked(b):
+        mo_selection = mo_se.value
+        ts_selection = ts_dd.value
+        lv_selection = lv_dd.value
+        ci_selection = ci_dd.value
+        se_selection = se_se.value
+        with output:
+            clear_output()
+            display_user_selections(
+                mo_selection, ts_selection, lv_selection, ci_selection, se_selection
+            )
+
+    all_models = []
+    n_series = 2
+    for k, f in f_dict.items():
+        all_models += [fcst for fcst in f.history.keys() if fcst not in all_models]
+        n_series = max(n_series, f.n_series)
+    series = [f"series{i+1}" for i in range(n_series)]
+    ts_dd = widgets.Dropdown(options=f_dict.keys(), description="Time Series:")
+    mo_se = widgets.SelectMultiple(
+        options=all_models, description="Models", selected=all_models
+    )
+    se_se = widgets.SelectMultiple(
+        options=series, description="Series", selected=series
+    )
+    lv_dd = widgets.Dropdown(options=[True, False], description="View Level")
+    ci_dd = widgets.Dropdown(
+        options=[True, False], description="View Confidence Intervals"
+    )
+
+    # never changes
+    button = widgets.Button(description="Select Time Series")
+    output = widgets.Output()
+
+    display(ts_dd, se_se, mo_se, lv_dd, ci_dd)
+    display(button, output)
+
+    button.on_click(on_button_clicked)
+
+
+def tune_test_forecast(
+    f,
+    models,
+    cross_validate=False,
+    dynamic_tuning=False,
+    dynamic_testing=True,
+    probabilistic=False,
+    n_iter=20,
+    summary_stats=False,
+    feature_importance=False,
+    fi_method="pfi",
+    limit_grid_size=None,
+    suffix=None,
+    **cvkwargs,
+):
+    """ tunes, tests, and forecasts a series of models with a progress bar through tqdm.
+
+    Args:
+        f (Forecaster or MVForecaster): the object to run the models through.
+        models (list-like):
+            each element must be in _can_be_tuned_.
+        cross_validate (bool): default False
+                whether to tune the model with cross validation. 
+                if False, uses the validation slice of data to tune.
+        dynamic_tuning (bool or int): default False.
+            whether to dynamically tune the forecast (meaning AR terms will be propogated with predicted values).
+            if True, evaluates dynamically over the entire out-of-sample slice of data.
+            if int, window evaluates over that many steps (2 for 2-step dynamic forecasting, 12 for 12-step, etc.).
+            setting this to False or 1 means faster performance, 
+            but gives a less-good indication of how well the forecast will perform out x amount of periods.
+        dynamic_testing (bool or int):
+            whether to dynamically test the forecast (meaning AR terms will be propogated with predicted values).
+            if True, evaluates dynamically over the entire out-of-sample slice of data.
+            if int, window evaluates over that many steps (2 for 2-step dynamic forecasting, 12 for 12-step, etc.).
+            setting this to False or 1 means faster performance, 
+            but gives a less-good indication of how well the forecast will perform out x amount of periods.
+        probabilistic (bool, str, or list-like): default False.
+            if bool, whether to use a probabilistic forecasting process to set confidence intervals for all models.
+            if str, the name of a single model to apply a probabilistic process to.
+            if list-like, a list of models to apply a probabilistic process to.
+        n_iter (int): default 20.
+            how many iterations to use in probabilistic forecasting. ignored if probabilistic = False.
+        summary_stats (bool): default False.
+            whether to save summary stats for the models that offer those.
+            does not work for `MVForecaster` objects.
+        feature_importance (bool): default False.
+            whether to save permutation feature importance information for the models that offer those.
+            does not work for `MVForecaster` objects.
+        fi_method (str): one of {'pfi','shap'}, default 'pfi'.
+            the type of feature importance to save for the models that support it.
+            ignored if feature_importance is False.
+            does not work for `MVForecaster` objects.
+        limit_grid_size (int or float): optional. pass an argument here to limit each of the grids being read.
+            see https://scalecast.readthedocs.io/en/latest/Forecaster/Forecaster.html#src.scalecast.Forecaster.Forecaster.limit_grid_size
+        suffix (str): optional. a suffix to add to each model as it is evaluate to differentiate them when called
+            later. if unspecified, each model can be called by its estimator name.
+        **cvkwargs: passed to the cross_validate() method.
+
+    Returns:
+        None
+    """
+    if (
+        len([m for m in models if m not in [m for m in _estimators_ if m != "combo"]])
+        > 0
+    ):
+        raise ValueError(
+            "values passed to models must be list-like and in {}".format(
+                [m for m in _estimators_ if m != "combo"]
+            )
+        )
+    for m in log_progress(models):
+        m_prob = (
+            probabilistic if isinstance(probabilistic,bool) 
+            else m == probabilistic if isinstance(probabilistic,str) 
+            else m in probabilistic
+        )
+        call_me = m if suffix is None else m + suffix
+        f.set_estimator(m)
+        if limit_grid_size is not None:
+            f.ingest_grid(m)
+            f.limit_grid_size(limit_grid_size)
+        if cross_validate:
+            f.cross_validate(dynamic_tuning=dynamic_tuning, **cvkwargs)
+        else:
+            f.tune(dynamic_tuning=dynamic_tuning)
+        f.auto_forecast(
+            dynamic_testing=dynamic_testing,
+            call_me=call_me,
+            probabilistic=m_prob,
+            n_iter=n_iter,
+        )
+
+        if summary_stats:
+            f.save_summary_stats()
+        if feature_importance:
+            f.save_feature_importance(fi_method)
