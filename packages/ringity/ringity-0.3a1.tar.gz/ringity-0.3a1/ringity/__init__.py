a3f36774b3068e0abf81cd8c2882d6d671bc30bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:20 +0000

    Add package ringity/0.3a1/ringity-0.3a1.tar.gz

---
+++ packages/ringity/ringity-0.3a1.tar.gz/ringity-0.3a1/ringity/__init__.py
@@ -0,0 +1,48 @@
+name = "ringity"
+__author__ = "Markus K. Youssef"
+__version__ = "0.3a1"
+
+# FUNCTIONS
+from ringity.core import (
+                    pdiagram, 
+                    pdiagram_from_network,
+                    pdiagram_from_point_cloud,
+                    pdiagram_from_distance_matrix,
+                    
+                    ring_score,
+                    ring_score_from_network,
+                    ring_score_from_point_cloud,
+                    ring_score_from_distance_matrix,
+
+                    ring_score_from_sequence,
+                    ring_score_from_pdiagram,
+
+                    pwdistance,
+                    pwdistance_from_network,
+                    pwdistance_from_point_cloud,
+                    pwdistance_from_adjacency_matrix
+                    )
+
+# MODULES
+from ringity.core import (statistics)
+
+from ringity.generators import (
+                    random_pdgm,
+                    network_model,
+                    )
+import ringity.generators.geometric_networks as geometric_networks
+
+from ringity.plotting import (plot, plot_nx, plot_dgm)
+
+from ringity.readwrite.pdiagrams import (write_pdiagram, read_pdiagram)
+
+from ringity.singlecell import singlecell
+from ringity.classes.pdiagram import PDiagram
+
+import sys
+# Check for appropriate Python version
+major, minor = sys.version_info[:2]
+if (major, minor) < (3, 6):
+    msg = f'Python 3.6 or later is required for ringity ' \
+          f'({major}.{minor} detected).'
+    raise ImportError(msg)
