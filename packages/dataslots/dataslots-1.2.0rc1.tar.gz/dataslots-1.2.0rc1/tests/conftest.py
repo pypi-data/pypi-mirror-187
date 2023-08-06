d6e44fc0ea5cc3a1eeac5794d1fc7e876da7d2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:40 +0000

    Add package dataslots/1.2.0rc1/dataslots-1.2.0rc1.tar.gz

---
+++ packages/dataslots/dataslots-1.2.0rc1.tar.gz/dataslots-1.2.0rc1/tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+class Assertions:
+    @staticmethod
+    def assert_not_member(name, obj):
+        assert name not in dir(obj), 'Property {} found'.format(name)
+
+    @staticmethod
+    def assert_member(name, obj):
+        assert name in dir(obj), 'Property {} not found'.format(name)
+
+    @staticmethod
+    def assert_assign_variable(instance, name='random_variable'):
+        try:
+            setattr(instance, name, 10)
+        except AttributeError:
+            assert False, 'Cannot add variable to class "{}" with __dict__'.format(instance.__class__)
+
+    @staticmethod
+    def assert_slots(class_or_instance, slots):
+        assert sorted(class_or_instance.__slots__) == sorted(slots)
+
+    @staticmethod
+    def assert_init_raises(datacls, *args, exception, msg):
+        with pytest.raises(exception) as exc_info:
+            datacls(*args)
+        assert exc_info.match(msg)
+
+
+@pytest.fixture
+def assertions():
+    return Assertions()
