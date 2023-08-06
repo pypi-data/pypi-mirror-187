07b084ff90b2ac56968045acb919ef94f7221b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:49 +0000

    Add package nonebot-adapter-bilibili/0.0.3/nonebot-adapter-bilibili-0.0.3.tar.gz

---
+++ packages/nonebot-adapter-bilibili/nonebot-adapter-bilibili-0.0.3.tar.gz/nonebot-adapter-bilibili-0.0.3/nonebot/adapters/bilibili/bot.py
@@ -0,0 +1,24 @@
+import asyncio
+from typing import (
+    Any,
+    Union,
+)
+
+from nonebot.adapters import Bot as BaseBot
+from nonebot.typing import overrides
+
+from .event import Event
+from .message import Message, MessageSegment
+
+
+class Bot(BaseBot):
+
+    @overrides(BaseBot)
+    async def send(
+            self,
+            event: Event,
+            message: Union[str, Message, MessageSegment],
+            **kwargs,
+    ) -> Any:
+        asyncio.create_task(self.adapter.bili.send(message))
+        ...
