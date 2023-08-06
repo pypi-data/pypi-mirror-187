4957d73d8c6c89e38cb5d35bf7c37439df98e11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:21 +0000

    Add package datadog-lambda/4.67.0/datadog_lambda-4.67.0.tar.gz

---
+++ packages/datadog-lambda/datadog_lambda-4.67.0.tar.gz/datadog_lambda-4.67.0/datadog_lambda/__init__.py
@@ -0,0 +1,14 @@
+# The minor version corresponds to the Lambda layer version.
+# E.g.,, version 0.5.0 gets packaged into layer version 5.
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
+
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.getLevelName(os.environ.get("DD_LOG_LEVEL", "INFO").upper()))
