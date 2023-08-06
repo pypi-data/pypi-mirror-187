06e9ab67b549dd0089056008ec9a8e4a43fce3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:33 +0000

    Add package streamlit-on-Hover-tabs-ven/2.0.3/streamlit-on-Hover-tabs-ven-2.0.3.tar.gz

---
+++ packages/streamlit-on-Hover-tabs-ven/streamlit-on-Hover-tabs-ven-2.0.3.tar.gz/streamlit-on-Hover-tabs-ven-2.0.3/st_on_hover_tabs_ven/frontend/build/precache-manifest.d91e326022dd6d475d93cf0e5b12cca9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd556670ebaf8cc31aa0b58a89c8a2c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a68e54998180a1a3938f",
+    "url": "./static/css/2.0a4cb73c.chunk.css"
+  },
+  {
+    "revision": "9a573a98ba8ec2f82985",
+    "url": "./static/css/main.db61388b.chunk.css"
+  },
+  {
+    "revision": "a68e54998180a1a3938f",
+    "url": "./static/js/2.f1e083b5.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.f1e083b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a573a98ba8ec2f82985",
+    "url": "./static/js/main.86be0011.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
