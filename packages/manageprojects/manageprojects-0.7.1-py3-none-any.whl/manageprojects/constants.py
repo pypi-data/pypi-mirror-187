e69a54ce63d898f2a6f8e8491a4efa64a3f90e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:15 +0000

    Add package manageprojects/0.7.1/manageprojects-0.7.1-py3-none-any.whl

---
+++ packages/manageprojects/manageprojects-0.7.1-py3-none-any.whl/manageprojects/constants.py
@@ -0,0 +1,8 @@
+INITIAL_REVISION = 'initial_revision'
+INITIAL_DATE = 'initial_date'
+APPLIED_MIGRATIONS = 'applied_migrations'
+COOKIECUTTER_TEMPLATE = 'cookiecutter_template'
+COOKIECUTTER_DIRECTORY = 'cookiecutter_directory'
+COOKIECUTTER_CONTEXT = 'cookiecutter_context'
+
+CLI_EPILOG = 'Project Homepage: https://github.com/jedie/manageprojects'
