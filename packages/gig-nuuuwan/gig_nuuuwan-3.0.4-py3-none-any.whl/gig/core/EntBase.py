44e16a04a8d430c799684a24e67f507871bad457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:47 +0000

    Add package gig-nuuuwan/3.0.4/gig_nuuuwan-3.0.4-py3-none-any.whl

---
+++ packages/gig-nuuuwan/gig_nuuuwan-3.0.4-py3-none-any.whl/gig/core/EntBase.py
@@ -0,0 +1,30 @@
+class EntBase:
+    def __init__(self, d):
+        self.d = d
+
+    @property
+    def id(self):
+        return self.d["id"]
+
+    @property
+    def name(self):
+        return self.d["name"]
+
+    def is_parent_id(self, cand_parent_id: str):
+        return cand_parent_id in self.id
+
+    def __getattr__(self, key: str):
+        if key in self.d.keys():
+            return self.d.get(key)
+        raise AttributeError
+
+    def __str__(self):
+        return str(self.d)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        if isinstance(other, EntBase):
+            return self.d == other.d
+        return False
