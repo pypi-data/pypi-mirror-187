444b88bbb561cf4fe02a38e19e22363727dec07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:05 +0000

    Add package txl-cell/0.1.6/txl_cell-0.1.6.tar.gz

---
+++ packages/txl-cell/txl_cell-0.1.6.tar.gz/txl_cell-0.1.6/txl_cell/components.py
@@ -0,0 +1,46 @@
+from typing import Any, Dict, List
+
+from asphalt.core import Component, Context
+
+from txl.base import Cell, CellFactory
+from txl.hooks import register_component
+
+
+class _Cell(Cell):
+    """A cell."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.source = kwargs.pop("source", [])
+        self.outputs = kwargs.pop("outputs", [])
+
+    @property
+    def source(self) -> List[str]:
+        return self.source
+
+    @source.setter
+    def source(self, value: List[str]):
+        self.source = value
+
+    @property
+    def outputs(self) -> List[Dict[str:Any]]:
+        return self.outputs
+
+    @outputs.setter
+    def outputs(self, value: List[Dict[str:Any]]):
+        self.outputs = value
+
+
+class CellFactoryComponent(Component):
+    async def start(
+        self,
+        ctx: Context,
+    ) -> None:
+        def cell_factory(
+            source: List[str] = "", outputs: List[Dict[str:Any]] = ""
+        ) -> Cell:
+            return _Cell(source=source, outputs=outputs)
+
+        ctx.add_resource(cell_factory, name="cell_factory", types=CellFactory)
+
+
+c = register_component("cell", CellFactoryComponent)
