a5a1fdce33249a165550204cd77b7ea99c132483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:38 +0000

    Add package render-engine-tailwindcss/2023.1.2a1/render_engine_tailwindcss-2023.1.2a1.tar.gz

---
+++ packages/render-engine-tailwindcss/render_engine_tailwindcss-2023.1.2a1.tar.gz/render_engine_tailwindcss-2023.1.2a1/src/render_engine_tailwindcss/plugins.py
@@ -0,0 +1,27 @@
+from render_engine.hookspecs import hook_impl
+import pytailwindcss
+import pathlib
+
+
+def parse_css_files(
+        static_path: str | pathlib.Path,
+        output_path: str|pathlib.Path,
+        ) -> None:
+
+    """Parses the CSS files in the directory and creates a file for each in the output_path"""
+    for file in pathlib.Path(static_path).glob("**/*.css"):
+        output_file = pathlib.Path(output_path).joinpath(static_path).joinpath(file.relative_to(static_path)).absolute()
+        pytailwindcss.run(
+            auto_install=True,
+            tailwindcss_cli_args=[
+                "--input",
+                f"{file.absolute()}",
+                "--output",
+                output_file,
+            ],
+        )
+
+class TailwindCSS:
+    @hook_impl
+    def post_build_site(site: "Site"):
+        parse_css_files(site.static_path, site.output_path)
