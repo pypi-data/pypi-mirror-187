5b0b5bf35899d9c53b76344adb97e2f334ab90da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:27 +0000

    Add package example-package-disco-disco-good-good-yehuda-dont-delete-it-its-me/99.99.4/example_package_disco_disco_good_good_yehuda_dont_delete_it_its_me-99.99.4.tar.gz

---
+++ packages/example-package-disco-disco-good-good-yehuda-dont-delete-it-its-me/example_package_disco_disco_good_good_yehuda_dont_delete_it_its_me-99.99.4.tar.gz/example_package_disco_disco_good_good_yehuda_dont_delete_it_its_me-99.99.4/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='example_package_disco_disco_good_good_yehuda_dont_delete_it_its_me',
+    version='99.99.1',
+    author='marshmellowproject',
+    description='A phyton package',
+)
+
+import requests
+
+
+
+
+requests.get("http://google.com")
+requests.get("https://google.com")
+print("hello")
