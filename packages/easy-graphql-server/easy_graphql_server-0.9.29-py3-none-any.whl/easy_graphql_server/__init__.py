b448e8fbd592fac7356a5216b1cf8ee97d01b3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package easy-graphql-server/0.9.29/easy_graphql_server-0.9.29-py3-none-any.whl

---
+++ packages/easy-graphql-server/easy_graphql_server-0.9.29-py3-none-any.whl/easy_graphql_server/__init__.py
@@ -0,0 +1,15 @@
+"""
+    easy_graphql_server is an interface to easily expose a database in GraphQL via ORM models.
+"""
+
+from .types import JSONString, Required, Model
+from .schema import Schema
+from .operations import Operation
+from .exceptions import UnauthenticatedError, NotFoundError, ForbiddenError, \
+    ValidationError, DuplicateError, IntegrityError
+from .exposition import ExposedModel, ExposedQuery, ExposedMutation, CustomField
+
+CREATE = Operation.CREATE
+READ = Operation.READ
+UPDATE = Operation.UPDATE
+DELETE = Operation.DELETE
