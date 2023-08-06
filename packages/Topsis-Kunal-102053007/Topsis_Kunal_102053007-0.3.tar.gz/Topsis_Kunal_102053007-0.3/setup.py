f44e4d97e58a0a5f7d9795a0a31458ecd9ebc7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:30 +0000

    Add package Topsis-Kunal-102053007/0.3/Topsis_Kunal_102053007-0.3.tar.gz

---
+++ packages/Topsis-Kunal-102053007/Topsis_Kunal_102053007-0.3.tar.gz/Topsis_Kunal_102053007-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(name="Topsis_Kunal_102053007",version="0.3",
+description="This is a topsis package of version 0.3",
+long_description=long_description,
+    long_description_content_type="text/markdown",
+author="Kunal Madan",
+author_email="kmadan_bemba20@thapar.edu",
+packages=['Topsis_Kunal_102053007'],
+install_requires=['pandas'],
+include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "topsis=Topsis_Kunal_102053007.Kunal_102053007:main",
+        ]
+    }
+)
+
