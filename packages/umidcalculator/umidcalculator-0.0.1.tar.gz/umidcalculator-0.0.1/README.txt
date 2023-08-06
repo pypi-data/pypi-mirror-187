da21801c2eab145040b6ef4e7e89978494ebd587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:00 +0000

    Add package umidcalculator/0.0.1/umidcalculator-0.0.1.tar.gz

---
+++ packages/umidcalculator/umidcalculator-0.0.1.tar.gz/umidcalculator-0.0.1/README.txt
@@ -0,0 +1,4 @@
+Bu juda oddiy calculator kutubxona
+
+from umidcalculator.calculator import add_numb,sub_numb,mult_numb,div_numb
+print(add_numb(10,10))
---
+++ packages/umidcalculator/umidcalculator-0.0.1.tar.gz/umidcalculator-0.0.1/umidcalculator/__init__.py
@@ -0,0 +1,14 @@
+def add_numb(num1, num2):
+    return num1 + num2
+
+
+def sub_numb(num1, num2):
+    return num1 - num2
+
+
+def mult_numb(num1, num2):
+    return num1 * num2
+
+
+def div_numb(num1, num2):
+    return num1 / num2
