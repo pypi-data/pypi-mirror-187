51e7952d55536deea4778ce75ca71176f8bc76b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:57 +0000

    Add package kelimepy/0.2.1/kelimepy-0.2.1.tar.gz

---
+++ packages/kelimepy/kelimepy-0.2.1.tar.gz/kelimepy-0.2.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+
+VERSION = '0.2.1'
+DESCRIPTION = 'Türkçe sözlükten bir kelimenin anlamını almak için bir paket'
+LONG_DESCRIPTION = 'Türkçe sözlükten bir kelimenin anlamını almak için bir paket'
+AUTHOR = 'Hüseyin Averbek'
+
+setup(
+    name="kelimepy",
+    version=VERSION,
+    author=AUTHOR,
+    author_email="<huseyinaverbek@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    package_dir={"": "src"},
+    packages=find_packages(where="src", include=["kelimepy", "kelimepy.*"]),
+    install_requires=['requests'],
+    keywords=['python', 'kelime', 'anlam', 'sözlük', 'türkçe'],
+    classifiers= [
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
