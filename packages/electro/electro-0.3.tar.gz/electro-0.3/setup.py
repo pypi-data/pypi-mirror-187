ba44381c081cfcb8f94da9d5d22d66e494b533c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:00:44 +0000

    Add package electro/0.3/electro-0.3.tar.gz

---
+++ packages/electro/electro-0.3.tar.gz/electro-0.3/setup.py
@@ -0,0 +1,12 @@
+import setuptools 
+setuptools.setup(
+    name='electro', 
+    version='0.3', 
+    author="ADITYA PANAYAL", 
+    author_email="adpanayal@gmail.com", 
+    description="A simple calculator", 
+    packages=setuptools.find_packages(), 
+    classifiers=[ "Programming Language :: Python :: 3", 
+    "License :: OSI Approved :: MIT License", 
+    "Operating System :: OS Independent", ], 
+)
