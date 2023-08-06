5c84734a3bb84e509513e0959c60168f9d096688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:40 +0000

    Add package BDevManager2/0.0.5/BDevManager2-0.0.5.tar.gz

---
+++ packages/BDevManager2/BDevManager2-0.0.5.tar.gz/BDevManager2-0.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='BDevManager2',
+    version='0.0.5',
+    author='John Doe',
+    author_email='johndoe@example.com',
+    description='A business development manager library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/BlackFoxgamingstudio/andrew_bdm.git',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'google-auth',
+        'google-auth-oauthlib',
+        'google-auth-httplib2',
+        'google-api-python-client',
+       
+    ],
+)
