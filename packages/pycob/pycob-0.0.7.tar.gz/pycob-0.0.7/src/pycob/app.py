8a50a0056ca8bc635bc0bdfbf65883cbb02a2d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:00 +0000

    Add package pycob/0.0.7/pycob-0.0.7.tar.gz

---
+++ packages/pycob/pycob-0.0.7.tar.gz/pycob-0.0.7/src/pycob/app.py
@@ -0,0 +1,58 @@
+import flask
+from .all_components import Page
+from .request import Request
+from .handler import Handler
+import inspect
+
+demo_page = Page('Demo Page')
+demo_page.add_header('Demo Page', "2")
+
+class App:
+    flask_app = None
+
+    def __init__(self, name: str, app_nav=[]):
+        self.flask_app = flask.Flask(__name__)
+        self.name = name
+        self.nav = app_nav
+        self.pages = {}
+        self.flask_app.add_url_rule('/favicon.ico', 'favicon.ico', redirect_to="https://cdn.pycob.com/favicon.ico")
+
+    def add_page(self, route: str, page_name: str, page_function, show_in_navbar=True, footer_category="All Pages"):
+        endpoint_name = _strip_slashes(route)
+        self.pages[endpoint_name] = {"page_name": page_name, "show_in_navbar": show_in_navbar, "footer_category": footer_category}
+        self.flask_app.add_url_rule("/" + endpoint_name, endpoint_name, Handler(self, page_function), methods=["GET", "POST"])
+
+    def run(self, port=8080, force_dev_mode=False):
+        caller = inspect.currentframe().f_back
+        called_from_module = caller.f_globals['__name__']
+        print("Called from module ", called_from_module)
+        if called_from_module == "__main__" or force_dev_mode:
+            self.flask_app.run(debug=True, host='0.0.0.0', port=port)
+            return None
+        else:
+            print(__name__)
+            print("App is being run by a production server. Returning a runnable server object.")
+            return self.flask_app
+
+
+def _strip_slashes_from_pages_dict(pages_dict: dict) -> dict:
+    new_pages_dict = {}
+    for key, value in pages_dict.items():
+        new_pages_dict[_strip_prefix_suffix(key, '/', '/')] = value
+    return new_pages_dict
+
+def _strip_slashes(text: str) -> str:
+    return _strip_prefix_suffix(text, '/', '/')
+
+def _strip_prefix(text: str, prefix: str) -> str:
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
+def _strip_suffix(text: str, suffix: str) -> str:
+    if text.endswith(suffix):
+        return text[:-len(suffix)]
+    return text
+
+def _strip_prefix_suffix(text: str, prefix: str, suffix: str) -> str:
+    return _strip_suffix(_strip_prefix(text, prefix), suffix)
