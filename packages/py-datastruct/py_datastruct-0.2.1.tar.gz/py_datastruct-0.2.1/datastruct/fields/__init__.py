91ff4ba6f60d2301a8c6c4318c81234821a4ec9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:47 +0000

    Add package py-datastruct/0.2.1/py_datastruct-0.2.1.tar.gz

---
+++ packages/py-datastruct/py_datastruct-0.2.1.tar.gz/py_datastruct-0.2.1/datastruct/fields/__init__.py
@@ -0,0 +1,56 @@
+#  Copyright (c) Kuba Szczodrzyński 2023-1-7.
+
+from .helper import (
+    bitfield,
+    buffer_end,
+    buffer_start,
+    checksum_end,
+    checksum_field,
+    checksum_start,
+    const_into,
+    eval_into,
+    hook_end,
+    packing,
+    probe,
+    tell,
+    tell_into,
+    unpacking,
+    validate,
+    virtual,
+)
+from .special import action, align, alignto, hook, padding, seek, skip
+from .standard import built, field, subfield
+from .wrapper import adapter, cond, repeat, switch
+
+__all__ = [
+    "action",
+    "adapter",
+    "align",
+    "alignto",
+    "bitfield",
+    "buffer_end",
+    "buffer_start",
+    "built",
+    "checksum_end",
+    "checksum_field",
+    "checksum_start",
+    "cond",
+    "const_into",
+    "eval_into",
+    "field",
+    "hook",
+    "hook_end",
+    "packing",
+    "padding",
+    "probe",
+    "repeat",
+    "seek",
+    "skip",
+    "subfield",
+    "switch",
+    "tell",
+    "tell_into",
+    "unpacking",
+    "validate",
+    "virtual",
+]
