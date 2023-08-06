9faa3253e6bdfaf447f11adfe3b3241ab40927a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:12 +0000

    Add package hexacolors/0.6.2/hexacolors-0.6.2.tar.gz

---
+++ packages/hexacolors/hexacolors-0.6.2.tar.gz/hexacolors-0.6.2/hexacolors/online/client/clientlogin.py
@@ -0,0 +1,16 @@
+import requests
+
+class ClientAPI:
+
+    def __init__(
+            self,
+            type: str,
+            value: str,
+        ) -> int:
+
+        self.url = 'https://www.thecolorapi.com/id?{}={}'.format(type, value)
+    
+    def get(self):
+
+        res = requests.get(self.url).json()
+        return res['hex']['clean']
