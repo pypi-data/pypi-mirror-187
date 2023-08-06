f0b9300432d3d6a0e9f1b5c743f3ebbf67c3417f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:06 +0000

    Add package rakesh/0.1/rakesh-0.1.tar.gz

---
+++ packages/rakesh/rakesh-0.1.tar.gz/rakesh-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rakesh',
+    version='0.1',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/rakeshbandeppa/rakesh',
+    packages=find_packages(),
+    install_requires=[],
+)
