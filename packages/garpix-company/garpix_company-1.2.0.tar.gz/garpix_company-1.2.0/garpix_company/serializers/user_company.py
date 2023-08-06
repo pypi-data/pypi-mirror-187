a2495d99598f84bd1dd7daf7dfd5975fcfcf378e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:28 +0000

    Add package garpix-company/1.2.0/garpix_company-1.2.0.tar.gz

---
+++ packages/garpix-company/garpix_company-1.2.0.tar.gz/garpix_company-1.2.0/garpix_company/serializers/user_company.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.utils.module_loading import import_string
+from rest_framework import serializers
+
+from garpix_company.models.user_company import UserCompany
+
+
+UserSerializer = import_string(getattr(settings, 'GARPIX_COMPANY_USER_SERIALIZER', 'garpix_company.serializers.GarpixCompanyUserSerializer'))
+RoleSerializer = import_string(getattr(settings, 'GARPIX_COMPANY_ROLE_SERIALIZER', 'garpix_company.serializers.GarpixCompanyRoleSerializer'))
+
+
+class UserCompanySerializer(serializers.ModelSerializer):
+
+    user = UserSerializer()
+    role = RoleSerializer()
+
+    class Meta:
+        model = UserCompany
+        fields = '__all__'
