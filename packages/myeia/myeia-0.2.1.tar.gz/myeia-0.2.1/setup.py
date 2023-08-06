fd6af3a67b59a3624092895aba58f6e82bf0da7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:38 +0000

    Add package myeia/0.2.1/myeia-0.2.1.tar.gz

---
+++ packages/myeia/myeia-0.2.1.tar.gz/myeia-0.2.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+from src.version import __version__
+
+with open("README.md", "r") as ld:
+    long_description = ld.read()
+
+setuptools.setup(
+    name="myeia",
+    version=__version__,
+    url="https://github.com/philsv/myeia",
+    license="MIT",
+    author="philsv",
+    author_email="frphsv@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=["pandas", "requests", "python-dotenv"],
+    keywords=["eia", "eia-api", "open-data", "python"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
