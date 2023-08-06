1e086907600c2d5d8646fb02a2189e843e5c5534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:40 +0000

    Add package super-cereal/0.1.1/super_cereal-0.1.1.tar.gz

---
+++ packages/super-cereal/super_cereal-0.1.1.tar.gz/super_cereal-0.1.1/super_cereal/cerealizer/json.py
@@ -0,0 +1,41 @@
+import typing
+
+from super_cereal.cerealizer import Cerealizer, T, TheTypeRegistry
+from super_cereal.cerealizer.builtins import PassthruCerealizer, ListCerealizer, UnionCerealizer, DictCerealizer
+
+JsonTypes = typing.Union[str, float, int, bool, type(None), list, dict]
+
+
+class JsonCerealizer(Cerealizer[T, JsonTypes]):
+    def __init__(self):
+        registry = TheTypeRegistry()
+        registry[type(None)] = PassthruCerealizer()
+        registry[str] = PassthruCerealizer()
+        registry[float] = PassthruCerealizer()
+        registry[int] = PassthruCerealizer()
+        registry[bool] = PassthruCerealizer()
+        registry[list] = ListCerealizer()
+        registry[typing.List] = ListCerealizer()
+        registry[typing.Union] = UnionCerealizer()
+        registry.default = DictCerealizer()
+
+        self.registry = registry
+
+    def serialize(self, obj: any, expected_type: T = None) -> JsonTypes:
+        if expected_type is None:
+            expected_type = type(obj)
+
+        if expected_type in self.registry:
+            return self.registry[expected_type].serialize(obj, expected_type)
+        if typing.get_origin(expected_type) in self.registry:
+            return self.registry[typing.get_origin(expected_type)].serialize(obj, expected_type)
+
+        return self.registry.default.serialize(obj, expected_type)
+
+    def deserialize(self, obj: JsonTypes, t: T) -> T:
+        if t in self.registry:
+            return self.registry[t].deserialize(obj, t)
+        if typing.get_origin(t) in self.registry:
+            return self.registry[typing.get_origin(t)].deserialize(obj, t)
+
+        return self.registry.default.deserialize(obj, t)
