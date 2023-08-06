25cc9829bee5ff7121ba57d1edbc0a929e489bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:22 +0000

    Add package pyqt-custom-titlebar-window/0.0.49/pyqt-custom-titlebar-window-0.0.49.tar.gz

---
+++ packages/pyqt-custom-titlebar-window/pyqt-custom-titlebar-window-0.0.49.tar.gz/pyqt-custom-titlebar-window-0.0.49/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name='pyqt-custom-titlebar-window',
+    version='0.0.49',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='PyQt custom titlebar window (resizable, movable, minimize/maximize/close, etc.)',
+    url='https://github.com/yjg30737/pyqt-custom-titlebar-window.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=[
+        'PyQt5>=5.15',
+        'pyqt-frameless-window==0.0.61',
+        'pyqt-windows-buttons-widget>=0.0.1',
+        'pyqt-mac-buttons-widget>=0.0.1',
+        'pyqt-top-titlebar-widget>=0.0.1',
+        'pyqt-svg-label>=0.0.1'
+    ]
+)
