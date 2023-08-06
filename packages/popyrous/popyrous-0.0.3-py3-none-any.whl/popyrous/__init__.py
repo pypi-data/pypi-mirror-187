b4208cca27267b6301367da8e530df9cbb51748f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:52 +0000

    Add package popyrous/0.0.3/popyrous-0.0.3-py3-none-any.whl

---
+++ packages/popyrous/popyrous-0.0.3-py3-none-any.whl/popyrous/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.3"
+
+_POPYROUS_IMPORT_EVERYTHING_ELSE_ = True
+
+if _POPYROUS_IMPORT_EVERYTHING_ELSE_:
+    from . matlab import *
+    from . packages import *
+    from . web import *
+    from . zipfiles import *
+    from . timeseries import *
+    from . utils import *
