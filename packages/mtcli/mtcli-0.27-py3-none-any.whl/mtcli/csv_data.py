2d18949c163ef173f2d18358bc2c7ae3acc5189d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:23:04 +0000

    Add package mtcli/0.27/mtcli-0.27-py3-none-any.whl

---
+++ packages/mtcli/mtcli-0.27-py3-none-any.whl/mtcli/csv_data.py
@@ -0,0 +1,30 @@
+# mtcli
+# Copyright 2023 Valmir França da Silva
+# http://github.com/vfranca
+import csv
+
+
+class Data:
+
+    data = []
+
+    def __init__(self, csv_file):
+        raise NotImplementedError
+
+    def __iter__(self):
+        raise NotImplementedError
+
+    def __len__(self):
+        return len(self.data)
+
+
+class Rates(Data):
+    def __init__(self, csv_file):
+        with open(csv_file, encoding="utf-16", newline="") as f:
+            lines = csv.reader(f, delimiter=",", quotechar="'")
+            for line in lines:
+                self.data.append(line)
+
+    def __iter__(self):
+        for item in self.data:
+            yield item
