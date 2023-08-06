6981286a0c79fa9e891b7fa9ceaf29fff44b8136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:07 +0000

    Add package rejected-peps/1.0.0.dev2/rejected_peps-1.0.0.dev2-cp39-cp39-win32.whl

---
+++ packages/rejected-peps/rejected_peps-1.0.0.dev2-cp39-cp39-win32.whl/rejected_peps/pep336.py
@@ -0,0 +1,43 @@
+"""\
+PEP INFO
+
+PEP 336 -- Make None callable
+Status: Rejected
+Created: 2004-10-28
+
+MODULE INFO
+
+This module adds NoneType that makes None callable,
+and instance none (None is a keyword). This breaks the `x is None` usage,
+so `isNone(obj)` checks if obj is None or none.
+
+REFERENCES
+
+PEP 336: <https://peps.python.org/pep-0336/>
+"""
+PEP = 336
+
+class _singleton(type):
+    # None is a singleton
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+class NoneType(metaclass=_singleton):
+    """Same as type(None), but callable."""
+    def __repr__(self) -> str:
+        return 'None'
+    __str__ = __repr__
+    def __call__(self, *args, **kwargs) -> 'NoneType':
+        """Return none."""
+        return self
+    def __eq__(self, other) -> bool:
+        return isinstance(other, type(self)) or other is None
+    def __hash__(self):
+        # none should be treated just like None, but callable.
+        return hash(None)
+none = NoneType()
+def isNone(object) -> bool:
+    """Return True if object is None or none, else False."""
+    return object is none or object is None
