908ee5e63d3c49fc71cfe71efd7da638649331ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:14 +0000

    Add package GRLMerger/0.0.16/GRLMerger-0.0.16.tar.gz

---
+++ packages/GRLMerger/GRLMerger-0.0.16.tar.gz/GRLMerger-0.0.16/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Setting up
+setup(
+    name="GRLMerger",
+    version='0.0.16',
+    description='GRL Merger is a package that allows to merge two GRL models that are written in TGRL syntax.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    py_modules=['grl_merger'],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'pandas', 
+        'statistics',
+        'neattext',
+        'nltk',
+        'sentence_transformers',        
+    ],
+    author="Nadeen AlAmoudi",
+    author_email="<nadeenamoudi1@gmail.com>",
+#    packages = setuptools.find_packages(where="src"),
+)
