4f8c0ea00392c60ccf35bf320dc1bac7474a3f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:06 +0000

    Add package dragg/2.1.0/dragg-2.1.0.tar.gz

---
+++ packages/dragg/dragg-2.1.0.tar.gz/dragg-2.1.0/dragg/devices/electric_vehicle.py
@@ -0,0 +1,97 @@
+import os
+import numpy as np
+import cvxpy as cp
+
+class EV:
+    def __init__(self, hems):
+        self.hems = hems 
+
+        # Define constants
+        self.ev_max_rate = 5 # kWh
+        self.ev_cap_total = 16 # kWh
+        self.ev_cap_min = 0 #0.2*self.ev_cap_total
+        self.ev_cap_max = self.ev_cap_total
+        self.ev_ch_eff = cp.Constant(0.95)
+        self.ev_disch_eff = cp.Constant(0.97)
+
+        # Define battery optimization variables
+        self.min_horizon = int(12 * self.hems.dt)
+        self.horizon = max(self.hems.horizon, self.min_horizon)
+        self.h_plus = self.horizon + 1
+        self.p_ev_ch = cp.Variable(self.horizon)
+        self.p_ev_disch = cp.Constant(self.horizon)
+        self.p_v2g = cp.Variable(self.horizon)
+        self.p_elec = cp.Variable(self.horizon)
+        self.e_ev = cp.Variable(self.h_plus)
+        self.ev_preference = cp.Constant(np.random.uniform(0.5,0.7))
+
+        self.e_ev_init = cp.Constant(16)
+        self.ev_override_profile = None
+
+        self.opt_keys = {'p_ev_ch', 'p_ev_disch', 'p_v2g', 'e_ev_opt', 'returning_horizon', 'leaving_horizon'}
+
+        self.override = False
+
+    def add_constraints(self, enforce_bounds=True):
+        """
+        Creates constraints that make the battery act as an EV with charge/discharge constraints
+        based on occupancy and travel distance.
+        
+        :return: None
+        """
+        trip_mi = 41 # round trip avg (home to work)
+        full_charge = 150 # low end for nissan leaf
+        min_daily_soc = 41/150
+        ev_batt_cap = 16 # kWh
+        e_disch_trip = min_daily_soc * ev_batt_cap
+        p_disch_trip = -1 * e_disch_trip * self.hems.dt 
+
+        self.p_ev_disch = cp.Constant([p_disch_trip/2 if i in [self.hems.today_leaving, self.hems.today_returning, self.hems.tomorrow_leaving, self.hems.tomorrow_returning] else 0 for i in self.hems.subhour_of_day_current[:self.horizon]])
+        self.v2g_max = -1 * np.multiply(self.ev_max_rate, self.hems.occ_current[:self.horizon])
+        self.p_ch_max = np.multiply(self.ev_max_rate, self.hems.occ_current[:self.horizon])
+
+        cons = [
+            self.p_v2g <= 0,
+            self.p_v2g >= self.v2g_max,
+            self.p_v2g == 0,
+            self.e_ev[1:] == self.e_ev[:-1]
+                                        + (self.ev_ch_eff * self.p_ev_ch
+                                        + self.p_ev_disch / self.ev_disch_eff
+                                        + self.p_v2g / self.ev_disch_eff) / self.hems.dt,
+            self.e_ev[0] == self.hems.e_ev_init,
+            self.p_ev_ch <= self.ev_max_rate,
+            self.p_ev_ch >= 0,
+            self.e_ev[1:] <= self.ev_cap_max,
+            self.e_ev[1:] >= self.ev_cap_min,
+            self.p_elec == self.p_ev_ch - self.p_v2g
+        ]
+
+        if enforce_bounds:
+            cons += [
+                self.p_ev_ch <= self.p_ch_max,
+                self.e_ev >= self.ev_preference * self.ev_cap_max
+            ]
+
+        return cons
+
+    def resolve(self):
+        cons = self.add_constraints()
+        obj = cp.Minimize(cp.sum(self.p_elec))
+        prob = cp.Problem(obj, cons)
+        prob.solve(solver=self.hems.solver)
+
+        if not prob.status == "optimal":
+            cons = self.add_constraints(enforce_bounds=False)
+            obj = cp.Minimize(1)
+            prob = cp.Problem(obj, cons)
+            prob.solve(solver=self.hems.solver)
+            print(prob.status)
+
+    def override_charge(self, cmd):
+        self.obj = cp.Variable(1)
+        if cmd >= 0:
+            cons = [self.obj == cmd * self.ev_max_rate - self.p_ev_ch]
+        else:
+            cons = [self.obj == cmd * self.ev_max_rate - self.p_v2g]
+        return cons
+
---
+++ packages/dragg/dragg-2.1.0.tar.gz/dragg-2.1.0/dragg/devices/water_heater.py
@@ -0,0 +1,127 @@
+import os
+import numpy as np
+import cvxpy as cp
+
+class WH:
+    """
+    A class for the water heater device in a smart home. This model uses a linear R1-C1 model 
+    of the water heater tank with electric resistance heating (efficiency ~=100%)
+    """
+    def __init__(self, hems):
+        self.hems = hems
+        self.temp_wh_ev = cp.Variable(self.hems.h_plus)
+        self.temp_wh = cp.Variable(1)
+        self.temp_wh0 = cp.Variable(1)
+        self.wh_heat_max = self.hems.sub_subhourly_steps
+        self.wh_heat_min = 0
+
+        # Water heater temperature constants
+        self.r = cp.Constant(float(self.hems.home["wh"]["r"]) * 1000)
+        self.p = cp.Constant(float(self.hems.home["wh"]["p"]) / self.hems.sub_subhourly_steps)
+        self.temp_wh_min = cp.Constant(float(self.hems.home["wh"]["temp_wh_min"]))
+        self.temp_wh_max = cp.Constant(float(self.hems.home["wh"]["temp_wh_max"]))
+        self.temp_wh_sp = cp.Constant(float(self.hems.home["wh"]["temp_wh_sp"]))
+        self.wh_size = float(self.hems.home["wh"]["tank_size"])
+        self.tap_temp = 15 # assumed cold tap water is about 55 deg F
+        wh_capacitance = self.wh_size * 4.2 # kJ/deg C
+        self.c = cp.Constant(wh_capacitance)
+
+        self.heat_on = cp.Variable(self.hems.horizon, integer=True)
+        self.p_elec = cp.Variable(self.hems.horizon)
+
+        self._get_water_draws()
+        self.opt_keys = {"waterdraws", "temp_wh_opt", "wh_heat_on_opt"}
+
+    def _get_water_draws(self):
+        """
+        :input: None
+        :return: None
+
+        A method to determine the current hot water consumption. Data based on NREL database (to cite)
+        """
+        daily_draw_sizes = 2 * self.hems.home["wh"]["draw_sizes"] # repeat list for 2 weeks
+
+        current_ts = [i % (24 * 7 * self.hems.dt) for i in range(self.hems.timestep, self.hems.timestep + self.hems.dt)]
+        current_draw_size_actual = [daily_draw_sizes[i] for i in current_ts]
+        future_ts = [i % (24 * 7 * self.hems.dt) for i in range(self.hems.timestep + self.hems.dt, self.hems.timestep + self.hems.h_plus)]
+        future_draw_size_ev = [np.average(daily_draw_sizes[i-self.hems.dt:i+self.hems.dt]) for i in future_ts]
+
+        self.draw_size = current_draw_size_actual + future_draw_size_ev
+        df = np.divide(self.draw_size, self.wh_size)
+        self.draw_frac = cp.Constant(df)
+        self.remainder_frac = cp.Constant(1-df)
+
+        return
+
+    def add_constraints(self, enforce_bounds=True):
+        """
+        :parameter enforce_bounds: boolean determines whether comfort bounds are strictly enforced
+        :return: cons, a list of CVXPY constraints
+
+        A method to introduce physical constraints to the water heater. 
+        """
+        self._get_water_draws()
+        cons = [
+            # Hot water heater contraints, expected value after approx waterdraws
+            self.temp_wh_ev[0] == self.hems.temp_wh_init,
+            self.temp_wh_ev[1:] == (cp.multiply(self.remainder_frac[1:],self.temp_wh_ev[:-1]) 
+                                + self.draw_frac[1:]*self.tap_temp)
+                                + 3600 * ((((self.hems.hvac.temp_in_ev[1:] 
+                                    - (cp.multiply(self.remainder_frac[1:],self.temp_wh_ev[:-1]) 
+                                    + self.draw_frac[1:]*self.tap_temp)) / self.r))
+                                    + self.heat_on * self.p) / (self.c * self.hems.dt),
+
+            self.temp_wh0 == self.hems.temp_wh_init
+                            + 3600 * (((self.hems.hvac.temp_in - self.hems.temp_wh_init) / self.r)
+                            + self.heat_on[0] * self.p) / (self.c * self.hems.dt),
+
+            self.temp_wh == ((self.temp_wh0 * (self.wh_size - self.draw_size[0])) + (self.tap_temp * self.draw_size[0])) / self.wh_size,
+
+            self.heat_on <= self.wh_heat_max,
+            self.heat_on >= self.wh_heat_min,
+            self.p_elec == self.heat_on * self.p]
+
+        if enforce_bounds:
+            cons += [
+                    self.temp_wh >= self.temp_wh_min,
+                    self.temp_wh <= self.temp_wh_max,
+                    self.temp_wh_ev >= self.temp_wh_min,
+                    self.temp_wh_ev <= self.temp_wh_max,
+                ]
+
+        return cons 
+
+    def resolve(self):
+        """
+        :input: None
+        :return: None
+
+        A method for re-solving the constraints specific to the hot water heater in the event
+        that the whole house HEMS cannot satisfy all constraints -- first attempts to minimize the 
+        device-specific electricity consumption while satisfying comfort bounds, second attempt 
+        minimizes the deviation of the new temperature and the desired setpoint.
+        """
+        cons = self.add_constraints()
+        obj = cp.Minimize(cp.abs(self.temp_wh - self.temp_wh_min))
+        prob = cp.Problem(obj, cons)
+        prob.solve(solver=self.hems.solver)
+
+        if not prob.status == 'optimal':
+            cons = self.add_constraints(enforce_bounds=False)
+            obj = cp.Minimize(cp.abs(self.temp_wh - self.temp_wh_min))
+            prob = cp.Problem(obj, cons)
+            prob.solve(solver=self.hems.solver, verbose=False)
+            print(prob.status)
+
+    def override_p_wh(self, cmd):
+        """
+        :parameter cmd: float in [-1,1]
+        :return: None
+        
+        A method to override the current on/off status of the hot water heater. Directly controls
+        the power consumed with a conservative check that the resulting water temperature will not
+        exceed bounds in either direction.
+        """
+        self.obj = cp.Variable(1)
+        cons = [self.obj == (2 * cmd + 1) - (self.heat_on / self.wh_heat_max)]
+        return cons 
