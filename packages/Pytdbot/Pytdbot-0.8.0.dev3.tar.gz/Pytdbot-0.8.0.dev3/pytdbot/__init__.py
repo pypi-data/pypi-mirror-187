fec3b856cc836f44257b553adf29c96cc885c474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:57 +0000

    Add package Pytdbot/0.8.0.dev3/Pytdbot-0.8.0.dev3.tar.gz

---
+++ packages/Pytdbot/Pytdbot-0.8.0.dev3.tar.gz/Pytdbot-0.8.0.dev3/pytdbot/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.8.0dev3"
+__copyright__ = "Copyright (c) 2023 AYMEN Mohammed ~ https://github.com/AYMENJD"
+
+VERSION = __version__
+
+from . import types, tdjson, utils, filters, exception
+from .client import Client
