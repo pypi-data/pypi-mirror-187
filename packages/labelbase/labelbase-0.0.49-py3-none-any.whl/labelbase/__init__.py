81ab8f38207fc2553ba17947ce9876c4dd768ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:20 +0000

    Add package labelbase/0.0.49/labelbase-0.0.49-py3-none-any.whl

---
+++ packages/labelbase/labelbase-0.0.49-py3-none-any.whl/labelbase/__init__.py
@@ -0,0 +1,8 @@
+name = "labelbase"
+__version__ = "0.0.48"
+
+from labelbase import converters
+from labelbase.uploaders import *
+from labelbase.metadata import *
+from labelbase.ontology import *
+from labelbase.dataset import *
