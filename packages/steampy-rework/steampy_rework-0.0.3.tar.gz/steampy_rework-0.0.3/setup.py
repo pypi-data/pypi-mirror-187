41775d1d0344467fe8881f1ade3c03212bae4576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:17 +0000

    Add package steampy-rework/0.0.3/steampy_rework-0.0.3.tar.gz

---
+++ packages/steampy-rework/steampy_rework-0.0.3.tar.gz/steampy_rework-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import sys
+
+if not sys.version_info[0] == 3 and sys.version_info[1] < 8:
+    sys.exit('Python < 3.8 is not supported')
+
+version = '0.0.3'
+
+setup(
+    name='steampy_rework',
+    packages=['steampy_rework', 'examples'],
+    version=version,
+    description='A Steam lib for trade automation',
+    author='Viacheslav Patokin',
+    author_email='viacheslavpatokin@gmail.com',
+    license='MIT',
+    url='https://github.com/wizzzz9',
+    download_url='https://github.com/wizzzz9/steampy_rework/tarball/' + version,
+    keywords=['steam', 'trade', 'steamapi'],
+    classifiers=[],
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "rsa"
+    ],
+)
