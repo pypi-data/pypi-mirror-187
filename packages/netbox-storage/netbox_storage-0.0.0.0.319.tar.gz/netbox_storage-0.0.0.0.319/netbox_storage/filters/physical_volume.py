38fb799365672f23e9b9d8312f64a48fc8e0468c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:28 +0000

    Add package netbox-storage/0.0.0.0.319/netbox_storage-0.0.0.0.319.tar.gz

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.319.tar.gz/netbox_storage-0.0.0.0.319/netbox_storage/filters/physical_volume.py
@@ -0,0 +1,23 @@
+from django.db.models import Q
+
+from netbox.filtersets import NetBoxModelFilterSet
+
+from netbox_storage.models import PhysicalVolume
+
+
+class PhysicalVolumeFilter(NetBoxModelFilterSet):
+    """Filter capabilities for PhysicalVolume instances."""
+
+    class Meta:
+        model = PhysicalVolume
+        fields = ["partition", "pv_name"]
+
+    def search(self, queryset, name, value):
+        """Perform the filtered search."""
+        if not value.strip():
+            return queryset
+        qs_filter = (
+            Q(partition__icontains=value)
+            | Q(pv_name__icontains=value)
+        )
+        return queryset.filter(qs_filter)
