bc20a46f0817a705a1b0a50821c1a8c6888d0ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:36 +0000

    Add package py-chan-calculator/0.0.1/py-chan-calculator-0.0.1.tar.gz

---
+++ packages/py-chan-calculator/py-chan-calculator-0.0.1.tar.gz/py-chan-calculator-0.0.1/pychancalculator/calculator.py
@@ -0,0 +1,17 @@
+class Calculator:
+
+    def add(self, num1, num2):
+        return num1 + num2
+
+    def difference(self, num1, num2):
+        return num1 - num2
+
+    def divide(self, num1, num2):
+        return num1 / num2
+
+    def mod(self, num1, num2):
+        return num1 % num2
+
+    def multi(self, num1, num2):
+        return num1 * num2
+
---
+++ packages/py-chan-calculator/py-chan-calculator-0.0.1.tar.gz/py-chan-calculator-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='py-chan-calculator',
+    version='0.0.1',
+    description='This is a simple calculator package',
+    url='https://github.com/ChanduArepalli/py-chan-calculator',
+    author='Chandu Arepalli',
+    author_email='chandumanikumar4@gmail.com',
+    license='BSD 2-clause',
+    packages=['pychancalculator'],
+    install_requires=[],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
