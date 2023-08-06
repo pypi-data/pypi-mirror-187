c772ef6274d318de46ac6b7911b01dd86955dfe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:30 +0000

    Add package kiki-utils/1.2.9/kiki_utils-1.2.9.tar.gz

---
+++ packages/kiki-utils/kiki_utils-1.2.9.tar.gz/kiki_utils-1.2.9/kikiutils/file.py
@@ -0,0 +1,120 @@
+import io
+import magic
+import os
+import shutil
+
+from typing import Callable, Union
+
+from .decorators import try_and_get_bool, try_and_get_data
+
+
+# File
+
+def clear_dir(path: str):
+    """Clear dir (Remove and create)."""
+
+    rmdir(path)
+    mkdirs(path)
+
+
+@try_and_get_bool
+def del_file(path: str):
+    """Del file."""
+
+    os.remove(path)
+
+
+def get_file_mime(file: Union[bytes, io.BytesIO, io.FileIO]):
+    """Get file mime."""
+
+    is_file = getattr(file, 'read', None) != None
+    data = file.read(2048) if is_file else file[:2048]
+    file_mime = magic.from_buffer(data, mime=True)
+
+    if is_file:
+        file.seek(0)
+
+    return file_mime.split('/')
+
+
+@try_and_get_data
+def get_file_size(path: str):
+    return os.stat(path).st_size
+
+
+@try_and_get_bool
+def mkdir(path: str):
+    """Create dir."""
+
+    os.mkdir(path)
+
+
+@try_and_get_bool
+def mkdirs(path: str):
+    """Create dir (use makedirs)."""
+
+    os.makedirs(path, exist_ok=True)
+
+
+@try_and_get_bool
+def move_file(path: str, target_path: str):
+    """Move file or dir."""
+
+    shutil.move(path, target_path)
+
+
+@try_and_get_data
+def read_file(path: str):
+    """Read file."""
+
+    with open(path, 'rb') as f:
+        return f.read()
+
+
+@try_and_get_bool
+def rename(path: str, name: str):
+    """Rename file or dir."""
+
+    os.rename(path, name)
+
+
+@try_and_get_bool
+def rmdir(path: str):
+    """Remove dir."""
+
+    shutil.rmtree(path)
+
+
+@try_and_get_data
+def save_file(
+    path: str,
+    file: Union[bytes, io.BytesIO, io.FileIO, str],
+    replace: bool = True
+):
+    """Save file."""
+
+    mode = 'w' if isinstance(file, str) else 'wb'
+
+    if os.path.exists(path) and not replace:
+        raise FileExistsError()
+    if getattr(file, 'read', None):
+        file = file.read()
+    with open(path, mode) as f:
+        return f.write(file)
+
+
+def save_file_as_bytesio(
+    save_fnc: Callable,
+    get_bytes: bool = False,
+    **kwargs
+):
+    """Save file to io.BytesIO."""
+
+    with io.BytesIO() as output:
+        save_fnc(output, **kwargs)
+        file_bytes = output.getvalue()
+
+    if get_bytes:
+        return file_bytes
+
+    return io.BytesIO(file_bytes)
