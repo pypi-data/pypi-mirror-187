8560adc19afe30934f2e094a64ccf663adcae17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:23 +0000

    Add package dictionarify/0.0.9/dictionarify-0.0.9.tar.gz

---
+++ packages/dictionarify/dictionarify-0.0.9.tar.gz/dictionarify-0.0.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+    name='dictionarify',
+    version='0.0.9',
+    author='Surya Narayanan K V',
+    author_email='githubsurya@gmail.com',
+    description='it is a class which have similar methods to list methods for dictionary',
+    long_description='link to documentation- https://github.com/suryavenom/Flexi/blob/main/README.md',
+    keywords=['dictionary', 'dictionarify','flexi'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires = '>=3.7'
+)
