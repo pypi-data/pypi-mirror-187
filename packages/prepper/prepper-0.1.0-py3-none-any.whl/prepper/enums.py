ed397e9026103dfcd26c41151c06e2debc09f7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:06 +0000

    Add package prepper/0.1.0/prepper-0.1.0-py3-none-any.whl

---
+++ packages/prepper/prepper-0.1.0-py3-none-any.whl/prepper/enums.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from aenum import auto, Enum, unique, extend_enum
+
+__all__ = ["H5StoreTypes", "add_enum_item"]
+
+
+@unique
+class H5StoreTypes(Enum):
+    Sequence = auto()
+    Dictionary = auto()
+    ClassConstructor = auto()
+    PythonClass = auto()
+    HDF5Dataset = auto()
+    HDF5Group = auto()
+    FunctionCache = auto()
+    Null = auto()
+    Enumerator = auto()
+
+
+def add_enum_item(name:str):
+    extend_enum(H5StoreTypes, name, auto())
