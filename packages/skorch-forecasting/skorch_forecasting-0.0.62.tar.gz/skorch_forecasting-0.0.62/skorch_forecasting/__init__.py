5961c73232a56eeb9aaf6fa3b1421bfb5017dc6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:02 +0000

    Add package skorch-forecasting/0.0.62/skorch_forecasting-0.0.62.tar.gz

---
+++ packages/skorch-forecasting/skorch_forecasting-0.0.62.tar.gz/skorch_forecasting-0.0.62/skorch_forecasting/__init__.py
@@ -0,0 +1,19 @@
+from . import (
+    ensemble,
+    model_selection,
+    nn,
+    preprocessing,
+    utils,
+    model_wrappers
+)
+
+__version__ = '0.0.62'
+
+__all__ = [
+    'ensemble',
+    'model_selection',
+    'nn',
+    'preprocessing',
+    'utils',
+    'model_wrappers'
+]
