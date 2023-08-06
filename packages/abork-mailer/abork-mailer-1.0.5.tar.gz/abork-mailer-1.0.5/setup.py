feeae447e1c61101254fb239443b79b1eb21cff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:58 +0000

    Add package abork-mailer/1.0.5/abork-mailer-1.0.5.tar.gz

---
+++ packages/abork-mailer/abork-mailer-1.0.5.tar.gz/abork-mailer-1.0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='abork-mailer',
+    version='1.0.5', 
+    packages=['mailer'], 
+    package_dir={'mailer': 'src'},
+    author='Alex Bork',
+    description='Mailer Library'
+    )
