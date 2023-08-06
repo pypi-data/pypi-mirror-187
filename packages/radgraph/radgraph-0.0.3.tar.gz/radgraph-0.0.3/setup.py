1e0502aa56848a2a0208c87534959ae08fd82505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:29 +0000

    Add package radgraph/0.0.3/radgraph-0.0.3.tar.gz

---
+++ packages/radgraph/radgraph-0.0.3.tar.gz/radgraph-0.0.3/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 8):
+    sys.exit('Sorry, Python >=3.8 is required for ViLMedic.')
+
+setup(
+    name='radgraph',
+    version='0.0.3',
+    author='Jean-Benoit Delbrouck',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    python_requires='>=3.8',
+    setup_requires="Cython",
+    install_requires=['torch>=1.8.1',
+                      'transformers==4.23.1',
+                      "appdirs",
+                      'overrides==3.1.0',
+                      'jsonpickle',
+                      'filelock',
+                      'h5py',
+                      'spacy',
+                      'nltk',
+                      ],
+    include_package_data=True,
+    exclude_package_data={'': ['.git']},
+    packages=find_packages("."),
+    package_dir={"": "."},
+    zip_safe=False)
