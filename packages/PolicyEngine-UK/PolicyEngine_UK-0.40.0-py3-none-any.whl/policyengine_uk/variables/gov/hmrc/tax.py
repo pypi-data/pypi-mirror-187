d36003e1a49dd20c9368224b8c9388bc88bce88d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:28 +0000

    Add package PolicyEngine-UK/0.40.0/PolicyEngine_UK-0.40.0-py3-none-any.whl

---
+++ packages/PolicyEngine-UK/PolicyEngine_UK-0.40.0-py3-none-any.whl/policyengine_uk/variables/gov/hmrc/tax.py
@@ -0,0 +1,66 @@
+from policyengine_uk.model_api import *
+
+
+class tax(Variable):
+    value_type = float
+    entity = Person
+    label = "Taxes"
+    definition_period = YEAR
+    unit = GBP
+
+    adds = ["income_tax", "national_insurance"]
+
+
+class household_tax(Variable):
+    value_type = float
+    entity = Household
+    label = "taxes"
+    documentation = "Total taxes owed by the household"
+    definition_period = YEAR
+    unit = GBP
+    adds = [
+        "change_in_expected_sdlt",
+        "change_in_expected_ltt",
+        "change_in_expected_lbtt",
+        "corporate_sdlt_change_incidence",
+        "business_rates_change_incidence",
+        "council_tax",
+        "domestic_rates",
+        "change_in_fuel_duty",
+        "tv_licence",
+        "wealth_tax",
+        "income_tax",
+        "national_insurance",
+        "LVT",
+        "carbon_tax",
+        "vat_change",
+    ]
+
+
+class benunit_tax(Variable):
+    value_type = float
+    entity = BenUnit
+    label = "Benefit unit tax paid"
+    definition_period = YEAR
+    unit = GBP
+
+    adds = ["tax"]
+
+
+class tax_reported(Variable):
+    value_type = float
+    entity = Person
+    label = "Reported tax paid"
+    definition_period = YEAR
+    unit = GBP
+
+
+class tax_modelling(Variable):
+    value_type = float
+    entity = Person
+    label = "Difference between reported and imputed tax liabilities"
+    definition_period = YEAR
+    unit = GBP
+
+    adds = ["tax"]
+    subtracts = ["tax_reported"]
