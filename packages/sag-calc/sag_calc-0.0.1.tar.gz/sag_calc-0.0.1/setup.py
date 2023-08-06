8fdac78d5ba4c164a9f3bb6c919bad29fbc40a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:45 +0000

    Add package sag-calc/0.0.1/sag_calc-0.0.1.tar.gz

---
+++ packages/sag-calc/sag_calc-0.0.1.tar.gz/sag_calc-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'sag_calc'
+LONG_DESCRIPTION = 'A package to perform arithmetic operations'
+
+# Setting up
+setup(
+    name="sag_calc",
+    version=VERSION,
+    author="sagar",
+    author_email="sagarmall232003@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['arithmetic', 'math', 'mathematics', 'python tutorial', 'avi upadhyay'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
