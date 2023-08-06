485c5c3a523a49d72999a91b93ebd06c79ee7745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:14 +0000

    Add package Topsis-Ikshit-102003403/1.1.3/Topsis-Ikshit-102003403-1.1.3.tar.gz

---
+++ packages/Topsis-Ikshit-102003403/Topsis-Ikshit-102003403-1.1.3.tar.gz/Topsis-Ikshit-102003403-1.1.3/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="Topsis-Ikshit-102003403",
+    version="1.1.3",
+    description="Implements topsis on the given input data file and generates topsis score and ranks accordingly.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ikshit2809/Topsis",
+    author="Ikshit",
+    author_email="iikshit_be20@thapar.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=["topsis"],
+    include_package_data=True,
+    install_requires=['pandas','sys','os','math'],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.__main__:main",
+        ]
+    },
+)
