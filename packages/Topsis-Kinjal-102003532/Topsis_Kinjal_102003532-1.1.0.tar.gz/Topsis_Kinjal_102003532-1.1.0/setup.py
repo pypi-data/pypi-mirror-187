6ce688c919aadcc9fa2d9dbd9e66559e9657eb86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:26 +0000

    Add package Topsis-Kinjal-102003532/1.1.0/Topsis_Kinjal_102003532-1.1.0.tar.gz

---
+++ packages/Topsis-Kinjal-102003532/Topsis_Kinjal_102003532-1.1.0.tar.gz/Topsis_Kinjal_102003532-1.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.1.0'
+DESCRIPTION = 'It evaluates alternatives based on multiple criteria using TOPSIS method'
+
+# Setting up
+setup(
+    name="Topsis_Kinjal_102003532",
+    version=VERSION,
+    author="Kinjal Goyal",
+    author_email="<kgoyal1_be20@thapar.edu>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
