4a232bcd11165dcd2ec20c753edfe8e0936fc14d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:04 +0000

    Add package Leetease/0.2.1/Leetease-0.2.1.tar.gz

---
+++ packages/Leetease/Leetease-0.2.1.tar.gz/Leetease-0.2.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+name = 'Leetease',
+version = '0.2.1',
+description = "This makes LeetCode Interview programs easy",
+long_description = "This Python Package Helps Python Programmers Crack LeetCode programs with comparatively less lines of code",
+author = "SaatwikCodz",
+packages = ['Leetease'],
+install_requires = []
+	)
