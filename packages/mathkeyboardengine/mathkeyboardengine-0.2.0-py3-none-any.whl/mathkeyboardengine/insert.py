f50024c8bd3a557ce0085ca2ef5f93a6630bb8be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:39 +0000

    Add package mathkeyboardengine/0.2.0/mathkeyboardengine-0.2.0-py3-none-any.whl

---
+++ packages/mathkeyboardengine/mathkeyboardengine-0.2.0-py3-none-any.whl/mathkeyboardengine/insert.py
@@ -0,0 +1,13 @@
+from mathkeyboardengine import KeyboardMemory, Placeholder, TreeNode, move_right
+
+
+def insert(k: KeyboardMemory, new_node: TreeNode):
+    if isinstance(k.current, Placeholder):
+        k.current.nodes.insert(0, new_node)
+        new_node.parent_placeholder = k.current
+    else:
+        parent: Placeholder = k.current.parent_placeholder
+        index_of_current = parent.nodes.index(k.current)
+        parent.nodes[index_of_current + 1 : index_of_current + 1] = [new_node]
+        new_node.parent_placeholder = parent
+    move_right(k)
