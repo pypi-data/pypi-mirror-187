12204db18c9cc53adf32cfff7864c739ef16da76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:58 +0000

    Add package cldfofflinebrowser/0.2.0/cldfofflinebrowser-0.2.0-py2.py3-none-any.whl

---
+++ packages/cldfofflinebrowser/cldfofflinebrowser-0.2.0-py2.py3-none-any.whl/cldfofflinebrowser/commands/add.py
@@ -0,0 +1,20 @@
+"""
+Add a sound file to an offline version
+
+Not Implemented yet!
+"""
+
+
+def register(parser):
+    pass
+    #
+    # FIXME: language and parameter ID, sound file path, optionally transcription
+    # -> ask Mary what the most likely scenario for adding data would be.
+    #
+
+
+def run(args):
+    raise NotImplementedError()  # pragma: no cover
+    #
+    # FIXME: must add to HTML as well!
+    #
---
+++ packages/cldfofflinebrowser/cldfofflinebrowser-0.2.0-py2.py3-none-any.whl/cldfofflinebrowser/media.py
@@ -0,0 +1,43 @@
+import shutil
+import pathlib
+import collections
+from urllib.request import urlretrieve
+
+import rfc3986
+from clldutils.path import md5
+
+__all__ = ['PREFERRED_AUDIO', 'download', 'get_best_audio']
+
+PREFERRED_AUDIO = collections.OrderedDict([
+    ('audio/mpeg', '.mp3'),
+    ('audio/wav', '.wav'),
+    ('audio/x-wav', '.wav'),
+    ('audio/ogg', '.ogg'),
+])
+
+
+def download(cldf, media_row, outdir, fname, media_table, md5sum=None):
+    target = pathlib.Path(outdir) / fname
+    if not target.exists() or (md5sum and md5sum != md5(target)):
+        url = cldf.get_row_url(media_table, media_row)
+        url = url.unsplit() if isinstance(url, rfc3986.URIReference) else url
+        if cldf.directory.joinpath(url).exists():
+            shutil.copy(str(cldf.directory / url), str(target))
+        else:  # pragma: no cover
+            urlretrieve(url, target)
+    return target
+
+
+def get_best_audio(audios):
+    """
+    For offline usage, we optimize filesize over widest browser support, so only choose one audio
+    file per form.
+    :param audios:
+    :return:
+    """
+    if audios:
+        pref = {mtype: i for i, mtype in enumerate(PREFERRED_AUDIO)}
+        return sorted(
+            audios, key=lambda r: pref.get(r['mediaType'], len(pref)))[0]
+    else:
+        return None
