6523cec19b4f28001a6a994fd8d5fb1c48aff16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:13 +0000

    Add package openmtpk/0.6.10/openmtpk-0.6.10.tar.gz

---
+++ packages/openmtpk/openmtpk-0.6.10.tar.gz/openmtpk-0.6.10/modules/ml/mlp_network.cpp
@@ -0,0 +1,50 @@
+/*
+ * Implementation of a Multi-Layered Perceptron Neural Network
+ */
+#include "../../include/ml/mlp_net.hpp"
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+
+using namespace mtpk::ml;
+
+/*
+ * Initialize randomly generated values for network's method
+ */
+void mtpk::ml::PrimaryMLP::rand_init() {
+    srand((uint64_t) time(NULL));
+}
+
+/* verify the random is an integer */
+int64_t mtpk::ml::PrimaryMLP::rand_int(int64_t hi, int64_t low) {
+    return rand() % (hi - low + 1) + low;
+}
+
+/* verify generated random is a real number */
+long double mtpk::ml::PrimaryMLP::rand_real(long double low, 
+                                            long double hi) {
+    return ((long double) rand() / RAND_MAX) * (hi - low) + low;
+}
+
+/* MLP CONSTRUCTOR */
+mtpk::ml::PrimaryMLP::PrimaryMLP(int64_t nl, int64_t npl[]) : 
+    num_layers(0), 
+    layer_ptr(0),
+    _Eta(0.25),
+    _Alpha(0.9),
+    _Gain(1.0),
+    _MSE(0.0),
+    _MAE(0.0),
+    _AvgTestError(0.0) { 
+    
+    int64_t i, j;
+
+    // create network layers
+    num_layers = nl;
+    layer_ptr = new layer[nl];
+
+
+}
+
+
