b28743ac979a5b7c1f4af9cc936704a516fe4753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:03 +0000

    Add package mtworker/0.1.2/mtworker-0.1.2.tar.gz

---
+++ packages/mtworker/mtworker-0.1.2.tar.gz/mtworker-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['demo_package', 'mtworker', 'mtworker.tasks']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'mtworker',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': '# Example Package\n',
+    'author': 'a',
+    'author_email': 'a@a.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
