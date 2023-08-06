66d27802e1bf8145baeaedc5b70e7896e3c85951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:05 +0000

    Add package Topsis-Vanshaj-102003346/1.0.8/Topsis-Vanshaj-102003346-1.0.8.tar.gz

---
+++ packages/Topsis-Vanshaj-102003346/Topsis-Vanshaj-102003346-1.0.8.tar.gz/Topsis-Vanshaj-102003346-1.0.8/setup.py
@@ -0,0 +1,31 @@
+
+from  setuptools import setup,find_packages
+
+with open("readme.txt", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Topsis-Vanshaj-102003346",
+    version="1.0.8",
+    author="Vanshaj Singla",
+    author_email="vanshajrnv2002@gmail.com",
+    description="Calculates Topsis Score and Rank them accordingly",
+    long_description=long_description,
+    keywords=['TOPSIS'],
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/vanshajsingla/Topsis-Vanshaj-102003346",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires='pandas',
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.topsis:main"
+        ]
+    },
+)
