fecec2607ca0aa60ddb1a1abeed76a814595a524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:53 +0000

    Add package cppython-core/0.6.1.dev34/cppython_core-0.6.1.dev34-py3-none-any.whl

---
+++ packages/cppython-core/cppython_core-0.6.1.dev34-py3-none-any.whl/cppython_core/plugin_schema/generator.py
@@ -0,0 +1,54 @@
+"""Generator data plugin definitions"""
+from abc import abstractmethod
+from pathlib import Path
+from typing import Any, Generic, TypeVar
+
+from pydantic import Field
+from pydantic.types import DirectoryPath
+
+from cppython_core.schema import DataPlugin, PluginGroupData, SyncDataT
+
+
+class GeneratorGroupData(PluginGroupData):
+    """Base class for the configuration data that is set by the project for the generator"""
+
+    root_directory: DirectoryPath = Field(description="The directory where the pyproject.toml lives")
+
+
+class Generator(DataPlugin[GeneratorGroupData], Generic[SyncDataT]):
+    """Abstract type to be inherited by CPPython Generator plugins"""
+
+    @staticmethod
+    @abstractmethod
+    def supported(directory: Path) -> bool:
+        """Queries a given directory for generator related files
+
+        Args:
+            directory: The directory to investigate
+
+        Returns:
+            Whether the directory has pre-existing generator support
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    @abstractmethod
+    def sync_data_type() -> type[SyncDataT]:
+        """The sync data type to use for data transfer
+
+        Returns:
+            The data type
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def sync(self, sync_data: SyncDataT) -> None:
+        """Synchronizes generator files and state with the providers input
+
+        Args:
+            sync_data: List of information gathered from providers
+        """
+        raise NotImplementedError()
+
+
+GeneratorT = TypeVar("GeneratorT", bound=Generator[Any])
