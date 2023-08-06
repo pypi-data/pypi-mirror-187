556af76c46925e33ee5096615de7bad72a4d7e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:08:12 +0000

    Add package custom-maya/0.11/custom_maya-0.11.tar.gz

---
+++ packages/custom-maya/custom_maya-0.11.tar.gz/custom_maya-0.11/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='custom_maya',
+    version='0.11',
+    description='Maya的便利工具',
+    long_description='',
+    author='qiao yuanzhen',
+    author_email='narutozbjp@hotmail.com',
+    python_requires='>3.7'
+
+)
