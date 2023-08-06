15ecb701ba3208147d68f529213d42aab498b8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:55 +0000

    Add package datasette-ui-extras/0.4/datasette_ui_extras-0.4-py3-none-any.whl

---
+++ packages/datasette-ui-extras/datasette_ui_extras-0.4-py3-none-any.whl/datasette_ui_extras/static/focus-search-box.js
@@ -0,0 +1,26 @@
+(function () {
+  function focusSearchBox() {
+    const box = document.querySelector('.search-row #_search');
+
+    if (!box)
+      return;
+
+    const maybeFocus = (e) => {
+      const tagName = (e.target || {}).tagName;
+
+      if (tagName === 'INPUT' || tagName === 'TEXTAREA' || tagName === 'SELECT')
+        return;
+
+      if (e.key !== '/')
+        return;
+
+      box.focus();
+      e.preventDefault();
+      return false;
+    };
+
+    document.addEventListener('keydown', maybeFocus);
+  }
+
+  addEventListener('DOMContentLoaded', focusSearchBox);
+})();
