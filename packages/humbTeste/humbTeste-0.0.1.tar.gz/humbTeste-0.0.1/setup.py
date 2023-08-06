6240fa94a4c9773051155de671ddda446365f6d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:23 +0000

    Add package humbTeste/0.0.1/humbTeste-0.0.1.tar.gz

---
+++ packages/humbTeste/humbTeste-0.0.1.tar.gz/humbTeste-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+packages = \
+['testeHumb']
+
+package_data = \
+{'': ['*']}
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'  
+]
+
+setup(
+    name='humbTeste',
+    version='0.0.1',
+    description='A basic hello word',
+    long_description=open('README.txt').read() + '\n\n' + open("CHANGELOG.txt").read(),
+    url='',
+    author='Humberto Távora',
+    author_email='humbertocct@hotmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='Hello Word',
+    packages=find_packages(),
+    install_requires = ['']
+)
