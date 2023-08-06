ed9d088b9b2cea3d338043e20f29552a4f5dd223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:27:10 +0000

    Add package sympy-add/1.0/sympy_add-1.0.tar.gz

---
+++ packages/sympy-add/sympy_add-1.0.tar.gz/sympy_add-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='sympy_add',
+
+      version='1.0',
+
+      url="https://github.com/DaSh-More/SympyAdd",
+
+      license='MIT',
+
+      author='DaSh-More',
+
+      author_email='danila-763@yandex.ru',
+
+      description='Addition for sympy',
+
+      platforms = ['any'])
