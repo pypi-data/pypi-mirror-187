21c3e660221b5445501af027a77333c5b5399dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:13 +0000

    Add package power-sysGraph/0.0.6/power_sysGraph-0.0.6.tar.gz

---
+++ packages/power-sysGraph/power_sysGraph-0.0.6.tar.gz/power_sysGraph-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.6' 
+DESCRIPTION = 'Relations of power in graphs'
+LONG_DESCRIPTION = 'Given a directed graph, this Python package calculates the colonization matrix and properties such as hierarchy and mutualism.'
+
+setup(
+        name="power_sysGraph", 
+        version=VERSION,
+        author="Daniele De Luca",
+        author_email="<prof.daniele.deluca@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=["numpy", "matplotlib", "numpy", "networkx"],
+        
+        keywords=['power', 'system of power', 'cooperation'],
+        classifiers= [
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+        ]
+)
