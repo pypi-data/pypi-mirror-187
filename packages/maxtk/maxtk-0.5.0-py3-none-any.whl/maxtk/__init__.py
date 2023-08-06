4a9c043d032a930f84c14266b6faed19c27fb2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:02 +0000

    Add package maxtk/0.5.0/maxtk-0.5.0-py3-none-any.whl

---
+++ packages/maxtk/maxtk-0.5.0-py3-none-any.whl/maxtk/__init__.py
@@ -0,0 +1,30 @@
+# !/usr/bin/python
+# coding=utf-8
+from maxtk.core import *
+from maxtk.nodetk import Node
+from maxtk.cmpttk import Cmpt
+from maxtk.edittk import Edit
+from maxtk.scripttk import Script
+from maxtk.rigtk import Rig
+from maxtk.xformtk import Xform
+
+
+name = 'maxtk'
+__version__ = '0.5.0'
+
+# --------------------------------------------------------------------------------------------
+
+
+
+
+
+
+# print (__package__, __file__)
+# --------------------------------------------------------------------------------------------
+# Notes
+# --------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------
+# deprecated:
+# --------------------------------------------------------------------------------------------
