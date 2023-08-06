22ca0a64d15f05ea1909028e9d265ce7a3d5201f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:47 +0000

    Add package egen111222-my-docs-libs/1.0/egen111222-my_docs_libs-1.0.tar.gz

---
+++ packages/egen111222-my-docs-libs/egen111222-my_docs_libs-1.0.tar.gz/egen111222-my_docs_libs-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='egen111222-my_docs_libs',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Kirill',
+      author_email='egen13@ukr.net',
+      packages=['my_project'],
+     )
