7ec4a4ee4c3b809091ac43b40e2bbca2f601f4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:40 +0000

    Add package msgram-core/1.0.0/msgram_core-1.0.0.tar.gz

---
+++ packages/msgram-core/msgram_core-1.0.0.tar.gz/msgram_core-1.0.0/src/core/dataframe.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+from util.check_functions import check_component_is_valid
+
+
+def create_dataframe(measures, components, language_extension):
+
+    df_columns = measures + ["qualifier"]
+
+    df = pd.DataFrame(columns=df_columns)
+
+    for component in components:
+        if check_component_is_valid(component, language_extension):
+            df.at[component["path"], "qualifier"] = component["qualifier"]
+            for metric in component["measures"]:
+                df.at[component["path"], metric["metric"]] = metric["value"]
+
+    df.reset_index(inplace=True)
+    return df.rename({"index": "path"}, axis=1)
---
+++ packages/msgram-core/msgram_core-1.0.0.tar.gz/msgram_core-1.0.0/src/resources/__init__.py
@@ -0,0 +1,2 @@
+from .analysis_duplicate import *  # noqa: F401, F403
+from .available import *  # noqa: F401, F403
