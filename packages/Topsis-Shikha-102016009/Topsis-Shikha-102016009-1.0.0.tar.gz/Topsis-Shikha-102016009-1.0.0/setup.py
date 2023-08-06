bf58d47db5bd33fc9fb69aa98b993f9388fb26ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:43 +0000

    Add package Topsis-Shikha-102016009/1.0.0/Topsis-Shikha-102016009-1.0.0.tar.gz

---
+++ packages/Topsis-Shikha-102016009/Topsis-Shikha-102016009-1.0.0.tar.gz/Topsis-Shikha-102016009-1.0.0/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+NAME = "Topsis-Shikha-102016009"
+VERSION = "1.0.0"
+DESCRIPTION = "A package for topsis score generation."
+AUTHOR = "Shikha"
+AUTHOR_EMAIL = "sshikha_be20@thapar.edu"
+PACKAGES_PRESENT = ['src']
+PACKAGES_NEED = ['pandas','numpy']
+PACKAGE_DIR={'':'src'}
+
+def read_file(name):
+    path = os.getcwd()
+    return open(f"{path}\{name}").read()
+
+setup(
+    name = NAME,
+    version = VERSION,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    description = DESCRIPTION,
+    packages = PACKAGES_PRESENT,
+    install_requires = PACKAGES_NEED,
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    license = read_file('LICENSE.md')
+)
