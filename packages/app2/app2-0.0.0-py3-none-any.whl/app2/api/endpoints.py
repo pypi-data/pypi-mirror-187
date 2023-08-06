9f754ab9cccf275ee1b7aefbcdc34d324255044c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:07 +0000

    Add package app2/0.0.0/app2-0.0.0-py3-none-any.whl

---
+++ packages/app2/app2-0.0.0-py3-none-any.whl/app2/api/endpoints.py
@@ -0,0 +1,33 @@
+from flask import Response, request
+import logging
+import pandas as pd
+
+
+from kortical.app import get_config
+from kortical.app import requests
+from app2.authentication import safe_api_call
+
+logger = logging.getLogger(__name__)
+
+app_config = get_config(format='yaml')
+model_name = app_config['model_name']
+
+
+def register_routes(app):
+
+    @app.route('/health', methods=['get'])
+    def health():
+        return {"result": "success"}
+
+    @app.route('/predict', methods=['post'])
+    @safe_api_call
+    def predict():
+        input_text = request.json['input_text']
+        request_data = {
+            'text': [input_text]
+        }
+        df = pd.DataFrame(request_data)
+        df = requests.predict(model_name, df)
+        predicted_category = df['predicted_category'][0]
+
+        return Response(predicted_category)
