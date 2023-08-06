51668b7f9993b17743b73c05d6b9d359b14ebfbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:25 +0000

    Add package Topsis-Pranav-102003432/0.5/Topsis_Pranav_102003432-0.5.tar.gz

---
+++ packages/Topsis-Pranav-102003432/Topsis_Pranav_102003432-0.5.tar.gz/Topsis_Pranav_102003432-0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Topsis_Pranav_102003432",
+    version="0.5",
+    author="Pranav Singh",
+    author_email="psingh2_be20@thapar.edu",
+    description ="Evaluation of alternatives based on multiple criteria using TOPSIS method.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['Topsis_Pranav_102003432'],
+    install_requires=['pandas'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "topsis=Topsis_Pranav_102003432.Pranav_102003432:main",
+        ]
+    } 
+)
