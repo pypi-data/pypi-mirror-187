887d18e154117b818a440621a65e9ec753369ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:44 +0000

    Add package rachit-madhegaria/0.1/rachit_madhegaria-0.1.tar.gz

---
+++ packages/rachit-madhegaria/rachit_madhegaria-0.1.tar.gz/rachit_madhegaria-0.1/README.md
@@ -0,0 +1,3 @@
+To find the  average of a nxn matrix by separating it into a 2x2 matrix where n is even number
+Line1-The order of the square matrix is supposed to be enter which should be an even number.
+Line8-Enter the elements to be entered into your matrix. 
---
+++ packages/rachit-madhegaria/rachit_madhegaria-0.1.tar.gz/rachit_madhegaria-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='rachit_madhegaria',
+ version='0.1',
+ author="Rachit Madhegaria",
+ author_email="rachit.20122003@gmail.com",
+ description="To find the  average of a nxn matrix by separating it into a 2x2 matrix where n is even number",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
