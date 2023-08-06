b0ff659b277a3e47fdcce7ea5a3f92500fc54ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:40 +0000

    Add package pythonUtilityFiles/0.0.1/pythonUtilityFiles-0.0.1.tar.gz

---
+++ packages/pythonUtilityFiles/pythonUtilityFiles-0.0.1.tar.gz/pythonUtilityFiles-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'pythonUtilityFiles'
+LONG_DESCRIPTION = 'A package for common methods'
+
+# Setting up
+setup(
+    name="pythonUtilityFiles",
+    version=VERSION,
+    author="Ritika",
+    author_email="ritika.kumar90@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['arithmetic', 'CommonKeywords', 'mathematics', 'python tutorial', 'Ritika kumar'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+
+)
