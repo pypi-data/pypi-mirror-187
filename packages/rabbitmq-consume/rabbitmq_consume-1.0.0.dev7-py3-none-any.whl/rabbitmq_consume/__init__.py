fc5eac02b407cfe2b9e0b1e0e5c2252ac65a6b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:04 +0000

    Add package rabbitmq-consume/1.0.0.dev7/rabbitmq_consume-1.0.0.dev7-py3-none-any.whl

---
+++ packages/rabbitmq-consume/rabbitmq_consume-1.0.0.dev7-py3-none-any.whl/rabbitmq_consume/__init__.py
@@ -0,0 +1,5 @@
+"""Declares public members of this module"""
+
+from .consumption import Consumption, ConsumptionManager
+from .consumer import Consumer
+from .null_consumption import NullConsumption
