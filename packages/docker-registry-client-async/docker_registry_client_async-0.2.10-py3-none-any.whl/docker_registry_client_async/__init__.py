6f61b5e79f25e353baf851765c4607610d95dfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:54 +0000

    Add package docker-registry-client-async/0.2.10/docker_registry_client_async-0.2.10-py3-none-any.whl

---
+++ packages/docker-registry-client-async/docker_registry_client_async-0.2.10-py3-none-any.whl/docker_registry_client_async/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""An AIOHTTP based Python REST client for the Docker Registry."""
+
+from .dockerregistryclientasync import DockerRegistryClientAsync
+from .formattedsha256 import FormattedSHA256
+from .imagename import ImageName
+from .jsonbytes import JsonBytes
+from .manifest import Manifest
+from .specs import (
+    DockerAuthentication,
+    DockerMediaTypes,
+    Indices,
+    MediaTypes,
+    OCIMediaTypes,
+    QuayAuthentication,
+    RedHatAuthentication,
+)
+
+__version__ = "0.2.10"
