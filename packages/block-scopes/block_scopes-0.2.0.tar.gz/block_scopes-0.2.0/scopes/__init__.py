1410bde660cd3dba0b2011be7a03209eae4733e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:54 +0000

    Add package block-scopes/0.2.0/block_scopes-0.2.0.tar.gz

---
+++ packages/block-scopes/block_scopes-0.2.0.tar.gz/block_scopes-0.2.0/scopes/__init__.py
@@ -0,0 +1,45 @@
+"""
+Block scoping in Python!
+"""
+
+__export__ = {"only"}
+
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("block-scopes")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+
+#
+# A context manager for local scoping!
+#
+
+from contextlib import contextmanager
+
+
+@contextmanager
+def only(*names: str):
+    import inspect
+
+    if current := inspect.currentframe():
+        if context := current.f_back:
+            if local := context.f_back:
+                keep = {*local.f_locals, *names}
+                yield keep
+
+    if current := inspect.currentframe():
+        if context := current.f_back:
+            if local := context.f_back:
+                for _ in {
+                    *local.f_locals,
+                }:
+                    if _ not in keep:
+                        del local.f_locals[_]
+
+
+if __name__ != "__main__":
+    import hygiene  # type: ignore
+    hygiene.cleanup()
---
+++ packages/block-scopes/block_scopes-0.2.0.tar.gz/block_scopes-0.2.0/tests/test_imports.py
@@ -0,0 +1,8 @@
+"""
+Does the package import correctly?
+"""
+
+
+def test_imports():
+    """Does the package import correctly?"""
+    import scopes
