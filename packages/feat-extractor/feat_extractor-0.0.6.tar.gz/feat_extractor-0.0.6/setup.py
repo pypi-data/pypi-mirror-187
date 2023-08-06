25ce936e70caaf16163a0ee7a65229af9e72a8df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:40 +0000

    Add package feat-extractor/0.0.6/feat_extractor-0.0.6.tar.gz

---
+++ packages/feat-extractor/feat_extractor-0.0.6.tar.gz/feat_extractor-0.0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='feat_extractor',
+    packages=["feat_extractor"],
+    version='0.0.6',
+    description='Gtzan feature Extractor',
+    long_description='Gtzan feature Extractor',
+    long_description_content_type="text/markdown",
+    author='ranierifr',
+    license='MIT',
+    python_requires=">=3.6"
+)
