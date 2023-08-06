34c2770f53c0bc30af665470e1a63d3669c529b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:29 +0000

    Add package jac-misc/1.4.0.6/jac_misc-1.4.0.6-py3-none-any.whl

---
+++ packages/jac-misc/jac_misc-1.4.0.6-py3-none-any.whl/jac_misc/config.py
@@ -0,0 +1,9 @@
+from .action_configs.pdf_ext_action_config import PDF_EXT_ACTION_CONFIG
+from .action_configs.cluster_action_config import CLUSTER_ACTION_CONFIG
+from .action_configs.test_module_action_config import TEST_MODULE_ACTION_CONFIG
+
+ACTION_CONFIGS = {
+    "pdf_ext": PDF_EXT_ACTION_CONFIG,
+    "cluster": CLUSTER_ACTION_CONFIG,
+    "test_module": TEST_MODULE_ACTION_CONFIG,
+}
