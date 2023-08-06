b6b64019600f549cb8d24c00f6ae51cbb13528b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:18 +0000

    Add package jsmetrics/0.1.0/jsmetrics-0.1.0-py3-none-any.whl

---
+++ packages/jsmetrics/jsmetrics-0.1.0-py3-none-any.whl/jsmetrics/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for jsmetrics."""
+
+__author__ = """Tom Keel"""
+__email__ = "thomas.keel.18@ucl.ac.uk"
+__version__ = "0.1.0"
+
+from . import details_for_all_metrics
+from .metrics import jet_metrics, jet_core_algorithms, waviness_metrics
