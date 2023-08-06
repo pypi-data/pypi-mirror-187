ac25a8061807061c235882cf38f2eea3abd0815a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:07 +0000

    Add package co-deployer/1.0.3/co-deployer-1.0.3.tar.gz

---
+++ packages/co-deployer/co-deployer-1.0.3.tar.gz/co-deployer-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="co-deployer",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+	author="Elwan Mayencourt",
+    version="1.0.3",
+    packages=find_packages(),
+    install_requires=[
+		"rich",
+		"paramiko",
+		"jsonschema"
+    ],
+	   entry_points={
+        "console_scripts": [
+            "co-deployer = co_deployer.co_deployer:main",
+        ],
+    },
+)
