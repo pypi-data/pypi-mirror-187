2e1672208a4a361ae5be8c4686807ec42e29957c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:19:09 +0000

    Add package shopee-wrapper/0.4/shopee_wrapper-0.4.tar.gz

---
+++ packages/shopee-wrapper/shopee_wrapper-0.4.tar.gz/shopee_wrapper-0.4/shopee_wrapper/__init__.py
@@ -0,0 +1,21 @@
+"""
+Shopee API Wrapper
+~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for the Shopee API.
+
+:copyright: (c) 2022-present munawariz
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'shopee-wrapper'
+__author__ = 'munawariz'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-present munawariz'
+__version__ = '0.4'
+
+from .base import *
+from .product import *
+from .review import *
+from .shop import *
