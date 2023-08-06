473897a51c0fb06bdcc488d63c701247e544b8a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:38 +0000

    Add package safe-ds/0.1.0/safe_ds-0.1.0-py3-none-any.whl

---
+++ packages/safe-ds/safe_ds-0.1.0-py3-none-any.whl/safe_ds/exceptions/__init__.py
@@ -0,0 +1,11 @@
+from ._data_exceptions import (
+    ColumnLengthMismatchError,
+    ColumnSizeError,
+    DuplicateColumnNameError,
+    IndexOutOfBoundsError,
+    MissingSchemaError,
+    NonNumericColumnError,
+    SchemaMismatchError,
+    UnknownColumnNameError,
+)
+from ._learning_exceptions import LearningError, NotFittedError, PredictionError
---
+++ packages/safe-ds/safe_ds-0.1.0-py3-none-any.whl/safe_ds/plotting/_plot_boxplot.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from safe_ds.data import Column
+from safe_ds.exceptions import NonNumericColumnError
+
+
+def plot_boxplot(column: Column) -> None:
+    """
+    Plot a column in a boxplot. This function can only plot real numerical data
+
+    Parameters
+    ----------
+    column : Column
+                The column you want to plot
+
+    Raises
+    -------
+    TypeError
+        if the column contains non-numerical data or complex data
+    """
+    # noinspection PyProtectedMember
+    for data in column._data:
+        if (
+            not isinstance(data, int)
+            and not isinstance(data, float)
+            and not isinstance(data, complex)
+        ):
+            raise NonNumericColumnError(column.name)
+        if isinstance(data, complex):
+            raise TypeError(
+                "The column contains complex data. Boxplots cannot plot the imaginary part of complex "
+                "data. Please provide a Column with only real numbers"
+            )
+    # noinspection PyProtectedMember
+    sns.boxplot(data=column._data)
+    plt.tight_layout()
+    plt.show()
