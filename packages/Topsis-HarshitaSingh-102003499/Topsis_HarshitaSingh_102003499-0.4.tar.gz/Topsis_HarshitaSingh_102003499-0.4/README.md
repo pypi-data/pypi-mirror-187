9cc375c2b503ab617d5a396ef6d40feee435c2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:56 +0000

    Add package Topsis-HarshitaSingh-102003499/0.4/Topsis_HarshitaSingh_102003499-0.4.tar.gz

---
+++ packages/Topsis-HarshitaSingh-102003499/Topsis_HarshitaSingh_102003499-0.4.tar.gz/Topsis_HarshitaSingh_102003499-0.4/README.md
@@ -0,0 +1,21 @@
+## Topsis_HarshitaSingh
+
+# TOPSIS
+
+**Submitted By**: Harshita Singh - 102003499.
+
+Description: **Evaluation of alternatives based on multiple criteria using TOPSIS method.**.
+
+---
+
+## How to install this package:
+
+```
+>> pip install Topsis_HarshitaSingh_102003499
+```
+
+### In Command Prompt
+
+```
+>> python <program.py> <InputDataFile> <Weights> <Impacts> <ResultFileName>
+```
---
+++ packages/Topsis-HarshitaSingh-102003499/Topsis_HarshitaSingh_102003499-0.4.tar.gz/Topsis_HarshitaSingh_102003499-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(name="Topsis_HarshitaSingh_102003499",version="0.4",
+description="Given package has the code for topsis, which is one of the techniques used for multiple criteria decision making. It chooses the Euclidean distance from the ideal solution and greatest distance from the negative ideal solution. ",
+long_description=long_description,
+    long_description_content_type="text/markdown",
+# long_description="Technique for Order Preference by Similarity to Ideal Solution (TOPSIS) originated in the 1980s as a multi-criteria decision making method. TOPSIS chooses the alternative of shortest Euclidean distance from the ideal solution, and greatest distance from the negative-ideal solution.",
+author="Harshita Singh",
+packages=['Topsis_HarshitaSingh_102003499'],
+install_requires=['pandas'],
+entry_points={
+        "console_scripts": [
+            "topsis=Topsis_HarshitaSingh_102003499.topsis:main",
+        ]
+    },
+)
