07c223a1903c28cdc6cdcf04815f84b85d8e7445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:20 +0000

    Add package grpc-auth-service/0.0.22/grpc_auth_service-0.0.22.tar.gz

---
+++ packages/grpc-auth-service/grpc_auth_service-0.0.22.tar.gz/grpc_auth_service-0.0.22/src/grpc_auth_service/utils/constants.py
@@ -0,0 +1,7 @@
+class Constants:
+    RESOURCE: str = "resource"
+    RESOURCE_PATH: str = "path"
+    RESOURCE_SERVICE: str = "service"
+    RESOURCE_QUERY_PARAMS: str = "query_params"
+    RESOURCE_PATH_PARAMS: str = "path_params"
+    ACTION: str = "action"
