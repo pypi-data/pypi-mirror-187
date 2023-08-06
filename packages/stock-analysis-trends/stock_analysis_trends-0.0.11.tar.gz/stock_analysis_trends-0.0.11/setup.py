6c30f12ea249f7d28b16580b4d8e0cfb77cc0b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:40 +0000

    Add package stock-analysis-trends/0.0.11/stock_analysis_trends-0.0.11.tar.gz

---
+++ packages/stock-analysis-trends/stock_analysis_trends-0.0.11.tar.gz/stock_analysis_trends-0.0.11/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.11'
+DESCRIPTION = 'Perform stock analysis on Indian and American stocks'
+LONG_DESCRIPTION = 'A package that allows to perform analysis and check if a stock is bullish or bearish'
+
+# Setting up
+setup(
+    name="stock_analysis_trends",
+    version=VERSION,
+    author="Arjun P",
+    author_email="<arjunprakash027@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['yahoofinancials'],
+    keywords=['python', 'stock', 'bullish', 'bearish', 'analysis', 'stockmarket'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
---
+++ packages/stock-analysis-trends/stock_analysis_trends-0.0.11.tar.gz/stock_analysis_trends-0.0.11/stock_analysis/__init__.py
@@ -0,0 +1,1 @@
+from stock_analysis_trends import perform_analysis
