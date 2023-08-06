8aea15b30de80b9748b356328ec6eb0c0889fb8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:25 +0000

    Add package haupt/0.0.2/haupt-0.0.2.tar.gz

---
+++ packages/haupt/haupt-0.0.2.tar.gz/haupt-0.0.2/haupt/proxies/schemas/streams/base.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+#
+# Copyright 2018-2023 Polyaxon, Inc.
+# This file and its contents are licensed under the AGPLv3 License.
+# Please see the included NOTICE for copyright information and
+# LICENSE-AGPL for a copy of the license.
+
+from haupt import settings
+from haupt.proxies.schemas.auth import get_auth_config
+from haupt.proxies.schemas.base import clean_config
+from haupt.proxies.schemas.dns import get_resolver
+from haupt.proxies.schemas.gateway.api import get_api_location_config
+from haupt.proxies.schemas.locations import get_streams_locations_config
+from haupt.proxies.schemas.scaffold import get_scaffold_config
+from haupt.proxies.schemas.streams.api import get_streams_config
+from haupt.proxies.schemas.streams.k8s import get_k8s_root_location_config
+
+
+def get_base_config(is_gateway: bool = True):
+    resolver = get_resolver()
+    auth = get_auth_config()
+    api_configs = [
+        get_streams_config(),
+    ]
+    api_location_configs = [
+        get_streams_locations_config(),
+        get_k8s_root_location_config(),
+    ]
+    if is_gateway:
+        api_location_configs.append(
+            get_api_location_config(resolver=resolver, auth=auth)
+        )
+    config = get_scaffold_config(
+        is_proxy=False,
+        port=settings.PROXIES_CONFIG.streams_target_port,
+        use_ssl_config=is_gateway,
+        use_assets_config=is_gateway,
+        use_services_configs=is_gateway,
+        resolver=resolver,
+        auth=auth,
+        api_configs=api_configs,
+        api_location_configs=api_location_configs,
+        is_local_streams_service=True,
+    )
+    return clean_config(config)
---
+++ packages/haupt/haupt-0.0.2.tar.gz/haupt-0.0.2/haupt/settings.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+#
+# Copyright 2018-2023 Polyaxon, Inc.
+# This file and its contents are licensed under the AGPLv3 License.
+# Please see the included NOTICE for copyright information and
+# LICENSE-AGPL for a copy of the license.
+
+from marshmallow import ValidationError
+
+from polyaxon.services.values import PolyaxonServices
+from polyaxon.utils.formatting import Printer
+
+PROXIES_CONFIG = None
+SANDBOX_CONFIG = None
+
+PolyaxonServices.set_service_name()
+
+
+def set_proxies_config():
+    from haupt.managers.proxies import ProxiesManager
+
+    global PROXIES_CONFIG
+
+    PROXIES_CONFIG = ProxiesManager.get_config_from_env()
+
+
+def set_sandbox_config():
+    from haupt.managers.sandbox import SandboxConfigManager
+    from polyaxon.contexts.paths import mount_sandbox
+    from polyaxon.settings import set_agent_config
+
+    mount_sandbox()
+    PolyaxonServices.set_service_name(PolyaxonServices.SANDBOX)
+
+    global SANDBOX_CONFIG
+
+    try:
+        SANDBOX_CONFIG = SandboxConfigManager.get_config_or_default()
+        set_agent_config(SANDBOX_CONFIG)
+    except (TypeError, ValidationError):
+        SandboxConfigManager.purge()
+        Printer.warning("Your sandbox configuration was purged!")
