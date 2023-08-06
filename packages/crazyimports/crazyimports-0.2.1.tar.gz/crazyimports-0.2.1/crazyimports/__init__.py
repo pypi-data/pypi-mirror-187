44631ae7029305e60bc866538743e3324b93aac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:29 +0000

    Add package crazyimports/0.2.1/crazyimports-0.2.1.tar.gz

---
+++ packages/crazyimports/crazyimports-0.2.1.tar.gz/crazyimports-0.2.1/crazyimports/__init__.py
@@ -0,0 +1,16 @@
+"""crazyimports module allows you to use your data files as if it was python modules"""
+import sys
+
+from ._common import ExPathFinder
+from ._csv import CSV
+from ._json import JSON
+from ._pickle import Pickle
+from ._sqlite import SQLite3
+from ._yaml import YAML
+from ._toml import TOML
+
+__version__ = "0.2.1"
+
+__all__ = []
+
+sys.meta_path.append(ExPathFinder())
