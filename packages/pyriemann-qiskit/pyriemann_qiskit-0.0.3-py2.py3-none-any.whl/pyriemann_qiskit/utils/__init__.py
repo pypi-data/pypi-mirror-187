b83334554d31a73c85341c031c95f49736e7f1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:03 +0000

    Add package pyriemann-qiskit/0.0.3/pyriemann_qiskit-0.0.3-py2.py3-none-any.whl

---
+++ packages/pyriemann-qiskit/pyriemann_qiskit-0.0.3-py2.py3-none-any.whl/pyriemann_qiskit/utils/__init__.py
@@ -0,0 +1,21 @@
+from . import hyper_params_factory, filtering
+from .math import cov_to_corr_matrix
+from .docplex import (square_cont_mat_var,
+                      square_int_mat_var,
+                      square_bin_mat_var,
+                      ClassicalOptimizer,
+                      NaiveQAOAOptimizer)
+from .firebase_connector import FirebaseConnector, Cache
+
+__all__ = [
+    'hyper_params_factory',
+    'filtering',
+    'cov_to_corr_matrix',
+    'square_cont_mat_var',
+    'square_int_mat_var',
+    'square_bin_mat_var',
+    'ClassicalOptimizer',
+    'NaiveQAOAOptimizer',
+    'FirebaseConnector',
+    'Cache'
+]
