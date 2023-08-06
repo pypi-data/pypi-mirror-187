1de33c2fd5239442ae59549725690c29eba018d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:07 +0000

    Add package tricky/0.0.1rc0/tricky-0.0.1rc0.tar.gz

---
+++ packages/tricky/tricky-0.0.1rc0.tar.gz/tricky-0.0.1rc0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tricky']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'tricky',
+    'version': '0.0.1rc0',
+    'description': 'A set of useful features to make working with your code easier.',
+    'long_description': '# tricky',
+    'author': 'Alexander Walther',
+    'author_email': 'alexander.walther.engineering@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/Walther-s-Engineering/tricky',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
