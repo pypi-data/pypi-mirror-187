4eb7e0ab5ecf33a87594ff6e1040e26a8078d042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:20 +0000

    Add package algormeter/0.9.3/algormeter-0.9.3.tar.gz

---
+++ packages/algormeter/algormeter-0.9.3.tar.gz/algormeter-0.9.3/tests/test_kernel.py
@@ -0,0 +1,50 @@
+
+from algormeter.libs import *
+
+def test_cache():
+    p = Parab(2)
+    x = np.ones(2)
+    p.f1(x)
+    assert counter.get('f1') == 1
+    p.f1(x)
+    assert counter.get('f1') == 1
+    assert counter.get('f2') == None 
+    p.f(x)
+    assert counter.get('f1') == 1
+    assert counter.get('f2') == 1 
+    assert counter.get('gf2') == None 
+    p.gf(x)
+    assert counter.get('f1') == 1
+    assert counter.get('f2') == 1 
+    assert counter.get('gf2') == 1 
+
+def test_cacheSize():
+    p = Parab(2)
+    for i in range(Kernel.CACHESIZE):
+        x = np.zeros(2)+i
+        p.f(x)
+    assert counter.get('f1') == Kernel.CACHESIZE 
+    p.f(x) # type: ignore
+    assert counter.get('f1') == Kernel.CACHESIZE 
+
+def test_cache_life(): 
+    p = Parab(2)
+    y = -np.ones(2)
+    for i in range(Kernel.CACHESIZE-1):
+        p.f(y)
+        x = np.zeros(2)+i
+        p.f(x)
+    assert counter.get('f1') == Kernel.CACHESIZE 
+    p.f(y)
+    assert counter.get('f1') == Kernel.CACHESIZE 
+
+
+def test_explib():
+    def explib(lib):
+        for el, dims in lib:
+            for d in dims:
+                p = el(d)
+                assert np.isclose(p.f(p.optimumPoint), p.optimumValue) 
+    explib(probList_DCJBKM)
+    explib(probList_coax)
+    explib(probList_base)
