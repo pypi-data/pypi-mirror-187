cc692d843abc8f5e794d2a6f7f738841527ce86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:16 +0000

    Add package uplc/0.5.0/uplc-0.5.0.tar.gz

---
+++ packages/uplc/uplc-0.5.0.tar.gz/uplc-0.5.0/uplc/lexer.py
@@ -0,0 +1,44 @@
+from rply import LexerGenerator
+import re
+
+
+def strip_comments(s: str):
+    ssub = re.sub(r"--.*$", "", s, flags=re.RegexFlag.MULTILINE)
+    return ssub
+
+
+TOKENS = {
+    "PROGRAM": r"\bprogram\b",
+    "LAMBDA": r"\blam\b",
+    "FORCE": r"\bforce\b",
+    "DELAY": r"\bdelay\b",
+    "BUILTIN": r"\bbuiltin\b",
+    "CON": r"\bcon\b",
+    "ERROR": r"\berror\b",
+    "PAREN_OPEN": r"\(",
+    "PAREN_CLOSE": r"\)",
+    "BRACK_OPEN": r"\[",
+    "BRACK_CLOSE": r"\]",
+    "CARET_OPEN": r"\<",
+    "CARET_CLOSE": r"\>",
+    "TEXT": r'"[^\r\n"]*"',
+    "COMMA": r",",
+    "DOT": r"\.",
+    "NUMBER": r"\-?\d+",
+    "NAME": r"[\w_~'][\w\d_~'!#]*",
+    "HEX": r"#([\dabcdefABCDEF][\dabcdefABCDEF])*",
+}
+
+
+class Lexer:
+    def __init__(self):
+        self.lexer = LexerGenerator()
+
+    def _add_tokens(self):
+        for k, v in TOKENS.items():
+            self.lexer.add(k, v)
+        self.lexer.ignore("\s+")
+
+    def get_lexer(self):
+        self._add_tokens()
+        return self.lexer.build()
