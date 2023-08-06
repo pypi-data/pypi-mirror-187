31d8f757843f5148443b0ad6fa9b5fab27529fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:29 +0000

    Add package reloadium/0.9.9/reloadium-0.9.9-py3-none-any.whl

---
+++ packages/reloadium/reloadium-0.9.9-py3-none-any.whl/reloadium/lib/extensions/pytest_extension_guard.py
@@ -0,0 +1,22 @@
+import sys
+
+from reloadium.corium.utils.comp import fix_sep
+
+fix_sep()
+
+
+try:
+    import _pytest.assertion.rewrite
+except ImportError:
+    class AssertionRewritingHook:
+        pass
+
+    _pytest = lambda: None  # type: ignore
+    sys.modules["_pytest"] = _pytest
+
+    _pytest.assertion = lambda: None  # type: ignore
+    sys.modules["_pytest.assertion"] = _pytest.assertion
+
+    _pytest.assertion.rewrite = lambda: None  # type: ignore
+    _pytest.assertion.rewrite.AssertionRewritingHook = AssertionRewritingHook  # type: ignore
+    sys.modules["_pytest.assertion.rewrite"] = _pytest.assertion.rewrite
