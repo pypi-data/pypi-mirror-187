17352259f23b8fc148a4b4ada9f385529ac21efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:09 +0000

    Add package PyKotor/1.6.0/PyKotor-1.6.0-py3-none-any.whl

---
+++ packages/PyKotor/PyKotor-1.6.0-py3-none-any.whl/pykotor/resource/formats/ncs/optimizers.py
@@ -0,0 +1,60 @@
+from copy import copy
+
+from pykotor.resource.formats.ncs import NCS
+from pykotor.resource.formats.ncs.ncs_data import NCSOptimizer, NCSInstructionType
+
+
+class RemoveNopOptimizer(NCSOptimizer):
+    """
+    NCS Compiler uses NOP instructions as stubs to simplify the compilation process however as their name suggests
+    they do not perform any actual function. This optimizer removes all occureses of NOP instructions from the
+    compiled script.
+    """
+
+    def optimize(self, ncs: NCS) -> None:
+        nops = [inst for inst in ncs.instructions if inst.ins_type == NCSInstructionType.NOP]
+
+        # Process instructions which jump to a NOP and set them to jump to the proceeding instruction instead
+        for nop in nops:
+            nop_index = ncs.instructions.index(nop)
+            for link in ncs.links_to(nop):
+                link.jump = ncs.instructions[nop_index+1]
+
+        # It is now safe to remove all NOP instructions
+        ncs.instructions = [inst for inst in ncs.instructions if inst.ins_type != NCSInstructionType.NOP]
+
+
+class RemoveMoveSPEqualsZeroOptimizer(NCSOptimizer):
+    def optimize(self, ncs: NCS) -> None:
+        movsp0 = [inst for inst in ncs.instructions if inst.ins_type == NCSInstructionType.MOVSP and inst.args[0] == 0]
+
+        # Process instructions which jump to a MOVSP=0 and set them to jump to the proceeding instruction instead
+        for op in movsp0:
+            nop_index = ncs.instructions.index(op)
+            for link in ncs.links_to(op):
+                link.jump = ncs.instructions[nop_index + 1]
+
+        # It is now safe to remove all MOVSP=0 instructions
+        for inst in copy(ncs.instructions):
+            if inst.ins_type == NCSInstructionType.MOVSP and inst.args[0] == 0:
+                ncs.instructions.remove(inst)
+
+
+class MergeAdjacentMoveSPOptimizer(NCSOptimizer):
+    def optimize(self, ncs: NCS) -> None:
+        raise NotImplementedError()
+
+
+class RemoveJMPToAdjacentOptimizer(NCSOptimizer):
+    def optimize(self, ncs: NCS) -> None:
+        raise NotImplementedError()
+
+
+class RemoveUnusedBlocksOptimizer(NCSOptimizer):
+    def optimize(self, ncs: NCS) -> None:
+        raise NotImplementedError()
+
+
+class RemoveUnusedGlobalsInStackOptimizer(NCSOptimizer):
+    def optimize(self, ncs: NCS) -> None:
+        raise NotImplementedError()
