c02a03b9d03b2fbb8075cee4b5540346139ca723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:42 +0000

    Add package robyn/0.23.0/robyn-0.23.0.tar.gz

---
+++ packages/robyn/robyn-0.23.0.tar.gz/robyn-0.23.0/integration_tests/conftest.py
@@ -0,0 +1,93 @@
+import os
+import pathlib
+import signal
+import subprocess
+import sys
+import time
+from typing import List
+
+import pytest
+
+
+def spawn_process(command: List[str]) -> subprocess.Popen:
+    if sys.platform.startswith("win32"):
+        command[0] = "python"
+        process = subprocess.Popen(
+            command, shell=True, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP
+        )
+        return process
+    process = subprocess.Popen(command, preexec_fn=os.setsid)
+    return process
+
+
+def kill_process(process: subprocess.Popen) -> None:
+    if sys.platform.startswith("win32"):
+        process.send_signal(signal.CTRL_BREAK_EVENT)
+        process.kill()
+        return
+
+    try:
+        os.killpg(os.getpgid(process.pid), signal.SIGKILL)
+    except ProcessLookupError:
+        pass
+
+
+@pytest.fixture(scope="session")
+def session():
+    os.environ["ROBYN_URL"] = "127.0.0.1"
+    current_file_path = pathlib.Path(__file__).parent.resolve()
+    base_routes = os.path.join(current_file_path, "./base_routes.py")
+    command = ["python3", base_routes]
+    process = spawn_process(command)
+    time.sleep(1)
+    yield
+    kill_process(process)
+
+
+@pytest.fixture(scope="session")
+def default_session():
+    current_file_path = pathlib.Path(__file__).parent.resolve()
+    base_routes = os.path.join(current_file_path, "./base_routes.py")
+    command = ["python3", base_routes]
+    process = spawn_process(command)
+    time.sleep(1)
+    yield
+    kill_process(process)
+
+
+@pytest.fixture(scope="session")
+def global_session():
+    os.environ["ROBYN_URL"] = "0.0.0.0"
+    current_file_path = pathlib.Path(__file__).parent.resolve()
+    base_routes = os.path.join(current_file_path, "./base_routes.py")
+    command = ["python3", base_routes]
+    process = spawn_process(command)
+    time.sleep(1)
+    yield
+    kill_process(process)
+
+
+@pytest.fixture(scope="session")
+def dev_session():
+    os.environ["ROBYN_URL"] = "127.0.0.1"
+    os.environ["ROBYN_PORT"] = "8081"
+    current_file_path = pathlib.Path(__file__).parent.resolve()
+    base_routes = os.path.join(current_file_path, "./base_routes.py")
+    command = ["python3", base_routes, "--dev"]
+    process = spawn_process(command)
+    time.sleep(1)
+    yield
+    kill_process(process)
+
+
+@pytest.fixture(scope="session")
+def test_session():
+    os.environ["ROBYN_URL"] = "127.0.0.1"
+    os.environ["ROBYN_PORT"] = "8080"
+    current_file_path = pathlib.Path(__file__).parent.resolve()
+    base_routes = os.path.join(current_file_path, "./base_routes.py")
+    command = ["python3", base_routes, "--dev"]
+    process = spawn_process(command)
+    time.sleep(1)
+    yield
+    kill_process(process)
---
+++ packages/robyn/robyn-0.23.0.tar.gz/robyn-0.23.0/integration_tests/test_status_code.py
@@ -0,0 +1,23 @@
+import requests
+
+BASE_URL = "http://127.0.0.1:8080"
+
+
+def test_404_status_code(session):
+    res = requests.get(f"{BASE_URL}/404")
+    assert res.status_code == 404
+
+
+def test_404_post_request_status_code(session):
+    r = requests.post(f"{BASE_URL}/404")
+    assert r.status_code == 404
+
+
+def test_307_get_request(session):
+    r = requests.get(f"{BASE_URL}/redirect")
+    assert r.text == "This is the redirected route"
+
+
+def test_int_status_code(session):
+    r = requests.get(f"{BASE_URL}/int_status_code")
+    assert r.status_code == 202
