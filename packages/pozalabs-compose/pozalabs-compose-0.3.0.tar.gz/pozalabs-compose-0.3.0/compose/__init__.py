fceb072d9c1db6ba293cca3510120c72abe050a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:53 +0000

    Add package pozalabs-compose/0.3.0/pozalabs-compose-0.3.0.tar.gz

---
+++ packages/pozalabs-compose/pozalabs-compose-0.3.0.tar.gz/pozalabs-compose-0.3.0/compose/__init__.py
@@ -0,0 +1,13 @@
+from . import entity, field, query, repository, schema, types
+from .container import BaseModel, TimeStampedModel
+
+__all__ = [
+    "BaseModel",
+    "TimeStampedModel",
+    "entity",
+    "field",
+    "schema",
+    "repository",
+    "query",
+    "types",
+]
