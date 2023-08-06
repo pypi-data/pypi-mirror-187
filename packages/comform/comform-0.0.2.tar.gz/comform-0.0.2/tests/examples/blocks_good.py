7f3bd95c56566274fe82c30a9a5e97bd6ca251f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:01 +0000

    Add package comform/0.0.2/comform-0.0.2.tar.gz

---
+++ packages/comform/comform-0.0.2.tar.gz/comform-0.0.2/tests/examples/blocks_good.py
@@ -0,0 +1,9 @@
+# this is a really long piece of text. lets get some bad spacing some random short new
+# lines as well as some lines which absolutely exceed any reasonable length with no
+# consideration for people with small screens. we're not all rich maybe some bullets?
+#
+# - hey there person will this bullet be too long? oh nooooooo please keep it shorter so
+#   i can read it ughhh
+# - this bullet is shorter
+
+print("hello, world")
---
+++ packages/comform/comform-0.0.2.tar.gz/comform-0.0.2/tests/examples/dividers_bad.py
@@ -0,0 +1,9 @@
+# ---- this is a section divider -------- #
+
+print("section 1")
+
+# - this is      another  with bad   spacing    - #
+
+print("section 2")
+
+# ------ finally here      is one that's too   long ---------------------------------------------------------------- #
