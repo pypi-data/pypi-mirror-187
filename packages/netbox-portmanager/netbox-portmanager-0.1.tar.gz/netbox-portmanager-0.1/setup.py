fb2cabd48ed38805ab4518702a57b554aa0a32de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:30 +0000

    Add package netbox-portmanager/0.1/netbox-portmanager-0.1.tar.gz

---
+++ packages/netbox-portmanager/netbox-portmanager-0.1.tar.gz/netbox-portmanager-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-portmanager',
+    version='0.1',
+    description='Netbox Plugin - Portmanager',
+    long_description='Netbox Plugin - Portmanager',
+    url='https://github.com/dansheps/netbox-secretstore/',
+    download_url='https://www.pypi.org/project/netbox-secretstore/',
+    author='Michal Drobny',
+    author_email='ddrobny@ics.muni.cz',
+    license='Apache 2.0',
+    install_requires=[
+        'pysnmp'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8",
+    zip_safe=False,
+)
