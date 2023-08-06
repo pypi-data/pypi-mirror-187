cd64aa6c8f7be19880326fde78f43005f3377a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:38 +0000

    Add package IPAM-PACMan/0.9.5/ipam_pacman-0.9.5-py3-none-any.whl

---
+++ packages/IPAM-PACMan/ipam_pacman-0.9.5-py3-none-any.whl/ipam-pacman/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 22 21:33:19 2022
+
+@author: ollep
+"""
+
+__version__ = '0.9.5'
+
+submodules = [
+    'IRHM',
+    'gui',
+    'SCM',
+]
+
+
+from .PacMan import PACMan
