4bbb00a284893876cd82c7ae986c824310c92bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:04 +0000

    Add package pyhtmlgui/3.2/pyhtmlgui-3.2.tar.gz

---
+++ packages/pyhtmlgui/pyhtmlgui-3.2.tar.gz/pyhtmlgui-3.2/pyhtmlgui/lib/__init__.py
@@ -0,0 +1,5 @@
+from .weakFunctionReferences import WeakFunctionReferences
+from .browser import Browser
+from .observable import Observable
+from .observableDict import ObservableDict
+from .observableList import ObservableList
