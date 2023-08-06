29ee8cf1baa3501a934c58460ab5b89e2f7d7316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:20 +0000

    Add package newsdataapi/0.1.6/newsdataapi-0.1.6.tar.gz

---
+++ packages/newsdataapi/newsdataapi-0.1.6.tar.gz/newsdataapi-0.1.6/newsdataapi/constants.py
@@ -0,0 +1,21 @@
+# All the Newsdata supported API URL
+BASE_URL = 'https://newsdata.io/api/1/'
+
+# Latest News URL
+NEWS_URL = BASE_URL + 'news'
+
+
+# News Archive URL
+ARCHIVE_URL = BASE_URL + 'archive'
+
+
+# News Sources URL
+SOURCES_URL = BASE_URL + 'sources'
+
+
+# News Crypto URL
+CRYPTO_URL = BASE_URL + 'crypto'
+
+
+# Default request timeout is 300 seconds
+DEFAULT_REQUEST_TIMEOUT = 300
