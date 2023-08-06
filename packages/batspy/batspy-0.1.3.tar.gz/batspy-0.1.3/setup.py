4c14d8283668901f7cae19bc99f4cc5f8afaa293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:45 +0000

    Add package batspy/0.1.3/batspy-0.1.3.tar.gz

---
+++ packages/batspy/batspy-0.1.3.tar.gz/batspy-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='batspy',
+    version='0.1.3',
+    description='Testing framework for bash scripts',
+    author='Tom Shabtay',
+    author_email='tomshabtay@gmail.com',
+    url='https://github.com/tomshabtay/batspy',
+    packages=['batspy'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+)
