fe35ad1c14458af93e04e1c19f99a1188fc8bf0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:02 +0000

    Add package mkdocs-png-to-webp/0.0.1/mkdocs-png-to-webp-0.0.1.tar.gz

---
+++ packages/mkdocs-png-to-webp/mkdocs-png-to-webp-0.0.1.tar.gz/mkdocs-png-to-webp-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pip>=22", "mkdocs>=1", "webptools==0.0.9"]
+
+setup(
+    name="mkdocs-png-to-webp",
+    version="0.0.1",
+    author="mur4d1n",
+    author_email="mur4d1n@yandex.ru",
+    description="A package to convert your mkdocs images from png to webp",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mur4d1n-lib/mkdocs-png-to-webp/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
