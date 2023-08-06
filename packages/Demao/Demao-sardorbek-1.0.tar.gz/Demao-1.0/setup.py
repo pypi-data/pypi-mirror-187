b447188ca139657cdcfc7d1cf39446367b5c0627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:55 +0000

    Add package Demao/1.0/Demao-sardorbek-1.0.tar.gz

---
+++ packages/Demao/Demao-sardorbek-1.0.tar.gz/Demao-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name='Demao',  #          
+    version='1.0',  # version number
+    description='Test Local Publish Module',  #
+    author='SardorbekDeveloper22',  # Author
+    author_email='sardorbekqozoqov2281@gmail.com',
+    py_modules=['Demao.demo1', 'Demao.demo2'],  # To release the module
+)
