a21b103f6148472b97b516627af1ada192bc38cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:41 +0000

    Add package gaitmap-datasets/0.7.0/gaitmap_datasets-0.7.0-py3-none-any.whl

---
+++ packages/gaitmap-datasets/gaitmap_datasets-0.7.0-py3-none-any.whl/gaitmap_datasets/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Methods to access the open source gait datasets of the MaD-Lab."""
+from gaitmap_datasets.egait_parameter_validation_2013 import EgaitParameterValidation2013
+from gaitmap_datasets.egait_segmentation_validation_2014 import EgaitSegmentationValidation2014
+from gaitmap_datasets.pyshoe_2019 import PyShoe2019Hallway, PyShoe2019Stairs, PyShoe2019Vicon
+from gaitmap_datasets.sensor_position_comparison_2019 import (
+    SensorPositionComparison2019Mocap,
+    SensorPositionComparison2019Segmentation,
+)
+from gaitmap_datasets.stair_ambulation_healthy_2021 import (
+    StairAmbulationHealthy2021Full,
+    StairAmbulationHealthy2021PerTest,
+)
+
+__all__ = [
+    "EgaitSegmentationValidation2014",
+    "EgaitParameterValidation2013",
+    "StairAmbulationHealthy2021PerTest",
+    "StairAmbulationHealthy2021Full",
+    "SensorPositionComparison2019Segmentation",
+    "SensorPositionComparison2019Mocap",
+    "PyShoe2019Vicon",
+    "PyShoe2019Hallway",
+    "PyShoe2019Stairs",
+]
+__version__ = "0.7.0"
