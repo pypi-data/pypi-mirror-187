3ff2d419932664e2225ea5ce9f4fc2b9314e9215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:24 +0000

    Add package Topsis-Aditi-102003380/0.1/Topsis-Aditi-102003380-0.1.tar.gz

---
+++ packages/Topsis-Aditi-102003380/Topsis-Aditi-102003380-0.1.tar.gz/Topsis-Aditi-102003380-0.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+  name = 'Topsis-Aditi-102003380',         
+  packages = ['Topsis-Aditi-102003380'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'A Python package to find TOPSIS for multi-criteria decision analysis method',   
+  author = 'ADITI NIRWAN',                   
+  author_email = 'anirwan_be20@thapar.edu',      
+  url = 'https://github.com/aditinirwan/Topsis-Aditi-102003380',   
+  download_url = 'https://github.com/aditinirwan/Topsis-Aditi-102003380/archive/refs/tags/v_01.tar.gz',    
+  keywords = ['Topsis', 'Data Science', 'UCS654'],   
+  install_requires=[            
+          'pandas', 'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
