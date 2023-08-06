36ef13b4ed55e6ecfcdeb3ddff76bff665df2aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:16 +0000

    Add package Flask-RDN/1.0.0/Flask-RDN-1.0.0.tar.gz

---
+++ packages/Flask-RDN/Flask-RDN-1.0.0.tar.gz/Flask-RDN-1.0.0/tests/main.py
@@ -0,0 +1,10 @@
+import sys
+import unittest
+
+from test_basic_ops import *
+from test_query_params import *
+from test_query_params_with_body import *
+from test_b64_query_params import *
+from test_b64_query_params_with_body import *
+
+unittest.main(argv=sys.argv)
