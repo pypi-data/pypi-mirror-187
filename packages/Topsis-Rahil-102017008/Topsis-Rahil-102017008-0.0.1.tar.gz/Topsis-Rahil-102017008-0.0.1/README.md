efced5de59030f260b3a9371156fd28b79b887e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:51 +0000

    Add package Topsis-Rahil-102017008/0.0.1/Topsis-Rahil-102017008-0.0.1.tar.gz

---
+++ packages/Topsis-Rahil-102017008/Topsis-Rahil-102017008-0.0.1.tar.gz/Topsis-Rahil-102017008-0.0.1/README.md
@@ -0,0 +1,2 @@
+It is a python library for dealing with MCDM problems using Technique for Order of Preference by Similarity to Ideal Solution(TOPSIS).
+Also it gives Ranks on basis of Topsis Score.
---
+++ packages/Topsis-Rahil-102017008/Topsis-Rahil-102017008-0.0.1.tar.gz/Topsis-Rahil-102017008-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description=fh.read()
+
+setuptools.setup(
+    name="Topsis-Rahil-102017008",
+    version="0.0.1",
+    author="Rahil Raj Verma",
+    description="Used for calculation of Topsis Score and Rank according to it",
+    python_requires='>=3.6'
+)
