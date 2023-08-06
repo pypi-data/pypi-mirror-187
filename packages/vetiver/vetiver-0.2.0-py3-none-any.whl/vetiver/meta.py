4714ff7813d2f477290b21ded8493706f47ff685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:24 +0000

    Add package vetiver/0.2.0/vetiver-0.2.0-py3-none-any.whl

---
+++ packages/vetiver/vetiver-0.2.0-py3-none-any.whl/vetiver/meta.py
@@ -0,0 +1,36 @@
+import sys
+from dataclasses import dataclass, asdict, field
+from typing import Mapping
+
+
+@dataclass
+class VetiverMeta:
+    """Metadata in a VetiverModel"""
+
+    user: "dict | None" = field(default_factory=dict)
+    version: "str | None" = None
+    url: "str | None" = None
+    required_pkgs: "list | None" = field(default_factory=list)
+    python_version: "tuple | None" = None
+
+    def to_dict(self) -> Mapping:
+        data = asdict(self)
+
+        return data
+
+    @classmethod
+    def from_dict(cls, metadata, pip_name=None) -> "VetiverMeta":
+
+        metadata = {} if metadata is None else metadata
+
+        user = metadata.get("user", metadata)
+        version = metadata.get("version", None)
+        url = metadata.get("url", None)
+        required_pkgs = metadata.get("required_pkgs", [])
+        python_version = tuple(metadata.get("python_version", sys.version_info))
+
+        if pip_name:
+            if not list(filter(lambda x: pip_name in x, required_pkgs)):
+                required_pkgs = required_pkgs + [f"{pip_name}"]
+
+        return cls(user, version, url, required_pkgs, python_version)
---
+++ packages/vetiver/vetiver-0.2.0-py3-none-any.whl/vetiver/tests/test_pytorch.py
@@ -0,0 +1,127 @@
+import pytest
+
+torch = pytest.importorskip("torch", reason="torch library not installed")
+
+import numpy as np  # noqa
+from fastapi.testclient import TestClient  # noqa
+
+from vetiver.vetiver_model import VetiverModel  # noqa
+from vetiver import VetiverAPI  # noqa
+
+
+def _build_torch_v():
+
+    input_size = 1
+    output_size = 1
+
+    x_train = np.array(
+        [
+            [3.3],
+            [4.4],
+            [5.5],
+            [6.71],
+            [6.93],
+            [4.168],
+            [9.779],
+            [6.182],
+            [7.59],
+            [2.167],
+            [7.042],
+            [10.791],
+            [5.313],
+            [7.997],
+            [3.1],
+        ],
+        dtype=np.float32,
+    )
+
+    torch_model = torch.nn.Linear(input_size, output_size)
+    return x_train, torch_model
+
+
+def test_vetiver_build():
+
+    x_train, torch_model = _build_torch_v()
+
+    vt2 = VetiverModel(
+        model=torch_model,
+        prototype_data=x_train,
+        model_name="torch",
+        versioned=None,
+        description=None,
+        metadata=None,
+    )
+
+    assert vt2.model == torch_model
+    assert vt2.metadata.required_pkgs == ["torch"]
+
+
+def test_torch_predict_ptype():
+    torch.manual_seed(3)
+    x_train, torch_model = _build_torch_v()
+    v = VetiverModel(torch_model, model_name="torch", prototype_data=x_train)
+    v_api = VetiverAPI(v)
+
+    client = TestClient(v_api.app)
+    data = {"0": 3.3}
+    response = client.post("/predict", json=data)
+
+    assert response.status_code == 200, response.text
+    assert response.json() == {"predict": [-4.060722351074219]}, response.text
+
+
+def test_torch_predict_ptype_batch():
+    torch.manual_seed(3)
+    x_train, torch_model = _build_torch_v()
+    v = VetiverModel(torch_model, model_name="torch", prototype_data=x_train)
+    v_api = VetiverAPI(v)
+
+    client = TestClient(v_api.app)
+    data = [{"0": 3.3}, {"0": 3.3}]
+    response = client.post("/predict", json=data)
+
+    assert response.status_code == 200, response.text
+    assert response.json() == {
+        "predict": [[-4.060722351074219], [-4.060722351074219]]
+    }, response.text
+
+
+def test_torch_predict_ptype_error():
+
+    x_train, torch_model = _build_torch_v()
+    v = VetiverModel(torch_model, model_name="torch", prototype_data=x_train)
+    v_api = VetiverAPI(v)
+
+    client = TestClient(v_api.app)
+    data = {"0": "bad"}
+    response = client.post("/predict", json=data)
+
+    assert response.status_code == 422, response.text  # value is not a valid float
+
+
+def test_torch_predict_no_ptype_batch():
+    torch.manual_seed(3)
+    x_train, torch_model = _build_torch_v()
+    v = VetiverModel(torch_model, model_name="torch")
+    v_api = VetiverAPI(v, check_prototype=False)
+
+    client = TestClient(v_api.app)
+    data = [[3.3], [3.3]]
+    response = client.post("/predict", json=data)
+    assert response.status_code == 200, response.text
+    assert response.json() == {
+        "predict": [[-4.060722351074219], [-4.060722351074219]]
+    }, response.text
+
+
+def test_torch_predict_no_ptype():
+    torch.manual_seed(3)
+    x_train, torch_model = _build_torch_v()
+    v = VetiverModel(torch_model, model_name="torch")
+    v_api = VetiverAPI(v, check_prototype=False)
+
+    client = TestClient(v_api.app)
+    data = [[3.3]]
+    response = client.post("/predict", json=data)
+    assert response.status_code == 200, response.text
+    assert response.json() == {"predict": [[-4.060722351074219]]}, response.text
