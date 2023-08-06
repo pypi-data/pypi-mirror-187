40a36e099a771f2a9afc0560f651d23e18ad56ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:54 +0000

    Add package funcx-common/0.0.24/funcx_common-0.0.24-py3-none-any.whl

---
+++ packages/funcx-common/funcx_common-0.0.24-py3-none-any.whl/funcx_common/messagepack/message_types/ep_status_report.py
@@ -0,0 +1,18 @@
+import typing as t
+import uuid
+
+from .base import Message, meta
+from .task_transition import TaskTransition
+
+
+@meta(message_type="ep_status_report")
+class EPStatusReport(Message):
+    """
+    Status report for an endpoint, sent from Endpoint to Forwarder.
+
+    Includes EP-wide info such as utilization, as well as per-task status information.
+    """
+
+    endpoint_id: uuid.UUID
+    ep_status_report: t.Dict[str, t.Any]
+    task_statuses: t.Dict[str, t.List[TaskTransition]]
---
+++ packages/funcx-common/funcx_common-0.0.24-py3-none-any.whl/funcx_common/tasks/constants.py
@@ -0,0 +1,30 @@
+import enum
+
+
+# subclass from str so that the enum can be JSON-encoded without adjustment
+class TaskState(str, enum.Enum):
+    RECEIVED = "received"  # on receiving a task web-side
+    WAITING_FOR_EP = "waiting-for-ep"  # while waiting for ep to accept/be online
+    WAITING_FOR_NODES = "waiting-for-nodes"  # jobs are pending at the scheduler
+    WAITING_FOR_LAUNCH = "waiting-for-launch"
+    EXEC_START = "execution-start"
+    EXEC_END = "execution-end"
+    RUNNING = "running"
+    SUCCESS = "success"
+    FAILED = "failed"
+    RESULT_RECEIVED = "result-received"
+    RESULT_ENQUEUED = "result-enqueued"
+
+
+class InternalTaskState(str, enum.Enum):
+    INCOMPLETE = "incomplete"
+    COMPLETE = "complete"
+
+
+class ActorName(str, enum.Enum):
+    WORKER = "worker"
+    MANAGER = "manager"
+    INTERCHANGE = "interchange"
+    ENDPOINT = "endpoint"
+    RESULT_PROCESSOR = "result-processor"
+    WEB_SERVICE = "web-service"
