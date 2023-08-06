9e7b7a87f9e6b6946bcaf7839a933c9b600150eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:20 +0000

    Add package graphpro/0.1.2/graphpro-0.1.2.tar.gz

---
+++ packages/graphpro/graphpro-0.1.2.tar.gz/graphpro-0.1.2/src/graphpro/graphgen.py
@@ -0,0 +1,31 @@
+import networkx as nx 
+from scipy.spatial import distance
+from .graph import Graph
+
+class RepresentationMethod():
+    def res_map(self, ag):
+        pass
+        
+    def generate(ag):
+        pass
+
+
+class ContactMap(RepresentationMethod):
+    def __init__(self, cutoff, chain=None):
+        self.cutoff = cutoff
+        self.chain = chain
+    
+    def generate(self, ag):
+        ca_position = ag.c_alphas_positions(self.chain)
+        dist = distance.squareform(distance.pdist(ca_position))
+        dist[dist > self.cutoff] = 0
+        return Graph(dist, ca_position, ag.c_alphas_residues(self.chain))
+
+
+class ProGraphGenerator:
+    def __init__(self, ag):
+        self.ag = ag
+
+    def generate(self, rep):
+        G = rep.generate(self.ag)
+        return G
