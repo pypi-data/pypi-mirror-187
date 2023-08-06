5ec1e1cf3d81848f8a64a86f5b6f900425d4ed73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:55 +0000

    Add package example-robot-data/4.0.3.2/example_robot_data-4.0.3.2-1-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/example-robot-data/example_robot_data-4.0.3.2-1-cp39-cp39-macosx_10_9_x86_64.whl/cmeel.prefix/include/example-robot-data/path.hpp
@@ -0,0 +1,6 @@
+#ifndef EXAMPLE_ROBOT_DATA_PATH_HPP
+#define EXAMPLE_ROBOT_DATA_PATH_HPP
+
+#define EXAMPLE_ROBOT_DATA_MODEL_DIR "/var/folders/24/8k48jl6d249_n_qfxwsl6xvm0000gn/T/cmeel-14os85yg/pfx/cmeel.prefix/share/example-robot-data/robots"
+
+#endif
