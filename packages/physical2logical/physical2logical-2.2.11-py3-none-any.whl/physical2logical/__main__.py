de10c30d0efa4dcd5d87ce8cf90ea14f71a469e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:08 +0000

    Add package physical2logical/2.2.11/physical2logical-2.2.11-py3-none-any.whl

---
+++ packages/physical2logical/physical2logical-2.2.11-py3-none-any.whl/physical2logical/__main__.py
@@ -0,0 +1,13 @@
+from physical2logical.analyze import analyze
+from physical2logical.arguments import get_args
+from physical2logical.process_files import update_files
+
+
+def cli():
+    is_analyze, is_update, is_recursive, root_path, result_file = get_args()
+
+    if is_analyze:
+        analyze(root_path, is_recursive, result_file)
+
+    if is_update:
+        update_files(root_path, is_recursive)
