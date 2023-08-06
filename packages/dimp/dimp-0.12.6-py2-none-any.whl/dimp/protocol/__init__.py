af7aed81028776601e4dbd42f00de46cf161d0bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:28 +0000

    Add package dimp/0.12.6/dimp-0.12.6-py2-none-any.whl

---
+++ packages/dimp/dimp-0.12.6-py2-none-any.whl/dimp/protocol/__init__.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+#
+#   DIMP : Decentralized Instant Messaging Protocol
+#
+#                                Written in 2019 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2019 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+"""
+    DIMP - Message Contents & Commands
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Define universal message types as contents and commands
+"""
+
+from mkm.protocol import *
+from dkd.protocol import *
+
+from .text import TextContent
+from .forward import ForwardContent
+from .array import ArrayContent
+from .money import MoneyContent, TransferContent
+from .file import FileContent, ImageContent, AudioContent, VideoContent
+from .page import PageContent
+from .customized import CustomizedContent
+
+from .command import Command, CommandFactory
+from .meta import MetaCommand
+from .document import DocumentCommand
+
+from .history import HistoryCommand
+from .group import GroupCommand
+from .group import InviteCommand, ExpelCommand, JoinCommand
+from .group import QuitCommand, QueryCommand, ResetCommand
+
+__all__ = [
+
+    #
+    #   MingKeMing
+    #
+    'EntityType', 'MetaType',
+
+    'Address', 'AddressFactory',
+    'ID', 'IDFactory',
+    'Meta', 'MetaFactory',
+    'Document', 'DocumentFactory',
+    'Visa', 'Bulletin',
+
+    'entity_is_user', 'entity_is_group', 'entity_is_broadcast',
+    'meta_has_seed',
+
+    #
+    #   DaoKeDao
+    #
+    'ContentType', 'Content', 'ContentFactory',
+    'Envelope', 'EnvelopeFactory',
+
+    'Message', 'InstantMessage', 'SecureMessage', 'ReliableMessage',
+    'InstantMessageFactory', 'SecureMessageFactory', 'ReliableMessageFactory',
+    'InstantMessageDelegate', 'SecureMessageDelegate', 'ReliableMessageDelegate',
+
+    #
+    #   DaoKeDao protocol extends
+    #
+    'TextContent', 'ForwardContent', 'ArrayContent',
+    'MoneyContent', 'TransferContent',
+    'FileContent', 'ImageContent', 'AudioContent', 'VideoContent',
+    'PageContent', 'CustomizedContent',
+
+    'Command', 'CommandFactory',
+    'MetaCommand', 'DocumentCommand',
+
+    'HistoryCommand', 'GroupCommand',
+    'InviteCommand', 'ExpelCommand', 'JoinCommand',
+    'QuitCommand', 'QueryCommand', 'ResetCommand',
+]
