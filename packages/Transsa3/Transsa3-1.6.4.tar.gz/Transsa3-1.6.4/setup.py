91eda1efb3a082c8ce043e394ca36bc71b806a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:06 +0000

    Add package Transsa3/1.6.4/Transsa3-1.6.4.tar.gz

---
+++ packages/Transsa3/Transsa3-1.6.4.tar.gz/Transsa3-1.6.4/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import find_packages, setup
+setup(
+    name='Transsa3',
+    version='1.6.4',
+    packages=find_packages(),
+    py_modules=['Transsa3']
+
+)
+
+
