c36ed0f014c1a5a631ac547fc4f7947ad3fa030b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:44 +0000

    Add package nspfile/0.1.4/nspfile-0.1.4.tar.gz

---
+++ packages/nspfile/nspfile-0.1.4.tar.gz/nspfile-0.1.4/test/test_read.py
@@ -0,0 +1,12 @@
+from nspfile import read
+
+def test():
+    testfile = "test/addf8.nsp"
+    fs, x, hdr, note = read(testfile, return_header=True, return_note=True)
+
+    print(fs, hdr, note)
+
+    # from matplotlib import pyplot as plt
+
+    # plt.plot(x)
+    # plt.show()
