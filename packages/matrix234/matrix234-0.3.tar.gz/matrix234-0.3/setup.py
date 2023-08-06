50867ec21c334020e87b3edbc3ca5dea5acd65a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:56 +0000

    Add package matrix234/0.3/matrix234-0.3.tar.gz

---
+++ packages/matrix234/matrix234-0.3.tar.gz/matrix234-0.3/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+setuptools.setup( 
+ name='matrix234',
+ version='0.3',
+ author='Rishabh Bhaumick',
+ author_email='rishabh.bhaumick2022@vitstudent.ac.in',
+ description='matrix algebra',
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Development Status :: 1 - Planning",
+ ],
+)
