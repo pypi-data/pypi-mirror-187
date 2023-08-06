cf93d29b6228cde75c1da33ceb877d1c13e35adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:42 +0000

    Add package cpanlp/0.0.49/cpanlp-0.0.49.tar.gz

---
+++ packages/cpanlp/cpanlp-0.0.49.tar.gz/cpanlp-0.0.49/cpanlp/strategy/strategy.py
@@ -0,0 +1,25 @@
+from cpanlp.decorator.decorator import *
+class Strategy:
+    def __init__(self, name, market_focus, strategy):
+        self.name = name
+        self.market_focus = market_focus
+        self.strategy = strategy
+#财务杠杆是指公司使用借贷来提高资产收益率的策略。
+class FinancialStrategy(Strategy):
+    def __init__(self, name, market_focus, strategy):
+        super().__init__(name, market_focus, strategy)
+        self.strategy_outcome = strategy
+@with_side_effects(["Increased risk of default.","Amplified losses","Increased volatility","Difficulty in repaying debt","Reduced flexibility"])
+def leverage_strategy(total_debt, total_equity):
+    leverage_ratio = total_debt / total_equity
+    return leverage_ratio
+#毒丸计划(Poison Pill)是指公司采取的一种防御策略，用于阻止其他公司对其进行恶意收购。毒丸计划通常是通过增加公司的股票数量来降低股票价格，使收购变得更加困难。
+@with_side_effects(["Harm the company's reputation and relationships with investors."])
+def poison_pill(shares_outstanding, dilution_factor):
+    new_shares_outstanding = shares_outstanding * (1 + dilution_factor)
+    return new_shares_outstanding
+def main():
+    a=FinancialStrategy("huawei","gowth",poison_pill(1000,0.1))
+    print(a.strategy_outcome)
+if __name__ == '__main__':
+    main()
