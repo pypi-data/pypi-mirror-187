385ccd8e7d1d1b5161d3b3795e7a2ba8b52c2f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:59 +0000

    Add package algora-sdk/1.4.65/algora_sdk-1.4.65-py3-none-any.whl

---
+++ packages/algora-sdk/algora_sdk-1.4.65-py3-none-any.whl/algora/common/decorators/authorization/__init__.py
@@ -0,0 +1,5 @@
+"""
+Authenticated request decorators.
+"""
+from algora.common.decorators.authorization.asynchronous import async_authenticated_request
+from algora.common.decorators.authorization.synchronous import authenticated_request
