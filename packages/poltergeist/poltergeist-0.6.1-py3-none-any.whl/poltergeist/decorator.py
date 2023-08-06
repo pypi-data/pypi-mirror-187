49910f695cab638c5983029ecb232f85bdfb1c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:10 +0000

    Add package poltergeist/0.6.1/poltergeist-0.6.1-py3-none-any.whl

---
+++ packages/poltergeist/poltergeist-0.6.1-py3-none-any.whl/poltergeist/decorator.py
@@ -0,0 +1,48 @@
+import functools
+from typing import Any, Callable, ParamSpec, Type, overload
+
+from poltergeist.result import E, Err, Ok, Result, T
+
+P = ParamSpec("P")
+
+
+@overload
+def poltergeist(func: Callable[P, T], /) -> Callable[P, Result[T, Exception]]:
+    # Called as @poltergeist
+    ...
+
+
+@overload
+def poltergeist() -> Callable[[Callable[P, T]], Callable[P, Result[T, Exception]]]:
+    # Called as @poltergeist()
+    ...
+
+
+@overload
+def poltergeist(
+    *,
+    error: Type[E],
+) -> Callable[[Callable[P, T]], Callable[P, Result[T, E]]]:
+    # Called as @poltergeist(error=SomeError)
+    ...
+
+
+def poltergeist(func: Any = None, /, *, error: Any = Exception) -> Any:
+    """
+    Decorator that wraps the result of a function into an Ok object if it
+    executes without raising an exception. Otherwise, returns an Err object with
+    the exception raised by the function.
+    """
+
+    if func is None:
+        return functools.partial(poltergeist, error=error)
+
+    @functools.wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        try:
+            result = func(*args, **kwargs)
+        except error as e:
+            return Err(e)
+        return Ok(result)
+
+    return wrapper
