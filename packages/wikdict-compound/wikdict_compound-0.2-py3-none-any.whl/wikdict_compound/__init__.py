ded8438f42a4ef380422826dc45ac99aa85b7bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:58 +0000

    Add package wikdict-compound/0.2/wikdict_compound-0.2-py3-none-any.whl

---
+++ packages/wikdict-compound/wikdict_compound-0.2-py3-none-any.whl/wikdict_compound/__init__.py
@@ -0,0 +1,251 @@
+import sqlite3
+from pathlib import Path
+import statistics
+from dataclasses import dataclass, replace
+from typing import Optional, Iterable
+from functools import cached_property
+
+from .make_db import make_db
+
+supported_langs = "de en fi nl sv".split()
+query_count = 0
+
+
+DEBUG_QUERY_LOG = False
+
+
+def find_matches_in_db(conn, compound: str, ignore_word=None, first_part=True):
+    global query_count
+    query = """
+        SELECT *
+        FROM (
+            SELECT DISTINCT
+                other_written,
+                length(other_written)*length(other_written) * rel_score AS rel_score,
+                affix_type,
+                written_rep,
+                part_of_speech_list
+            FROM compound_splitter
+            WHERE (
+                (
+                    other_written <= :compound
+                    AND other_written >= substr(:compound, 1, 4)
+                    AND :compound LIKE other_written || '%'
+                )
+                OR other_written = substr(:compound, 1, 1)
+                OR other_written = substr(:compound, 1, 2)
+                OR other_written = substr(:compound, 1, 3)
+            )
+            --WHERE other_written <= :compound AND :compound LIKE other_written || '%'
+            --WHERE other_written <= :compound AND other_written || 'z' > :compound
+              AND (affix_type IS NULL OR :first_part = (affix_type = "prefix"))
+
+              -- For test data evaluation only. Without this, we could not
+              -- split compound words which are in the dictionary themselves.
+              AND other_written IS NOT lower(:ignore_word)
+              AND lower(written_rep) IS NOT lower(:ignore_word)
+        )
+        ORDER BY rel_score DESC
+        LIMIT 3
+    """
+    bindings = dict(compound=compound, ignore_word=ignore_word, first_part=first_part)
+    # if query_count == 0:
+    #     print_query_plan(conn, query, bindings)
+    result = conn.execute(query, bindings)
+
+    query_count += 1
+    if DEBUG_QUERY_LOG:
+        result = list(result)
+        with open("query.log", "a") as f:
+            f.write(compound + "\n")
+            for r in result:
+                f.write(str(dict(r)) + "\n")
+
+    return result
+
+
+@dataclass(frozen=True)
+class Part:
+    written_rep: str
+    score: float
+    match: str
+    affix_type: Optional[str]
+
+
+@dataclass(frozen=True)
+class Solution:
+    parts: list[Part]
+
+    @cached_property
+    def score(self):
+        return (
+            statistics.geometric_mean(p.score for p in self.parts)
+            / len(self.parts) ** 2
+        )
+
+
+@dataclass(frozen=True)
+class PartialSolution(Solution):
+    compound: str  # full compound which is to be split
+
+    @cached_property
+    def score(self):
+        if not self.parts:
+            return 0
+        return super().score * sum(len(p.match) for p in self.parts)
+
+
+@dataclass
+class SplitContext:
+    """Context for the process of splitting a compound into all parts."""
+
+    conn: sqlite3.Connection
+    lang: str
+    compound: str
+    queries: int = 0
+    graph_str: str = ""  # graphviz dot format visualization of splitting graph
+    best_partial_solution: Optional[PartialSolution] = None
+    best_solution: Optional[Solution] = None
+
+    @property
+    def graph(self) -> str:
+        return "digraph {\n" + self.graph_str + "}\n"
+
+
+def prune_branch(partial_solution, context) -> bool:
+    """Is the current splitting branch unlikely to provide a good result?"""
+    if context.queries > 100:
+        return True
+
+    best_score = (
+        context.best_partial_solution.score if context.best_partial_solution else 0
+    )
+    if partial_solution.score > best_score:
+        context.best_partial_solution = partial_solution
+    elif partial_solution.score < 0.1 * best_score:
+        return True
+
+    # if context.best_solution and len(solution.parts) == len(
+    #     context.best_solution.parts
+    # ):
+    #     return True
+
+    return False
+
+
+def get_potential_matches_for_row(compound, r, lang):
+    match = r["other_written"].lower()
+    yield match
+    pos_list = r["part_of_speech_list"].split(",")
+
+    if lang == "de":
+        if "noun" in pos_list:
+            if not match.endswith("s") and compound.startswith(match + "s"):
+                yield match + "s"
+
+
+def get_potential_next_parts(
+    compound, ignore_word, first_part, context
+) -> Iterable[Part]:
+    context.queries += 1
+    result = find_matches_in_db(context.conn, compound, ignore_word, first_part)
+    if not result:
+        return
+
+    for r in result:
+        for match in get_potential_matches_for_row(compound, r, context.lang):
+            yield Part(r["written_rep"], r["rel_score"], match, r["affix_type"])
+
+
+def split_compound_interal(
+    compound: str,
+    context: SplitContext,
+    partial_solution: PartialSolution,
+    ignore_word=None,
+    first_part=False,
+    node_name="START",
+) -> list[Solution]:
+    if prune_branch(partial_solution, context):
+        return []
+
+    solutions = []
+    for new_part in get_potential_next_parts(
+        compound, ignore_word, first_part, context
+    ):
+        new_partial_solution = replace(
+            partial_solution, parts=partial_solution.parts + [new_part]
+        )
+
+        new_node_name = f"{context.queries}-{new_part.written_rep}"
+        context.graph_str += f'\t"{node_name}" -> "{new_node_name}"\n'
+        context.graph_str += f'\t"{new_node_name}" [label="{new_part.written_rep}\\n{new_part.match}\\n{new_part.score:.2f}\\n{new_partial_solution.score:.2f}"]\n'
+
+        # Did find the last part and have a complete solution?
+        rest = compound.replace(new_part.match, "", 1)
+        if not rest:
+            if new_part.affix_type in [None, "suffix"]:
+                solutions.append(Solution(parts=[new_part]))
+                context.graph_str += f'\t"{new_node_name}" [shape=box]\n'
+            continue
+
+        # Recurse with the rest of the compound
+        recursive_results = split_compound_interal(
+            rest,
+            partial_solution=new_partial_solution,
+            context=context,
+            node_name=new_node_name,
+        )
+        if not recursive_results:
+            continue
+        splitted_rest = recursive_results[0].parts
+        solutions.append(Solution(parts=[new_part] + splitted_rest))
+
+    if not solutions:
+        return []
+
+    solutions.sort(key=lambda s: s.score, reverse=True)
+
+    if context.best_solution and solutions[0].score > context.best_solution.score:
+        context.best_solution = solutions[0]
+
+    return solutions
+
+
+def split_compound(
+    db_path,
+    lang: str,
+    compound: str,
+    ignore_word=None,
+    all_results=False,
+    write_graph_to_file: Optional[str] = None,
+):
+    compound = compound.lower()
+    conn = sqlite3.connect(Path(db_path) / f"{lang}-compound.sqlite3")
+    conn.row_factory = sqlite3.Row
+    context = SplitContext(conn=conn, lang=lang, compound=compound)
+    results = split_compound_interal(
+        compound,
+        partial_solution=PartialSolution(parts=[], compound=compound),
+        ignore_word=ignore_word,
+        first_part=True,
+        context=context,
+    )
+    conn.close()
+
+    if write_graph_to_file:
+        with open(write_graph_to_file, "w") as f:
+            f.write(context.graph)
+
+    if all_results:
+        return results
+    else:
+        return results[0] if results else None
+
+
+def print_query_plan(conn, query, bindings={}):
+    depth_of = {0: -1}
+    result = conn.execute("EXPLAIN QUERY PLAN " + query, bindings).fetchall()
+    for r in result:
+        depth = depth_of[r["parent"]] + 1
+        depth_of[r["id"]] = depth
+        print("|  " * depth + r["detail"])
---
+++ packages/wikdict-compound/wikdict_compound-0.2-py3-none-any.whl/wikdict_compound/make_db.py
@@ -0,0 +1,195 @@
+import sqlite3
+from pathlib import Path
+import hashlib
+import sys
+
+
+DEBUG_DB = False
+
+
+with open(__file__, "rb") as f:
+    md5sum = hashlib.md5(f.read()).hexdigest()
+
+
+def make_db(lang: str, input_path, output_path) -> None:
+    output_path = Path(output_path)
+    output_path.mkdir(exist_ok=True)
+    outfile = output_path / f"{lang}-compound.sqlite3"
+
+    # Skip recreation if up to date, otherwise delete existing
+    if outfile.exists():
+        conn = sqlite3.connect(outfile)
+        try:
+            existing_md5sum = conn.execute("SELECT md5sum FROM version").fetchone()[0]
+        except sqlite3.OperationalError:
+            existing_md5sum = None
+        if existing_md5sum == md5sum:
+            print(
+                f"Compound splitting db {outfile} is already up to date.",
+                file=sys.stderr,
+            )
+            return
+        else:
+            conn.close()
+            outfile.unlink(missing_ok=True)
+
+    # For debugging, it is very helpful to store all intermediate results
+    temp_table = "TEMPORARY TABLE" if not DEBUG_DB else "TABLE"
+    temp_view = "TEMPORARY VIEW" if not DEBUG_DB else "TABLE"
+
+    conn = sqlite3.connect(outfile, isolation_level="IMMEDIATE")
+
+    # sqlite's lower can only handle ascii (no Ä->ä)
+    conn.create_function("py_lower", 1, lambda x: x.lower(), deterministic=True)
+    lower = "lower" if lang != "de" else "py_lower"
+
+    conn.executescript(
+        rf"""
+        CREATE TABLE version AS SELECT "{md5sum}" AS md5sum;
+
+        ATTACH DATABASE '{input_path}/{lang}.sqlite3' AS generic;
+
+        CREATE {temp_table} form_with_entry AS
+        SELECT *
+        FROM generic.form
+            JOIN entry USING (lexentry)
+        WHERE
+            -- Multi-word entries oftentimes don't have all words included
+            -- in the form, resulting in misleading forms, so let's exclude
+            -- those.
+            written_rep NOT LIKE '% %'
+        ;
+
+        CREATE {temp_view} terms_from_forms AS
+        SELECT other_written, written_rep, part_of_speech,
+            (
+                CASE tense
+                    WHEN 'Past' THEN 0.2
+                    ELSE 1
+                END *
+                0.5 -- prefer base entries to inflected forms
+            ) AS score_factor
+        FROM form_with_entry
+        ;
+
+        CREATE {temp_view} terms_from_entries AS
+        SELECT written_rep AS other_written, written_rep, part_of_speech, 1 AS score_factor
+        FROM generic.entry
+        ;
+
+        CREATE {temp_table} terms AS
+        SELECT *, null AS rule FROM terms_from_forms
+        UNION ALL
+        SELECT *, null AS rule FROM terms_from_entries
+        ;
+    """
+    )
+
+    def remove_end(
+        end, where="true", score_factor=0.2, replacement="", from_table="terms"
+    ):
+        if from_table == "terms":
+            where += " AND rule IS NULL"
+        conn.execute(
+            f"""
+            INSERT INTO terms
+            SELECT
+                substr(other_written, 1, length(other_written) - :l)
+                    || :replacement
+                    AS other_written,
+                written_rep,
+                part_of_speech,
+                :score_factor AS score_factor,
+                'remove-' || :end AS rule
+            FROM {from_table}
+            WHERE substr(other_written, -:l, :l) = :end
+              AND {where}
+        """,
+            dict(
+                l=len(end), end=end, score_factor=score_factor, replacement=replacement
+            ),
+        )
+
+    # Language specific data changes
+    if lang == "de":
+        conn.executescript(
+            """
+            -- "sein" has the form "ist" which would override the "-ist" suffix due to its high importance
+            -- "in" is easily used instead of the "-in" suffix
+            DELETE FROM terms WHERE written_rep IN ('sein', 'in');
+        """
+        )
+        remove_end("logie", replacement="log", score_factor=0.5)
+        remove_end("e")
+    if lang == "sv":
+        # "-a" is better handled by the -a rule below
+        conn.execute("DELETE FROM terms WHERE written_rep = '-a'")
+        remove_end(
+            "a",
+            where="pos = 'verb' AND mood = 'Infinitive' AND voice = 'ActiveVoice'",
+            from_table="form_with_entry",
+        )
+
+    conn.executescript(
+        f"""
+        CREATE {temp_view} terms_view AS
+        SELECT
+            written_rep,
+            other_written,
+            part_of_speech,
+            -- Affixes are not that important words in a normal
+            -- dictionary, but for in compound words, they are very likely
+            -- and should be prioritized.
+            score_factor * CASE
+                WHEN affix_type == 'suffix' THEN 3
+                WHEN affix_type IN ('infix', 'prefix') THEN 2
+                ELSE 1
+            END AS score_factor,
+            affix_type
+        FROM (
+            SELECT
+                written_rep,
+                {lower}(trim(other_written, '-')) AS other_written,
+                part_of_speech,
+                score_factor,
+                CASE
+                    WHEN substr(other_written, 1, 1) = '-' AND substr(other_written, -1, 1) = '-' THEN 'infix'
+                    WHEN substr(other_written, 1, 1) = '-' THEN 'suffix'
+                    WHEN substr(other_written, -1, 1) = '-' THEN 'prefix'
+                END AS affix_type
+            FROM terms
+        );
+
+        CREATE {temp_view} compound_splitter_ungrouped AS
+        SELECT
+            other_written,
+            affix_type,
+            coalesce(rel_score, 0.1) * score_factor AS rel_score,
+            written_rep,
+            part_of_speech
+        FROM terms_view
+            LEFT JOIN rel_importance ON (written_rep = written_rep_guess)
+        WHERE other_written != '' -- Why are there forms without text?
+          AND NOT (length(other_written) = 1 AND affix_type IS NULL)
+          AND (
+            part_of_speech NOT IN (
+                'interjection', 'pronoun', 'indefinitePronoun', 'proverb', 'phraseologicalUnit', 'symbol',
+                'article', 'idiom', 'properNoun')
+            );
+
+        CREATE TABLE compound_splitter AS
+        SELECT 
+            other_written AS other_written,
+            affix_type,
+            group_concat(DISTINCT part_of_speech) AS part_of_speech_list,
+            max(rel_score) AS rel_score,
+            first_value(written_rep) OVER (
+                PARTITION BY other_written, affix_type
+                ORDER BY rel_score DESC
+            ) AS written_rep
+        FROM compound_splitter_ungrouped
+        GROUP BY 1, 2;
+        
+        CREATE INDEX compound_splitter_idx ON compound_splitter(other_written);
+    """
+    )
