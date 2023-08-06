7bf693911c3f9661b793c2ef983a1390bb906e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:32 +0000

    Add package mode-analyzer/0.0.5/mode_analyzer-0.0.5-py3-none-any.whl

---
+++ packages/mode-analyzer/mode_analyzer-0.0.5-py3-none-any.whl/mode_analyzer/__main__.py
@@ -0,0 +1,20 @@
+from mode_analyzer import *
+
+Dir = []
+Image_List = []
+Image_original = []
+Image_rotated = []
+Image_gray = []
+Image_Shape= []
+pixel_to_um = 0
+dx = 1
+ydata_fit =[]
+number_of_peaks = []
+
+app = main()
+app.run()
+# Run = RunSimulation()
+# root = Tk.Tk()
+# main_gui = MainWindow(master=root)
+# root.read = read_data(f)
+# root.mainloop()
