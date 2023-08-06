01ab513a4d4aada82337d53054c9acc7c93c92ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:12 +0000

    Add package PdfInfoExtractor/0.1.8/PdfInfoExtractor-0.1.8.tar.gz

---
+++ packages/PdfInfoExtractor/PdfInfoExtractor-0.1.8.tar.gz/PdfInfoExtractor-0.1.8/PdfInfoExtractor/__init__.py
@@ -0,0 +1,10 @@
+"""
+PdfInfoExtractor
+
+"""
+
+from PdfInfoExtractor import *
+
+__version__ = "0.1.8"
+__author__ = 'Steve Richards, Mehak Singal, Nidhish Kumar'
+# __credits__ = 'Argonne National Laboratory'
