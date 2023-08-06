a230fba722738ea1c1229181e0da878218ecdca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:47 +0000

    Add package dadeschools/1.3/dadeschools-1.3.tar.gz

---
+++ packages/dadeschools/dadeschools-1.3.tar.gz/dadeschools-1.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name='dadeschools',
+    version='1.3',
+    author='Daniel Li',
+    author_email='daniel.miami2005@gmail.com',
+    description='The Ultimate Dadeschools CLI',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/BananaApache/dadeschools',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'requests',
+        'bs4',
+    ],
+    # scripts=['./bin/main'],
+    python_requires='>=3.6'
+)
