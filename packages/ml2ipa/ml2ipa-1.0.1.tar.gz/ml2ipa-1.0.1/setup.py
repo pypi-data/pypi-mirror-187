ad997eb26646dd8dc475b6c28788542c57dc3b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:42 +0000

    Add package ml2ipa/1.0.1/ml2ipa-1.0.1.tar.gz

---
+++ packages/ml2ipa/ml2ipa-1.0.1.tar.gz/ml2ipa-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import io
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+	name="ml2ipa",
+	version="1.0.1",
+	description="A linguistic tool that generates IPA pronunciation for Malayalam words",
+	long_description=long_description,
+    long_description_content_type='text/markdown',
+	author="Elaine Mary Rose",
+	author_email="elainerose311@gmail.com",
+	url="https://github.com/erose311/ml2ipa",
+	#packages=['TransLitML'],
+	packages=find_packages(),
+	license="MIT",
+	classifiers=[
+		"Intended Audience :: Developers",
+		"Programming Language :: Python :: 3.9",
+		"Operating System :: OS Independent",
+		"Topic :: Software Development :: Libraries :: Python Modules"
+	]
+)
