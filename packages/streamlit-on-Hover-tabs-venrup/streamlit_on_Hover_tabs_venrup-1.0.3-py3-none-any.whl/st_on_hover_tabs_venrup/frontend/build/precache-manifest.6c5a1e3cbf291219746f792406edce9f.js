bc26cc003feb90c7b759505442664865350426f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:24 +0000

    Add package streamlit-on-Hover-tabs-venrup/1.0.3/streamlit_on_Hover_tabs_venrup-1.0.3-py3-none-any.whl

---
+++ packages/streamlit-on-Hover-tabs-venrup/streamlit_on_Hover_tabs_venrup-1.0.3-py3-none-any.whl/st_on_hover_tabs_venrup/frontend/build/precache-manifest.6c5a1e3cbf291219746f792406edce9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae3a3dee0c289acd6dd2dd1ddf6fbb3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "274abd8d42e434fd71b6",
+    "url": "./static/css/2.0a4cb73c.chunk.css"
+  },
+  {
+    "revision": "ebba4083787cf7586b31",
+    "url": "./static/css/main.db61388b.chunk.css"
+  },
+  {
+    "revision": "274abd8d42e434fd71b6",
+    "url": "./static/js/2.6b4aaf9c.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.6b4aaf9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebba4083787cf7586b31",
+    "url": "./static/js/main.0a8cbbf2.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
