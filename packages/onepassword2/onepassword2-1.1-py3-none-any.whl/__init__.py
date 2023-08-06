3db56ddf9ebd41cbfa67819308e8fbbd99cda3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:23 +0000

    Add package onepassword2/1.1/onepassword2-1.1-py3-none-any.whl

---
+++ packages/onepassword2/onepassword2-1.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,5 @@
+from onepassword2 import OP2, OP2Item, MultipleMatchesException, OPException, NoSuchVaultException
+
+__all__ = [
+     'OP2', 'OP2Item', 'op_signin',  'MultipleMatchesException', 'OPException', 'NoSuchVaultException'
+] 
