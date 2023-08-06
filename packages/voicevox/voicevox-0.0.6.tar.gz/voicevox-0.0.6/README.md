cd150ef1bc232925e1a7eea4a2ad14f50b69702b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:26 +0000

    Add package voicevox/0.0.6/voicevox-0.0.6.tar.gz

---
+++ packages/voicevox/voicevox-0.0.6.tar.gz/voicevox-0.0.6/README.md
@@ -0,0 +1,28 @@
+# voicevox
+Unoffical API wrapper that you can use voicevox easy!
+
+## Install
+```sh
+pip install voicevox
+```
+
+All that!
+
+## Example
+```python
+from voicevox import Client
+import asyncio
+
+
+async def main():
+    client = Client()
+    audio_query = await client.create_audio_query(
+        "こんにちは！", speaker=1
+    )
+    with open("voice.wav", "wb") as f:
+        f.write(await audio_query.synthesis())
+
+
+if __main__ == "__main__":
+    asyncio.run(main())
+```
---
+++ packages/voicevox/voicevox-0.0.6.tar.gz/voicevox-0.0.6/voicevox/audio_query.py
@@ -0,0 +1,30 @@
+# voicevox - audio_query
+
+from __future__ import annotations
+from typing import TYPE_CHECKING, Optional
+
+from .types import AudioQueryType
+
+if TYPE_CHECKING:
+    from .client import Client
+
+
+class AudioQuery:
+
+    def __init__(
+        self, client: Client, audio_query: AudioQueryType,
+        speaker: int
+    ):
+        self.client = client
+        self.data = audio_query
+        self.speaker = speaker
+
+    async def synthesis(
+        self, *, enable_interrogative_upspeak: bool = True,
+        core_version: Optional[str] = None
+    ) -> bytes:
+        return await self.client.synthesis(
+            self.speaker, self.data,
+            enable_interrogative_upspeak=enable_interrogative_upspeak,
+            core_version=core_version
+        )
