60f0d22c5b6af60f612552ad57bff0ad0dbd704c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:04 +0000

    Add package cellpose-vollseg/0.0.1/cellpose-vollseg-0.0.1.tar.gz

---
+++ packages/cellpose-vollseg/cellpose-vollseg-0.0.1.tar.gz/cellpose-vollseg-0.0.1/cellpose/version.py
@@ -0,0 +1,12 @@
+from importlib.metadata import PackageNotFoundError, version
+import sys 
+from platform import python_version
+import torch 
+
+version = "0.0.1"
+
+version_str = f"""
+cellpose version: \t{version} 
+platform:       \t{sys.platform} 
+python version: \t{python_version()} 
+torch version:  \t{torch.__version__}"""
