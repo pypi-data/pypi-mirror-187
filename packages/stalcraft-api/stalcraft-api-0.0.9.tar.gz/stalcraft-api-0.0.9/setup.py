dc2a0c988a31db9225dd72678d71c8882ada6a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:26 +0000

    Add package stalcraft-api/0.0.9/stalcraft-api-0.0.9.tar.gz

---
+++ packages/stalcraft-api/stalcraft-api-0.0.9.tar.gz/stalcraft-api-0.0.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.9'
+DESCRIPTION = 'stalcraft-api unofficial python library'
+
+# Setting up
+setup(
+    name="stalcraft-api",
+    version=VERSION,
+    author="onejeuu",
+    author_email="<bloodtrail@beber1k.ru>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    license='MIT',
+    keywords=['python', 'stalcraft', 'api'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
