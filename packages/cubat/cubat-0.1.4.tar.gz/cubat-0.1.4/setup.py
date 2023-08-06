48ab872ab683d68e808ffd63728d3ff0e081bb8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:31 +0000

    Add package cubat/0.1.4/cubat-0.1.4.tar.gz

---
+++ packages/cubat/cubat-0.1.4.tar.gz/cubat-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="cubat",
+    version="0.1.4",
+    py_modules=["cubat"],
+    include_package_data=True,
+    install_requires=["click","numpy~=1.21.1","pandas~=1.3.0","statsmodels~=0.13.2","click~=8.0.3","setuptools~=66.0.0","biopython~=1.79","click_option_group"],
+    entry_points="""
+        [console_scripts]
+        cubat=cubat.__main__:cli
+    """,
+)
