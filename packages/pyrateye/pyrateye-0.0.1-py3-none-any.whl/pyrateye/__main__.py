1e785519303d553463e042947a492d52d8a3c2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:39 +0000

    Add package pyrateye/0.0.1/pyrateye-0.0.1-py3-none-any.whl

---
+++ packages/pyrateye/pyrateye-0.0.1-py3-none-any.whl/pyrateye/__main__.py
@@ -0,0 +1,17 @@
+# import clr
+
+# # Add the path to the DLL file to the assembly search path
+# clr.AddReference("./RatEye/RatEye
+
+# # Import the namespace and the class containing the function
+# from Namespace import ClassName
+
+# # Create an instance of the class
+# obj = ClassName()
+
+# # Call the function
+# result = obj.FunctionName(param1, param2, ...)
+
+# print(result)
+
+print("Hello World")
