e5eab176dd4ea6f8e55e2d9a75a8dba06a795dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:13 +0000

    Add package mpes/1.1.4/mpes-1.1.4.tar.gz

---
+++ packages/mpes/mpes-1.1.4.tar.gz/mpes-1.1.4/mpes/__init__.py
@@ -0,0 +1,6 @@
+from . import utils
+
+__version_info__ = ('1', '1', '4')
+__version__ = '.'.join(__version_info__)
+__author__ = 'R. Patrick Xian, Laurenz Rettig, Abeer Arora'
+__all__ = ['fprocessing', 'analysis', 'visualization', 'utils', 'mirrorutil']
