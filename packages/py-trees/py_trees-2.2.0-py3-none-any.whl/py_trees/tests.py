b29ddb419a99b221de6b63f70714a0d613c797e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:04 +0000

    Add package py-trees/2.2.0/py_trees-2.2.0-py3-none-any.whl

---
+++ packages/py-trees/py_trees-2.2.0-py3-none-any.whl/py_trees/tests.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.githubusercontent.com/splintered-reality/py_trees/devel/LICENSE
+#
+##############################################################################
+# Documentation
+##############################################################################
+
+"""
+Library of common methods for the tests.
+
+Oh my spaghettified magnificence,
+Bless my noggin with a tickle from your noodly appendages!
+"""
+
+##############################################################################
+# Imports
+##############################################################################
+
+import typing
+
+from . import behaviour
+from . import blackboard
+from . import console
+from . import display
+from . import trees
+from . import visitors
+
+##############################################################################
+# Methods
+##############################################################################
+
+
+def print_assert_banner() -> None:
+    print(console.green + "\n--------- Assertions ---------\n" + console.reset)
+
+
+AssertResultType = typing.TypeVar("AssertResultType")
+
+
+def print_assert_details(
+    text: str,
+    expected: AssertResultType,
+    result: AssertResultType
+) -> None:
+    print(console.green + text
+          + "." * (70 - len(text))
+          + console.cyan + "{}".format(expected)
+          + console.yellow + " [{}]".format(result)
+          + console.reset)
+
+
+def pre_tick_visitor(behaviour_tree: trees.BehaviourTree) -> None:
+    print("\n--------- Run %s ---------\n" % behaviour_tree.count)
+
+
+def tick_tree(
+    root: behaviour.Behaviour,
+    from_tick: int,
+    to_tick: int,
+    *,
+    visitors: typing.Optional[typing.List[visitors.VisitorBase]] = None,
+    print_snapshot: bool = False,
+    print_blackboard: bool = False
+) -> None:
+    if visitors is None:
+        visitors = []
+    print("\n================== Iteration {}-{} ==================\n".format(from_tick, to_tick))
+    for i in range(from_tick, to_tick + 1):
+        for visitor in visitors:
+            visitor.initialise()
+        print(("\n--------- Run %s ---------\n" % i))
+        for node in root.tick():
+            for visitor in visitors:
+                node.visit(visitor)
+    if print_snapshot:
+        print(console.green + "\nTree Snapshot" + console.reset)
+        print(display.unicode_tree(root=root, show_status=True))
+    if print_blackboard:
+        print(display.unicode_blackboard())
+
+
+def clear_blackboard() -> None:
+    # Useful between tests
+    blackboard.Blackboard.storage = {}
+    blackboard.Blackboard.clients = {}
+    blackboard.Blackboard.metadata = {}
+
+
+def print_summary(nodes: typing.List[behaviour.Behaviour]) -> None:
+    print("\n--------- Summary ---------\n")
+    for node in nodes:
+        print("%s" % node)
