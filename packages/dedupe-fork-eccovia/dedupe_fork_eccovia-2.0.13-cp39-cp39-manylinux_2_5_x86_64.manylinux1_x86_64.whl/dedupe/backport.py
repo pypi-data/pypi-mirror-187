6ae18775b3ed06a8dd0aaebfba2a28cd34e4d420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:38 +0000

    Add package dedupe-fork-eccovia/2.0.13/dedupe_fork_eccovia-2.0.13-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/dedupe-fork-eccovia/dedupe_fork_eccovia-2.0.13-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.whl/dedupe/backport.py
@@ -0,0 +1,14 @@
+
+import platform
+
+if platform.system() == 'Darwin':
+    import multiprocessing
+    ctx = multiprocessing.get_context('spawn')
+    Queue = ctx.Queue
+    Process = ctx.Process  # type: ignore
+    Pool = ctx.Pool
+    SimpleQueue = ctx.SimpleQueue
+    Lock = ctx.Lock
+    RLock = ctx.RLock
+else:
+    from multiprocessing import Process, Pool, Queue, SimpleQueue, Lock, RLock  # type: ignore # noqa 
