a8c8191c315c8d548886d0ddc71a2711fc70245e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:21 +0000

    Add package com.castsoftware.uc.python.common/0.1.6/com.castsoftware.uc.python.common-0.1.6.tar.gz

---
+++ packages/com.castsoftware.uc.python.common/com.castsoftware.uc.python.common-0.1.6.tar.gz/com.castsoftware.uc.python.common-0.1.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (
+    name="com.castsoftware.uc.python.common",
+    version="0.1.6",  #production version
+    #version="0.1.11",   #test version
+    author="Nevin Kaplan",
+    description="A set of common classes and methods for use with python projects",
+    packages=["cast_common"]
+)
