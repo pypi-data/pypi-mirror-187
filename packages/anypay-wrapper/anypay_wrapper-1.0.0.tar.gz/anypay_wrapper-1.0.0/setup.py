cd2d6e2d89ee7c0f17d7b627db6efc10a4e94475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:24 +0000

    Add package anypay-wrapper/1.0.0/anypay_wrapper-1.0.0.tar.gz

---
+++ packages/anypay-wrapper/anypay_wrapper-1.0.0.tar.gz/anypay_wrapper-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+VERSION = "1.0.0"
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="anypay_wrapper",
+    version=VERSION,
+    author="MagistrUMDev",
+    description="AnyPay API Wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MagistrUMDev/anypay_api",
+    install_requires=["aiohttp>=3.8.3"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9",
+)
