175a95552a643352e59e739173432ce2bbe21ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:16 +0000

    Add package pretty-forms/0.0.2/pretty-forms-0.0.2.tar.gz

---
+++ packages/pretty-forms/pretty-forms-0.0.2.tar.gz/pretty-forms-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="pretty-forms",
+        version="0.0.2",
+        license="BSD",
+        author="Jorge Monforte González",
+        author_email="yo@llou.net",
+        packages=find_packages("."),
+        package_dir={'':'.'},
+        url="https://github.com/llou/pretty-forms",
+        keywords="django bulma",
+        install_requires=['django'],
+)
+
+
+
