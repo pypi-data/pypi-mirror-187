665c01758b510b6266ef2ba758ee6f56b468a7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:36 +0000

    Add package Topsis-Aryan-102003580/0.10/Topsis_Aryan_102003580-0.10.tar.gz

---
+++ packages/Topsis-Aryan-102003580/Topsis_Aryan_102003580-0.10.tar.gz/Topsis_Aryan_102003580-0.10/Topsis_Aryan_102003580/topsis.py
@@ -0,0 +1,57 @@
+
+def topsis(Nope,df,weights,impacts,output_name):
+    import sys
+    import pandas as pd 
+    import numpy as np
+    import os
+    import copy
+    df1=copy.deepcopy(df)
+    df =df.iloc[:,1:]
+
+    colsize=len(df.axes[1])
+    rowsize=len(df.axes[0])
+    if colsize<3:
+        print("invalid number of columns")
+        sys.exit()
+
+    if(colsize!=len(impacts)):
+        print("Invalid number of impacts")
+        sys.exit()
+    if(colsize!=len(weights)):
+        print("Invalid number of weights")
+        sys.exit()
+    for i in range(colsize):
+        temp=0
+        for j in range(rowsize):
+            temp=temp+df.iloc[j,i]
+        for j in range(rowsize):
+            df.iloc[j,i]=df.iloc[j,i]*weights[i-1]/temp
+    best=df.max()
+    worst=df.min()
+    #print(df)
+    for i in impacts:
+        if i=='-':
+            best[i],worst[i]=worst[i],best[i]
+    
+    score = [] 
+    p = [] 
+    n = [] 
+ 
+
+
+    for i in range(len(df)):
+        temp_p, temp_n = 0, 0
+        for j in range(1, colsize):
+            temp_p = temp_p + (best[j-1] - df.iloc[i, j])**2
+            temp_n = temp_n + (worst[j-1] - df.iloc[i, j])**2
+        temp_p, temp_n = temp_p**0.5, temp_n**0.5
+        score.append(temp_n/(temp_p + temp_n))
+        n.append(temp_n)
+        p.append(temp_p)
+        
+    
+    df1['score']=score
+    df1['Rank'] = (df1['score'].rank(method='max', ascending=False))
+    df1 = df1.astype({"Rank": int})
+    print(df1)
+    df1.to_csv(output_name)
---
+++ packages/Topsis-Aryan-102003580/Topsis_Aryan_102003580-0.10.tar.gz/Topsis_Aryan_102003580-0.10/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+  name = 'Topsis_Aryan_102003580',         # How you named your package folder (MyLib)
+  packages = ['Topsis_Aryan_102003580'],   # Chose the same as "name"
+  version = '0.10',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A topsis package',
+     # Give a short description about your library
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Aryan Sharma',                   # Type in your name
+  author_email = 'aryansharma5669@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/aryansharma56/topsis',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/aryansharma56/topsis',    # I explain this later on
+  keywords = ['Aryan', 'Topsis'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'panda',
+          'numpy'
+
+      ],
+  
+)
