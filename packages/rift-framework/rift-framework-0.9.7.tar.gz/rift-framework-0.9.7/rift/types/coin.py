8b19e24ceee14e305bf324a51251c3ba7845e8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:49 +0000

    Add package rift-framework/0.9.7/rift-framework-0.9.7.tar.gz

---
+++ packages/rift-framework/rift-framework-0.9.7.tar.gz/rift-framework-0.9.7/rift/types/coin.py
@@ -0,0 +1,28 @@
+from rift.core import Entity
+from rift.types.bases import Builder, Int, Slice
+
+
+class Coin(Int):
+    @classmethod
+    def __serialize__(cls, to: "Builder", value: "Entity") -> "Builder":
+        if isinstance(value, Int) and value.value == 0:
+            if hasattr(value, "NAMED") and not value.NAMED:
+                b = to.uint(0, 4)
+                return b
+        b = to.coins(value)
+        return b
+
+    @classmethod
+    def __deserialize__(
+        cls,
+        from_: "Slice",
+        name: str = None,
+        inplace: bool = True,
+        lazy: bool = True,
+        **kwargs,
+    ):
+        # TODO: HANDLE INPLACE STUFF
+        v = from_.coin()
+        if name is not None:
+            v.__assign__(name)
+        return v
