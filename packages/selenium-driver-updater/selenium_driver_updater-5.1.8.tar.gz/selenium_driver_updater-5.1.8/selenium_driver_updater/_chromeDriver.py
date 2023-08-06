5ce1a61e74f4529381244e6429fa1f0f42bbfb10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:04 +0000

    Add package selenium-driver-updater/5.1.8/selenium_driver_updater-5.1.8.tar.gz

---
+++ packages/selenium-driver-updater/selenium_driver_updater-5.1.8.tar.gz/selenium_driver_updater-5.1.8/selenium_driver_updater/_chromeDriver.py
@@ -0,0 +1,265 @@
+#pylint: disable=logging-fstring-interpolation
+#Standart library imports
+import time
+from pathlib import Path
+from typing import Tuple
+
+# Third party imports
+import wget
+
+# Local imports
+from selenium_driver_updater.browsers._chromeBrowser import ChromeBrowser
+
+from selenium_driver_updater.util.logger import logger
+from selenium_driver_updater.driver_base import DriverBase
+
+class ChromeDriver(DriverBase):
+    """Class for working with Selenium chromedriver binary"""
+
+    _tmp_folder_path = 'tmp'
+
+    def __init__(self, **kwargs):
+
+        DriverBase.__init__(self, **kwargs)
+
+        self.system_name = ''
+
+        #assign of specific os
+        specific_system = str(kwargs.get('system_name', ''))
+        specific_system = specific_system.replace('win64', 'win32')
+        if specific_system:
+            self.system_name = f"chromedriver_{specific_system}.zip"
+
+        self.chromedriver_path = self.driver_path
+
+        kwargs.update(path=self.chromedriver_path)
+        self.chromebrowser = ChromeBrowser(**kwargs)
+
+    def main(self) -> str:
+        """Main function, checks for the latest version, downloads or updates chromedriver binary or
+        downloads specific version of chromedriver.
+
+        Returns:
+            str
+
+            driver_path (str) : Path where chromedriver was downloaded or updated.
+
+        """
+        driver_path : str = ''
+
+        self.chromebrowser.main()
+
+        if not self.version:
+
+            #additional checking for main versions to equal - for example, chromedriver version main is 90 and chrome browser is still 89
+            is_equal, latest_version_driver, latest_version_browser = self._compare_latest_version_main_chromedriver_and_latest_version_main_chrome_browser()
+
+            if is_equal:
+
+                driver_path = self._check_if_chromedriver_is_up_to_date()
+
+            if not is_equal:
+
+                message = (f' Problem with chromedriver latest_version_driver:'
+                    f'{latest_version_driver} latest_version_browser: {latest_version_browser}\n'
+                    ' It often happen when new version of chromedriver released, but new version of chrome browser is not\n'
+                    ' Trying to download the latest previous version of chromedriver')
+                logger.error(message)
+
+                driver_path = self._download_driver(previous_version=True)
+
+        else:
+
+            driver_path = self._download_driver(version=self.version)
+
+        return driver_path
+
+    def _compare_latest_version_main_chromedriver_and_latest_version_main_chrome_browser(self) -> Tuple[bool, str, str]:
+        """Compares latest main version of chromedriver and latest main version of chrome browser"""
+        is_equal : bool = False
+        latest_version_chromedriver_main : str = ''
+        latest_version_browser_main : str = ''
+
+        latest_version_chromedriver = super()._get_latest_version_driver(no_messages=True)
+
+        latest_version_browser = self.chromebrowser._get_latest_version_chrome_browser(no_messages=True)
+
+        latest_version_chromedriver_main = latest_version_chromedriver.split('.', maxsplit=1)[0]
+        latest_version_browser_main = latest_version_browser.split('.', maxsplit=1)[0]
+
+        if int(latest_version_chromedriver_main) <= int(latest_version_browser_main):
+            is_equal = True
+
+        return is_equal, latest_version_chromedriver_main, latest_version_browser_main
+
+    def _check_if_chromedriver_is_up_to_date(self) -> str:
+        """Сhecks for the latest version, downloads or updates chromedriver binary
+
+        Returns:
+            str
+
+            driver_path (str) : Path where chromedriver was downloaded or updated.
+
+        """
+        driver_path : str = ''
+
+        if self.check_driver_is_up_to_date and not self.system_name:
+
+            is_driver_up_to_date, current_version, latest_version = super()._compare_current_version_and_latest_version()
+
+            if is_driver_up_to_date:
+                return self.chromedriver_path
+
+        driver_path = self._download_driver()
+
+        if self.check_driver_is_up_to_date and not self.system_name:
+
+            is_driver_up_to_date, current_version, latest_version = super()._compare_current_version_and_latest_version()
+
+            if not is_driver_up_to_date:
+
+                message = (f'Problem with updating chromedriver'
+                        f'current_version: {current_version} latest_version: {latest_version}')
+                logger.error(message)
+                message = 'Trying to download previous latest version of chromedriver'
+                logger.info(message)
+
+                driver_path = self._download_driver(previous_version=True)
+
+        return driver_path
+
+    def _get_latest_previous_version_chromedriver_via_requests(self) -> str:
+        """Gets previous latest chromedriver version
+
+
+        Returns:
+            str
+
+            latest_version_previous (str)   : Latest previous version of chromedriver.
+
+        """
+
+        latest_version_previous : str = ''
+
+        url = self.setting["ChromeDriver"]["LinkLastRelease"]
+        json_data = self.requests_getter.get_result_by_request(url=url)
+
+        latest_version = str(json_data)
+        latest_version_main = latest_version.split(".", maxsplit=1)[0]
+
+        latest_version_main_previous = int(latest_version_main) - 1
+
+        url = self.setting["ChromeDriver"]["LinkLatestReleaseSpecificVersion"].format(latest_version_main_previous)
+        json_data = self.requests_getter.get_result_by_request(url=url)
+
+        latest_version_previous = str(json_data)
+
+        logger.info(f'Latest previous version of chromedriver: {latest_version_previous}')
+
+        return latest_version_previous
+
+    def _download_driver(self, version : str = '', previous_version : bool = False) -> str:
+        """Function to download, delete or upgrade current chromedriver
+
+        Args:
+            version (str)               : Specific chromedriver version to download. Defaults to empty string.
+            previous_version (boll)     : If true, chromedriver latest previous version will be downloaded. Defaults to False.
+
+        Returns:
+            str
+
+            driver_path (str) : Path to unzipped driver.
+
+        """
+
+        url : str = ''
+        latest_previous_version : str = ''
+        latest_version : str = ''
+        driver_path : str = ''
+
+        if self.upgrade:
+
+            super()._delete_current_driver_for_current_os()
+
+        if version:
+
+            url = self.setting["ChromeDriver"]["LinkLastReleaseFile"].format(version)
+            logger.info(f'Started download chromedriver specific_version: {version}')
+
+        elif previous_version:
+
+            latest_previous_version = self._get_latest_previous_version_chromedriver_via_requests()
+
+            url = self.setting["ChromeDriver"]["LinkLastReleaseFile"].format(latest_previous_version)
+            logger.info(f'Started download chromedriver latest_previous_version: {latest_previous_version}')
+
+        else:
+
+            latest_version = super()._get_latest_version_driver()
+
+            url = self.setting["ChromeDriver"]["LinkLastReleaseFile"].format(latest_version)
+            logger.info(f'Started download chromedriver latest_version: {latest_version}')
+
+        if self.system_name:
+            url = url.replace(url.split("/")[-1], '')
+            url = url + self.system_name
+
+            logger.info(f'Started downloading chromedriver for specific system: {self.system_name}')
+
+        if any([version, self.system_name ,latest_previous_version]):
+            if 'mac_arm64' in url:
+                try:
+                    super()._check_if_version_is_valid(url=url)
+                except:
+                    logger.warning('Could not find binary with mac_arm64 name, trying to check version using different name')
+                    url = url.replace('mac_arm64', 'mac64_m1')
+                    super()._check_if_version_is_valid(url=url)
+            else:
+                super()._check_if_version_is_valid(url=url)
+
+        archive_name = url.split("/")[-1]
+        out_path = self.path + archive_name
+
+        if Path(out_path).exists():
+            Path(out_path).unlink()
+
+        logger.info(f'Started download chromedriver by url: {url}')
+        archive_path = self._wget_download_driver(url, out_path)
+        time.sleep(2)
+
+        logger.info(f'Chromedriver was downloaded to path: {archive_path}')
+
+        out_path : str = self.path
+
+        parameters = dict(archive_path=archive_path, out_path=out_path)
+
+        if not self.filename:
+
+            self.extractor.extract_and_detect_archive_format(**parameters)
+
+        else:
+
+            filename = self.setting['ChromeDriver']['LastReleasePlatform']
+            parameters.update(dict(filename=filename, filename_replace=self.filename))
+
+            self.extractor.extract_all_zip_archive_with_specific_name(**parameters)
+
+        if Path(archive_path).exists():
+            Path(archive_path).unlink()
+
+        driver_path = self.chromedriver_path
+
+        logger.info(f'Chromedriver was successfully unpacked by path: {driver_path}')
+
+        if self.chmod:
+
+            super()._chmod_driver()
+
+        return driver_path
+
+    def _wget_download_driver(self, url, path):
+        if self.info_messages:
+            archive_path = wget.download(url=url, out=path)
+        else:
+            archive_path = wget.download(url=url, out=path, bar=None)
+        return archive_path
---
+++ packages/selenium-driver-updater/selenium_driver_updater-5.1.8.tar.gz/selenium_driver_updater-5.1.8/selenium_driver_updater/_setting.py
@@ -0,0 +1,174 @@
+#pylint: disable=invalid-name
+#Standart library imports
+import os
+import platform
+
+base_dir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
+
+os_bit = platform.architecture()[0][:-3]
+
+latest_release = 'https://chromedriver.storage.googleapis.com/{}/'
+
+chromedriver_latest_release =   "chromedriver_win32.zip" if platform.system() == 'Windows' else\
+                                "chromedriver_linux64.zip" if platform.system() == "Linux" else\
+                                "chromedriver_mac_arm64.zip" if 'arm' in str(os.uname().machine)\
+                                and platform.system() == 'Darwin' else\
+                                "chromedriver_mac64.zip"
+chromedriver_latest_release = latest_release + chromedriver_latest_release
+
+
+latest_release_geckodriver = 'https://github.com/mozilla/geckodriver/releases/download/v{}/'
+geckodriver_platform_release =  "geckodriver-v{}-" + f"win{os_bit}.zip" if platform.system() == 'Windows' else\
+                                "geckodriver-v{}-" + f"linux{os_bit}.tar.gz" if platform.system() == "Linux" else\
+                                "geckodriver-v{}-macos-aarch64.tar.gz" if 'arm' in str(os.uname().machine)\
+                                and platform.system() == 'Darwin' else\
+                                "geckodriver-v{}-macos.tar.gz"
+geckodriver_platform_release = latest_release_geckodriver + geckodriver_platform_release
+
+
+latest_release_operadriver = 'https://github.com/operasoftware/operachromiumdriver/releases/download/v.{}/'
+operadriver_latest_release =    f"operadriver_win{os_bit}.zip" if platform.system() == 'Windows' else\
+                                "operadriver_linux64.zip" if platform.system() == "Linux" else\
+                                "operadriver_mac64.zip"
+operadriver_latest_release = latest_release_operadriver + operadriver_latest_release
+
+latest_release_edgedriver = 'https://msedgedriver.azureedge.net/{}/'
+edgedriver_latest_release =     f"edgedriver_win{os_bit}.zip" if platform.system() == 'Windows' and not 'arm' in platform.processor().lower() else\
+                                "edgedriver_mac64.zip" if platform.system() == 'Darwin' and not 'arm' in str(os.uname().machine) else\
+                                "edgedriver_mac64_m1.zip" if platform.system() == 'Darwin' and 'arm' in str(os.uname().machine) else\
+                                "edgedriver_linux64.zip" if platform.system() == 'Linux' else\
+                                "edgedriver_arm64.zip"
+edgedriver_latest_release = latest_release_edgedriver + edgedriver_latest_release
+
+
+url_release_phantomjs = "https://api.bitbucket.org/2.0/repositories/ariya/phantomjs/downloads/"
+os_bit_linux = 'x86_64' if os_bit == '64' else "i686"
+phantomjs_latest_release =  "phantomjs-{}-windows.zip" if platform.system() == 'Windows' else\
+                            "phantomjs-{}-" + f"linux-{os_bit_linux}.tar.bz2" if platform.system() == "Linux" else\
+                            "phantomjs-{}-macosx.zip"
+phantomjs_latest_release = url_release_phantomjs + phantomjs_latest_release
+
+#
+# BROWSERS AND THEIR UPDATERS
+#                                 
+
+chrome_browser_path = ['/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+"/Applications/Chromium.app/Contents/MacOS/Chromium"] if platform.system() == 'Darwin' else \
+['reg query "HKEY_CURRENT_USER\Software\Google\Chrome\BLBeacon" /v version',
+r'reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Google Chrome" /v version'] if platform.system() == 'Windows' else \
+"google-chrome-stable" if platform.system() == 'Linux' else ''
+
+
+firefox_browser_path = '/Applications/Firefox.app/Contents/MacOS/firefox' if platform.system() == 'Darwin' else \
+['reg query "HKEY_CURRENT_USER\Software\Mozilla\Mozilla Firefox" /v CurrentVersion',
+'reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Mozilla\Mozilla Firefox" /v CurrentVersion',
+r"Powershell (Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\firefox.exe').'(Default)').VersionInfo.ProductVersion"] if platform.system() == 'Windows' else\
+"firefox" if platform.system() == 'Linux' else ''
+
+
+
+edge_browser_path = '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge' if platform.system() == 'Darwin' else\
+'reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Edge\BLBeacon" /v version' if platform.system() == 'Windows' else ''
+
+edge_browser_release = 'https://go.microsoft.com/fwlink/?linkid=2069148&platform=Mac&Consent=1&channel=Stable' if platform.system() == 'Darwin' and not 'arm' in str(os.uname().machine) else \
+                        'https://go.microsoft.com/fwlink/?linkid=2093504&platform=Mac&Consent=1&channel=Stable' if platform.system() == 'Darwin' and 'arm' in str(os.uname().machine) else ''  
+
+
+opera_browser_path = r'reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall" /f Opera' if platform.system() == 'Windows' else \
+'/Applications/Opera.app/Contents/MacOS/Opera' if platform.system() == 'Darwin' else\
+"opera" if platform.system() == 'Linux' else ''
+
+from dataclasses import dataclass
+
+@dataclass
+class info:
+    version = "5.1.8"
+
+setting = dict(
+    {
+        "Program":
+        {
+            'version'                   : info.version,
+            'wedriverVersionPattern'    : r'([0-9.]*\.[0-9]+)',
+            'driversPath'               : base_dir + 'test' + os.path.sep + 'drivers' + os.path.sep,
+            'DriversFileFormat'         : ".exe" if platform.system() == 'Windows' else '',
+            'OSBitness'                 : os_bit,
+        },
+        "ChromeDriver":
+        {
+            "LinkLastRelease"                   : "https://chromedriver.storage.googleapis.com/LATEST_RELEASE",
+            "LinkLastReleaseFile"               : chromedriver_latest_release,
+            "LastReleasePlatform"               : 'chromedriver',
+            "LinkLatestReleaseSpecificVersion"  : "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_{}",
+            "LinkCheckVersionIsValid"           : "https://chromedriver.storage.googleapis.com/?delimiter=/&prefix={}/",
+        },
+        "GeckoDriver":
+        {
+            "LinkLastReleasePlatform"   : geckodriver_platform_release,
+            "LastReleasePlatform"       : 'geckodriver',
+        },
+        "OperaDriver":
+        {
+            "LinkLastReleasePlatform"   : operadriver_latest_release,
+            "LastReleasePlatform"       : 'operadriver',
+        },
+        "EdgeDriver":
+        {
+            "LinkLastRelease"                   : 'https://msedgewebdriverstorage.blob.core.windows.net/edgewebdriver/LATEST_STABLE',
+            "LinkLastReleaseFile"               : edgedriver_latest_release,
+            "LastReleasePlatform"               : 'msedgedriver',
+            "LinkCheckVersionIsValid"           : "https://msedgewebdriverstorage.blob.core.windows.net/edgewebdriver?prefix={}%2F&delimiter=%2F&maxresults=100&restype=container&comp=list&_=1622714933676&timeout=60000",
+            "LinkLatestReleaseSpecificVersion"  : "https://msedgewebdriverstorage.blob.core.windows.net/edgewebdriver/LATEST_RELEASE_{}_{}",
+        },
+        "PhantomJS":
+        {
+            "LinkLastReleaseFile"   : phantomjs_latest_release,
+            "LastReleasePlatform"   : 'phantomjs',
+            "LinkAllReleases"       : url_release_phantomjs,
+        },
+        "SafariDriver":
+        {   
+            "LinkLastRelease"       : 'https://support.apple.com/en-us/HT201222',
+            "LastReleasePlatform"   : 'safaridriver',
+        },
+        "ChromeBrowser":
+        {
+            "Path"                      : chrome_browser_path,
+            "LinkAllLatestRelease"      : 'https://chromereleases.googleblog.com/search/label/Stable%20updates',
+            "LinkAllLatestReleaseFile"  : 'https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg',
+        },
+        "FirefoxBrowser":
+        {
+            "Path"                          : firefox_browser_path,
+            "LinkAllLatestReleases"         : 'https://www.mozilla.org/en-US/firefox/releases/',
+            "LinkAllLatestRelease"          : 'https://download-installer.cdn.mozilla.net/pub/firefox/releases/{}/{}/{}/Firefox {}.{}',
+        },
+        "EdgeBrowser":
+        {
+            "Path"                          : edge_browser_path,
+            "LinkAllLatestRelease"          : 'https://docs.microsoft.com/en-us/deployedge/microsoft-edge-relnote-stable-channel',
+            "LinkAllLatestReleaseFile"      : edge_browser_release,
+        },
+        "OperaBrowser":
+        {
+            "Path"                          : opera_browser_path,
+            "LinkAllLatestRelease"          : 'https://get.geo.opera.com/pub/opera/desktop/',
+        },
+        "JsonSchema":
+        {
+            "githubAssetSchema"         : base_dir + 'schemas' + os.path.sep + 'github_asset_schema.json',
+            "githubReleaseSchema"       : base_dir + 'schemas' + os.path.sep + 'github_release_schema.json',
+            "githubReleaseTagSchema"    : base_dir + 'schemas' + os.path.sep + 'github_release_tag_schema.json',
+        },
+        "Github":
+        {
+            "linkLatestReleaseBySpecificRepoName"   : 'https://api.github.com/repos/{}/releases/latest',
+            "linkAllReleasesTags"                   : 'https://api.github.com/repos/{}/git/refs/tags',
+            "linkAllReleases"                       : 'https://api.github.com/repos/{}/releases',
+        },
+        "PyPi":
+        {
+            'urlProjectJson'    : 'https://pypi.python.org/pypi/selenium-driver-updater/json',
+        },
+    }
+)
