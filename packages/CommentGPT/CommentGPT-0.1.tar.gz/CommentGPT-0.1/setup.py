214271deec01428c113abd196d29bf668e23f85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:36 +0000

    Add package CommentGPT/0.1/CommentGPT-0.1.tar.gz

---
+++ packages/CommentGPT/CommentGPT-0.1.tar.gz/CommentGPT-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='CommentGPT',
+      version='0.1',
+      description='A python module that automatically comments code with ChatGPT',
+      url='https://github.com/brendankane04/CommentGPT',
+      author='Brendan Kane',
+      author_email='brendankane04@gmail.com',
+      license='MIT',
+      packages=['CommentGPT'],
+      zip_safe=False)
