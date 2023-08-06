c10f9e9ad256ea839ee5225699db6ef7e72649e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:05 +0000

    Add package netbox-storage/0.0.0.0.230/netbox_storage-0.0.0.0.230-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.230-py3-none-any.whl/netbox_storage/filters/partition.py
@@ -0,0 +1,23 @@
+from django.db.models import Q
+
+from netbox.filtersets import NetBoxModelFilterSet
+
+from netbox_storage.models import Partition
+
+
+class PartitionFilter(NetBoxModelFilterSet):
+    """Filter capabilities for Partition instances."""
+
+    class Meta:
+        model = Partition
+        fields = ("drive", "device", "size")
+
+    def search(self, queryset, name, value):
+        """Perform the filtered search."""
+        if not value.strip():
+            return queryset
+        qs_filter = (
+            Q(size__icontains=value)
+            | Q(device__icontains=value)
+        )
+        return queryset.filter(qs_filter)
