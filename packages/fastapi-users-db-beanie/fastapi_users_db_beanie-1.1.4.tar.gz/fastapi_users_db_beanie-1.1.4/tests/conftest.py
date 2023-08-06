858d5ae0bc0156d1ee5a43b1823ea817480ccf6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:13 +0000

    Add package fastapi-users-db-beanie/1.1.4/fastapi_users_db_beanie-1.1.4.tar.gz

---
+++ packages/fastapi-users-db-beanie/fastapi_users_db_beanie-1.1.4.tar.gz/fastapi_users_db_beanie-1.1.4/tests/conftest.py
@@ -0,0 +1,41 @@
+import asyncio
+from typing import Any, Dict
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    """
+    Force the pytest-asyncio loop to be the main one.
+    If there is no running event loop, create one and
+    set as the current one.
+    """
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    yield loop
+
+
+@pytest.fixture
+def oauth_account1() -> Dict[str, Any]:
+    return {
+        "oauth_name": "service1",
+        "access_token": "TOKEN",
+        "expires_at": 1579000751,
+        "account_id": "user_oauth1",
+        "account_email": "king.arthur@camelot.bt",
+    }
+
+
+@pytest.fixture
+def oauth_account2() -> Dict[str, Any]:
+    return {
+        "oauth_name": "service2",
+        "access_token": "TOKEN",
+        "expires_at": 1579000751,
+        "account_id": "user_oauth2",
+        "account_email": "king.arthur@camelot.bt",
+    }
