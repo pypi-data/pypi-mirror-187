a0c94f42d830dcb1428a7629761ba3ddad87bdff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:07 +0000

    Add package setupr/0.5.2/setupr-0.5.2-py3-none-any.whl

---
+++ packages/setupr/setupr-0.5.2-py3-none-any.whl/setupr/gbucket.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2022-present Worldr Technologies Limited. All Rights Reserved.
+"""Get installation data from Google Cloud Storage bucket.
+
+```python
+from google.cloud import storage
+storage_client = storage.Client.from_service_account_json('test.sa.json')
+bucket = storage_client.bucket("worldr-customer-test")
+blob = bucket.blob("test-values.yaml")
+blob.download_to_filename("test-values.yaml")
+```
+"""
+from __future__ import annotations
+
+from pathlib import Path
+
+import structlog
+from google.cloud import storage  # type: ignore[attr-defined]
+from google.cloud.exceptions import NotFound
+
+from setupr.utils import join_with_oxford_commas
+
+rlog = structlog.get_logger("setupr.get-url")
+
+
+class InstallationDataError(AttributeError):
+    """Installation data error."""
+
+
+class InstallationData:
+    """Get installation data from Google Cloud Storage bucket."""
+
+    def __init__(self, service_account_json: Path | None = None) -> None:
+        """Initialize."""
+        self.service_account_json = service_account_json
+        if not self.service_account_json:
+            sa_files = sorted(Path(".").glob("*.sa.json"))
+            if len(sa_files) == 1:
+                self.service_account_json = sa_files[0]
+            else:
+                text = "No service account file found"
+                if len(sa_files) > 1:
+                    text = "Too many service account file found"
+                msg = f"{text}: {join_with_oxford_commas(sa_files)}"
+                raise InstallationDataError(msg)
+        _stem = self.service_account_json.name.replace(".sa.json", "")
+        self.bucket_name = f"worldr-customer-{_stem}"
+        self.blob_value = f"{_stem}-values.yaml"
+        self.blob_name = (
+            f"{self.service_account_json.parent.as_posix()}/{self.blob_value}"
+        )
+        rlog.info("Bucket name", value=self.bucket_name)
+        rlog.info("Blob value", value=self.blob_value)
+        rlog.info("Blob name", value=self.blob_name)
+
+    def get(self) -> bool:
+        """Get installation data from Google Cloud Storage bucket.
+
+        If `self.blob_name` exists on the file system, it will be
+        overwritten. This is desired behaviour.
+
+        It is best to validate the yaml file after downloading it, however,
+        there is not point in enforcing it. If that is desired, use the `fetch`
+        method.
+        """
+        storage_client = storage.Client.from_service_account_json(
+            self.service_account_json
+        )
+        bucket = storage_client.bucket(self.bucket_name)
+        blob = bucket.blob(self.blob_value)
+        try:
+            blob.download_to_filename(self.blob_name)
+            rlog.info(f"Downloaded {self.blob_name} from {self.bucket_name}")
+        except NotFound as err:
+            msg = f"Installation data {self.blob_name} not found because {err}"
+            rlog.error(msg)
+            return False
+        return True
+
+    def fetch(self) -> bool:
+        """Fetch and verify installation data from bucket."""
+        return self.get()
---
+++ packages/setupr/setupr-0.5.2-py3-none-any.whl/setupr/utils.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2022-present Worldr Technologies Limited. All Rights Reserved.
+"""Utilities."""
+import enum
+from typing import Any, Sequence
+
+import requests
+
+from setupr import __version__
+
+GITHUB_URL = "https://api.github.com/repos/worldr/setupr/releases/latest"
+
+
+class VersionCheck(enum.Enum):
+    """Version check ENUM."""
+
+    LATEST = enum.auto()
+    LAGGING = enum.auto()
+    UNKNOWN = enum.auto()
+
+
+def join_with_oxford_commas(obj_list: Sequence[Any]) -> str:
+    """Oxford commas for lists.
+
+    Takes a list of objects and returns their string representations,
+    separated by commas and with 'and' between the penultimate and final
+    items
+    """
+    if not obj_list:
+        return ""
+    size = len(obj_list)
+    if size == 1:
+        return f"{str(obj_list[0])}"
+    return (
+        ", ".join(str(obj) for obj in obj_list[: size - 1])
+        + f", and {str(obj_list[size - 1])}"
+    )
+
+
+def check_if_latest_version() -> VersionCheck:
+    """Check if there is a new version published on GitHub."""
+    response = requests.get(GITHUB_URL, timeout=60)  # A minute time out.
+    if response.status_code == 200:
+        latest_version = response.json()["tag_name"]
+        if latest_version == f"v{__version__}":
+            return VersionCheck.LATEST
+        return VersionCheck.LAGGING
+    return VersionCheck.UNKNOWN
