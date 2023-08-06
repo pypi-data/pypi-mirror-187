4a0815bb81e4e2bb937ab1080a880ba3688b25d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:26 +0000

    Add package carbon-simulator/2.2.2/carbon-simulator-2.2.2.tar.gz

---
+++ packages/carbon-simulator/carbon-simulator-2.2.2.tar.gz/carbon-simulator-2.2.2/carbon/helpers/stdimports.py
@@ -0,0 +1,20 @@
+"""
+Carbon helper module - standard Carbon imports for sims
+
+USAGE
+
+    from carbon.helper.stdimports import *
+
+This will print version numbers of key Carbon modules
+"""
+from math import sqrt, exp, log
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from .. import CarbonSimulatorUI, CarbonOrderUI, P, __version__, __date__
+from .version import require_version
+
+print(f"[stdimports] Carbon v{__version__} ({__date__})")
+print( "[stdimports] {0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(CarbonSimulatorUI))
+print( "[stdimports] {0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(CarbonOrderUI))
