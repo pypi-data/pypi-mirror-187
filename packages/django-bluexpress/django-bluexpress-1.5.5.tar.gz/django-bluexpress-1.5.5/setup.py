df910d9c1619691a3b280eaf8babeed21890989b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:52 +0000

    Add package django-bluexpress/1.5.5/django-bluexpress-1.5.5.tar.gz

---
+++ packages/django-bluexpress/django-bluexpress-1.5.5.tar.gz/django-bluexpress-1.5.5/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='django-bluexpress',
+    version='1.5.5',
+    packages=['bluexpress'],
+    description='Django Bluexpress Integration',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Linets Development Team',
+    author_email='dteam@linets.cl',
+    url='https://gitlab.com/linets/ecommerce/oms/integrations/oms-bluexpress/',
+    license='MIT',
+    python_requires=">=3.7",
+    install_requires=[
+        'Django>=3',
+        'zeep>=4.0.0',
+        'xmltodict==0.12.0',
+        'requests>=2.25.1'
+    ]
+)
