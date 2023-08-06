933f21aa5efb8846eb8cbf8f78090737ebad1fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:57 +0000

    Add package blendertk/0.5.0/blendertk-0.5.0-py3-none-any.whl

---
+++ packages/blendertk/blendertk-0.5.0-py3-none-any.whl/blendertk/__init__.py
@@ -0,0 +1,30 @@
+# !/usr/bin/python
+# coding=utf-8
+from blendertk.core import *
+from blendertk.nodetk import Node
+from blendertk.cmpttk import Cmpt
+from blendertk.edittk import Edit
+from blendertk.scripttk import Script
+from blendertk.rigtk import Rig
+from blendertk.xformtk import Xform
+
+
+name = 'blendertk'
+__version__ = '0.5.0'
+
+# --------------------------------------------------------------------------------------------
+
+
+
+
+
+
+# print (__package__, __file__)
+# --------------------------------------------------------------------------------------------
+# Notes
+# --------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------
+# deprecated:
+# --------------------------------------------------------------------------------------------
