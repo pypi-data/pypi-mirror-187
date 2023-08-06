c5cf748bef88b3cd4dceb6d3a668c6d3ea8179c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:28 +0000

    Add package surquest-utils-config-formatter/0.0.1rc1/surquest_utils_config_formatter-0.0.1rc1-py2.py3-none-any.whl

---
+++ packages/surquest-utils-config-formatter/surquest_utils_config_formatter-0.0.1rc1-py2.py3-none-any.whl/surquest/utils/config_formatter/__init__.py
@@ -0,0 +1,9 @@
+"""
+The pyconfig module contains collection of these objects:
+ * DotDict
+ * Config
+"""
+
+from .config_formatter import ConfigFormatter, DotDict
+
+__all__ = ["ConfigFormatter", "DotDict"]
