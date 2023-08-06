dd7668b401382d137ab8f28102dcdd750c9ef56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:50 +0000

    Add package fa-archiver/1.0.0/fa-archiver-1.0.0.tar.gz

---
+++ packages/fa-archiver/fa-archiver-1.0.0.tar.gz/fa-archiver-1.0.0/fa/audio/audio.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env dls-python
+
+# Audio player for playing FA position data through PC speakers
+
+# Copyright (c) 2011 Michael Abbott, Diamond Light Source Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# Contact:
+#      Dr. Michael Abbott,
+#      Diamond Light Source Ltd,
+#      Diamond House,
+#      Chilton,
+#      Didcot,
+#      Oxfordshire,
+#      OX11 0DE
+#      michael.abbott@diamond.ac.uk
+
+import optparse
+import subprocess
+import os
+import sys
+import fcntl
+import math
+import numpy
+
+import cothread
+from fa import falib
+
+
+# Offset applied to user programmed volume, in dB.
+volume_offset = 50
+
+DEFAULT_LOCATION = 'SR'
+
+
+class Sub:
+    '''Manages subscription to archiver.  Received data is written to an event
+    queue, and we take care not to let the length of the queue grow.'''
+
+    def __init__(self, queue, bpm, server):
+        self.queue = queue
+        self.server = server
+
+        self.running = True
+        self.process = cothread.Spawn(self.__subscriber, bpm)
+        self.bpm = bpm
+
+    def __subscriber(self, bpm):
+        try:
+            sub = server.subscription([bpm])
+        except Exception as error:
+            print(f'Error {error} connecting to archiver')
+            os._exit(0)
+
+        while self.running:
+            block = sub.read(1000)
+            if len(self.queue) <= 1:
+                # Drop extra blocks to ensure we don't run too far ahead of the
+                # player, which is deliberately running about 0.7% slower.
+                self.queue.Signal(block)
+        sub.close()
+
+    def close(self):
+        self.running = False
+        self.process.Wait()
+
+
+class Player:
+    def __init__(self, bpm, server, volume):
+        self.server = server
+        self.volume = volume
+        self.channels = 'b'
+
+        cothread.Spawn(self.player)
+
+        self.queue = cothread.EventQueue()
+        self.sub = None
+
+        # Hang onto input volume level history for last 5 seconds.
+        self.mvolume = 100 * numpy.ones(50)
+        self.set_bpm(bpm)
+
+    def set_bpm(self, bpm):
+        if self.sub:
+            self.sub.close()
+        self.sub = Sub(self.queue, bpm, self.server)
+        # Reset volume history when changing FA id
+        self.mvolume[:] = 100
+
+
+    # Rescales audio to avoid clipping after volume scaling.
+    def rescale(self, block):
+        # First remove any DC component as this makes no sense for sound
+        block = numpy.float32(block)
+        block -= block.mean(axis = 0)
+
+        # Compute the available dynamic range
+        range = numpy.amax(numpy.abs(block))
+        vscale = 10 ** ((self.volume + volume_offset) / 20.)
+        mscale = (2**31 - 1.) / range
+        block *= min(vscale, mscale)
+
+        # Convert mscale into a measure of raw volume
+        mvol = 20 * numpy.log10(mscale) - volume_offset
+        self.mvolume = numpy.roll(self.mvolume, 1)
+        self.mvolume[0] = mvol
+
+        return block
+
+
+    def player(self):
+        aplay = subprocess.Popen(
+            ['aplay', '-c2', '-fS32_LE', '-r10000'], stdin = subprocess.PIPE)
+        fcntl.fcntl(aplay.stdin, fcntl.F_SETFL, os.O_NONBLOCK)
+
+        to_write = b''
+        while True:
+            block = self.queue.Wait()[:,0,:]
+            if self.channels == 'l':
+                block[:, 1] = block[:, 0]
+            elif self.channels == 'r':
+                block[:, 0] = block[:, 1]
+
+            block = self.rescale(block)
+
+            to_write = to_write + block.astype('l').tobytes()
+            while len(to_write) > 4096:
+                assert cothread.poll_list([(aplay.stdin, cothread.POLLOUT)])
+                written = os.write(aplay.stdin.fileno(), to_write)
+                to_write = to_write[written:]
+
+
+    # Command definitions
+    def command_h(self, arg):
+        '''h        Show this list of possible commands'''
+        for command in self.commands:
+            print(self.get_command(command).__doc__)
+
+    def command_q(self, arg):
+        '''q        Quit'''
+        sys.exit(0)
+
+    def command_b(self, arg):
+        '''b<n>     Set FA id <n> for playback'''
+        bpm = int(arg)
+        assert 0 <= bpm <= 255
+        self.set_bpm(bpm)
+
+    def command_v(self, arg):
+        '''v<n>     Set volume to <n>dB.  Default volume is 0dB'''
+        self.volume = float(arg)
+
+    def command_x(self, arg):
+        '''x        Only play X channel'''
+        self.channels = 'l'
+    def command_y(self, arg):
+        '''y        Only play Y channel'''
+        self.channels = 'r'
+    def command_a(self, arg):
+        '''a        Play X channel in left speaker, Y channel in right'''
+        self.channels = 'b'
+
+    def command_i(self, arg):
+        '''i        Show information about current settings'''
+        print(f'BPM {self.sub.bpm}')
+        print(f'Volume {self.volume} dB')
+        print(f'Channel {self.channels}')
+        print(f'Max volume: {numpy.min(self.mvolume):.1f} dB')
+
+    commands = 'hqbvxyai'
+
+
+    def get_command(self, command):
+        return getattr(self, 'command_%s' % command)
+
+    def shell(self):
+        '''Runs a simple command shell.'''
+        while True:
+            try:
+                input_cmd = input('> ').lstrip()
+            except Exception as e:
+                print(e)
+                sys.exit(0)
+
+            if input:
+                command, arg = input_cmd[0], input_cmd[1:]
+                if command in self.commands:
+                    try:
+                        self.get_command(command)(arg)
+                    except Exception as error:
+                        print(f'Error {error} in command')
+                else:
+                    print('Unknown command.  Possible commands are:')
+                    self.command_h(arg)
+
+parser = optparse.OptionParser(usage = '''\
+fa-audio [options] [location]
+
+Plays back FA data stream as audio through the PC speakers.''')
+parser.add_option(
+    '-v', dest = 'volume', default = 0, type = 'float',
+    help = 'Set initial volume in dB, default is 0dB')
+parser.add_option(
+    '-b', dest = 'bpm_id', default = 1, type = 'int',
+    help = 'Choose initial FA id for playback, default is 1')
+parser.add_option(
+    '-f', dest = 'full_path', default = False, action = 'store_true',
+    help = 'Location is full path to location file')
+parser.add_option(
+    '-S', dest = 'server', default = None,
+    help = 'Override server address in location file')
+options, args = parser.parse_args()
+
+if len(args) > 1:
+    parser.error('Unexpected arguments')
+elif args:
+    location = args[0]
+else:
+    location = DEFAULT_LOCATION
+
+falib.load_location_file(
+    globals(), location, options.full_path, options.server)
+
+
+server = falib.Server(server = FA_SERVER, port = FA_PORT)
+player = Player(options.bpm_id, server, options.volume)
+
+def main():
+    player.shell()
---
+++ packages/fa-archiver/fa-archiver-1.0.0.tar.gz/fa-archiver-1.0.0/fa/viewer/buffer.py
@@ -0,0 +1,131 @@
+#   Data Acquisition
+#
+# A stream of data for one selected BPM is acquired by connecting to the FA
+# sniffer server.  This is maintained in a "circular" buffer containing the last
+# 50 seconds worth of data (500,000 points) and delivered on demand to the
+# display layer.
+
+# Copyright (c) 2011 Michael Abbott, Diamond Light Source Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# Contact:
+#      Dr. Michael Abbott,
+#      Diamond Light Source Ltd,
+#      Diamond House,
+#      Chilton,
+#      Didcot,
+#      Oxfordshire,
+#      OX11 0DE
+#      michael.abbott@diamond.ac.uk
+
+import cothread
+import numpy
+
+
+class buffer:
+    '''Circular buffer.'''
+    # Super lazy implementation: we always just copy the data to the bottom!
+
+    def __init__(self, buffer_size):
+        self.buffer = numpy.zeros((buffer_size, 2))
+        self.buffer_size = buffer_size
+
+    def write(self, block):
+        blen = len(block)
+        self.buffer[:-blen] = self.buffer[blen:]
+        self.buffer[-blen:] = block
+
+    def size(self):
+        return self.data_size
+
+    def read(self, size):
+        return self.buffer[-int(size):]
+
+    def reset(self):
+        self.buffer[:] = 0
+
+
+class monitor:
+    def __init__(self,
+            server, on_event, on_connect, on_eof, buffer_size, read_size):
+        self.server = server
+        self.on_event = on_event
+        self.on_connect = on_connect
+        self.on_eof = on_eof
+        self.buffer = buffer(buffer_size)
+        self.update_size = read_size
+        self.notify_size = read_size
+        self.data_ready = 0
+        self.running = False
+        self.decimated = False
+        self.id = 0
+
+    def start(self):
+        assert not self.running, 'Strange: we are already running'
+        try:
+            self.subscription = self.server.subscription(
+                [self.id], decimated = self.decimated, uncork = self.decimated)
+        except Exception as message:
+            import traceback
+            traceback.print_exc()
+            self.on_eof('Unable to connect to server: %s' % message)
+        else:
+            self.running = True
+            self.buffer.reset()
+            self.task = cothread.Spawn(self.__monitor)
+
+    def stop(self):
+        if self.running:
+            self.running = False
+            self.task.Wait()
+
+    def set_channel(self, id=None, decimated=None):
+        running = self.running
+        self.stop()
+        if id is not None:
+            self.id = id
+        if decimated is not None:
+            self.decimated = decimated
+        if running:
+            self.start()
+
+    def resize(self, notify_size, update_size):
+        '''The notify_size is the data size delivered in each update, while
+        the update_size determines how frequently an update is delivered.'''
+        self.notify_size = notify_size
+        self.update_size = update_size
+        self.data_ready = 0
+
+    def __monitor(self):
+        stop_reason = 'Stopped'
+        self.on_connect()
+        while self.running:
+            try:
+                block = self.subscription.read(int(self.update_size))[:,0,:]
+            except Exception as exception:
+                stop_reason = str(exception)
+                self.running = False
+            else:
+                self.buffer.write(block)
+                self.data_ready += self.update_size
+                self.on_event(self.read())
+                self.data_ready -= self.update_size
+        self.subscription.close()
+        self.on_eof(stop_reason)
+
+    def read(self):
+        '''Can be called at any time to read the most recent buffer.'''
+        return 1e-3 * self.buffer.read(self.notify_size)
