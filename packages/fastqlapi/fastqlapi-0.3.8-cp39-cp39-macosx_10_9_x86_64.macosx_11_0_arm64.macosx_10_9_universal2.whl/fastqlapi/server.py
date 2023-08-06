87c30438d9a1ef322bc74300029c3563549563b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:53 +0000

    Add package fastqlapi/0.3.8/fastqlapi-0.3.8-cp39-cp39-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl

---
+++ packages/fastqlapi/fastqlapi-0.3.8-cp39-cp39-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl/fastqlapi/server.py
@@ -0,0 +1,44 @@
+import os;
+import zmq;
+import fastqlapi;
+import json;
+import subprocess;
+
+class Wrapper:
+    def __init__(self):
+        print('Initialising FastQL...')
+        self.callback = None
+        self.query_name = "Model"
+        self.args = {}
+        self.fields = {}
+        self.context = zmq.Context()
+    def listen(self):
+        fastqlapi.init(self.query_name, self.args, self.fields)
+        print(f"Started GraphQL server on http://{os.getenv('GRAPHQL_HOST', default='localhost')}:{os.getenv('GRAPHQL_PORT', default='8000')}.")
+        while True:
+            socket = self.context.socket(zmq.REP)
+            socket.connect(f"tcp://{os.getenv('ZEROMQ_HOST', default='localhost')}:{os.getenv('ZEROMQ_PORT', default='5555')}")
+            while True:
+                message = socket.recv_string()
+                parsed = json.loads(message)
+                out = {}
+                for k,v in parsed.items():
+                    try:
+                      out[k] = json.loads(v)
+                    except:
+                        out[k] = v
+                result = json.dumps(self.run_model(**out))
+                socket.send_string(result)
+    def run_model(self, **kwargs):
+        assert self.callback is not None
+        result = self.callback(**kwargs)
+        return result
+    def start(self, **kwargs):
+        assert self.fields is not []
+        self.args = kwargs['args']
+        self.query_name = kwargs['query_name']
+        self.fields = kwargs['fields']
+        self.callback = kwargs['callback']
+        self.listen()
+
+fastql_server = Wrapper()
