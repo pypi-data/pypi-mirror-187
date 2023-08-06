560b300cf1719c36c5d84b2ee093befcff370964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:24 +0000

    Add package opigallus/0.9.2/opigallus-0.9.2.tar.gz

---
+++ packages/opigallus/opigallus-0.9.2.tar.gz/opigallus-0.9.2/setup.py
@@ -0,0 +1,10 @@
+from io import open
+from setuptools import setup, find_packages
+
+setup(name='opigallus',
+      version='0.9.2',
+      author='Gallus',
+      author_email='bkmz_repvby@mail.ru',
+      description='opigallus',
+      packages=['opigallus']
+      )
