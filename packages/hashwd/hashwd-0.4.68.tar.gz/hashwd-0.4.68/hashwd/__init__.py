e49a28d237188d03865104cda5f8b585191084e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:26 +0000

    Add package hashwd/0.4.68/hashwd-0.4.68.tar.gz

---
+++ packages/hashwd/hashwd-0.4.68.tar.gz/hashwd-0.4.68/hashwd/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+from __main__ import main
+from hashwd import generate_password, clear_clipboard
+from defaults import WORDS_DEFAULT, NUMBERS_DEFAULT, SYMBOLS_DEFAULT, DICTIONARY_FILE, SYMBOLS, MAX_SYMBOLS, \
+    MAX_NUMBERS
