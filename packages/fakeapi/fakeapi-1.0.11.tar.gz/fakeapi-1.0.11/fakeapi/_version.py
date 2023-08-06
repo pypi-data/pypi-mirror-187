9bd74f60b1abf91c6303d571d95ef8a80433942a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:15 +0000

    Add package fakeapi/1.0.11/fakeapi-1.0.11.tar.gz

---
+++ packages/fakeapi/fakeapi-1.0.11.tar.gz/fakeapi-1.0.11/fakeapi/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides fakeapi version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update fakeapi` to change this file.
+
+from incremental import Version
+
+__version__ = Version("fakeapi", 1, 0, 11)
+__all__ = ["__version__"]
