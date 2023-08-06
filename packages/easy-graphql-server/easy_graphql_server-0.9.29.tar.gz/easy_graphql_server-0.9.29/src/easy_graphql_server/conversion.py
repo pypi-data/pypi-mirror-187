f1b716a3976cae36757fb59d2b93333ea8d4a2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package easy-graphql-server/0.9.29/easy_graphql_server-0.9.29.tar.gz

---
+++ packages/easy-graphql-server/easy_graphql_server-0.9.29.tar.gz/easy_graphql_server-0.9.29/src/easy_graphql_server/conversion.py
@@ -0,0 +1,224 @@
+"""
+    Helper functions to convert (almost) anything into a GraphQL type, object type or argument.
+"""
+
+
+import inspect
+import re
+import enum
+import datetime
+import decimal
+import threading
+
+from graphql.type.definition import GraphQLType, GraphQLInputField # pylint: disable=
+from graphql import \
+    GraphQLEnumType, GraphQLEnumValue, \
+    GraphQLField, GraphQLArgument, \
+    GraphQLObjectType, GraphQLInputObjectType
+
+from . import types, graphql_types, introspection
+
+
+PYTHON_GRAPHQL_TYPES_MAPPING = {
+    # boolean
+    bool: graphql_types.Boolean,
+    # integers
+    int: graphql_types.Int,
+    # other numbers
+    float: graphql_types.Float,
+    decimal.Decimal: graphql_types.Decimal,
+    # texts
+    str: graphql_types.String,
+    # date
+    datetime.date: graphql_types.Date,
+    datetime.datetime: graphql_types.DateTime,
+    datetime.time: graphql_types.Time,
+}
+
+
+_enums_cache = {}
+_enums_cache_lock = threading.Lock()
+
+
+def to_graphql_enum_key(name, capitalize=True):
+    """
+        Convert an enum name to its GraphQL-compatible counterpart
+
+        (only uppercase letters, underscores and digits, cannot start with a digit)
+    """
+    key = re.sub(r'(?:^\d|[^a-zA-Z0-9])', '_', str(name))
+    if capitalize:
+        return key.upper()
+    return key
+
+def to_graphql_enum_from_choices(prefix, choices, description=None, capitalize=True, schema=None):
+    # pylint: disable=unused-argument
+    """
+        Create a `GraphQLEnumType` from a list of choices.
+
+        Choices must presented as a list of key-value pairs.
+    """
+    with _enums_cache_lock:
+        if prefix in _enums_cache:
+            graphql_type = _enums_cache[prefix]
+        else:
+            graphql_type = _enums_cache[prefix] = GraphQLEnumType(f'{prefix}__enum_type', {
+                to_graphql_enum_key(key, capitalize): GraphQLEnumValue(key, value)
+                for key, value in choices
+            }, description=description)
+    return graphql_type
+
+def to_graphql_enum_from_enum(prefix, enum):
+    """
+        Create a `GraphQLEnumType` from a subclass of native Python Enum.
+    """
+    raise NotImplementedError()
+
+def to_graphql_type(type_, prefix, for_input=False, schema=None):
+    """
+        Returns a GraphQL type given a `GraphQLType`, a Python native type,
+        a mapping, a single-item list or a subclass of `enum.Enum`.
+    """
+    # if this is already a graphql type, return it as is
+    if introspection.is_instance_or_subclass(type_, GraphQLType):
+        return type_
+    # mapping
+    if isinstance(type_, dict):
+        return to_graphql_objecttype(type_, prefix, for_input=for_input, schema=schema)
+    if inspect.isclass(type_):
+        # convert native Python types
+        if type_ in PYTHON_GRAPHQL_TYPES_MAPPING:
+            return PYTHON_GRAPHQL_TYPES_MAPPING[type_]
+        # enum
+        if issubclass(type_, enum.Enum):
+            return to_graphql_enum_from_choices(
+                prefix = prefix,
+                choices = [(key, value.value) for key, value in type_.__members__.items()],
+                schema = schema)
+    # list
+    if isinstance(type_, list) and len(type_) == 1:
+        return graphql_types.List(
+            to_graphql_type(type_[0], prefix, for_input=for_input, schema=schema)
+        )
+    # mandatory
+    if isinstance(type_, types.Required):
+        return graphql_types.NonNull(
+            to_graphql_type(type_.type_, prefix, for_input=for_input, schema=schema)
+        )
+    # existing model interface
+    if isinstance(type_, types.ModelInterface):
+        model_config = schema.get_model_config(name=type_.model_name)
+        if model_config is None:
+            raise ValueError(f'No model in schema with name `{type_.model_name}` '
+                '(consider changing the order of expositions declaration)')
+        mapping = model_config.get_type_mapping(
+            operation = type_.operation,
+            with_custom_fields = True)
+        if not type_.exclude and not type_.additional:
+            return to_graphql_type(
+                type_ = mapping,
+                prefix = model_config.name,
+                for_input = for_input,
+                schema = schema)
+        for field_name in type_.exclude:
+            mapping.pop(field_name, None)
+        mapping.update(type_.additional)
+        return to_graphql_type(
+            type_ = mapping,
+            prefix = prefix,
+            for_input = for_input,
+            schema = schema)
+    # existing model field
+    if isinstance(type_, types.ModelField):
+        model_config = schema.get_model_config(name=type_.model_name)
+        if model_config is None:
+            raise ValueError(f'No model in schema with name `{type_.model_name}` '
+                '(consider changing the order of expositions declaration)')
+        mapping = model_config.get_type_mapping(with_custom_fields = True)
+        try:
+            for field_name in type_.field_path:
+                mapping = mapping[field_name]
+        except KeyError as error:
+            raise ValueError(
+                f'Invalid field path `{".".join(type_.field_path)}` '
+                f'for model named `{type_.model_name}`') from error
+        return to_graphql_type(
+            type_ = mapping,
+            prefix = prefix,
+            for_input = for_input,
+            schema = schema)
+    # oops.
+    if isinstance(type_, types.Model):
+        raise ValueError('Could not convert `easy_graphql_server.Model` instance '
+            'to graphql type, use instead `Model(...).output_format`, '
+            '`Model(...).create_input_format` or `Model(...).update_input_format`')
+    raise ValueError(f'Could not convert {type_} to graphql type')
+
+
+_objecttype_cache = {}
+
+def to_graphql_objecttype(type_, prefix, for_input=False, schema=None):
+    """
+        Returns a `GraphQLInputObjectType` or a `GraphQLObjectType` from a given type.
+
+        Possible input `type_` are `GraphQLObjectType`, or a `dict` mapping of strings
+        to types that are supported by `to_graphql_type()`.
+
+        Function returns a `GraphQLObjectType` that uses `GraphQLField` when `for_input == False`,
+        a `GraphQLInputObjectType` with `GraphQLInputField` when `for_input == True`.
+    """
+    # already a field
+    if isinstance(type_, GraphQLObjectType):
+        return type_
+    # in cache
+    cache_key = (prefix, for_input)
+    if cache_key in _objecttype_cache:
+        return _objecttype_cache[cache_key]
+    # mapping
+    if isinstance(type_, dict):
+        if for_input:
+            object_type_class = GraphQLInputObjectType
+            object_name = f'{prefix}__input_type'
+            field_class = GraphQLInputField
+        else:
+            object_type_class = GraphQLObjectType
+            object_name = f'{prefix}__output_type'
+            field_class = GraphQLField
+        object_type = object_type_class(object_name, lambda : {
+            key: field_class(to_graphql_type(
+                type_ = value,
+                prefix = f'{prefix}__{key}',
+                for_input = for_input,
+                schema = schema))
+            for key, value in type_.items()
+        })
+        _objecttype_cache[cache_key] = object_type
+        return object_type
+    # otherwise
+    return GraphQLInputField(type_) if for_input else GraphQLField(type_)
+
+def to_graphql_argument(type_, prefix, schema=None):
+    """
+        Returns a `GraphQLArgument`, taking as argument either a `GraphQLArgument`
+        or `dict` mapping of `str` to types that are supported by `to_graphql_type()`.
+    """
+    # already a field
+    if isinstance(type_, GraphQLArgument):
+        return type_
+    # mapping
+    if isinstance(type_, dict):
+        return {
+            key: (
+                value
+                if isinstance(value, GraphQLArgument) else
+                GraphQLArgument(to_graphql_type(
+                    type_ = value,
+                    prefix = f'{prefix}__{key}',
+                    for_input = True,
+                    schema = schema))
+            )
+            for key, value in type_.items()
+        }
+    # otherwise
+    raise ValueError(f'Could not convert `{type_}` of type `{type(type_)}` to GraphQL argument, '
+        'expecting either a instance of `GraphQLArgument` or `dict`')
---
+++ packages/easy-graphql-server/easy_graphql_server-0.9.29.tar.gz/easy_graphql_server-0.9.29/src/easy_graphql_server/exceptions.py
@@ -0,0 +1,201 @@
+"""
+    Definition of easily serializable exceptions, meant to be thrown from the GraphQL API.
+"""
+
+import re
+
+from graphql import GraphQLError
+from graphql.language.ast import Node
+
+from . import custom_json
+
+
+class BaseError(Exception):
+    """
+        Base exception for `easy_graphql_server`
+    """
+    def __init__(self, type_, payload):
+        self.type_ = type_
+        self.payload = payload
+        self.message = custom_json.dumps({
+            'type': self.type_,
+            'payload': self.payload,
+        })
+        Exception.__init__(self, self.message)
+
+    def format_for_gcp(self):
+        if isinstance(self.payload, list) and isinstance(self, ValidationError):
+            return custom_json.dumps({
+                'type': self.type_,
+                'payload': {'issues': self.payload},
+            })
+        return self.message
+
+
+_graphqlerror__init__ = GraphQLError.__init__
+def graphqlerror__init__(self, message, nodes=None, source=None, positions=None, path=None,
+        original_error=None, extensions=None):
+    """
+        Patch for the constructor of GraphQLError class, defined in graphql-core.
+
+        Gives more details, in the same format as BaseError output.
+    """
+    if original_error is None or not isinstance(original_error, BaseError):
+        type_ = None
+        # parse original error message
+        possible_errors = (
+            ('MISSING_ARGUMENT',
+                r"^Field '(?P<parent_type_name>.*?)\.(?P<expected_field_name>.*?)' of required type "
+                r"'(?P<expected_type>.*?)' was not provided\."),
+            ('MISSING_ARGUMENT',
+                r"^Field '(?P<parent_type_name>.*?)' argument '(?P<expected_argument_name>.*?)' of type "
+                r"'(?P<expected_type>.*?)' is required, but it was not provided\."),
+            ('UNEXPECTED_ARGUMENT',
+                r"^Field '(?P<unexpected_argument_name>.*?)' is not defined by type '(?P<parent_type_name>.*?)'\."),
+            ('UNEXPECTED_ARGUMENT',
+                r"^Unknown argument '(?P<unexpected_argument_name>.*?)' on field '(?:Mutation|Query)\.(?P<method_name>.*?)'\."),
+            ('METHOD_NOT_FOUND',
+                r"^Cannot query field '(?P<method_name>.*?)' on type '(?:Mutation|Query)'\.$"),
+            ('METHOD_NOT_FOUND',
+                r"^Cannot query field '(?P<method_name>.*?)' on type '(?:Mutation|Query)'\. Did you mean "
+                r"(?P<suggestions>.*?)\?$"),
+            ('UNEXPECTED_QUERIED_FIELD',
+                r"^Cannot query field '(?P<unexpected_field_name>.*?)' on type '(?P<parent_type_name>.*?)'\.$"),
+            ('UNEXPECTED_QUERIED_FIELD',
+                r"^Cannot query field '(?P<unexpected_field_name>.*?)' on type '(?P<parent_type_name>.*?)'\. Did you "
+                r"mean (?P<suggestions>.*?)\?$"),
+            ('WRONGLY_TYPED_ARGUMENT',
+                r"^Expected value of type '(?P<expected_field_type>.*?)', found (?P<provided_field_value>.*?)\."),
+            ('NON_NULL_FIELD',
+                r"^Cannot return null for non\-nullable field (?P<field_path>)\."),
+            ('SYNTAX_ERROR',
+                r"^Syntax error: (?P<message>.*?)$"),
+            ('WRONG_ENUM_VALUE',
+                r"^Value '(?P<provided_field_value>.*?)' does not exist in '(?P<enum_type>.*?)' enum\.$"),
+            ('UNKNOWN_TYPE',
+                r"^Unknown type '(?P<UNKNOWN_TYPE>.*?)'\."),
+        )
+        for possible_error, regex in possible_errors:
+            match = re.search(regex, message)
+            if match:
+                type_ = possible_error
+                payload = match.groupdict() or {}
+                break
+        if type_ is None:
+            type_ = 'GRAPHQL'
+            payload = {'message': message}
+        # parse suggestions
+        if type_ in ('UNEXPECTED_QUERIED_FIELD', 'METHOD_NOT_FOUND'):
+            if 'suggestions' in payload:
+                payload['suggestions'] = re.findall(r"'([^']+)'", payload['suggestions'])
+            else:
+                payload['suggestions'] = []
+        # build path when possible
+        if isinstance(nodes, Node):
+            token = nodes.loc.start_token
+            if token.kind.value == 'Name':
+                path = [token.value]
+            ready = False
+            while token.prev:
+                token = token.prev
+                if token.kind.value in '({':
+                    ready = True
+                elif ready and token.kind.value == 'Name':
+                    if path:
+                        path.append(token.value)
+                    else:
+                        path = [token.value]
+                    ready = False
+            payload['path'] = path = path[::-1]
+        # compute message for path
+        message = custom_json.dumps({
+            'type': type_,
+            'payload': payload,
+        })
+    return _graphqlerror__init__(
+        self, message, nodes, source, positions, path, original_error, extensions)
+setattr(GraphQLError, '__init__', graphqlerror__init__)
+
+
+class InternalError(BaseError):
+    """
+        Internal coding error
+    """
+    def __init__(self, exception=None):
+        if isinstance(exception, Exception):
+            # extract traceback
+            tracebacks = []
+            traceback = getattr(exception, '__traceback__')
+            while traceback:
+                tracebacks.append(traceback)
+                traceback = traceback.tb_next
+            # build payload
+            exception_class = type(exception)
+            payload = {
+                'class': (
+                    exception_class.__qualname__
+                    if exception_class.__module__ == 'builtins' else
+                    f'{exception_class.__module__}.{exception_class.__qualname__}'
+                ),
+                'args': list(map(str, exception.args)),
+                'traceback': [
+                    f'{traceback.tb_frame.f_code.co_filename}:{traceback.tb_lineno}'
+                    for traceback in tracebacks
+                ]
+            }
+        else:
+            payload = {}
+        BaseError.__init__(self, 'INTERNAL', payload)
+
+class UnauthenticatedError(BaseError):
+    """
+        Thrown when authentication is required for a query, but not provided.
+    """
+    def __init__(self):
+        BaseError.__init__(self, 'UNAUTHENTICATED', {})
+
+class NotFoundError(BaseError):
+    """
+        Thrown when an item was not found in database.
+    """
+    def __init__(self, filters):
+        BaseError.__init__(self, 'NOT_FOUND', {
+            'filters': filters,
+        })
+
+class ForbiddenError(BaseError):
+    """
+        Thrown when an authenticated user is not allowed to perform an operation
+        on a specific item.
+    """
+    def __init__(self, operation, authenticated_user, path):
+        BaseError.__init__(self, 'FORBIDDEN', {
+            'operation': operation.name,
+            'authenticated_user': str(authenticated_user),
+            'path': path,
+        })
+
+class ValidationError(BaseError):
+    """
+        Thrown when attempting to save wrong data into database.
+    """
+    def __init__(self, issues):
+        BaseError.__init__(self, 'VALIDATION', issues)
+
+class DuplicateError(BaseError):
+    """
+        Thrown when an item is seen as duplicate in database.
+    """
+    def __init__(self, path):
+        BaseError.__init__(self, 'DUPLICATE', {
+            'path': path,
+        })
+
+class IntegrityError(BaseError):
+    """
+        Thrown when an item cannot be removed without compromising the database.
+    """
+    def __init__(self, path):
+        BaseError.__init__(self, 'INTEGRITY', {
+            'path': path,
+        })
