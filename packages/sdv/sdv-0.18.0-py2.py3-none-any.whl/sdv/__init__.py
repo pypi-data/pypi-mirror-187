0f8680ce6e4af5f0fefebf94c338a1d847d21ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:48 +0000

    Add package sdv/0.18.0/sdv-0.18.0-py2.py3-none-any.whl

---
+++ packages/sdv/sdv-0.18.0-py2.py3-none-any.whl/sdv/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# configure logging for the library with a null handler (nothing is printed by default). See
+# http://docs.python-guide.org/en/latest/writing/logging/
+
+"""Top-level package for SDV."""
+
+__author__ = 'DataCebo, Inc.'
+__email__ = 'info@sdv.dev'
+__version__ = '0.18.0'
+
+from sdv import constraints, evaluation, metadata, relational, tabular
+from sdv.demo import get_available_demos, load_demo
+from sdv.metadata import Metadata, Table
+from sdv.sdv import SDV
+
+__all__ = (
+    'demo',
+    'constraints',
+    'evaluation',
+    'metadata',
+    'relational',
+    'tabular',
+    'get_available_demos',
+    'load_demo',
+    'Metadata',
+    'Table',
+    'SDV',
+)
