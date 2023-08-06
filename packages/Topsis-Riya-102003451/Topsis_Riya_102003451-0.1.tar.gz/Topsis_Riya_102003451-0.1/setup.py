ea5ff91dc55810a424aba56eed1f6e0c15329fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:11 +0000

    Add package Topsis-Riya-102003451/0.1/Topsis_Riya_102003451-0.1.tar.gz

---
+++ packages/Topsis-Riya-102003451/Topsis_Riya_102003451-0.1.tar.gz/Topsis_Riya_102003451-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setup(name="Topsis_Riya_102003451",
+version ="0.1",
+description ="This is package for topsis of version 0.3",
+# long_description=long_description,
+#     long_description_content_type="text/markdown",
+author="Taruna Jain",
+author_email="tjain_be20@thapar.edu",
+packages=['Topsis_Riya_102003451'],
+install_requires=['pandas'],
+include_package_data=True,
+    entry_points={
+            "console_scripts": [
+                "topsis123=Topsis_Riya_102003451.topsis:main",
+            ]
+    }
+)
+
