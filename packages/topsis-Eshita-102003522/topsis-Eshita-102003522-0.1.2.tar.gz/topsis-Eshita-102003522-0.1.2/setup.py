6a99fc731a80533cd4ed891e3962319e5aa5e103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:03 +0000

    Add package topsis-Eshita-102003522/0.1.2/topsis-Eshita-102003522-0.1.2.tar.gz

---
+++ packages/topsis-Eshita-102003522/topsis-Eshita-102003522-0.1.2.tar.gz/topsis-Eshita-102003522-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+with open("README.md", "r") as f:
+    long_description = f.read()
+setup(
+  name = 'topsis-Eshita-102003522',         
+  packages = ['topsis-Eshita-102003522'],  
+  version = '0.1.2',      
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  long_description=long_description,
+    long_description_content_type="text/markdown",
+  description = 'Calculate Topsis score and save it in a csv file',
+  author = 'Eshita Arora',                   
+  author_email = 'earora_be20@thapar.edu',     
+  url = 'https://github.com/eshitaarora/Topsis-Eshita-102003522',
+  download_url = 'https://github.com/eshitaarora/Topsis-Eshita-102003522/archive/refs/tags/v0.1.tar.gz',
+  keywords = ['TOPSISSCORE', 'RANK', 'DATAFRAME'],
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.9',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
