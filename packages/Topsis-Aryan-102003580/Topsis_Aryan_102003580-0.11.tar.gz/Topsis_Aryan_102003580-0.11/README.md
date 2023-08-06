3ec15d5f42bb9508b5627c8241d0d3ce065f0a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:34 +0000

    Add package Topsis-Aryan-102003580/0.11/Topsis_Aryan_102003580-0.11.tar.gz

---
+++ packages/Topsis-Aryan-102003580/Topsis_Aryan_102003580-0.11.tar.gz/Topsis_Aryan_102003580-0.11/README.md
@@ -0,0 +1,4 @@
+```sh
+pip3 install Topsis-Aryan-102003580
+topsis(dataframe,weights,impacts,outputfile)
+```
---
+++ packages/Topsis-Aryan-102003580/Topsis_Aryan_102003580-0.11.tar.gz/Topsis_Aryan_102003580-0.11/Topsis_Aryan_102003580/__init__.py
@@ -0,0 +1,6 @@
+from topsis import topsis
+# import pandas as pd
+# data=pd.read_excel(r"C:\Topsis-102003580\102003580-data.csv")
+# df=pd.DataFrame(data)
+# print(df)
+# topsis(df,[1,1,1,1,1],['+','+','+','+','+'],"output.csv")
