5c6351f3ac48eaabeb1845767cbac8cd5b23fc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:34 +0000

    Add package Topsis-Khushi-102183044/0.0.3/Topsis_Khushi_102183044-0.0.3.tar.gz

---
+++ packages/Topsis-Khushi-102183044/Topsis_Khushi_102183044-0.0.3.tar.gz/Topsis_Khushi_102183044-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup 
+
+setup(name="Topsis_Khushi_102183044",version="0.0.3",
+description="Topsis code",
+long_description="This is a very simple code",
+author="Khushi Prasad",
+packages=['Topsis_Khushi_102183044'],
+install_requires=['pandas']
+)
