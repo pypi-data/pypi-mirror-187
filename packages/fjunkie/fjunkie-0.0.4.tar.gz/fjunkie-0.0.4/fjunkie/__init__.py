f1e179de7d46f923ad918716d4f49f6a19371738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:47 +0000

    Add package fjunkie/0.0.4/fjunkie-0.0.4.tar.gz

---
+++ packages/fjunkie/fjunkie-0.0.4.tar.gz/fjunkie-0.0.4/fjunkie/__init__.py
@@ -0,0 +1,11 @@
+from fjunkie.File import File
+from fjunkie.PyFile import PyFile
+from fjunkie.HtmlFile import HtmlFile
+from fjunkie.CssFile import CssFile
+
+from fjunkie.Reader import Reader
+from fjunkie.Writer import Writer
+
+
+
+#from file_junkie.filejunkie.File import File
