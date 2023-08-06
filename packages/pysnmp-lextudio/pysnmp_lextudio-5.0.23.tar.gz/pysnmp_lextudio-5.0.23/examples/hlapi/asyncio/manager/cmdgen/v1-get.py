23844033f86253a349331e8ef7d345ce3fa45de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:48 +0000

    Add package pysnmp-lextudio/5.0.23/pysnmp_lextudio-5.0.23.tar.gz

---
+++ packages/pysnmp-lextudio/pysnmp_lextudio-5.0.23.tar.gz/pysnmp_lextudio-5.0.23/examples/hlapi/asyncio/manager/cmdgen/v1-get.py
@@ -0,0 +1,49 @@
+"""
+SNMPv1
+++++++
+
+Send SNMP GET request using the following options:
+
+  * with SNMPv1, community 'public'
+  * over IPv4/UDP
+  * to an Agent at demo.pysnmp.com:161
+  * for an instance of SNMPv2-MIB::sysDescr.0 MIB object
+  * Based on asyncio I/O framework
+
+Functionally similar to:
+
+| $ snmpget -v1 -c public demo.pysnmp.com SNMPv2-MIB::sysDescr.0
+
+"""  #
+import asyncio
+from pysnmp.hlapi.asyncio import *
+
+
+async def run():
+    snmpEngine = SnmpEngine()
+    get_result = await getCmd(
+        snmpEngine,
+        CommunityData('public', mpModel=0),
+        UdpTransportTarget(('demo.pysnmp.com', 161)),
+        ContextData(),
+        ObjectType(ObjectIdentity("SNMPv2-MIB", "sysDescr", 0)),
+    )
+
+    errorIndication, errorStatus, errorIndex, varBinds = await get_result
+    if errorIndication:
+        print(errorIndication)
+    elif errorStatus:
+        print(
+            "{} at {}".format(
+                errorStatus.prettyPrint(),
+                errorIndex and varBinds[int(errorIndex) - 1][0] or "?",
+            )
+        )
+    else:
+        for varBind in varBinds:
+            print(" = ".join([x.prettyPrint() for x in varBind]))
+
+    snmpEngine.transportDispatcher.closeDispatcher()
+
+
+asyncio.run(run())
