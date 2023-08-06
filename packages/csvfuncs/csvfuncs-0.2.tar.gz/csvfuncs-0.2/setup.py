8d308f664f0f08bc89355dccc1e3da178c674e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:44 +0000

    Add package csvfuncs/0.2/csvfuncs-0.2.tar.gz

---
+++ packages/csvfuncs/csvfuncs-0.2.tar.gz/csvfuncs-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='csvfuncs',
+    version='0.2',
+    packages=['csvfuncs'],
+    install_requires=[],
+    author = "Fraser Woodward"
+)
+
+
