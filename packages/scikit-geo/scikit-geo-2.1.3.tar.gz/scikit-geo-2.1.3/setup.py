7d89a3fd842ace8c84d94c8c8938262a441d5fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:26 +0000

    Add package scikit-geo/2.1.3/scikit-geo-2.1.3.tar.gz

---
+++ packages/scikit-geo/scikit-geo-2.1.3.tar.gz/scikit-geo-2.1.3/setup.py
@@ -0,0 +1,11 @@
+#coding:utf8
+from setuptools import setup
+
+setup(
+   name='scikit-geo',        # 项目名
+   version='2.1.3',       # 版本号
+   description='scikit geography',
+   packages=['CV'],   # 包括在安装包内的Python包
+   author='Zijie Wang',
+   install_requires=["numpy","pandas","geopandas","rasterio","rasterstats"]
+)
