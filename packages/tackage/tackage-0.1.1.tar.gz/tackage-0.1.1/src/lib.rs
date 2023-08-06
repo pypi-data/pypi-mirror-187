c5ae9977ae19d5efc5e911f1c7e3093c98a9770c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:08 +0000

    Add package tackage/0.1.1/tackage-0.1.1.tar.gz

---
+++ packages/tackage/tackage-0.1.1.tar.gz/tackage-0.1.1/src/lib.rs
@@ -0,0 +1,32 @@
+use pyo3::prelude::*;
+use rayon::prelude::*;
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+#[pyfunction]
+fn search(contents: &str, needle: &str) -> usize {
+    contents
+        .par_lines()
+        .map(|line| count_line(line, needle))
+        .sum()
+}
+
+fn count_line(line: &str, needle: &str) -> usize {
+    let mut total = 0;
+    for word in line.split(' ') {
+        if word == needle {
+            total += 1;
+        }
+    }
+    total
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn tackage(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(search, m)?)?;
+    Ok(())
+}
