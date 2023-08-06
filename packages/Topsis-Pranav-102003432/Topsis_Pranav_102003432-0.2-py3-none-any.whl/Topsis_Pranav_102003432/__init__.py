844e4b4c8a0f3f08f546e95f61f41a9de409bd88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:24 +0000

    Add package Topsis-Pranav-102003432/0.2/Topsis_Pranav_102003432-0.2-py3-none-any.whl

---
+++ packages/Topsis-Pranav-102003432/Topsis_Pranav_102003432-0.2-py3-none-any.whl/Topsis_Pranav_102003432/__init__.py
@@ -0,0 +1,9 @@
+#version of module
+__version__ = "1.0.0"
+
+class Hello:
+    def __init__():
+        print("Constructor ban gaya")
+    def pranav(self,singh):
+        print("version - 1.0.0")
+        return singh
