311a9d7c2678dc4a9fb2e9a0e29f225bc4bfda11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:29 +0000

    Add package simple-calculator-by-DT/1.0.0/simple_calculator_by_DT-1.0.0-py3-none-any.whl

---
+++ packages/simple-calculator-by-DT/simple_calculator_by_DT-1.0.0-py3-none-any.whl/Sub-Package/simple_calculator.py
@@ -0,0 +1,11 @@
+def add_nnumbers(num1,num2):
+    return num1 + num2
+
+def subtract_nnumbers(num1,num2):
+    return num1 - num2
+
+def multply_nnumbers(num1,num2):
+    return num1 * num2
+
+def divide_nnumbers(num1,num2):
+    return num1 / num2
