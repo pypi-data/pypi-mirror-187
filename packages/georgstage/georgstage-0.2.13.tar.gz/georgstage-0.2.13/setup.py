17db921fa36d2c897c5eac170a3fd38b5efd46d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:49 +0000

    Add package georgstage/0.2.13/georgstage-0.2.13.tar.gz

---
+++ packages/georgstage/georgstage-0.2.13.tar.gz/georgstage-0.2.13/setup.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='georgstage',
+    version='0.2.13',
+    download_url='https://github.com/skipperkongen/georgstage/archive/refs/tags/v0.2.13.tar.gz',
+    author="Pimin Konstantin Kefaloukos",
+    author_email="skipperkongen@gmail.com",
+    description="Hjælpeprogram til skoleskibet Georg Stage",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/skipperkongen/georgstage",
+    install_requires=[
+        'numpy~=1.22.0',
+        'pandas~=1.4.0',
+        'PuLP~=2.6',
+        'pyinstaller~=4.9',
+    ],
+    extras_require={'test': ['pytest', 'flake8']},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['georgstage=georgstage.cli:main'],
+    }
+)
