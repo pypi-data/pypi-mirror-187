3e8a507f6c6a8fa4d9fff96cf6bae1478ec93c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:13 +0000

    Add package AGidasScraper/0.5/AGidasScraper-0.5.tar.gz

---
+++ packages/AGidasScraper/AGidasScraper-0.5.tar.gz/AGidasScraper-0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='AGidasScraper',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'webdriver-manager',
+    ],
+    entry_points={
+        'console_scripts': [
+            'AGidasScraper = AGidasScraper.ascrape:main'
+        ]
+    },
+    author = "erikonasz",
+    author_email = "erikuzas123@gmail.com",
+    description = "Autogidas scraper",
+    url = "https://github.com/erikonasz/AGidasScrape"
+)
