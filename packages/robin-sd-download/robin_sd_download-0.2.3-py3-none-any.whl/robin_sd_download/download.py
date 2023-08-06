7a6e276a8c1fb50789ff01747264904c552026fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:16 +0000

    Add package robin-sd-download/0.2.3/robin_sd_download-0.2.3-py3-none-any.whl

---
+++ packages/robin-sd-download/robin_sd_download-0.2.3-py3-none-any.whl/robin_sd_download/download.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+from .arg_parse import arg_parse
+from .get_software import get_software
+
+home_dir = os.path.expanduser("~")
+config_file = home_dir + "/.config/robin/software_deployment.yaml"
+
+if __name__ == "__main__":
+
+    
+    arg_parse(config_file, home_dir)
