9840b0dda3b0271b06fae56b9d8b5197ff02af31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:36 +0000

    Add package damv1airtableprojectk8salert/0.5.15/damv1airtableprojectk8salert-0.5.15-py3-none-any.whl

---
+++ packages/damv1airtableprojectk8salert/damv1airtableprojectk8salert-0.5.15-py3-none-any.whl/damv1airtableprojectk8salert/__init__.py
@@ -0,0 +1,3 @@
+from .myairtable_projectk8salert import utils, const_type, const_sincelast, const_process, const_status, const_execmethod
+from .sandbox_myairtable_projectk8salert import sandbox, testing
+from .production_myairtable_projectk8salert import production
