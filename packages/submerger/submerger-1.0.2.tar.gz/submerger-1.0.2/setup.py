7aae38ea21450d31b36b61aa5df09905011a61a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:03:19 +0000

    Add package submerger/1.0.2/submerger-1.0.2.tar.gz

---
+++ packages/submerger/submerger-1.0.2.tar.gz/submerger-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+  long_description = fh.read()
+
+setup(
+    author='Andrii Valiukh',
+    author_email='andrii.valiukh0@gmail.com',
+    description='Subtitle merging tool',
+    long_description=long_description,
+    name='submerger',
+    version='1.0.2',
+    url='https://gitlab.com/andr1i/submerger',
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_requires=[],
+    extras_require={'dev': [
+        'mypy',
+        'pylint',
+    ]},
+    scripts=['bin/submerge'],
+)
