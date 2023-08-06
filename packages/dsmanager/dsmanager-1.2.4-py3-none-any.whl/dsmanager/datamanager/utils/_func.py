ac2be453a94628fbfca9a0cbee07584883e0336c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:12 +0000

    Add package dsmanager/1.2.4/dsmanager-1.2.4-py3-none-any.whl

---
+++ packages/dsmanager/dsmanager-1.2.4-py3-none-any.whl/dsmanager/datamanager/utils/_func.py
@@ -0,0 +1,19 @@
+"""@Author: Rayane AMROUCHE
+
+Utils functions for the DataManager.
+"""
+
+
+def find_type(path: str) -> str:
+    """Extract file's type form path.
+
+    Args:
+        path (str): Path of a file.
+
+    Returns:
+        str: Type of file.
+    """
+    file_type = path.split(".")[-1]
+    if file_type.startswith("xls"):
+        return "excel"
+    return file_type
