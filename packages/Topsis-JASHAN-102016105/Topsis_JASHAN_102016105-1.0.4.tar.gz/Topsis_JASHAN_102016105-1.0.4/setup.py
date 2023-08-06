ba84a8eee456ae12679cac2fe31f30d134c68ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:09 +0000

    Add package Topsis-JASHAN-102016105/1.0.4/Topsis_JASHAN_102016105-1.0.4.tar.gz

---
+++ packages/Topsis-JASHAN-102016105/Topsis_JASHAN_102016105-1.0.4.tar.gz/Topsis_JASHAN_102016105-1.0.4/setup.py
@@ -0,0 +1,30 @@
+
+from distutils.core import setup
+setup(
+  name = 'Topsis_JASHAN_102016105',         # How you named your package folder (MyLib)
+  packages = ['Topsis_JASHAN_102016105'],   # Chose the same as "name"
+  version = '1.0.4',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  
+  long_description_content_type='text/markdown',
+  description = 'Topsis',   # Give a short description about your library
+  
+  keywords = ['topsis'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.11',
+  ],
+)
