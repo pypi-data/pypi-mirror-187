c06374a21218b26d3be6a447f185cf9df2cc26a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:27 +0000

    Add package dbt-databricks/1.4.0/dbt-databricks-1.4.0.tar.gz

---
+++ packages/dbt-databricks/dbt-databricks-1.4.0.tar.gz/dbt-databricks-1.4.0/dbt/adapters/databricks/relation.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict, Optional
+
+from dbt.adapters.base.relation import BaseRelation, Policy
+from dbt.adapters.spark.impl import KEY_TABLE_OWNER, KEY_TABLE_STATISTICS
+
+from dbt.adapters.databricks.utils import remove_undefined
+
+
+KEY_TABLE_PROVIDER = "Provider"
+
+
+@dataclass
+class DatabricksQuotePolicy(Policy):
+    database: bool = True
+    schema: bool = True
+    identifier: bool = True
+
+
+@dataclass
+class DatabricksIncludePolicy(Policy):
+    database: bool = True
+    schema: bool = True
+    identifier: bool = True
+
+
+@dataclass(frozen=True, eq=False, repr=False)
+class DatabricksRelation(BaseRelation):
+    quote_policy: Policy = field(default_factory=lambda: DatabricksQuotePolicy())
+    include_policy: Policy = field(default_factory=lambda: DatabricksIncludePolicy())
+    quote_character: str = "`"
+
+    metadata: Optional[Dict[str, Any]] = None
+
+    @classmethod
+    def __pre_deserialize__(cls, data: Dict[Any, Any]) -> Dict[Any, Any]:
+        data = super().__pre_deserialize__(data)
+        if "database" not in data["path"]:
+            data["path"]["database"] = None
+        else:
+            data["path"]["database"] = remove_undefined(data["path"]["database"])
+        return data
+
+    def has_information(self) -> bool:
+        return self.metadata is not None
+
+    @property
+    def is_delta(self) -> bool:
+        assert self.metadata is not None
+        return self.metadata.get(KEY_TABLE_PROVIDER) == "delta"
+
+    @property
+    def is_hudi(self) -> bool:
+        assert self.metadata is not None
+        return self.metadata.get(KEY_TABLE_PROVIDER) == "hudi"
+
+    @property
+    def owner(self) -> Optional[str]:
+        return self.metadata.get(KEY_TABLE_OWNER) if self.metadata is not None else None
+
+    @property
+    def stats(self) -> Optional[str]:
+        return self.metadata.get(KEY_TABLE_STATISTICS) if self.metadata is not None else None
