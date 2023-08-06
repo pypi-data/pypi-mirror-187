0ba4a9eec42c080c4d87c594c1e61b45970ef3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:43 +0000

    Add package dumbo-asp/0.0.15/dumbo_asp-0.0.15-py3-none-any.whl

---
+++ packages/dumbo-asp/dumbo_asp-0.0.15-py3-none-any.whl/dumbo_asp/queries.py
@@ -0,0 +1,10 @@
+from typing import Any, Optional
+
+from dumbo_asp.primitives import SymbolicProgram
+
+
+def minimal_unsatisfiable_subsets(
+        program: SymbolicProgram,
+        # up_to: Optional[Positi]
+) -> Any:
+    ...
