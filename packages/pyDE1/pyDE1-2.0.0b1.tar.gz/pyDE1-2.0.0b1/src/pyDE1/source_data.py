6218727f033c00e0c0bbb4faad41da6da2932b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:32 +0000

    Add package pyDE1/2.0.0b1/pyDE1-2.0.0b1.tar.gz

---
+++ packages/pyDE1/pyDE1-2.0.0b1.tar.gz/pyDE1-2.0.0b1/src/pyDE1/source_data.py
@@ -0,0 +1,11 @@
+# DO NOT EDIT -- This file automatically generated -- DO NOT EDIT
+git_data = {
+    "hash": "174df9c7",
+    "subject": "WIP: Doc updates",
+    "ref_names": "HEAD -> alpha-2023-01",
+    "author_date": "Thu Dec 15 12:09:22 2022 -0800",
+    "commit_date": "Sun Jan 22 17:19:48 2023 -0800",
+}
+source_data = {
+    "git": git_data,
+}
