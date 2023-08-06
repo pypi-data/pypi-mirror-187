363daae4afa816cbb8ffcb3fc5e9a7409e577433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:46 +0000

    Add package maquinas/0.3.1.4/maquinas-0.3.1.4-py3-none-any.whl

---
+++ packages/maquinas/maquinas-0.3.1.4-py3-none-any.whl/maquinas/__init__.py
@@ -0,0 +1,12 @@
+from . import regular
+from . import contextfree
+from . import contextsensitive
+from . import recursivelyenumerable
+from . import parser
+from . import collections
+
+from . import exceptions
+
+from .simulation import Simulation
+
+__version__ = "0.3.1.4"
