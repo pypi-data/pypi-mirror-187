a1896b3cabe262dd88516f3169f39dbe5a768563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:19 +0000

    Add package apache-liminal/0.0.5/apache_liminal-0.0.5-py3-none-any.whl

---
+++ packages/apache-liminal/apache_liminal-0.0.5-py3-none-any.whl/liminal/runners/airflow/tasks/delete_cloudformation_stack.py
@@ -0,0 +1,81 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.utils.trigger_rule import TriggerRule
+
+from liminal.runners.airflow.operators.cloudformation import (
+    CloudFormationDeleteStackOperator,
+    CloudFormationDeleteStackSensor,
+)
+from liminal.runners.airflow.tasks import airflow
+
+
+class DeleteCloudFormationStackTask(airflow.AirflowTask):
+    """
+    Deletes cloud_formation stack.
+    """
+
+    def __init__(
+        self, task_id, dag, parent, trigger_rule, liminal_config, pipeline_config, task_config, variables=None
+    ):
+        super().__init__(task_id, dag, parent, trigger_rule, liminal_config, pipeline_config, task_config, variables)
+        self.stack_name = task_config['stack_name']
+
+    def apply_task_to_dag(self):
+        check_dags_queued_task = BranchPythonOperator(
+            task_id=f'{self.task_id}-is-dag-queue-empty',
+            python_callable=self.__queued_dag_runs_exists,
+            provide_context=True,
+            trigger_rule=TriggerRule.ALL_DONE,
+            dag=self.dag,
+        )
+
+        delete_stack_task = self._add_variables_to_operator(
+            CloudFormationDeleteStackOperator(
+                task_id=f'delete-cloudformation-{self.task_id}',
+                params={'StackName': self.stack_name},
+            )
+        )
+
+        delete_stack_sensor = self._add_variables_to_operator(
+            CloudFormationDeleteStackSensor(
+                task_id=f'cloudformation-watch-{self.task_id}-delete',
+                stack_name=self.stack_name,
+            )
+        )
+
+        stack_delete_end_task = DummyOperator(task_id=f'delete-end-{self.task_id}', dag=self.dag)
+
+        if self.parent:
+            self.parent.set_downstream(check_dags_queued_task)
+
+        check_dags_queued_task.set_downstream(stack_delete_end_task)
+        check_dags_queued_task.set_downstream(delete_stack_task)
+        delete_stack_task.set_downstream(delete_stack_sensor)
+        delete_stack_sensor.set_downstream(stack_delete_end_task)
+
+        return stack_delete_end_task
+
+    # noinspection PyUnusedLocal
+    def __queued_dag_runs_exists(self, **kwargs):
+        if self.dag.get_num_active_runs() > 1:
+            return f'delete-end-{self.task_id}'
+        else:
+            return f'delete-cloudformation-{self.task_id}'
---
+++ packages/apache-liminal/apache_liminal-0.0.5-py3-none-any.whl/tests/test_licenses.py
@@ -0,0 +1,199 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import os
+import pathlib
+from unittest import TestCase
+
+from termcolor import colored
+
+EXCLUDED_EXTENSIONS = ['.gif', '.png', '.pyc', 'LICENSE', 'DISCLAIMER', 'DISCLAIMER-WIP', 'NOTICE', '.whl']
+EXCLUDED_DIRS = [
+    'docs/build',
+    'build',
+    'dist',
+    '.git',
+    '.idea',
+    'venv',
+    '.venv',
+    'apache_liminal.egg-info',
+    '.pytest_cache',
+]
+EXCLUDED_FILES = ['DISCLAIMER-WIP', 'LICENSE.txt', '.autovenv', 'credentials-aws.txt']
+
+PYTHON_LICENSE_HEADER = """
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+""".strip().split(
+    "\n"
+)
+
+MD_LICENSE_HEADER = """
+<!--
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+-->
+""".strip().split(
+    "\n"
+)
+
+RST_LICENSE_HEADER = """
+..
+   Licensed to the Apache Software Foundation (ASF) under one
+   or more contributor license agreements.  See the NOTICE file
+   distributed with this work for additional information
+   regarding copyright ownership.  The ASF licenses this file
+   to you under the Apache License, Version 2.0 (the
+   "License"); you may not use this file except in compliance
+   with the License.  You may obtain a copy of the License at
+..
+
+..  http://www.apache.org/licenses/LICENSE-2.0
+
+..
+   Unless required by applicable law or agreed to in writing,
+   software distributed under the License is distributed on an
+   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+   KIND, either express or implied.  See the License for the
+   specific language governing permissions and limitations
+   under the License.
+..
+""".strip().split(
+    "\n"
+)
+
+BAT_LICENSE_HEADER = """
+REM
+REM Licensed to the Apache Software Foundation (ASF) under one
+REM or more contributor license agreements.  See the NOTICE file
+REM distributed with this work for additional information
+REM regarding copyright ownership.  The ASF licenses this file
+REM to you under the Apache License, Version 2.0 (the
+REM "License"); you may not use this file except in compliance
+REM with the License.  You may obtain a copy of the License at
+REM
+REM   http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing,
+REM software distributed under the License is distributed on an
+REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+REM KIND, either express or implied.  See the License for the
+REM specific language governing permissions and limitations
+REM under the License.
+""".strip().split(
+    "\n"
+)
+
+JSON_LICENSE_HEADER = """
+  "": [
+    "Licensed to the Apache Software Foundation (ASF) under one",
+    "or more contributor license agreements.  See the NOTICE file",
+    "distributed with this work for additional information",
+    "regarding copyright ownership.  The ASF licenses this file",
+    "to you under the Apache License, Version 2.0 (the",
+    "\\"License\\"); you may not use this file except in compliance",
+    "with the License.  You may obtain a copy of the License at",
+    "",
+    "  http://www.apache.org/licenses/LICENSE-2.0",
+    "",
+    "Unless required by applicable law or agreed to in writing,",
+    "software distributed under the License is distributed on an",
+    "\\"AS IS\\"BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY",
+    "KIND, either express or implied.  See the License for the",
+    "specific language governing permissions and limitations",
+    "under the License."
+  ],
+""".strip().split(
+    "\n"
+)
+
+
+class TestLicenses(TestCase):
+    def test_licenses(self):
+        files = []
+        base_dir = os.path.join(pathlib.Path(__file__).parent.parent.absolute())
+        print(f'Checking licenses for files in {base_dir}')
+        for r, d, f in os.walk(base_dir):
+            if not any(os.path.relpath(r, base_dir).startswith(excluded) for excluded in EXCLUDED_DIRS):
+                for file in f:
+                    if (
+                        not any(os.path.basename(file).endswith(ext) for ext in EXCLUDED_EXTENSIONS)
+                        and not os.path.basename(file) in EXCLUDED_FILES
+                    ):
+                        files.append(os.path.join(r, file))
+
+        output = ''
+        files_missing_license = []
+        success = True
+        for file in files:
+            print(f'Checking license for file {file}')
+            has_license = self.check_license(file)
+            if not has_license:
+                output += colored(f'Missing License: {file}\n', 'red')
+                files_missing_license.append(file)
+            success = success and has_license
+
+        print(output)
+
+        if not success:
+            self.assertListEqual([], files_missing_license)
+
+    @staticmethod
+    def check_license(file):
+        header_lines = PYTHON_LICENSE_HEADER
+        if file.endswith('.md'):
+            header_lines = MD_LICENSE_HEADER
+        elif file.endswith('.rst'):
+            header_lines = RST_LICENSE_HEADER
+        elif file.endswith('.bat'):
+            header_lines = BAT_LICENSE_HEADER
+        elif file.endswith('.json'):
+            header_lines = JSON_LICENSE_HEADER
+            header_lines[0] = f'  {header_lines[0]}'
+        with open(file) as f:
+            file_lines = f.readlines()
+        return all(f'{line}\n' in file_lines for line in header_lines)
