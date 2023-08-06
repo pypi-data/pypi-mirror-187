614cbc7c5e6f801accde30b7b1c5814d93772316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:45 +0000

    Add package Topsis-Aryan-102003580/0.1.1/Topsis-Aryan-102003580-0.1.1.tar.gz

---
+++ packages/Topsis-Aryan-102003580/Topsis-Aryan-102003580-0.1.1.tar.gz/Topsis-Aryan-102003580-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis-Aryan-102003580',         # How you named your package folder (MyLib)
+  packages = ['Topsis-Aryan-102003580'],   # Chose the same as "name"
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A topsis package',   # Give a short description about your library
+  author = 'Aryan Sharma',                   # Type in your name
+  author_email = 'aryansharma5669@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/aryansharma56/topsis',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/aryansharma56/topsis',    # I explain this later on
+  keywords = ['Aryan', 'Topsis'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'sys',
+          'os',
+          'pd',
+          'numpy',
+          'copy'
+
+      ],
+  
+)
