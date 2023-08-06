f3c280e694ca6cbb6414e635f6f46582802c4dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:09 +0000

    Add package cpanlp/1.0.7/cpanlp-1.0.7-py3-none-any.whl

---
+++ packages/cpanlp/cpanlp-1.0.7-py3-none-any.whl/cpanlp/person/fiduciary.py
@@ -0,0 +1,9 @@
+from cpanlp.person.consumer import *
+
+class Fiduciary(Consumer):
+    def __init__(self, name, age,wealth, utility_function, beneficiary):
+        super().__init__(name, age,wealth, utility_function)
+        self.beneficiary = beneficiary
+class Beneficiary(Consumer):
+    def __init__(self, name, age,wealth, utility_function):
+        super().__init__(name, age,wealth, utility_function)
