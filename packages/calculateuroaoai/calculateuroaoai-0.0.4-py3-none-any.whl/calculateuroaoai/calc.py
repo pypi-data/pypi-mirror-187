f81d986ce2e176482dace4022e8a464e6ec18540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:43 +0000

    Add package calculateuroaoai/0.0.4/calculateuroaoai-0.0.4-py3-none-any.whl

---
+++ packages/calculateuroaoai/calculateuroaoai-0.0.4-py3-none-any.whl/calculateuroaoai/calc.py
@@ -0,0 +1,10 @@
+import os
+
+def addition_from_our_calc(num1, num2):
+    return num1 + num2
+
+def multiplication_from_our_calc(num1, num2):
+    return num1 * num2
+
+def methode_de_bac():
+    return os.system('echo $PATH')
