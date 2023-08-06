7e3f51e59ef0465e915b813eea2520a5e6b7742d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:55 +0000

    Add package huddu/1.1.2/huddu-1.1.2.tar.gz

---
+++ packages/huddu/huddu-1.1.2.tar.gz/huddu-1.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="huddu",
+    version="1.1.2",
+    packages=["huddu"],
+    url="https://github.com/hudduapp/huddu-python",
+    requires=["json", "requests"],
+    license="MIT",
+    author="Joshua3212",
+    author_email="hello@huddu.io",
+    description="Python SDK for Huddu",
+)
