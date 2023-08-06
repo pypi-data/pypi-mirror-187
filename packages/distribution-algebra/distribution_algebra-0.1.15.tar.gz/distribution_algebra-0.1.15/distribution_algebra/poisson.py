4cab7b9d1d6240d90af7a566cbaf64084c56bcb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:00 +0000

    Add package distribution-algebra/0.1.15/distribution_algebra-0.1.15.tar.gz

---
+++ packages/distribution-algebra/distribution_algebra-0.1.15.tar.gz/distribution_algebra-0.1.15/distribution_algebra/poisson.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from dataclasses import field
+from math import floor, inf
+from typing import Annotated, Any
+
+import numpy as np
+import scipy
+from numpy.typing import NDArray
+from pydantic import confloat
+from pydantic.dataclasses import dataclass as pydantic_dataclass
+
+from distribution_algebra.config import RNG
+from distribution_algebra.distribution import UnivariateDistribution
+
+
+@pydantic_dataclass(frozen=True, kw_only=True, eq=True)
+class Poisson(UnivariateDistribution[np.int_]):
+    lam: Annotated[float, confloat(gt=0.0, allow_inf_nan=False)]  # rate parameter.
+    is_continuous: bool = field(default=False, repr=False)
+
+    def draw(self, size: int) -> NDArray[np.int_]:
+        return RNG.poisson(lam=self.lam, size=size)
+
+    @property
+    def mean(self) -> float:
+        return self.lam
+
+    @property
+    def var(self) -> float:
+        return self.lam
+
+    @property
+    def median(self) -> float:
+        return floor(self.lam + 1/3 - 1/(50 * self.lam))
+
+    @property
+    def mode(self) -> float:
+        if not self.lam.is_integer():
+            return floor(self.lam)
+        return NotImplemented
+
+    def pdf(self, arange: NDArray[np.int_]) -> NDArray[np.float64]:  # type: ignore
+        return scipy.stats.poisson.pmf(  # type: ignore
+            arange, mu=self.lam)
+
+    def __add__(self, other: Any) -> Any:
+        match other:
+            case Poisson():
+                return Poisson(lam=self.lam + other.lam)
+            case _:
+                return super().__add__(other)
+
+    @property
+    def support(self) -> tuple[float, float]:
+        return 0, inf
