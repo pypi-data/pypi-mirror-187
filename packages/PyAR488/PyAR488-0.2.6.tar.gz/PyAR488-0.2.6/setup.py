057a67355e50559ab07cb464268a2b7f622f5d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:39 +0000

    Add package PyAR488/0.2.6/PyAR488-0.2.6.tar.gz

---
+++ packages/PyAR488/PyAR488-0.2.6.tar.gz/PyAR488-0.2.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyAR488',
+    version='0.2.6',
+    packages=find_packages(exclude=("tests",)),
+    url="https://github.com/Minu-IU3IRR/PyAR488",
+    bugtrack_url = 'https://github.com/Minu-IU3IRR/PyAR488/issues',
+    license='MIT',
+    author='Manuel Minutello',
+    description='module to interface AR488 boards and wide instrument library',
+    long_description=open('README.md').read(),
+    install_requires='pyserial',
+    python_requeres = '>=3.6'
+)
