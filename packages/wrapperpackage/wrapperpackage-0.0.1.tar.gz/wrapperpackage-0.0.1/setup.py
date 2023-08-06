ce6d6b97da79b1d991b802407378452c3e4c20f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:07 +0000

    Add package wrapperpackage/0.0.1/wrapperpackage-0.0.1.tar.gz

---
+++ packages/wrapperpackage/wrapperpackage-0.0.1.tar.gz/wrapperpackage-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Streaming video data via networks'
+
+# Setting up
+setup(
+	name="wrapperpackage",
+	version=VERSION,
+    	author="NeuralNine (Florian Dedov)",
+    	author_email="<mail@neuralnine.com>",
+    	description=DESCRIPTION,
+    	packages=['wrapperpackage'],
+    	install_requires=[]
+)
