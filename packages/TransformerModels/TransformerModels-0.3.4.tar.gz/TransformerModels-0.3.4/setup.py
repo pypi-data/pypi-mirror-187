5e1d6064e4e965420c9a8d0bfb480e3391df7e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:41 +0000

    Add package TransformerModels/0.3.4/TransformerModels-0.3.4.tar.gz

---
+++ packages/TransformerModels/TransformerModels-0.3.4.tar.gz/TransformerModels-0.3.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TransformerModels",
+    version="0.3.4",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "": ["transformer_model_new_env"],
+    },
+    install_requires=['setfit','pandas'], 
+    download_url='https://github.com/JoPulido123/TransformerModelDI/archive/refs/tags/0.3.4.tar.gz'
+)
