fca7e060c5c78315fd57f1df9b30207213f85fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:14 +0000

    Add package boschshcpy/0.2.40/boschshcpy-0.2.40.tar.gz

---
+++ packages/boschshcpy/boschshcpy-0.2.40.tar.gz/boschshcpy-0.2.40/boschshcpy/__init__.py
@@ -0,0 +1,29 @@
+from .device import SHCDevice
+from .device_helper import (
+    SHCBatteryDevice,
+    SHCCamera360,
+    SHCCameraEyes,
+    SHCClimateControl,
+    SHCDeviceHelper,
+    SHCLight,
+    SHCMotionDetector,
+    SHCShutterContact,
+    SHCShutterControl,
+    SHCLightSwitch,
+    SHCPresenceSimulationSystem,
+    SHCSmartPlug,
+    SHCSmartPlugCompact,
+    SHCSmokeDetector,
+    SHCSmokeDetectionSystem,
+    SHCThermostat,
+    SHCTwinguard,
+    SHCUniversalSwitch,
+    SHCWallThermostat,
+    SHCWaterLeakageSensor,
+)
+from .domain_impl import SHCIntrusionSystem
+from .exceptions import SHCAuthenticationError, SHCConnectionError, SHCRegistrationError
+from .information import SHCInformation
+from .register_client import SHCRegisterClient
+from .scenario import SHCScenario
+from .session import SHCSession
