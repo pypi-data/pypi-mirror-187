70c13bd6286bf783f09616facb62b63677da7dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:07 +0000

    Add package openwisp-monitoring/1.0.4/openwisp_monitoring-1.0.4-py2.py3-none-any.whl

---
+++ packages/openwisp-monitoring/openwisp_monitoring-1.0.4-py2.py3-none-any.whl/openwisp_monitoring/check/settings.py
@@ -0,0 +1,16 @@
+from ..settings import get_settings_value
+
+CHECK_CLASSES = get_settings_value(
+    'CHECK_CLASSES',
+    (
+        ('openwisp_monitoring.check.classes.Ping', 'Ping'),
+        ('openwisp_monitoring.check.classes.ConfigApplied', 'Configuration Applied'),
+    ),
+)
+AUTO_PING = get_settings_value('AUTO_PING', True)
+AUTO_CONFIG_CHECK = get_settings_value('AUTO_DEVICE_CONFIG_CHECK', True)
+MANAGEMENT_IP_ONLY = get_settings_value('MANAGEMENT_IP_ONLY', True)
+PING_CHECK_CONFIG = get_settings_value('PING_CHECK_CONFIG', {})
+CONFIG_CHECK_INTERVAL = int(
+    get_settings_value('CONFIG_CHECK_INTERVAL', 5)
+)  # in minutes
