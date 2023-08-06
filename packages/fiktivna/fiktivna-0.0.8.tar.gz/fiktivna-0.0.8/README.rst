c9bae106dad557aaf47a80027fe099f7343a4e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:23:46 +0000

    Add package fiktivna/0.0.8/fiktivna-0.0.8.tar.gz

---
+++ packages/fiktivna/fiktivna-0.0.8.tar.gz/fiktivna-0.0.8/README.rst
@@ -0,0 +1,5 @@
+========
+Fiktivna
+========
+
+Fiktivna paket
---
+++ packages/fiktivna/fiktivna-0.0.8.tar.gz/fiktivna-0.0.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name="fiktivna",
+    version="0.0.8",
+    url="https://example.com",
+    project_urls={
+        "Documentation": "https://readthedocs.io",
+        "Source": "https://github.com/Laerte/fiktivna",
+        "Tracker": "https://github.com/Laerte/fiktivna/issues"
+    },
+    description="Fiktivna paket",
+    long_description=long_description,
+    author="Laerte Pereira",
+    packages=find_packages(include=["fiktivna*"], exclude=["tests.*"]),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.7",
+    include_package_data=True
+)
