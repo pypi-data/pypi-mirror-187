c27240ce871ee8b712e943f1a4504b20456c9f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:08:12 +0000

    Add package ThesisAZsc/0.0.9/ThesisAZsc-0.0.9-py3-none-any.whl

---
+++ packages/ThesisAZsc/ThesisAZsc-0.0.9-py3-none-any.whl/ThesisAZsc/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from ThesisAZsc import preprocessing
+from ThesisAZsc import Model_Selection
+from ThesisAZsc import PredictionModule
+from ThesisAZsc import Main
