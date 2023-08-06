8f4a58a81e07b77be8fccda289ec8a9ed2f07fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:46 +0000

    Add package PyColorTools/0.0.2/pycolortools-0.0.2-py3-none-any.whl

---
+++ packages/PyColorTools/pycolortools-0.0.2-py3-none-any.whl/PyColors/__init__.py
@@ -0,0 +1,23 @@
+class PyColors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class ColorLib:
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    WHITE = '\033[0m'
+
+class SpecFont:
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
