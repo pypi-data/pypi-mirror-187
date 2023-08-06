a69d07dde601e2f063a26c4f2082906e8b75dbd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:02 +0000

    Add package CaptionedFileFormat/1.1.0/CaptionedFileFormat-1.1.0.post2-py3-none-any.whl

---
+++ packages/CaptionedFileFormat/CaptionedFileFormat-1.1.0.post2-py3-none-any.whl/test.py
@@ -0,0 +1,13 @@
+from cptf import *
+'''import struct
+
+data = struct.pack('>5s', b'CPTF\n')
+
+with open('file.cptf', 'wb') as f:
+	f.write(data)'''
+
+#data = FileBuffer()
+#data.create_new("hello world!", "idk what to put here, lol", "testfile.txt")
+#print(data.read_from_bytes(b'CPTN\n\x0c\x00\x19testfile.txtidk what to put here, lolhello world!'))
+#print(data.get_bytes())
+print(errors)
