8c5e538b10002d9410e86f8c64547a3a1c6e7a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:38 +0000

    Add package mode-analyzer/0.0.7/mode_analyzer-0.0.7-py3-none-any.whl

---
+++ packages/mode-analyzer/mode_analyzer-0.0.7-py3-none-any.whl/modeanalyzer/__main__.py
@@ -0,0 +1,9 @@
+from .modeanalyzer import *
+
+app = main()
+app.run()
+# Run = RunSimulation()
+# root = Tk.Tk()
+# main_gui = MainWindow(master=root)
+# root.read = read_data(f)
+# root.mainloop()
