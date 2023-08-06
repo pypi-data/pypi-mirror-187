8e1e9915f282d2123b92ffad49a4ec9857b8e613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:58 +0000

    Add package devialet/1.3.1/devialet-1.3.1.tar.gz

---
+++ packages/devialet/devialet-1.3.1.tar.gz/devialet-1.3.1/devialet/const.py
@@ -0,0 +1,28 @@
+"""Constants for the Devialet integration."""
+import logging
+from typing import Final
+
+LOGGER = logging.getLogger(__package__)
+
+NORMAL_INPUTS = {
+    "Airplay": "airplay2",
+    "Bluetooth": "bluetooth",
+    "Digital left": "digital_left",
+    "Digital right": "digital_right",
+    "Line": "line",
+    "Online": "upnp",
+    "Optical": "optical",
+    "Optical left": "optical_left",
+    "Optical right": "optical_right",
+    "Optical jack": "opticaljack",
+    "Optical jack left": "opticaljack_left",
+    "Optical jack right": "opticaljack_right",
+    "Phono": "phono",
+    "Raat": "raat",
+    "Spotify Connect": "spotifyconnect",
+}
+
+SPEAKER_POSITIONS = {
+    "FrontLeft": "left",
+    "FrontRight": "right",
+}
