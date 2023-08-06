6048938702d8c5cfe6c46cd48cb219543153224b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:00:34 +0000

    Add package hstestaq/0.1.14/hstestaq-0.1.14-py3-none-any.whl

---
+++ packages/hstestaq/hstestaq-0.1.14-py3-none-any.whl/aqueduct_executor/operators/connectors/data/snowflake.py
@@ -0,0 +1,56 @@
+from aqueduct_executor.operators.connectors.data import config, relational, utils
+from sqlalchemy import create_engine, engine
+from pyspark.sql import SparkSession, DataFrame
+
+
+class SnowflakeConnector(relational.RelationalConnector):
+    def __init__(self, config: config.SnowflakeConfig):
+        url = "snowflake://{username}:{password}@{account_identifier}/{database}/{schema}?warehouse={warehouse}".format(
+            username=config.username,
+            password=utils.url_encode(config.password),
+            account_identifier=config.account_identifier,
+            database=config.database,
+            schema=config.db_schema,
+            warehouse=config.warehouse,
+        )
+        conn_engine = _create_engine(config)
+
+        self.snowflake_spark_options = {
+            'sfURL': config.url,
+            'sfAccount': config.account_identifier,
+            'sfUser': config.username,
+            'sfPassword': config.password,
+            'sfDatabase': config.database,
+            'sfSchema': config.schema,
+            'sfWarehouse': config.warehouse,
+        }
+        super().__init__(conn_engine)
+
+
+def _create_engine(config: config.SnowflakeConfig) -> engine.Engine:
+    # Snowflake Dialect:
+    # https://github.com/snowflakedb/snowflake-sqlalchemy
+    url = "snowflake://{username}:{password}@{account_identifier}/{database}/{schema}?warehouse={warehouse}".format(
+        username=config.username,
+        password=utils.url_encode(config.password),
+        account_identifier=config.account_identifier,
+        database=config.database,
+        schema=config.db_schema,
+        warehouse=config.warehouse,
+    )
+    return create_engine(url)
+
+def extract_spark(self, params: extract.RelationalParams) -> Any:
+    assert params.usable(), "Query is not usable. Did you forget to expand placeholders?"
+
+    df = spark.options(**self.snowflake_spark_options).option("query", params.query)
+
+def load_spark(
+        self, params: load.RelationalParams, df: DataFrame, artifact_type: ArtifactType
+    ) -> None:
+
+    df.write.format("snowflake") \
+    .options(**self.snowflake_spark_options) \
+    .option("dbtable", params.table) \
+    .mode(params.update_mode) \
+    .save()
