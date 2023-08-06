bf591462b1484f8d3f3262879a45963275c59d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:15 +0000

    Add package pytrafikverket/0.2.3/pytrafikverket-0.2.3.tar.gz

---
+++ packages/pytrafikverket/pytrafikverket-0.2.3.tar.gz/pytrafikverket-0.2.3/setup.py
@@ -0,0 +1,18 @@
+"""Setup for pytrafikverket."""
+
+from setuptools import setup
+
+setup(
+    name="pytrafikverket",
+    version="0.2.3",
+    description="Retreive values from public API at the Swedish Transport Administration (Trafikverket).",
+    url="https://github.com/endor-force/pytrafikverket",
+    author="Peter Andersson, Jonas Karlsson",
+    license="MIT",
+    install_requires=["aiohttp", "async-timeout", "lxml"],
+    packages=["pytrafikverket"],
+    zip_safe=True,
+    entry_point={
+        "console_scripts": ["pytrafikverket=pytrafikverket.pytrafikverket:main"]
+    },
+)
