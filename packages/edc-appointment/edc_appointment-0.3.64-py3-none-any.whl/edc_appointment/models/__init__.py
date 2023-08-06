5d19aff44279e200a3d5479a372323e1d276dfc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:53 +0000

    Add package edc-appointment/0.3.64/edc_appointment-0.3.64-py3-none-any.whl

---
+++ packages/edc-appointment/edc_appointment-0.3.64-py3-none-any.whl/edc_appointment/models/__init__.py
@@ -0,0 +1,8 @@
+from .appointment import Appointment
+from .signals import (
+    appointment_post_save,
+    appointments_on_post_delete,
+    appointments_on_pre_delete,
+    create_appointments_on_post_save,
+    update_appt_status_on_subject_visit_post_save,
+)
