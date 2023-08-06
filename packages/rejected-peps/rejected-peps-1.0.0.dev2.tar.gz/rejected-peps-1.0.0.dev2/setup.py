5ca12ee97d9765d9514423c5d1bf954e1f2be145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:07 +0000

    Add package rejected-peps/1.0.0.dev2/rejected-peps-1.0.0.dev2.tar.gz

---
+++ packages/rejected-peps/rejected-peps-1.0.0.dev2.tar.gz/rejected-peps-1.0.0.dev2/setup.py
@@ -0,0 +1,37 @@
+import rejected_peps
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="rejected-peps",
+    version=rejected_peps.__version__,
+    author="wyz23x2",
+    author_email="wyz23x2@163.com",
+    description="A Python package that implements some rejected, deferred or withdrawn PEPs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wyz23x2/rejected-peps/",
+    project_urls={
+        "Bug Tracker": "https://github.com/wyz23x2/rejected-peps/issues",
+        "Changelog": "https://github.com/wyz23x2/rejected-peps/blob/1.0/CHANGELOG.md"
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",  # To Jan 27, 2023
+        "Programming Language :: Python :: 3.8",  # To Oct 14, 2023 +
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Typing :: Typed",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+)
