7e20aec6e263ccd4ac393f9e59191352c1105f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:39 +0000

    Add package drf-tracking-logger/1.9.4/drf_tracking_logger-1.9.4.tar.gz

---
+++ packages/drf-tracking-logger/drf_tracking_logger-1.9.4.tar.gz/drf_tracking_logger-1.9.4/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='drf_tracking_logger',
+    version='1.9.4',
+    author="Ravi Singh",
+    author_email="ravisinghofficial@gmail.com",
+    description="Log DRF requests on specified logger",
+    url="https://github.com/singhravi1/drf-tracking-logger",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
