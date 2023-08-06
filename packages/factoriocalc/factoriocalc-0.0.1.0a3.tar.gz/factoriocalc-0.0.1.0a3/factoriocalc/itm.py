e8381a724405fd7c063e8a1d462557074bf11ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:09 +0000

    Add package factoriocalc/0.0.1.0a3/factoriocalc-0.0.1.0a3.tar.gz

---
+++ packages/factoriocalc/factoriocalc-0.0.1.0a3.tar.gz/factoriocalc-0.0.1.0a3/factoriocalc/itm.py
@@ -0,0 +1,10 @@
+"""Symbols for entities in factorio.
+
+The name of the symbol is the same as the internal name but with ``-``
+converted to ``_``.  For example, to refer to an "electronic-circuit" use
+`itm.electronic_circuit`.
+
+"""
+
+byName = {}
+
---
+++ packages/factoriocalc/factoriocalc-0.0.1.0a3.tar.gz/factoriocalc-0.0.1.0a3/factoriocalc/rcp.py
@@ -0,0 +1,10 @@
+"""Symbols for recipes (`~factoriocalc.Rcp`) in factorio.
+
+The name of the symbol is the same as the internal name but with ``-``
+converted to ``_``.  For example, to refer to the recipe for an
+"electronic-circuit" use `rcp.electronic_circuit`.
+
+"""
+
+byName = {}
+
