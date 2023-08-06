fa3391407b22548cca64bb2c929ffab54cda14cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:41 +0000

    Add package ja-stopword-remover/0.2.4/ja_stopword_remover-0.2.4.tar.gz

---
+++ packages/ja-stopword-remover/ja_stopword_remover-0.2.4.tar.gz/ja_stopword_remover-0.2.4/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open("README.rst", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name="ja_stopword_remover",
+    version="0.2.4",
+    description="形態素解析後の日本語の文のリストからストップワードを除去するためのモジュール",
+    long_description=readme,
+    author="picker.",
+    author_email="picker@pickerlav.jp",
+    url="https://pickerlab.net/",
+    license="MIT",
+    packages=["ja_stopword_remover"],
+    include_package_data=True,
+)
