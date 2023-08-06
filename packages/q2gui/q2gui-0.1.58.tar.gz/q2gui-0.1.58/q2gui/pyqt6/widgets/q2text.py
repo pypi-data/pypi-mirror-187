e14272084ff5979d02361cc3ca5e3fee58679e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:02 +0000

    Add package q2gui/0.1.58/q2gui-0.1.58.tar.gz

---
+++ packages/q2gui/q2gui-0.1.58.tar.gz/q2gui-0.1.58/q2gui/pyqt6/widgets/q2text.py
@@ -0,0 +1,27 @@
+import sys
+
+if __name__ == "__main__":
+
+    sys.path.insert(0, ".")
+
+    from demo.demo import demo
+
+    demo()
+
+from PyQt6.QtWidgets import QTextEdit, QSizePolicy
+
+from q2gui.pyqt6.q2widget import Q2Widget
+
+
+class q2text(QTextEdit, Q2Widget):
+    def __init__(self, meta):
+        super().__init__(meta)
+        self.setSizePolicy(QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding))
+        self.setTabChangesFocus(True)
+        self.set_text(meta.get('data'))
+
+    def set_text(self, text):
+        self.setHtml(text)
+
+    def get_text(self):
+        return f"{self.toPlainText()}"
