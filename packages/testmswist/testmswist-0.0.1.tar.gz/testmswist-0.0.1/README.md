b7bf0ddfb5883091ee0eb06e29af6b21efb567c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:23 +0000

    Add package testmswist/0.0.1/testmswist-0.0.1.tar.gz

---
+++ packages/testmswist/testmswist-0.0.1.tar.gz/testmswist-0.0.1/README.md
@@ -0,0 +1,22 @@
+# `testmswist`
+
+* op1
+* op2
+* op3
+
+
+# Install
+
+```python
+pip install testmswist
+```
+
+# Usage
+
+```python
+from testmswist.m_mod_mswist import name as n
+
+>> > n("Name", "Surname")
+Witaj: Name
+Surname
+```
---
+++ packages/testmswist/testmswist-0.0.1.tar.gz/testmswist-0.0.1/testmswist/m_mod_mswist.py
@@ -0,0 +1,11 @@
+import numpy
+
+def name(first, last):
+    """returns full data of parameters"""
+
+    full = first + " " + last
+    full = full.lower().title().strip()
+    return "Witaj: " + full
+
+def get_numpu_arr(lst):
+    pass
