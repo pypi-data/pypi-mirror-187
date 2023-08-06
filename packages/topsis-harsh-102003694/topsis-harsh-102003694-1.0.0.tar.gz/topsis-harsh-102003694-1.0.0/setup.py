68c33b0eea683b0127ff6049ae8f73f70ebcff7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:32 +0000

    Add package topsis-harsh-102003694/1.0.0/topsis-harsh-102003694-1.0.0.tar.gz

---
+++ packages/topsis-harsh-102003694/topsis-harsh-102003694-1.0.0.tar.gz/topsis-harsh-102003694-1.0.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="topsis-harsh-102003694",
+    version="1.0.0",
+    author="Harsh Bisla",
+    author_email="harshbisla10102001@gmail.com",
+    description="topsis implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
