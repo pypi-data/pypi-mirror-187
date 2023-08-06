ba14807ad058ace51b586a35c0b8bffdd7f77402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:20 +0000

    Add package yea-wandb/0.9.4/yea-wandb-0.9.4.tar.gz

---
+++ packages/yea-wandb/yea-wandb-0.9.4.tar.gz/yea-wandb-0.9.4/setup.py
@@ -0,0 +1,30 @@
+"""yea setup."""
+
+from setuptools import setup
+
+
+setup(
+    name="yea-wandb",
+    version="0.9.4",
+    description="Test harness wandb plugin",
+    packages=["yea_wandb"],
+    install_requires=[
+        "Flask",
+        "requests",
+        "responses",
+        "pandas",
+        "yea==0.9.0",
+        # "wandb>=0.13.10.dev1",
+        # "wandb @ git+https://github.com/wandb/wandb.git@jhr-relay-split",
+    ],
+    package_dir={"": "src"},
+    entry_points={
+        "yea.plugins": [
+            "yea_wandb = yea_wandb.plugin",
+        ]
+    },
+    zip_safe=False,
+    include_package_data=True,
+    license="MIT license",
+    python_requires=">=3.6",
+)
