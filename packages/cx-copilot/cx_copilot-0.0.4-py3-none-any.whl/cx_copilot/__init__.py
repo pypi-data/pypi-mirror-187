d1cb115f6021ede5062b67c7c7d68aba4c176906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:49 +0000

    Add package cx-copilot/0.0.4/cx_copilot-0.0.4-py3-none-any.whl

---
+++ packages/cx-copilot/cx_copilot-0.0.4-py3-none-any.whl/cx_copilot/__init__.py
@@ -0,0 +1,9 @@
+"""Python Package Template"""
+from __future__ import annotations
+from .blocks.embedding import OpenAIEmbeddingBlock
+from .blocks.vectordb import PineconeVectorDBBlock
+from .blocks.completion import GPTCompletionBlock
+from .blocks.cache import RedisCache
+from .compound.compound import CXCopilot
+
+__version__ = "0.0.4"
