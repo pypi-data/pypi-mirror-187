606b65686a789aafafb0546baf927445e2488e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:22 +0000

    Add package pytask-julia/0.3.0/pytask_julia-0.3.0.tar.gz

---
+++ packages/pytask-julia/pytask_julia-0.3.0.tar.gz/pytask_julia-0.3.0/src/pytask_julia/plugin.py
@@ -0,0 +1,18 @@
+"""Register hook specifications and implementations."""
+from __future__ import annotations
+
+from pluggy import PluginManager
+from pytask import hookimpl
+from pytask_julia import collect
+from pytask_julia import config
+from pytask_julia import execute
+from pytask_julia import parametrize
+
+
+@hookimpl
+def pytask_add_hooks(pm: PluginManager) -> None:
+    """Register hook implementations."""
+    pm.register(collect)
+    pm.register(config)
+    pm.register(execute)
+    pm.register(parametrize)
