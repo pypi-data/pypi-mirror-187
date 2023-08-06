47bdcfc4ed0c0d1437a4e8182f15baa31324fe75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:47 +0000

    Add package idoit-api-client/0.0.2/idoit_api_client-0.0.2-py2.py3-none-any.whl

---
+++ packages/idoit-api-client/idoit_api_client-0.0.2-py2.py3-none-any.whl/idoit_api_client/cli.py
@@ -0,0 +1,17 @@
+"""Console script for idoit_api."""
+import sys
+import click
+import idoit_api_client
+
+
+@click.command()
+def main(args=None):
+    """Console script for idoit_api."""
+    click.echo("Replace this message by putting your code into "
+               "idoit_api.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
