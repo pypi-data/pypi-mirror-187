c6d8325a2a7ab469faa4a6e56f98e0e4c7a43102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:43 +0000

    Add package joker-bilibili/1.0.1/joker_bilibili-1.0.1.tar.gz

---
+++ packages/joker-bilibili/joker_bilibili-1.0.1.tar.gz/joker_bilibili-1.0.1/setup.py
@@ -0,0 +1,18 @@
+import os
+import setuptools
+
+setuptools.setup(
+      name='joker_bilibili',
+      version='1.0.1',  # 版本号
+      keywords='joker',
+      description='Hello,welcome to my field',
+      long_description=open(
+          os.path.join(
+              os.path.dirname(__file__),
+              'README.rst')
+      ).read(),
+      author='jokerLang',
+      author_email='524276169@qq.com',
+      packages=setuptools.find_packages(),
+      license='MIT'
+      )
