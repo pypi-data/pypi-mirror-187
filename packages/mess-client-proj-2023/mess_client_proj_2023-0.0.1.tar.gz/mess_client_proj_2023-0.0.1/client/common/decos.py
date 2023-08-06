ffb59554e5ea61b5a6db26eaaa906c3f9488ce68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:15 +0000

    Add package mess-client-proj-2023/0.0.1/mess_client_proj_2023-0.0.1.tar.gz

---
+++ packages/mess-client-proj-2023/mess_client_proj_2023-0.0.1.tar.gz/mess_client_proj_2023-0.0.1/client/common/decos.py
@@ -0,0 +1,65 @@
+'''Декораторы'''
+
+import sys
+import logging
+import socket
+import logs.configs.server_log_config
+import logs.configs.client_log_config
+import traceback
+import inspect
+
+sys.path.append('../')
+
+
+def log(func_to_log):
+    def decorated(*args, **kwargs):
+        log_name = 'server_dist' if 'my_server.py' in sys.argv[0] else 'client_dist'
+        logger = logging.getLogger(log_name)
+
+        ret = func_to_log(*args, **kwargs)
+        logger.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}. '
+                     f'Вызов из модуля {func_to_log.__module__}.'
+                     f'Вызов из функции {traceback.format_stack()[0].strip().split()[-1]}.'
+                     f'Вызов из функции {inspect.stack()[1][3]}', stacklevel=2)
+        return ret
+    return decorated
+
+
+def login_required(func):
+    '''
+    Декоратор, проверяющий, что клиент авторизован на сервере.
+    Проверяет, что передаваемый объект сокета находится в
+    списке авторизованных клиентов.
+    За исключением передачи словаря-запроса
+    на авторизацию. Если клиент не авторизован,
+    генерирует исключение TypeError
+    '''
+
+    def checker(*args, **kwargs):
+        # проверяем, что первый аргумент - экземпляр MessageProcessor
+        # Импортить необходимо тут, иначе ошибка рекурсивного импорта.
+        from server.core import MessageProcessor
+        from common.variables import ACTION, PRESENCE
+        if isinstance(args[0], MessageProcessor):
+            found = False
+            for arg in args:
+                if isinstance(arg, socket.socket):
+                    # Проверяем, что данный сокет есть в списке names класса
+                    # MessageProcessor
+                    for client in args[0].names:
+                        if args[0].names[client] == arg:
+                            found = True
+
+            # Теперь надо проверить, что передаваемые аргументы не presence
+            # сообщение. Если presense, то разрешаем
+            for arg in args:
+                if isinstance(arg, dict):
+                    if ACTION in arg and arg[ACTION] == PRESENCE:
+                        found = True
+            # Если не не авторизован и не сообщение начала авторизации, то
+            # вызываем исключение.
+            if not found:
+                raise TypeError
+        return func(*args, **kwargs)
+
+    return checker
---
+++ packages/mess-client-proj-2023/mess_client_proj_2023-0.0.1.tar.gz/mess_client_proj_2023-0.0.1/setup_client.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="mess_client_proj_2023",
+      version="0.0.1",
+      description="mess_client_proj_new",
+      author="Vyacheslav Lashin",
+      author_email="glory665@mail.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
