24d9b5f138e4b06a486168df81d0d4065f5e4b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:06 +0000

    Add package archaea-simulation/1.0.5/archaea-simulation-1.0.5.tar.gz

---
+++ packages/archaea-simulation/archaea-simulation-1.0.5.tar.gz/archaea-simulation-1.0.5/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='archaea-simulation',
+    packages=setuptools.find_packages(),
+    version='1.0.5',
+    license='Apache 2.0',
+    description='Wrapper definitions for simulation tools.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    readme='README.md',
+    author='Oğuzhan Koral',
+    author_email='oguzhankoral@gmail.com',
+    url='https://github.com/archaeans/archaea-simulation',
+    download_url='https://github.com/archaeans/archaea-simulation/archive/refs/tags/1.0.5.tar.gz',
+    keywords=['geometry', 'simulation'],
+    install_requires=[
+        'archaea'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
