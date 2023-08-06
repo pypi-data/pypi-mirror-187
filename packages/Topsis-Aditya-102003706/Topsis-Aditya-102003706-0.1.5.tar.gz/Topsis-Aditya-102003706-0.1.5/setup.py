3c629cdfc3d8cbfaf086d2f7bfe8eb2291d0737e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:45 +0000

    Add package Topsis-Aditya-102003706/0.1.5/Topsis-Aditya-102003706-0.1.5.tar.gz

---
+++ packages/Topsis-Aditya-102003706/Topsis-Aditya-102003706-0.1.5.tar.gz/Topsis-Aditya-102003706-0.1.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+
+setup(
+    name="Topsis-Aditya-102003706",
+    version='0.1.5',
+    author='Aditya Aggarwal',
+    author_email='aditya.agg7@gmail.com',
+    description='Topsis, packaged for ease.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'topsis=Topsis.TOPSIS:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=requirements,
+    zip_safe=False,
+    project_urls={
+        'Source': "https://github.com/adityaagg7/Topsis-Package",
+        'Tracker': "https://github.com/adityaagg7/Topsis-Package/issues",
+    },
+
+)
