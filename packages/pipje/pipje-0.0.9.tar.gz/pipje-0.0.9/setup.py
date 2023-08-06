92247b7882cd14a338dd139d50d8c1e68233e891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:58 +0000

    Add package pipje/0.0.9/pipje-0.0.9.tar.gz

---
+++ packages/pipje/pipje-0.0.9.tar.gz/pipje-0.0.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = "0.0.9"
+DESCRIPTION = "Pipje was intended as a joke, it shows an image of a cat named pip."
+
+# Setting up
+setup(
+    name="pipje",
+    version=VERSION,
+    author="Hurbie48 (Matthijs Veldkamp)",
+    author_email="<matjepatatje2@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["tkinter", "os", "system","time"],
+    keywords=["python", "tkinter", "cat"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
